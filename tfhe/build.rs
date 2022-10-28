c229c8f613c99669504bf3f634de3dc6f9d7e05e
Arthur Meyre <arthur.meyre@zama.ai>
2022-10-28 15:16:17 +0200

    chore(ci): more clippy fixes

--- tfhe/build.rs
+++ tfhe/build.rs
@@ -20,13 +20,13 @@ fn gen_c_api() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     let package_name = env::var("CARGO_PKG_NAME").unwrap();
     let output_file = target_dir()
-        .join(format!("{}.h", package_name))
+        .join(format!("{package_name}.h"))
         .display()
         .to_string();
 
-    cbindgen::generate(&crate_dir)
+    cbindgen::generate(crate_dir)
         .unwrap()
-        .write_to_file(&output_file);
+        .write_to_file(output_file);
 }
 
 fn main() {
