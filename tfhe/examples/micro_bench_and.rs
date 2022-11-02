330064a12a2e7cfe049f94f76b7c5668d09b5e77
Arthur Meyre <arthur.meyre@zama.ai>
2022-11-02 14:01:42 +0100

    chore(booleans): fix some warnings for example micro bench

--- tfhe/examples/micro_bench_and.rs
+++ tfhe/examples/micro_bench_and.rs
@@ -1,5 +1,4 @@
 use tfhe::boolean::client_key::ClientKey;
-use tfhe::boolean::gen_keys;
 use tfhe::boolean::parameters::TFHE_LIB_PARAMETERS;
 use tfhe::boolean::prelude::BinaryBooleanGates;
 use tfhe::boolean::server_key::ServerKey;
@@ -20,7 +19,7 @@ fn main() {
     let num_loops: usize = 10000;
 
     for _ in 0..num_loops {
-        let result = sks.and(&ct_left, &ct_right);
+        let _ = sks.and(&ct_left, &ct_right);
     }
     let elapsed = start.elapsed().as_millis() as f64;
     let mean: f64 = elapsed / num_loops as f64;
