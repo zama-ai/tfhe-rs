fe62103a19dfc63000f8b1595e1cfe7136d39e5c
Arthur Meyre <arthur.meyre@zama.ai>
2022-11-22 16:53:22 +0100

    chore(tfhe): make imports globs for ease of use

--- tfhe/src/core_crypto/entities/mod.rs
+++ tfhe/src/core_crypto/entities/mod.rs
@@ -6,3 +6,12 @@ pub mod lwe_keyswitch_key;
 pub mod lwe_secret_key;
 pub mod plaintext;
 pub mod plaintext_list;
+
+pub use cleartext::*;
+pub use glwe_secret_key::*;
+pub use lwe_ciphertext::*;
+pub use lwe_ciphertext_list::*;
+pub use lwe_keyswitch_key::*;
+pub use lwe_secret_key::*;
+pub use plaintext::*;
+pub use plaintext_list::*;
