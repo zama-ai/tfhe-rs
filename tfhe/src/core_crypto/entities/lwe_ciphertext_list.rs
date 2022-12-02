b182d8ef0534b06c3d2860441bf31ae2b8cbda26
Arthur Meyre <arthur.meyre@zama.ai>
2022-12-02 14:18:35 +0100

    refactor(tfhe): remove core engines from ShortintEngine

--- tfhe/src/core_crypto/entities/lwe_ciphertext_list.rs
+++ tfhe/src/core_crypto/entities/lwe_ciphertext_list.rs
@@ -2,6 +2,7 @@ use crate::core_crypto::commons::traits::*;
 use crate::core_crypto::entities::*;
 use crate::core_crypto::specification::parameters::*;
 
+#[derive(Clone, Debug, PartialEq)]
 pub struct LweCiphertextListBase<C: Container> {
     data: C,
     lwe_size: LweSize,
