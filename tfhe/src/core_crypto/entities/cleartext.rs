c6060eb478a894187636f20e71c566ce137046e2
Arthur Meyre <arthur.meyre@zama.ai>
2022-12-02 17:38:08 +0100

    refactor(tfhe): refactor serizalization, unplug core_crypto::prelude

--- tfhe/src/core_crypto/entities/cleartext.rs
+++ tfhe/src/core_crypto/entities/cleartext.rs
@@ -1,5 +1,5 @@
 use crate::core_crypto::commons::numeric::Numeric;
 
 /// A cleartext, not encoded, value
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, serde::Serialize, serde::Deserialize)]
 pub struct Cleartext<T: Numeric>(pub T);
