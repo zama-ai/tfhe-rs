ab840a4a030305c1be3247f3e777715931b9197c
Arthur Meyre <arthur.meyre@zama.ai>
2022-12-06 17:31:01 +0100

    chore(core): update Plaintext docstring

--- tfhe/src/core_crypto/entities/plaintext.rs
+++ tfhe/src/core_crypto/entities/plaintext.rs
@@ -1,6 +1,28 @@
 use crate::core_crypto::commons::traits::*;
 
-/// A plaintext (encoded) value.
+/// A plaintext (encoded) value. This may contain a reference, in that case it can be converted to a
+/// plaintext containing the actual value via a call to `into`.
+///
+/// ```
+/// use tfhe::core_crypto::entities::*;
+///
+/// # pub fn main() {
+///
+/// pub fn takes_plaintext(plain: Plaintext<u64>) {
+///     println!("{plain:?}");
+/// }
+///
+/// let encoded_msg = 3u64 << 60;
+///
+/// let normal_plaintext = Plaintext(encoded_msg);
+/// takes_plaintext(normal_plaintext);
+///
+/// // A plaintext containing a reference can be returned by iterators for example, here is how
+/// // to convert them painlessly.
+/// let ref_plaintext = Plaintext(&encoded_msg);
+/// takes_plaintext(ref_plaintext.into());
+/// # }
+/// ```
 #[derive(Debug, Copy, Clone, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
 pub struct Plaintext<T>(pub T);
 
