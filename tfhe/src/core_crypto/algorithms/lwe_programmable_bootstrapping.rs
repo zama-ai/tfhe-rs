533e37e6ee770c9fdb8d82654bb4ecded210731d
Arthur Meyre <arthur.meyre@zama.ai>
2022-12-06 11:26:41 +0100

    refactor(tfhe): only one instance of FftBuffers, use for simple PBS algo

--- tfhe/src/core_crypto/algorithms/lwe_programmable_bootstrapping.rs
+++ tfhe/src/core_crypto/algorithms/lwe_programmable_bootstrapping.rs
@@ -1,8 +1,9 @@
+use crate::core_crypto::commons::fft_buffers::FftBuffers;
 use crate::core_crypto::commons::traits::*;
 use crate::core_crypto::entities::*;
 use crate::core_crypto::fft_impl::crypto::bootstrap::{bootstrap_scratch, FourierLweBootstrapKey};
 use crate::core_crypto::fft_impl::crypto::ggsw::cmux_scratch;
-use crate::core_crypto::fft_impl::math::fft::FftView;
+use crate::core_crypto::fft_impl::math::fft::{Fft, FftView};
 use crate::core_crypto::specification::parameters::*;
 use aligned_vec::CACHELINE_ALIGN;
 use concrete_fft::c64;
@@ -11,7 +12,38 @@ use dyn_stack::{DynStack, SizeOverflow, StackReq};
 pub fn blind_rotate<Scalar, InputCont, OutputCont, KeyCont>(
     input: &LweCiphertext<InputCont>,
     lut: &mut GlweCiphertext<OutputCont>,
-    bsk: &FourierLweBootstrapKey<KeyCont>,
+    fourier_bsk: &FourierLweBootstrapKey<KeyCont>,
+) where
+    // CastInto required for PBS modulus switch which returns a usize
+    Scalar: UnsignedTorus + CastInto<usize>,
+    InputCont: Container<Element = Scalar>,
+    OutputCont: ContainerMut<Element = Scalar>,
+    KeyCont: Container<Element = c64>,
+{
+    let mut fft_buffers = FftBuffers::new();
+
+    let fft = Fft::new(fourier_bsk.polynomial_size());
+    let fft = fft.as_view();
+
+    fft_buffers.resize(
+        blind_rotate_mem_optimized_scratch::<Scalar>(
+            fourier_bsk.glwe_size(),
+            fourier_bsk.polynomial_size(),
+            fft,
+        )
+        .unwrap()
+        .unaligned_bytes_required(),
+    );
+
+    let stack = fft_buffers.stack();
+
+    blind_rotate_mem_optimized(input, lut, fourier_bsk, fft, stack);
+}
+
+pub fn blind_rotate_mem_optimized<Scalar, InputCont, OutputCont, KeyCont>(
+    input: &LweCiphertext<InputCont>,
+    lut: &mut GlweCiphertext<OutputCont>,
+    fourier_bsk: &FourierLweBootstrapKey<KeyCont>,
     fft: FftView<'_>,
     stack: DynStack<'_>,
 ) where
@@ -21,12 +53,13 @@ pub fn blind_rotate<Scalar, InputCont, OutputCont, KeyCont>(
     OutputCont: ContainerMut<Element = Scalar>,
     KeyCont: Container<Element = c64>,
 {
-    bsk.as_view()
+    fourier_bsk
+        .as_view()
         .blind_rotate(lut.as_mut_view(), input.as_ref(), fft, stack);
 }
 
-/// Returns the required memory for [`blind_rotate`].
-pub fn blind_rotate_scratch<Scalar>(
+/// Returns the required memory for [`blind_rotate_mem_optimized`].
+pub fn blind_rotate_mem_optimized_scratch<Scalar>(
     glwe_size: GlweSize,
     polynomial_size: PolynomialSize,
     fft: FftView<'_>,
@@ -36,6 +69,52 @@ pub fn blind_rotate_scratch<Scalar>(
 }
 
 pub fn programmable_bootstrap_lwe_ciphertext<Scalar, InputCont, OutputCont, AccCont, KeyCont>(
+    input: &LweCiphertext<InputCont>,
+    output: &mut LweCiphertext<OutputCont>,
+    accumulator: &GlweCiphertext<AccCont>,
+    fourier_bsk: &FourierLweBootstrapKey<KeyCont>,
+) where
+    // CastInto required for PBS modulus switch which returns a usize
+    Scalar: UnsignedTorus + CastInto<usize>,
+    InputCont: Container<Element = Scalar>,
+    OutputCont: ContainerMut<Element = Scalar>,
+    AccCont: Container<Element = Scalar>,
+    KeyCont: Container<Element = c64>,
+{
+    let mut fft_buffers = FftBuffers::new();
+
+    let fft = Fft::new(fourier_bsk.polynomial_size());
+    let fft = fft.as_view();
+
+    fft_buffers.resize(
+        programmable_bootstrap_lwe_ciphertext_mem_optimized_scratch::<Scalar>(
+            fourier_bsk.glwe_size(),
+            fourier_bsk.polynomial_size(),
+            fft,
+        )
+        .unwrap()
+        .unaligned_bytes_required(),
+    );
+
+    let stack = fft_buffers.stack();
+
+    programmable_bootstrap_lwe_ciphertext_mem_optimized(
+        input,
+        output,
+        accumulator,
+        fourier_bsk,
+        fft,
+        stack,
+    )
+}
+
+pub fn programmable_bootstrap_lwe_ciphertext_mem_optimized<
+    Scalar,
+    InputCont,
+    OutputCont,
+    AccCont,
+    KeyCont,
+>(
     input: &LweCiphertext<InputCont>,
     output: &mut LweCiphertext<OutputCont>,
     accumulator: &GlweCiphertext<AccCont>,
@@ -59,8 +138,8 @@ pub fn programmable_bootstrap_lwe_ciphertext<Scalar, InputCont, OutputCont, AccC
     );
 }
 
-/// Returns the required memory for [`programmable_bootstrap_lwe_ciphertext`].
-pub fn programmable_bootstrap_lwe_ciphertext_scratch<Scalar>(
+/// Returns the required memory for [`programmable_bootstrap_lwe_ciphertext_mem_optimized`].
+pub fn programmable_bootstrap_lwe_ciphertext_mem_optimized_scratch<Scalar>(
     glwe_size: GlweSize,
     polynomial_size: PolynomialSize,
     fft: FftView<'_>,
