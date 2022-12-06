6c3b0697500747211279ddf9bf9809e95370d59e
Arthur Meyre <arthur.meyre@zama.ai>
2022-12-06 17:31:08 +0100

    feat(core): add prelude

---
+++ tfhe/src/core_crypto/prelude.rs
@@ -0,0 +1,5 @@
+pub use super::algorithms::{polynomial_algorithms, slice_algorithms, *};
+pub use super::commons::dispersion::*;
+pub use super::commons::parameters::*;
+pub use super::commons::traits::*;
+pub use super::entities::*;
