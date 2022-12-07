f132421b9745dbcf67a08ec98769f1625fabc73a
Arthur Meyre <arthur.meyre@zama.ai>
2022-12-07 10:34:46 +0100

    docs(tfhe): update common traits docs

--- tfhe/src/core_crypto/commons/traits/create_from.rs
+++ tfhe/src/core_crypto/commons/traits/create_from.rs
@@ -1,11 +1,15 @@
 /// Trait to be able to create structs in contexts where the concrete type may not be known ahead of
 /// time.
 pub trait CreateFrom<T> {
+    /// Concrete type containing enough information to instantiate a new T.
     type Metadata: Clone + Copy;
 
+    /// Intantiate a new T using the associated metadata type.
     fn create_from(from: T, meta: Self::Metadata) -> Self;
 }
 
+/// A dummy placeholder type that can be used in traits requiring [`CreateFrom`] if using an actual
+/// type does not make sense.
 pub struct DummyCreateFrom {}
 
 impl<'data, T> CreateFrom<&'data [T]> for DummyCreateFrom {
