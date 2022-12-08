5f284a7c5532c2adee41ea63ea40eb8c0c3cf42c
Arthur Meyre <arthur.meyre@zama.ai>
2022-12-08 14:28:00 +0100

    docs(tfhe): add docstrings for LweCiphertext

--- tfhe/src/core_crypto/entities/lwe_ciphertext.rs
+++ tfhe/src/core_crypto/entities/lwe_ciphertext.rs
@@ -1,6 +1,7 @@
 use crate::core_crypto::commons::parameters::*;
 use crate::core_crypto::commons::traits::*;
 
+/// A convenience structure to easily manipulate the body of an [`LweCiphertext`].
 #[derive(Clone, Debug)]
 pub struct LweBody<T>(pub T);
 #[derive(Clone, Debug)]
@@ -8,11 +9,31 @@ pub struct LweMask<C: Container> {
     data: C,
 }
 
+/// A convenience structure to easily manipulate the mask of an [`LweCiphertext`].
 impl<C: Container> LweMask<C> {
+    /// Create a [`LweMask`] from an existing container.
+    ///
+    /// # Note
+    ///
+    /// This docstring exhibits [`LweMask`] primitives usage.
+    ///
+    /// ```
+    /// use tfhe::core_crypto::prelude::*;
+    ///
+    /// // Define parameters for LweMask creation
+    /// let lwe_dimension = LweDimension(600);
+    ///
+    /// let lwe_body = LweMask::from_container(vec![0u64; lwe_dimension.0]);
+    ///
+    /// assert_eq!(lwe_body.lwe_dimension(), lwe_dimension);
+    /// ```
     pub fn from_container(container: C) -> Self {
         LweMask { data: container }
     }
 
+    /// Return the [`LweDimension`] of the [`LweMask`].
+    ///
+    /// See [`LweMask::from_container`] for usage.
     pub fn lwe_dimension(&self) -> LweDimension {
         LweDimension(self.data.container_len())
     }
@@ -30,6 +51,26 @@ impl<T, C: ContainerMut<Element = T>> AsMut<[T]> for LweMask<C> {
     }
 }
 
+/// An [`LWE ciphertext`](`LweCiphertext`).
+///
+/// # Formal Definition
+///
+/// ## LWE Ciphertext
+///
+/// An LWE ciphertext is an encryption of a plaintext.
+/// It is secure under the hardness assumption called Learning With Errors (LWE).
+/// It is a specialization of
+/// [`GLWE ciphertext`](`crate::core_crypto::entities::GlweCiphertext`).
+///
+/// We indicate an LWE ciphertext of a plaintext $\mathsf{pt} \in\mathbb{Z}\_q$ as the following
+/// couple: $$\mathsf{ct} = \left( \vec{a} , b\right) \in \mathsf{LWE}^n\_{\vec{s}}( \mathsf{pt}
+/// )\subseteq \mathbb{Z}\_q^{(n+1)}$$ We call $q$ the ciphertext modulus and $n$ the LWE dimension.
+///
+/// ## LWE dimension
+/// It corresponds to the number of element in the LWE secret key.
+/// In an LWE ciphertext, it is the length of the vector $\vec{a}$.
+/// At [`encryption`](`crate::core_crypto::algorithms::encrypt_lwe_ciphertext`) time, it is
+/// the number of uniformly random integers generated.
 #[derive(Clone, Debug, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
 pub struct LweCiphertext<C: Container> {
     data: C,
@@ -48,6 +89,35 @@ impl<T, C: ContainerMut<Element = T>> AsMut<[T]> for LweCiphertext<C> {
 }
 
 impl<Scalar, C: Container<Element = Scalar>> LweCiphertext<C> {
+    /// Create a [`LweCiphertext`] from an existing container.
+    ///
+    /// # Note
+    ///
+    /// This function only wraps a container in the appropriate type. If you want to encrypt data
+    /// you need to use [`crate::core_crypto::algorithms::encrypt_lwe_ciphertext`] using this
+    /// ciphertext as output.
+    ///
+    /// This docstring exhibits [`LweCiphertext`] primitives usage.
+    ///
+    /// ```
+    /// use tfhe::core_crypto::prelude::*;
+    ///
+    /// // Define parameters for LweCiphertext creation
+    /// let lwe_size = LweSize(601);
+    ///
+    /// // Create a new LweCiphertext
+    /// let lwe = LweCiphertext::new(0u64, lwe_size);
+    ///
+    /// assert_eq!(lwe.lwe_size(), lwe_size);
+    ///
+    /// // Demonstrate how to recover the allocated container
+    /// let underlying_container: Vec<u64> = lwe.into_container();
+    ///
+    /// // Recreate a ciphertext using from_container
+    /// let lwe = LweCiphertext::from_container(underlying_container);
+    ///
+    /// assert_eq!(lwe.lwe_size(), lwe_size);
+    /// ```
     pub fn from_container(container: C) -> LweCiphertext<C> {
         assert!(
             container.container_len() > 0,
@@ -56,69 +126,97 @@ impl<Scalar, C: Container<Element = Scalar>> LweCiphertext<C> {
         LweCiphertext { data: container }
     }
 
+    /// Return the [`LweSize`] of the [`LweCiphertext`].
+    ///
+    /// See [`LweCiphertext::from_container`] for usage.
     pub fn lwe_size(&self) -> LweSize {
         LweSize(self.data.container_len())
     }
 
+    /// Return immutable views to the [`LweMask`] and [`LweBody`] of a [`LweCiphertext`].
     pub fn get_mask_and_body(&self) -> (LweMask<&[Scalar]>, LweBody<&Scalar>) {
         let (body, mask) = self.data.as_ref().split_last().unwrap();
 
         (LweMask::from_container(mask), LweBody(body))
     }
 
+    /// Return an immutable view to the [`LweBody`] of a [`LweCiphertext`].
     pub fn get_body(&self) -> LweBody<&Scalar> {
         let body = self.data.as_ref().last().unwrap();
 
         LweBody(body)
     }
 
+    /// Return an immutable view to the [`LweMask`] of a [`LweCiphertext`].
     pub fn get_mask(&self) -> LweMask<&[Scalar]> {
         LweMask::from_container(&self.as_ref()[0..=self.lwe_size().to_lwe_dimension().0])
     }
 
+    /// Return a view of the [`LweCiphertext`]. This is useful if an algorithm takes a view by
+    /// value.
     pub fn as_view(&self) -> LweCiphertextView<'_, Scalar> {
         LweCiphertextView::from_container(self.as_ref())
     }
 
     /// Consume the entity and return its underlying container.
+    ///
+    /// See [`LweCiphertext::from_container`] for usage.
     pub fn into_container(self) -> C {
         self.data
     }
 }
 
 impl<Scalar, C: ContainerMut<Element = Scalar>> LweCiphertext<C> {
+    /// Mutable variant of [`LweCiphertext::get_mask_and_body`].
     pub fn get_mut_mask_and_body(&mut self) -> (LweMask<&mut [Scalar]>, LweBody<&mut Scalar>) {
         let (body, mask) = self.data.as_mut().split_last_mut().unwrap();
 
         (LweMask::from_container(mask), LweBody(body))
     }
 
+    /// Mutable variant of [`LweCiphertext::get_body`].
     pub fn get_mut_body(&mut self) -> LweBody<&mut Scalar> {
         let body = self.data.as_mut().last_mut().unwrap();
 
         LweBody(body)
     }
 
+    /// Mutable variant of [`LweCiphertext::get_mask`].
     pub fn get_mut_mask(&mut self) -> LweMask<&mut [Scalar]> {
         let lwe_dimension = self.lwe_size().to_lwe_dimension();
         LweMask::from_container(&mut self.as_mut()[0..=lwe_dimension.0])
     }
 
+    /// Mutable variant of [`LweCiphertext::as_view`].
     pub fn as_mut_view(&mut self) -> LweCiphertextMutView<'_, Scalar> {
         LweCiphertextMutView::from_container(self.as_mut())
     }
 }
 
+/// A [`LweCiphertext`] owning the memory for its own storage.
 pub type LweCiphertextOwned<Scalar> = LweCiphertext<Vec<Scalar>>;
+/// A [`LweCiphertext`] immutably borrowing memory for its own storage.
 pub type LweCiphertextView<'data, Scalar> = LweCiphertext<&'data [Scalar]>;
+/// A [`LweCiphertext`] mutably borrowing memory for its own storage.
 pub type LweCiphertextMutView<'data, Scalar> = LweCiphertext<&'data mut [Scalar]>;
 
 impl<Scalar: Copy> LweCiphertextOwned<Scalar> {
+    /// Allocate memory and create a new owned [`LweCiphertext`].
+    ///
+    /// # Note
+    ///
+    /// This function allocates a vector of the appropriate size and wraps it in the appropriate
+    /// type. If you want to encrypt data you need to use
+    /// [`crate::core_crypto::algorithms::encrypt_lwe_ciphertext`] using this ciphertext as
+    /// output.
+    ///
+    /// See [`LweCiphertext::from_container`] for usage.
     pub fn new(fill_with: Scalar, lwe_size: LweSize) -> LweCiphertextOwned<Scalar> {
         LweCiphertextOwned::from_container(vec![fill_with; lwe_size.0])
     }
 }
 
+/// Metadata used in the [`CreateFrom`] implementation to create [`LweCiphertext`] entities.
 #[derive(Clone, Copy)]
 pub struct LweCiphertextCreationMetadata();
 
