ae58ad55aba65e9806db56eccbeed97b2bb685fb
Arthur Meyre <arthur.meyre@zama.ai>
2022-12-08 18:26:16 +0100

    docs(tfhe): docstring for Plaintext
    - add more sensible bounds for Plaintext and add PlaintextRef and
    PlaintextRefMut for a more homogeneous and less confusing dev experience

--- tfhe/src/core_crypto/entities/plaintext_list.rs
+++ tfhe/src/core_crypto/entities/plaintext_list.rs
@@ -2,6 +2,7 @@ use crate::core_crypto::commons::parameters::*;
 use crate::core_crypto::commons::traits::*;
 use crate::core_crypto::entities::*;
 
+/// A [`plaintext list`](`PlaintextList`). See [`Plaintext`].
 #[derive(Clone, Debug, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
 pub struct PlaintextList<C: Container> {
     data: C,
@@ -20,46 +21,94 @@ impl<Scalar, C: ContainerMut<Element = Scalar>> AsMut<[Scalar]> for PlaintextLis
 }
 
 impl<Scalar, C: Container<Element = Scalar>> PlaintextList<C> {
+    /// Create a [`PlaintextList`] from an existing container.
+    ///
+    /// # Note
+    ///
+    /// This function only wraps a container in the appropriate type.
+    ///
+    /// This docstring exhibits [`PlaintextList`] primitives usage.
+    ///
+    /// ```
+    /// use tfhe::core_crypto::prelude::*;
+    ///
+    /// // Define parameters for LweSecretKey creation
+    /// let plaintext_count = PlaintextCount(1024);
+    ///
+    /// // Create a new PlaintextList
+    /// let plaintext_list = PlaintextList::new(0u64, plaintext_count);
+    ///
+    /// assert_eq!(plaintext_list.plaintext_count(), plaintext_count);
+    ///
+    /// // Demonstrate how to recover the allocated container
+    /// let underlying_container: Vec<u64> = plaintext_list.into_container();
+    ///
+    /// // Recreate a secret key using from_container
+    /// let plaintext_list = PlaintextList::from_container(underlying_container);
+    ///
+    /// assert_eq!(plaintext_list.plaintext_count(), plaintext_count);
+    /// ```
     pub fn from_container(container: C) -> PlaintextList<C> {
         PlaintextList { data: container }
     }
 
+    /// Return the [`PlaintextCount`] of the [`PlaintextList`].
+    ///
+    /// See [`PlaintextList::from_container`] for usage.
     pub fn plaintext_count(&self) -> PlaintextCount {
         PlaintextCount(self.data.container_len())
     }
 
+    /// Interpret the [`PlaintextList`] as a [`Polynomial`].
     pub fn as_polynomial(&self) -> PolynomialView<'_, Scalar> {
         PolynomialView::from_container(self.as_ref())
     }
 
     /// Consume the entity and return its underlying container.
+    ///
+    /// See [`PlaintextList::from_container`] for usage.
     pub fn into_container(self) -> C {
         self.data
     }
 
+    /// Return a view of the [`PlaintextList`]. This is useful if an algorithm takes a view by
+    /// value.
     pub fn as_view(&self) -> PlaintextListView<'_, Scalar> {
         PlaintextListView::from_container(self.as_ref())
     }
 }
 
 impl<Scalar, C: ContainerMut<Element = Scalar>> PlaintextList<C> {
+    /// Mutable variant of [`PlaintextList::as_polynomial`].
     pub fn as_mut_polynomial(&mut self) -> PolynomialMutView<'_, Scalar> {
         PolynomialMutView::from_container(self.as_mut())
     }
 
+    /// Mutable variant of [`PlaintextList::as_view`].
     pub fn as_mut_view(&mut self) -> PlaintextListMutView<'_, Scalar> {
         PlaintextListMutView::from_container(self.as_mut())
     }
 }
 
+/// A [`PlaintextList`] owning the memory for its own storage.
 pub type PlaintextListOwned<Scalar> = PlaintextList<Vec<Scalar>>;
+/// A [`PlaintextList`] immutably borrowing memory for its own storage.
 pub type PlaintextListView<'data, Scalar> = PlaintextList<&'data [Scalar]>;
+/// A [`PlaintextList`] mutably borrowing memory for its own storage.
 pub type PlaintextListMutView<'data, Scalar> = PlaintextList<&'data mut [Scalar]>;
 
 impl<Scalar> PlaintextListOwned<Scalar>
 where
     Scalar: Copy,
 {
+    /// Allocate memory and create a new owned [`PlaintextList`].
+    ///
+    /// # Note
+    ///
+    /// This function allocates a vector of the appropriate size and wraps it in the appropriate
+    /// type.
+    ///
+    /// See [`PlaintextList::from_container`] for usage.
     pub fn new(fill_with: Scalar, count: PlaintextCount) -> PlaintextListOwned<Scalar> {
         PlaintextListOwned::from_container(vec![fill_with; count.0])
     }
@@ -74,12 +123,14 @@ impl<C: Container> CreateFrom<C> for PlaintextList<C> {
     }
 }
 
-impl<C: Container> ContiguousEntityContainer for PlaintextList<C> {
+impl<Scalar: Numeric, C: Container<Element = Scalar>> ContiguousEntityContainer
+    for PlaintextList<C>
+{
     type Element = C::Element;
 
     type EntityViewMetadata = ();
 
-    type EntityView<'this> = Plaintext<&'this Self::Element>
+    type EntityView<'this> = PlaintextRef<'this, Self::Element>
     where
         Self: 'this;
 
@@ -98,8 +149,10 @@ impl<C: Container> ContiguousEntityContainer for PlaintextList<C> {
     fn get_self_view_creation_metadata(&self) -> Self::SelfViewMetadata {}
 }
 
-impl<C: ContainerMut> ContiguousEntityContainerMut for PlaintextList<C> {
-    type EntityMutView<'this>= Plaintext<&'this mut  Self::Element>
+impl<Scalar: Numeric, C: ContainerMut<Element = Scalar>> ContiguousEntityContainerMut
+    for PlaintextList<C>
+{
+    type EntityMutView<'this>= PlaintextRefMut<'this, Self::Element>
     where
         Self: 'this;
 
