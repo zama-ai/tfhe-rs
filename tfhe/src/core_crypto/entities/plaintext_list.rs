2bdad26a9a35fccb4ea568e8cb2bdd7e98020b42
Arthur Meyre <arthur.meyre@zama.ai>
2022-12-09 14:13:44 +0100

    docs(tfhe): add PolynomialList docstrings

--- tfhe/src/core_crypto/entities/plaintext_list.rs
+++ tfhe/src/core_crypto/entities/plaintext_list.rs
@@ -2,7 +2,7 @@ use crate::core_crypto::commons::parameters::*;
 use crate::core_crypto::commons::traits::*;
 use crate::core_crypto::entities::*;
 
-/// A [`plaintext list`](`PlaintextList`). See [`Plaintext`].
+/// A contiguous list containing [`plaintexts`](`crate::core_crypto::entities::Plaintext`).
 #[derive(Clone, Debug, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
 pub struct PlaintextList<C: Container> {
     data: C,
