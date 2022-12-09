d0650ad6acb9b62d812196ba498ed5fcaeab63f5
Arthur Meyre <arthur.meyre@zama.ai>
2022-12-09 12:23:38 +0100

    docs(tfhe): add docstring for glwe_secret_key_generation module

--- tfhe/src/core_crypto/algorithms/glwe_secret_key_generation.rs
+++ tfhe/src/core_crypto/algorithms/glwe_secret_key_generation.rs
@@ -5,6 +5,11 @@ use crate::core_crypto::commons::parameters::*;
 use crate::core_crypto::commons::traits::*;
 use crate::core_crypto::entities::*;
 
+/// Allocate a new [`GLWE secret key`](`GlweSecretKey`) and fill it with uniformly random binary
+/// coefficients.
+///
+/// See [`encrypt_glwe_ciphertext`](`super::glwe_encryption::encrypt_glwe_ciphertext`)
+/// for usage.
 pub fn allocate_and_generate_new_binary_glwe_secret_key<Scalar, Gen>(
     glwe_dimension: GlweDimension,
     polynomial_size: PolynomialSize,
@@ -22,6 +27,38 @@ where
     glwe_secret_key
 }
 
+/// Fill a [`GLWE secret key`](`GlweSecretKey`) with uniformly random binary coefficients.
+///
+/// # Example
+///
+/// ```
+/// use tfhe::core_crypto::commons::generators::SecretRandomGenerator;
+/// use tfhe::core_crypto::commons::math::random::ActivatedRandomGenerator;
+/// use tfhe::core_crypto::prelude::*;
+/// use tfhe::seeders::new_seeder;
+///
+/// // DISCLAIMER: these toy example parameters are not guaranteed to be secure or yield correct
+/// // computations
+/// // Define parameters for GgswCiphertext creation
+/// let glwe_size = GlweSize(2);
+/// let polynomial_size = PolynomialSize(1024);
+///
+/// // Create the PRNG
+/// let mut seeder = new_seeder();
+/// let mut seeder = seeder.as_mut();
+/// let mut secret_generator =
+///     SecretRandomGenerator::<ActivatedRandomGenerator>::new(seeder.seed());
+///
+/// let mut glwe_secret_key =
+///     GlweSecretKey::new(0u64, glwe_size.to_glwe_dimension(), polynomial_size);
+///
+/// generate_binary_glwe_secret_key(&mut glwe_secret_key, &mut secret_generator);
+///
+/// // Check all coefficients are not zero as we just generated a new key
+/// // Note probability of this assert failing is (1/2)^polynomial_size or ~5.6 * 10^-309 for a
+/// // polynomial size of 1024.
+/// assert!(glwe_secret_key.as_ref().iter().all(|&elt| elt == 0) == false);
+/// ```
 pub fn generate_binary_glwe_secret_key<Scalar, InCont, Gen>(
     glwe_secret_key: &mut GlweSecretKey<InCont>,
     generator: &mut SecretRandomGenerator<Gen>,
