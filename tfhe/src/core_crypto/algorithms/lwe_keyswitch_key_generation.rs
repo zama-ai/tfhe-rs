2904d5f9a89d3454560a020c6f3d60aa75b696ce
Arthur Meyre <arthur.meyre@zama.ai>
2022-12-16 11:27:17 +0100

    feat(tfhe): add SeededLweKeyswitchKey
    - add generation equivalence test

--- tfhe/src/core_crypto/algorithms/lwe_keyswitch_key_generation.rs
+++ tfhe/src/core_crypto/algorithms/lwe_keyswitch_key_generation.rs
@@ -2,6 +2,7 @@ use crate::core_crypto::algorithms::*;
 use crate::core_crypto::commons::dispersion::DispersionParameter;
 use crate::core_crypto::commons::generators::EncryptionRandomGenerator;
 use crate::core_crypto::commons::math::decomposition::{DecompositionLevel, DecompositionTerm};
+use crate::core_crypto::commons::math::random::ActivatedRandomGenerator;
 use crate::core_crypto::commons::parameters::*;
 use crate::core_crypto::commons::traits::*;
 use crate::core_crypto::entities::*;
@@ -13,7 +14,6 @@ use crate::core_crypto::entities::*;
 /// use tfhe::core_crypto::commons::generators::{
 ///     EncryptionRandomGenerator, SecretRandomGenerator,
 /// };
-/// use tfhe::core_crypto::commons::math::decomposition::SignedDecomposer;
 /// use tfhe::core_crypto::commons::math::random::ActivatedRandomGenerator;
 /// use tfhe::core_crypto::prelude::*;
 /// use tfhe::seeders::new_seeder;
@@ -156,4 +156,273 @@ where
     );
 
     new_lwe_keyswitch_key
+}
+
+/// Fill an [`LWE keyswitch key`](`SeededLweKeyswitchKey`) with an actual keyswitching key
+/// constructed from an input and an output key [`LWE secret key`](`LweSecretKey`).
+///
+/// ```
+/// use tfhe::core_crypto::commons::generators::{
+///     EncryptionRandomGenerator, SecretRandomGenerator,
+/// };
+/// use tfhe::core_crypto::commons::math::random::ActivatedRandomGenerator;
+/// use tfhe::core_crypto::prelude::*;
+/// use tfhe::seeders::new_seeder;
+///
+/// // DISCLAIMER: these toy example parameters are not guaranteed to be secure or yield correct
+/// // computations
+/// // Define parameters for LweCiphertext creation
+/// let input_lwe_dimension = LweDimension(742);
+/// let lwe_modular_std_dev = StandardDev(0.000007069849454709433);
+/// let output_lwe_dimension = LweDimension(2048);
+/// let decomp_base_log = DecompositionBaseLog(3);
+/// let decomp_level_count = DecompositionLevelCount(5);
+///
+/// // Create the PRNG
+/// let mut seeder = new_seeder();
+/// let seeder = seeder.as_mut();
+/// let mut secret_generator =
+///     SecretRandomGenerator::<ActivatedRandomGenerator>::new(seeder.seed());
+///
+/// // Create the LweSecretKey
+/// let input_lwe_secret_key =
+///     allocate_and_generate_new_binary_lwe_secret_key(input_lwe_dimension, &mut secret_generator);
+/// let output_lwe_secret_key = allocate_and_generate_new_binary_lwe_secret_key(
+///     output_lwe_dimension,
+///     &mut secret_generator,
+/// );
+///
+/// let mut ksk = SeededLweKeyswitchKey::new(
+///     0u64,
+///     decomp_base_log,
+///     decomp_level_count,
+///     input_lwe_dimension,
+///     output_lwe_dimension,
+///     seeder.seed().into(),
+/// );
+///
+/// generate_seeded_lwe_keyswitch_key(
+///     &input_lwe_secret_key,
+///     &output_lwe_secret_key,
+///     &mut ksk,
+///     lwe_modular_std_dev,
+///     seeder,
+/// );
+///
+/// assert!(ksk.as_ref().iter().all(|&x| x == 0) == false);
+/// ```
+pub fn generate_seeded_lwe_keyswitch_key<
+    Scalar,
+    InputKeyCont,
+    OutputKeyCont,
+    KSKeyCont,
+    NoiseSeeder,
+>(
+    input_lwe_sk: &LweSecretKey<InputKeyCont>,
+    output_lwe_sk: &LweSecretKey<OutputKeyCont>,
+    lwe_keyswitch_key: &mut SeededLweKeyswitchKey<KSKeyCont>,
+    noise_parameters: impl DispersionParameter,
+    noise_seeder: &mut NoiseSeeder,
+) where
+    Scalar: UnsignedTorus,
+    InputKeyCont: Container<Element = Scalar>,
+    OutputKeyCont: Container<Element = Scalar>,
+    KSKeyCont: ContainerMut<Element = Scalar>,
+    // Maybe Sized allows to pass Box<dyn Seeder>.
+    NoiseSeeder: Seeder + ?Sized,
+{
+    assert!(
+        lwe_keyswitch_key.input_key_lwe_dimension() == input_lwe_sk.lwe_dimension(),
+        "The destination SeededLweKeyswitchKey input LweDimension is not equal \
+    to the input LweSecretKey LweDimension. Destination: {:?}, input: {:?}",
+        lwe_keyswitch_key.input_key_lwe_dimension(),
+        input_lwe_sk.lwe_dimension()
+    );
+    assert!(
+        lwe_keyswitch_key.output_key_lwe_dimension() == output_lwe_sk.lwe_dimension(),
+        "The destination SeededLweKeyswitchKey output LweDimension is not equal \
+    to the output LweSecretKey LweDimension. Destination: {:?}, output: {:?}",
+        lwe_keyswitch_key.output_key_lwe_dimension(),
+        input_lwe_sk.lwe_dimension()
+    );
+
+    let decomp_base_log = lwe_keyswitch_key.decomposition_base_log();
+    let decomp_level_count = lwe_keyswitch_key.decomposition_level_count();
+
+    // The plaintexts used to encrypt a key element will be stored in this buffer
+    let mut decomposition_plaintexts_buffer =
+        PlaintextListOwned::new(Scalar::ZERO, PlaintextCount(decomp_level_count.0));
+
+    let mut generator = EncryptionRandomGenerator::<ActivatedRandomGenerator>::new(
+        lwe_keyswitch_key.compression_seed().seed,
+        noise_seeder,
+    );
+
+    // Iterate over the input key elements and the destination lwe_keyswitch_key memory
+    for (input_key_element, mut keyswitch_key_block) in input_lwe_sk
+        .as_ref()
+        .iter()
+        .zip(lwe_keyswitch_key.iter_mut())
+    {
+        // We fill the buffer with the powers of the key elmements
+        for (level, message) in (1..=decomp_level_count.0)
+            .map(DecompositionLevel)
+            .zip(decomposition_plaintexts_buffer.iter_mut())
+        {
+            *message.0 = DecompositionTerm::new(level, decomp_base_log, *input_key_element)
+                .to_recomposition_summand();
+        }
+
+        encrypt_seeded_lwe_ciphertext_list_with_existing_generator(
+            output_lwe_sk,
+            &mut keyswitch_key_block,
+            &decomposition_plaintexts_buffer,
+            noise_parameters,
+            &mut generator,
+        );
+    }
+}
+
+/// Allocate a new [`seeded LWE keyswitch key`](`SeededLweKeyswitchKey`) and fill it with an actual
+/// keyswitching key constructed from an input and an output key
+/// [`LWE secret key`](`LweSecretKey`).
+pub fn allocate_and_generate_new_seeded_lwe_keyswitch_key<
+    Scalar,
+    InputKeyCont,
+    OutputKeyCont,
+    NoiseSeeder,
+>(
+    input_lwe_sk: &LweSecretKey<InputKeyCont>,
+    output_lwe_sk: &LweSecretKey<OutputKeyCont>,
+    decomp_base_log: DecompositionBaseLog,
+    decomp_level_count: DecompositionLevelCount,
+    noise_parameters: impl DispersionParameter,
+    noise_seeder: &mut NoiseSeeder,
+) -> SeededLweKeyswitchKeyOwned<Scalar>
+where
+    Scalar: UnsignedTorus,
+    InputKeyCont: Container<Element = Scalar>,
+    OutputKeyCont: Container<Element = Scalar>,
+    // Maybe Sized allows to pass Box<dyn Seeder>.
+    NoiseSeeder: Seeder + ?Sized,
+{
+    let mut new_lwe_keyswitch_key = SeededLweKeyswitchKeyOwned::new(
+        Scalar::ZERO,
+        decomp_base_log,
+        decomp_level_count,
+        input_lwe_sk.lwe_dimension(),
+        output_lwe_sk.lwe_dimension(),
+        noise_seeder.seed().into(),
+    );
+
+    generate_seeded_lwe_keyswitch_key(
+        input_lwe_sk,
+        output_lwe_sk,
+        &mut new_lwe_keyswitch_key,
+        noise_parameters,
+        noise_seeder,
+    );
+
+    new_lwe_keyswitch_key
+}
+
+#[cfg(test)]
+mod test {
+    use crate::core_crypto::commons::generators::{
+        DeterministicSeeder, EncryptionRandomGenerator, SecretRandomGenerator,
+    };
+    use crate::core_crypto::commons::math::random::ActivatedRandomGenerator;
+    use crate::core_crypto::prelude::*;
+    use crate::seeders::new_seeder;
+
+    fn test_seeded_lwe_ksk_gen_equivalence<Scalar: UnsignedTorus>() {
+        // DISCLAIMER: these toy example parameters are not guaranteed to be secure or yield correct
+        // computations
+        // Define parameters for LweCiphertext creation
+        let input_lwe_dimension = LweDimension(742);
+        let lwe_modular_std_dev = StandardDev(0.000007069849454709433);
+        let output_lwe_dimension = LweDimension(2048);
+        let decomp_base_log = DecompositionBaseLog(3);
+        let decomp_level_count = DecompositionLevelCount(5);
+
+        // Create the PRNG
+        let mut seeder = new_seeder();
+        let seeder = seeder.as_mut();
+        let mask_seed = seeder.seed();
+        let deterministic_seeder_seed = seeder.seed();
+        let mut secret_generator =
+            SecretRandomGenerator::<ActivatedRandomGenerator>::new(seeder.seed());
+
+        const NB_TEST: usize = 10;
+
+        for _ in 0..NB_TEST {
+            // Create the LweSecretKey
+            let input_lwe_secret_key = allocate_and_generate_new_binary_lwe_secret_key(
+                input_lwe_dimension,
+                &mut secret_generator,
+            );
+            let output_lwe_secret_key = allocate_and_generate_new_binary_lwe_secret_key(
+                output_lwe_dimension,
+                &mut secret_generator,
+            );
+
+            let mut ksk = LweKeyswitchKey::new(
+                Scalar::ZERO,
+                decomp_base_log,
+                decomp_level_count,
+                input_lwe_dimension,
+                output_lwe_dimension,
+            );
+
+            let mut deterministic_seeder =
+                DeterministicSeeder::<ActivatedRandomGenerator>::new(deterministic_seeder_seed);
+            let mut encryption_generator =
+                EncryptionRandomGenerator::<ActivatedRandomGenerator>::new(
+                    mask_seed,
+                    &mut deterministic_seeder,
+                );
+
+            generate_lwe_keyswitch_key(
+                &input_lwe_secret_key,
+                &output_lwe_secret_key,
+                &mut ksk,
+                lwe_modular_std_dev,
+                &mut encryption_generator,
+            );
+
+            let mut seeded_ksk = SeededLweKeyswitchKey::new(
+                Scalar::ZERO,
+                decomp_base_log,
+                decomp_level_count,
+                input_lwe_dimension,
+                output_lwe_dimension,
+                mask_seed.into(),
+            );
+
+            let mut deterministic_seeder =
+                DeterministicSeeder::<ActivatedRandomGenerator>::new(deterministic_seeder_seed);
+
+            generate_seeded_lwe_keyswitch_key(
+                &input_lwe_secret_key,
+                &output_lwe_secret_key,
+                &mut seeded_ksk,
+                lwe_modular_std_dev,
+                &mut deterministic_seeder,
+            );
+
+            let decompressed_ksk = seeded_ksk.decompress_into_lwe_keyswitch_key();
+
+            assert_eq!(ksk, decompressed_ksk);
+        }
+    }
+
+    #[test]
+    fn test_seeded_lwe_ksk_gen_equivalence_u32() {
+        test_seeded_lwe_ksk_gen_equivalence::<u32>()
+    }
+
+    #[test]
+    fn test_seeded_lwe_ksk_gen_equivalence_u64() {
+        test_seeded_lwe_ksk_gen_equivalence::<u64>()
+    }
 }
---
+++ tfhe/src/core_crypto/entities/seeded_lwe_keyswitch_key.rs
@@ -0,0 +1,356 @@
+use crate::core_crypto::algorithms::*;
+use crate::core_crypto::commons::math::random::{ActivatedRandomGenerator, CompressionSeed};
+use crate::core_crypto::commons::parameters::*;
+use crate::core_crypto::commons::traits::*;
+use crate::core_crypto::entities::*;
+
+/// A [`seeded LWE keyswitch key`](`SeededLweKeyswitchKey`).
+#[derive(Clone, Debug, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+pub struct SeededLweKeyswitchKey<C: Container> {
+    data: C,
+    decomp_base_log: DecompositionBaseLog,
+    decomp_level_count: DecompositionLevelCount,
+    output_lwe_size: LweSize,
+    compression_seed: CompressionSeed,
+}
+
+impl<T, C: Container<Element = T>> AsRef<[T]> for SeededLweKeyswitchKey<C> {
+    fn as_ref(&self) -> &[T] {
+        self.data.as_ref()
+    }
+}
+
+impl<T, C: ContainerMut<Element = T>> AsMut<[T]> for SeededLweKeyswitchKey<C> {
+    fn as_mut(&mut self) -> &mut [T] {
+        self.data.as_mut()
+    }
+}
+
+/// Return the number of elements in an encryption of an input [`LweSecretKey`] element for a
+/// [`SeededLweKeyswitchKey`] given a [`DecompositionLevelCount`] and output [`LweSize`].
+pub fn seeded_lwe_keyswitch_key_input_key_element_encrypted_size(
+    decomp_level_count: DecompositionLevelCount,
+) -> usize {
+    // One seeded ciphertext per level
+    decomp_level_count.0
+}
+
+impl<Scalar, C: Container<Element = Scalar>> SeededLweKeyswitchKey<C> {
+    /// Create an [`SeededLweKeyswitchKey`] from an existing container.
+    ///
+    /// # Note
+    ///
+    /// This function only wraps a container in the appropriate type. If you want to generate an
+    /// [`SeededLweKeyswitchKey`] you need to call
+    /// [`crate::core_crypto::algorithms::generate_seeded_lwe_keyswitch_key`] using this key as
+    /// output.
+    ///
+    /// This docstring exhibits [`SeededLweKeyswitchKey`] primitives usage.
+    ///
+    /// ```
+    /// use tfhe::core_crypto::prelude::*;
+    /// use tfhe::seeders::new_seeder;
+    ///
+    /// // DISCLAIMER: these toy example parameters are not guaranteed to be secure or yield correct
+    /// // computations
+    /// // Define parameters for SeededLweKeyswitchKey creation
+    /// let input_lwe_dimension = LweDimension(600);
+    /// let output_lwe_dimension = LweDimension(1024);
+    /// let decomp_base_log = DecompositionBaseLog(4);
+    /// let decomp_level_count = DecompositionLevelCount(5);
+    ///
+    /// // Get a seeder
+    /// let mut seeder = new_seeder();
+    /// let seeder = seeder.as_mut();
+    ///
+    /// // Create a new SeededLweKeyswitchKey
+    /// let lwe_ksk = SeededLweKeyswitchKey::new(
+    ///     0u64,
+    ///     decomp_base_log,
+    ///     decomp_level_count,
+    ///     input_lwe_dimension,
+    ///     output_lwe_dimension,
+    ///     seeder.seed().into(),
+    /// );
+    ///
+    /// assert_eq!(lwe_ksk.decomposition_base_log(), decomp_base_log);
+    /// assert_eq!(lwe_ksk.decomposition_level_count(), decomp_level_count);
+    /// assert_eq!(lwe_ksk.input_key_lwe_dimension(), input_lwe_dimension);
+    /// assert_eq!(lwe_ksk.output_key_lwe_dimension(), output_lwe_dimension);
+    /// assert_eq!(
+    ///     lwe_ksk.output_lwe_size(),
+    ///     output_lwe_dimension.to_lwe_size()
+    /// );
+    ///
+    /// let compression_seed = lwe_ksk.compression_seed();
+    ///
+    /// // Demonstrate how to recover the allocated container
+    /// let underlying_container: Vec<u64> = lwe_ksk.into_container();
+    ///
+    /// // Recreate a secret key using from_container
+    /// let lwe_ksk = SeededLweKeyswitchKey::from_container(
+    ///     underlying_container,
+    ///     decomp_base_log,
+    ///     decomp_level_count,
+    ///     output_lwe_dimension.to_lwe_size(),
+    ///     compression_seed,
+    /// );
+    ///
+    /// assert_eq!(lwe_ksk.decomposition_base_log(), decomp_base_log);
+    /// assert_eq!(lwe_ksk.decomposition_level_count(), decomp_level_count);
+    /// assert_eq!(lwe_ksk.input_key_lwe_dimension(), input_lwe_dimension);
+    /// assert_eq!(lwe_ksk.output_key_lwe_dimension(), output_lwe_dimension);
+    /// assert_eq!(
+    ///     lwe_ksk.output_lwe_size(),
+    ///     output_lwe_dimension.to_lwe_size()
+    /// );
+    ///
+    /// let lwe_ksk = lwe_ksk.decompress_into_lwe_keyswitch_key();
+    ///
+    /// assert_eq!(lwe_ksk.decomposition_base_log(), decomp_base_log);
+    /// assert_eq!(lwe_ksk.decomposition_level_count(), decomp_level_count);
+    /// assert_eq!(lwe_ksk.input_key_lwe_dimension(), input_lwe_dimension);
+    /// assert_eq!(lwe_ksk.output_key_lwe_dimension(), output_lwe_dimension);
+    /// assert_eq!(
+    ///     lwe_ksk.output_lwe_size(),
+    ///     output_lwe_dimension.to_lwe_size()
+    /// );
+    /// ```
+    pub fn from_container(
+        container: C,
+        decomp_base_log: DecompositionBaseLog,
+        decomp_level_count: DecompositionLevelCount,
+        output_lwe_size: LweSize,
+        compression_seed: CompressionSeed,
+    ) -> Self {
+        assert!(
+            container.container_len() > 0,
+            "Got an empty container to create an SeededLweKeyswitchKey"
+        );
+        assert!(
+            container.container_len() % (decomp_level_count.0) == 0,
+            "The provided container length is not valid. \
+        It needs to be dividable by decomp_level_count: {}. \
+        Got container length: {} and decomp_level_count: {decomp_level_count:?}.",
+            decomp_level_count.0,
+            container.container_len()
+        );
+
+        SeededLweKeyswitchKey {
+            data: container,
+            decomp_base_log,
+            decomp_level_count,
+            output_lwe_size,
+            compression_seed,
+        }
+    }
+
+    /// Return the [`DecompositionBaseLog`] of the [`SeededLweKeyswitchKey`].
+    ///
+    /// See [`SeededLweKeyswitchKey::from_container`] for usage.
+    pub fn decomposition_base_log(&self) -> DecompositionBaseLog {
+        self.decomp_base_log
+    }
+
+    /// Return the [`DecompositionLevelCount`] of the [`SeededLweKeyswitchKey`].
+    ///
+    /// See [`SeededLweKeyswitchKey::from_container`] for usage.
+    pub fn decomposition_level_count(&self) -> DecompositionLevelCount {
+        self.decomp_level_count
+    }
+
+    /// Return the input [`LweDimension`] of the [`SeededLweKeyswitchKey`].
+    ///
+    /// See [`SeededLweKeyswitchKey::from_container`] for usage.
+    pub fn input_key_lwe_dimension(&self) -> LweDimension {
+        LweDimension(self.data.container_len() / self.seeded_input_key_element_encrypted_size())
+    }
+
+    /// Return the output [`LweDimension`] of the [`SeededLweKeyswitchKey`].
+    ///
+    /// See [`SeededLweKeyswitchKey::from_container`] for usage.
+    pub fn output_key_lwe_dimension(&self) -> LweDimension {
+        self.output_lwe_size.to_lwe_dimension()
+    }
+
+    /// Return the output [`LweSize`] of the [`SeededLweKeyswitchKey`].
+    ///
+    /// See [`SeededLweKeyswitchKey::from_container`] for usage.
+    pub fn output_lwe_size(&self) -> LweSize {
+        self.output_lwe_size
+    }
+
+    /// Return the output [`CompressionSeed`] of the [`SeededLweKeyswitchKey`].
+    ///
+    /// See [`SeededLweKeyswitchKey::from_container`] for usage.
+    pub fn compression_seed(&self) -> CompressionSeed {
+        self.compression_seed
+    }
+
+    /// Return the number of elements in an encryption of an input [`LweSecretKey`] element of the
+    /// current [`SeededLweKeyswitchKey`].
+    pub fn seeded_input_key_element_encrypted_size(&self) -> usize {
+        seeded_lwe_keyswitch_key_input_key_element_encrypted_size(self.decomp_level_count)
+    }
+
+    /// Return a view of the [`SeededLweKeyswitchKey`]. This is useful if an algorithm takes a view
+    /// by value.
+    pub fn as_view(&self) -> SeededLweKeyswitchKey<&'_ [Scalar]> {
+        SeededLweKeyswitchKey::from_container(
+            self.as_ref(),
+            self.decomp_base_log,
+            self.decomp_level_count,
+            self.output_lwe_size,
+            self.compression_seed,
+        )
+    }
+
+    /// Consume the entity and return its underlying container.
+    ///
+    /// See [`SeededLweKeyswitchKey::from_container`] for usage.
+    pub fn into_container(self) -> C {
+        self.data
+    }
+
+    /// Consume the [`SeededLweKeyswitchKey`] and decompress it into a standard
+    /// [`LweKeyswitchKey`].
+    ///
+    /// See [`SeededLweKeyswitchKey::from_container`] for usage.
+    pub fn decompress_into_lwe_keyswitch_key(self) -> LweKeyswitchKeyOwned<Scalar>
+    where
+        Scalar: UnsignedTorus,
+    {
+        let mut decompressed_ksk = LweKeyswitchKeyOwned::new(
+            Scalar::ZERO,
+            self.decomposition_base_log(),
+            self.decomposition_level_count(),
+            self.input_key_lwe_dimension(),
+            self.output_key_lwe_dimension(),
+        );
+        decompress_seeded_lwe_keyswitch_key::<_, _, _, ActivatedRandomGenerator>(
+            &mut decompressed_ksk,
+            &self,
+        );
+        decompressed_ksk
+    }
+
+    pub fn as_seeded_lwe_ciphertext_list(&self) -> SeededLweCiphertextListView<'_, Scalar> {
+        SeededLweCiphertextListView::from_container(
+            self.as_ref(),
+            self.output_lwe_size(),
+            self.compression_seed(),
+        )
+    }
+}
+
+impl<Scalar, C: ContainerMut<Element = Scalar>> SeededLweKeyswitchKey<C> {
+    /// Mutable variant of [`SeededLweKeyswitchKey::as_view`].
+    pub fn as_mut_view(&mut self) -> SeededLweKeyswitchKey<&'_ mut [Scalar]> {
+        let decomp_base_log = self.decomp_base_log;
+        let decomp_level_count = self.decomp_level_count;
+        let output_lwe_size = self.output_lwe_size;
+        let compression_seed = self.compression_seed;
+        SeededLweKeyswitchKey::from_container(
+            self.as_mut(),
+            decomp_base_log,
+            decomp_level_count,
+            output_lwe_size,
+            compression_seed,
+        )
+    }
+
+    pub fn as_mut_seeded_lwe_ciphertext_list(
+        &mut self,
+    ) -> SeededLweCiphertextListMutView<'_, Scalar> {
+        let output_lwe_size = self.output_lwe_size();
+        let compression_seed = self.compression_seed();
+        SeededLweCiphertextListMutView::from_container(
+            self.as_mut(),
+            output_lwe_size,
+            compression_seed,
+        )
+    }
+}
+
+/// An [`SeededLweKeyswitchKey`] owning the memory for its own storage.
+pub type SeededLweKeyswitchKeyOwned<Scalar> = SeededLweKeyswitchKey<Vec<Scalar>>;
+
+impl<Scalar: Copy> SeededLweKeyswitchKeyOwned<Scalar> {
+    /// Allocate memory and create a new owned [`SeededLweKeyswitchKey`].
+    ///
+    /// # Note
+    ///
+    /// This function allocates a vector of the appropriate size and wraps it in the appropriate
+    /// type. If you want to generate an [`SeededLweKeyswitchKey`] you need to call
+    /// [`crate::core_crypto::algorithms::generate_seeded_lwe_keyswitch_key`] using this key as
+    /// output.
+    ///
+    /// See [`SeededLweKeyswitchKey::from_container`] for usage.
+    pub fn new(
+        fill_with: Scalar,
+        decomp_base_log: DecompositionBaseLog,
+        decomp_level_count: DecompositionLevelCount,
+        input_key_lwe_dimension: LweDimension,
+        output_key_lwe_dimension: LweDimension,
+        compression_seed: CompressionSeed,
+    ) -> SeededLweKeyswitchKeyOwned<Scalar> {
+        SeededLweKeyswitchKeyOwned::from_container(
+            vec![
+                fill_with;
+                input_key_lwe_dimension.0
+                    * seeded_lwe_keyswitch_key_input_key_element_encrypted_size(decomp_level_count,)
+            ],
+            decomp_base_log,
+            decomp_level_count,
+            output_key_lwe_dimension.to_lwe_size(),
+            compression_seed,
+        )
+    }
+}
+
+impl<C: Container> ContiguousEntityContainer for SeededLweKeyswitchKey<C> {
+    type Element = C::Element;
+
+    type EntityViewMetadata = SeededLweCiphertextListCreationMetadata;
+
+    type EntityView<'this> = SeededLweCiphertextListView<'this, Self::Element>
+    where
+        Self: 'this;
+
+    type SelfViewMetadata = ();
+
+    // At the moment it does not make sense to return "sub" keyswitch keys. So we use a dummy
+    // placeholder type here.
+    type SelfView<'this> = DummyCreateFrom
+    where
+        Self: 'this;
+
+    fn get_entity_view_creation_metadata(&self) -> SeededLweCiphertextListCreationMetadata {
+        SeededLweCiphertextListCreationMetadata(self.output_lwe_size(), self.compression_seed())
+    }
+
+    fn get_entity_view_pod_size(&self) -> usize {
+        self.seeded_input_key_element_encrypted_size()
+    }
+
+    /// Unimplemented for [`SeededLweKeyswitchKey`]. At the moment it does not make sense to
+    /// return "sub" keyswitch keys.
+    fn get_self_view_creation_metadata(&self) {
+        unimplemented!(
+            "This function is not supported for SeededLweKeyswitchKey. \
+        At the moment it does not make sense to return 'sub' keyswitch keys."
+        )
+    }
+}
+
+impl<C: ContainerMut> ContiguousEntityContainerMut for SeededLweKeyswitchKey<C> {
+    type EntityMutView<'this> = SeededLweCiphertextListMutView<'this, Self::Element>
+    where
+        Self: 'this;
+
+    // At the moment it does not make sense to return "sub" keyswitch keys. So we use a dummy
+    // placeholder type here.
+    type SelfMutView<'this> = DummyCreateFrom
+    where
+        Self: 'this;
+}
