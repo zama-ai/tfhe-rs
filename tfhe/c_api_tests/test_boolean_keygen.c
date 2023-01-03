2df7ec29c92e7aeda65cfbe6a670a3ac5af52458
Arthur Meyre <arthur.meyre@zama.ai>
2023-01-03 15:30:11 +0100

    chore(tfhe): fix thfe typo

--- tfhe/c_api_tests/test_boolean_keygen.c
+++ tfhe/c_api_tests/test_boolean_keygen.c
@@ -57,7 +57,7 @@ void test_predefined_keygen_w_serde(void) {
   destroy_boolean_server_key(sks);
 
   gen_keys_ok = boolean_gen_keys_with_predefined_parameters_set(
-      BOOLEAN_PARAMETERS_SET_THFE_LIB_PARAMETERS, &cks, &sks);
+      BOOLEAN_PARAMETERS_SET_TFHE_LIB_PARAMETERS, &cks, &sks);
 
   assert(gen_keys_ok == 0);
 
