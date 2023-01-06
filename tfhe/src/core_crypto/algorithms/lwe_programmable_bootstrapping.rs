50170763f06d01bdfa126b91b9d34c2de0b7a7b5
Arthur Meyre <arthur.meyre@zama.ai>
2023-01-06 11:18:52 +0100

    chore(doc): add missing docstring for the lwe_programmable_bootstrap module
    - add the PBS algorithm in the LweBootstrapKey docstring

--- tfhe/src/core_crypto/algorithms/lwe_programmable_bootstrapping.rs
+++ tfhe/src/core_crypto/algorithms/lwe_programmable_bootstrapping.rs
@@ -1,3 +1,6 @@
+//! Module containing primitives pertaining to the [`LWE programmable
+//! bootstrap`](`LweBootstrapKey#programmable-bootstrapping`).
+
 use crate::core_crypto::commons::computation_buffers::ComputationBuffers;
 use crate::core_crypto::commons::parameters::*;
 use crate::core_crypto::commons::traits::*;
@@ -8,6 +11,12 @@ use crate::core_crypto::fft_impl::math::fft::{Fft, FftView};
 use concrete_fft::c64;
 use dyn_stack::{DynStack, SizeOverflow, StackReq};
 
+/// Perform a blind rotation given an input [`LWE ciphertext`](`LweCiphertext`), modifying a look-up
+/// table passed as a [`GLWE ciphertext`](`GlweCiphertext`) and an [`LWE bootstrap
+/// key`](`LweBootstrapKey`) in the fourier domain.
+///
+/// If you want to manage the computation memory manually you can use
+/// [`blind_rotate_assign_mem_optimized`].
 pub fn blind_rotate_assign<Scalar, InputCont, OutputCont, KeyCont>(
     input: &LweCiphertext<InputCont>,
     lut: &mut GlweCiphertext<OutputCont>,
@@ -39,6 +48,9 @@ pub fn blind_rotate_assign<Scalar, InputCont, OutputCont, KeyCont>(
     blind_rotate_assign_mem_optimized(input, lut, fourier_bsk, fft, stack);
 }
 
+/// Memory optimized version of [`blind_rotate_assign`], the caller must provide
+/// a properly configured [`FftView`] object and a `DynStack` used as a memory buffer having a
+/// capacity at least as large as the result of [`blind_rotate_assign_mem_optimized_requirement`].
 pub fn blind_rotate_assign_mem_optimized<Scalar, InputCont, OutputCont, KeyCont>(
     input: &LweCiphertext<InputCont>,
     lut: &mut GlweCiphertext<OutputCont>,
@@ -66,6 +78,13 @@ pub fn blind_rotate_assign_mem_optimized_requirement<Scalar>(
     blind_rotate_assign_scratch::<Scalar>(glwe_size, polynomial_size, fft)
 }
 
+/// Perform a programmable bootsrap given an input [`LWE ciphertext`](`LweCiphertext`), a
+/// look-up table passed as a [`GLWE ciphertext`](`GlweCiphertext`) and an [`LWE bootstrap
+/// key`](`LweBootstrapKey`) in the fourier domain. The result is written in the provided output
+/// [`LWE ciphertext`](`LweCiphertext`).
+///
+/// If you want to manage the computation memory manually you can use
+/// [`programmable_bootstrap_lwe_ciphertext_mem_optimized`].
 pub fn programmable_bootstrap_lwe_ciphertext<Scalar, InputCont, OutputCont, AccCont, KeyCont>(
     input: &LweCiphertext<InputCont>,
     output: &mut LweCiphertext<OutputCont>,
@@ -106,6 +125,10 @@ pub fn programmable_bootstrap_lwe_ciphertext<Scalar, InputCont, OutputCont, AccC
     )
 }
 
+/// Memory optimized version of [`programmable_bootstrap_lwe_ciphertext`], the caller must provide
+/// a properly configured [`FftView`] object and a `DynStack` used as a memory buffer having a
+/// capacity at least as large as the result of
+/// [`programmable_bootstrap_lwe_ciphertext_mem_optimized_requirement`].
 pub fn programmable_bootstrap_lwe_ciphertext_mem_optimized<
     Scalar,
     InputCont,
