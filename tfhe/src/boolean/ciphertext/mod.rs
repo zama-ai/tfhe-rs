384850f7fa15c69769eb5f4148d42880330bbfed
Arthur Meyre <arthur.meyre@zama.ai>
2023-01-12 13:22:37 +0100

    feat(boolean): add CompressedCiphertext

--- tfhe/src/boolean/ciphertext/mod.rs
+++ tfhe/src/boolean/ciphertext/mod.rs
@@ -53,3 +53,45 @@ impl<'de> Deserialize<'de> for Ciphertext {
         })
     }
 }
+
+/// A structure containing a compressed ciphertext, meant to encrypt a Boolean message.
+///
+/// It has to be decompressed before evaluating a Boolean circuit.
+#[derive(Clone, Debug)]
+pub struct CompressedCiphertext {
+    pub(crate) ciphertext: SeededLweCiphertext<u32>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct SerializableCompressedCiphertext {
+    data: Vec<u8>,
+}
+
+impl Serialize for CompressedCiphertext {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let data = bincode::serialize(&self.ciphertext).map_err(serde::ser::Error::custom)?;
+        let ser_ct = SerializableCompressedCiphertext { data };
+
+        ser_ct.serialize(serializer)
+    }
+}
+
+impl<'de> Deserialize<'de> for CompressedCiphertext {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let thing = SerializableCompressedCiphertext::deserialize(deserializer)?;
+        let lwe = bincode::deserialize(&thing.data).map_err(serde::de::Error::custom)?;
+        Ok(Self { ciphertext: lwe })
+    }
+}
+
+impl From<CompressedCiphertext> for Ciphertext {
+    fn from(value: CompressedCiphertext) -> Self {
+        Self::Encrypted(value.ciphertext.decompress_into_lwe_ciphertext())
+    }
+}
