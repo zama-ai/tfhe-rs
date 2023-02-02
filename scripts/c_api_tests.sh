d224821aaae0b2a89b598cc0f4a79efdd4b4767c
Arthur Meyre <arthur.meyre@zama.ai>
2023-02-02 10:40:19 +0100

    chore(tfhe): update testing script to allow custom RUSTFLAGS

--- scripts/c_api_tests.sh
+++ scripts/c_api_tests.sh
@@ -6,7 +6,6 @@ function usage() {
     echo "$0: build and/or run the C API tests"
     echo
     echo "--help                    Print this message"
-    echo "--rust-toolchain          The toolchain to check the version for with leading"
     echo "--build-only              Pass to only build the tests without running them"
     echo
 }
@@ -19,11 +18,6 @@ do
         "--help" | "-h" )
             usage
             exit 0
-            ;;
-
-        "--rust-toolchain" )
-            shift
-            RUST_TOOLCHAIN="$1"
             ;;
 
         "--build-only" )
@@ -39,7 +33,6 @@ do
 done
 
 CURR_DIR="$(dirname "$0")"
-ARCH_FEATURE="$("${CURR_DIR}/get_arch_feature.sh")"
 REPO_ROOT="${CURR_DIR}/.."
 TFHE_BUILD_DIR="${REPO_ROOT}/tfhe/build/"
 
@@ -48,9 +41,6 @@ mkdir -p "${TFHE_BUILD_DIR}"
 cd "${TFHE_BUILD_DIR}"
 
 cmake .. -DCMAKE_BUILD_TYPE=RELEASE
-
-RUSTFLAGS="-C target-cpu=native" cargo ${RUST_TOOLCHAIN:+"${RUST_TOOLCHAIN}"} build \
---release --features="${ARCH_FEATURE}",boolean-c-api,shortint-c-api -p tfhe
 
 make -j
 
