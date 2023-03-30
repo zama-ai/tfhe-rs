f894c33bfd50ecdd02305a465a1061202a9d1a66
Arthur Meyre <arthur.meyre@zama.ai>
2023-03-30 16:08:27 +0200

    feat(shortint): add "default" scalar sub operations

--- tfhe/src/shortint/server_key/scalar_sub.rs
+++ tfhe/src/shortint/server_key/scalar_sub.rs
@@ -9,6 +9,126 @@ impl ServerKey {
     ///
     /// The result is returned in a _new_ ciphertext.
     ///
+    /// This function, like all "default" operations (i.e. not smart, checked or unchecked), will
+    /// check that the input ciphertext carries are empty and clears them if it's not the case and
+    /// the operation requires it. It outputs a ciphertext whose carry is always empty.
+    ///
+    /// This means that when using only "default" operations, a given operation (like add for
+    /// example) has always the same performance characteristics from one call to another and
+    /// guarantees correctness by pre-emptively clearing carries of output ciphertexts.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use tfhe::shortint::gen_keys;
+    /// use tfhe::shortint::parameters::{PARAM_MESSAGE_2_CARRY_2, PARAM_SMALL_MESSAGE_2_CARRY_2};
+    ///
+    /// // Generate the client key and the server key:
+    /// let (cks, sks) = gen_keys(PARAM_MESSAGE_2_CARRY_2);
+    ///
+    /// let msg = 3;
+    /// let scalar = 3;
+    ///
+    /// // Encrypt a message
+    /// let mut ct = cks.encrypt(msg);
+    ///
+    /// // Compute homomorphically a scalar multiplication:
+    /// let ct_res = sks.scalar_sub(&mut ct, scalar);
+    ///
+    /// // The input ciphertext content is not changed
+    /// assert_eq!(cks.decrypt(&ct), msg);
+    ///
+    /// // Our result is what we expect
+    /// let clear = cks.decrypt(&ct_res);
+    ///
+    /// assert_eq!(msg - scalar as u64, clear);
+    ///
+    /// let (cks, sks) = gen_keys(PARAM_SMALL_MESSAGE_2_CARRY_2);
+    ///
+    /// // Encrypt a message
+    /// let mut ct = cks.encrypt_small(msg);
+    ///
+    /// // Compute homomorphically a scalar multiplication:
+    /// let ct_res = sks.scalar_sub(&mut ct, scalar);
+    ///
+    /// // The input ciphertext content is not changed
+    /// assert_eq!(cks.decrypt(&ct), msg);
+    ///
+    /// // Our result is what we expect
+    /// let clear = cks.decrypt(&ct_res);
+    ///
+    /// assert_eq!(msg - scalar as u64, clear);
+    /// ```
+    pub fn scalar_sub<OpOrder: PBSOrderMarker>(
+        &self,
+        ct: &CiphertextBase<OpOrder>,
+        scalar: u8,
+    ) -> CiphertextBase<OpOrder> {
+        let mut ct_res = ct.clone();
+        self.scalar_sub_assign(&mut ct_res, scalar);
+        ct_res
+    }
+
+    /// Compute homomorphically a subtraction of a ciphertext by a scalar.
+    ///
+    /// The result is _stored_ in the `ct` ciphertext.
+    ///
+    /// This function, like all "default" operations (i.e. not smart, checked or unchecked), will
+    /// check that the input ciphertext carries are empty and clears them if it's not the case and
+    /// the operation requires it. It outputs a ciphertext whose carry is always empty.
+    ///
+    /// This means that when using only "default" operations, a given operation (like add for
+    /// example) has always the same performance characteristics from one call to another and
+    /// guarantees correctness by pre-emptively clearing carries of output ciphertexts.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use tfhe::shortint::gen_keys;
+    /// use tfhe::shortint::parameters::{PARAM_MESSAGE_2_CARRY_2, PARAM_SMALL_MESSAGE_2_CARRY_2};
+    ///
+    /// // Generate the client key and the server key:
+    /// let (cks, sks) = gen_keys(PARAM_MESSAGE_2_CARRY_2);
+    ///
+    /// let msg = 5;
+    /// let scalar = 3;
+    ///
+    /// // Encrypt a message
+    /// let mut ct = cks.encrypt(msg);
+    ///
+    /// // Compute homomorphically a scalar multiplication:
+    /// sks.scalar_sub_assign(&mut ct, scalar);
+    ///
+    /// // Our result is what we expect
+    /// let clear = cks.decrypt(&ct);
+    /// assert_eq!(msg - scalar as u64, clear);
+    ///
+    /// let (cks, sks) = gen_keys(PARAM_SMALL_MESSAGE_2_CARRY_2);
+    ///
+    /// // Encrypt a message
+    /// let mut ct = cks.encrypt_small(msg);
+    ///
+    /// // Compute homomorphically a scalar multiplication:
+    /// sks.scalar_sub_assign(&mut ct, scalar);
+    ///
+    /// // Our result is what we expect
+    /// let clear = cks.decrypt(&ct);
+    /// assert_eq!(msg - scalar as u64, clear);
+    /// ```
+    pub fn scalar_sub_assign<OpOrder: PBSOrderMarker>(
+        &self,
+        ct: &mut CiphertextBase<OpOrder>,
+        scalar: u8,
+    ) {
+        let modulus = self.message_modulus.0 as u64;
+        let acc = self.generate_accumulator(|x| (x - scalar as u64) % modulus);
+        self.apply_lookup_table_assign(ct, &acc);
+    }
+
+    /// Compute homomorphically a subtraction of a ciphertext by a scalar.
+    ///
+    /// The result is returned in a _new_ ciphertext.
+    ///
     /// This function does _not_ check whether the capacity of the ciphertext is exceeded.
     ///
     /// # Example
