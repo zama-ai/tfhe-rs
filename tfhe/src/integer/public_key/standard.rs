1dfe9a78a32af2334718f0ef4cfdb001459b334f
tmontaigu <thomas.montaigu@laposte.net>
2023-05-19 11:24:41 +0200

    feat(integer): allow decompressing CompressedPublicKey

--- tfhe/src/integer/public_key/standard.rs
+++ tfhe/src/integer/public_key/standard.rs
@@ -1,6 +1,7 @@
 use crate::integer::ciphertext::{CrtCiphertext, RadixCiphertext};
 use crate::integer::client_key::ClientKey;
 use crate::integer::encryption::{encrypt_crt, encrypt_words_radix_impl, AsLittleEndianWords};
+use crate::integer::public_key::compressed::CompressedPublicKeyBase;
 use crate::shortint::ciphertext::{BootstrapKeyswitch, KeyswitchBootstrap};
 use crate::shortint::parameters::MessageModulus;
 use crate::shortint::{PBSOrderMarker, PublicKeyBase};
@@ -95,5 +96,44 @@ impl<PBSOrder: PBSOrderMarker> PublicKey<PBSOrder> {
         RadixCiphertextType: From<Vec<Block>>,
     {
         encrypt_words_radix_impl(&self.key, message_words, num_blocks, encrypt_block)
+    }
+}
+
+impl<PBSOrder: PBSOrderMarker> From<CompressedPublicKeyBase<PBSOrder>> for PublicKey<PBSOrder> {
+    fn from(value: CompressedPublicKeyBase<PBSOrder>) -> Self {
+        Self {
+            key: value.key.into(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::integer::keycache::KEY_CACHE;
+    use crate::shortint::parameters::*;
+    use crate::shortint::PBSParameters;
+
+    create_parametrized_test!(integer_public_key_decompression_small {
+        PARAM_SMALL_MESSAGE_2_CARRY_2,
+    });
+
+    fn integer_public_key_decompression_small(param: PBSParameters) {
+        let (cks, sks) = KEY_CACHE.get_from_params(param);
+
+        let compressed_pk = crate::integer::CompressedPublicKeySmall::new(&cks);
+        let pk = crate::integer::PublicKeySmall::from(compressed_pk);
+
+        let a = pk.encrypt_radix(255u64, 4);
+        let b = pk.encrypt_radix(1u64, 4);
+
+        let c = sks.unchecked_add(&a, &b);
+
+        let da: u64 = cks.decrypt_radix(&a);
+        let db: u64 = cks.decrypt_radix(&b);
+        let dc: u64 = cks.decrypt_radix(&c);
+
+        assert_eq!(da, 255);
+        assert_eq!(db, 1);
+        assert_eq!(dc, 0);
     }
 }
