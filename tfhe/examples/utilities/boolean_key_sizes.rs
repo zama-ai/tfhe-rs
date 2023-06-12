1b0f3631d466228f72630b1b852e4d84990d2ae2
David Testé <david.teste@zama.ai>
2023-06-12 15:50:59 +0200

    chore(ci): fix wasm benchmarks and boolean keys measurement
    Now use the CI version of the make recipe to run WASM client
    benchmarks. In addition, boolean keys and wasm parsing is fixed
    so that benchmarks_parameters directory is created and populated
    under tfhe directory.

--- tfhe/examples/utilities/boolean_key_sizes.rs
+++ tfhe/examples/utilities/boolean_key_sizes.rs
@@ -81,6 +81,11 @@ fn client_server_key_sizes(results_file: &Path) {
 }
 
 fn main() {
+    let work_dir = std::env::current_dir().unwrap();
+    let mut new_work_dir = work_dir;
+    new_work_dir.push("tfhe");
+    std::env::set_current_dir(new_work_dir).unwrap();
+
     let results_file = Path::new("tfhe/boolean_key_sizes.csv");
     client_server_key_sizes(results_file)
 }
