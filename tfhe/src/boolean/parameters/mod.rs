66f81f7376c5ea4815d1c7ee6fb8ff1ecf1eb56c
J-B Orfila <jb.orfila@zama.ai>
2023-08-04 09:39:36 +0200

    wip

--- tfhe/src/boolean/parameters/mod.rs
+++ tfhe/src/boolean/parameters/mod.rs
@@ -18,11 +18,16 @@
 //! Failing to properly fix the parameters will potentially result with an incorrect and/or insecure
 //! computation.
 
+use crate::core_crypto::commons::dispersion::DispersionParameter;
 pub use crate::core_crypto::commons::dispersion::StandardDev;
 pub use crate::core_crypto::commons::parameters::{
     DecompositionBaseLog, DecompositionLevelCount, GlweDimension, LweDimension, PolynomialSize,
 };
+
+use crate::shortint::keycache::NamedParam;
 use serde::{Deserialize, Serialize};
+use std::fs;
+use std::fs::File;
 
 /// A set of cryptographic parameters for homomorphic Boolean circuit evaluation.
 #[derive(Copy, Clone, Debug, PartialEq, Serialize, Deserialize)]
@@ -133,3 +138,71 @@ pub const TFHE_LIB_PARAMETERS: BooleanParameters = BooleanParameters {
     ks_base_log: DecompositionBaseLog(3),
     ks_level: DecompositionLevelCount(5),
 };
+
+pub const VEC_BOOLEAN_PARAM: [BooleanParameters; 2] = [DEFAULT_PARAMETERS, TFHE_LIB_PARAMETERS];
+
+///Function to print in the lattice_estimator format the parameters
+/// Format:   LWE.Parameters(n=722, q=2^32, Xs=ND.UniformMod(2), Xe=ND.DiscreteGaussian(56139.60810663548), tag='test_lattice_estimator')
+pub fn format_lwe_parameters_to_lattice_estimator(param: BooleanParameters) -> String {
+    let log_ciphertext_modulus = 32;
+    let modular_std_dev = param
+        .lwe_modular_std_dev
+        .get_modular_standard_dev(log_ciphertext_modulus);
+
+    format!(
+        "{}_LWE = LWE.Parameters(\n n = {},\n q = 4294967296,\n Xs=ND.UniformMod(2), \n Xe=ND.DiscreteGaussian({}),\n tag='{}_lwe' \n)\n\n",
+        param.name(), param.lwe_dimension.0, modular_std_dev, param.name())
+}
+
+///Function to print in the lattice_estimator format the parameters
+/// Format: LWE.Parameters(n=722, q=2^32, Xs=ND.UniformMod(2), Xe=ND.DiscreteGaussian(56139.60810663548), tag='test_lattice_estimator')
+pub fn format_glwe_parameters_to_lattice_estimator(param: BooleanParameters) -> String {
+    let log_ciphertext_modulus = 32;
+    let modular_std_dev = param
+        .glwe_modular_std_dev
+        .get_modular_standard_dev(log_ciphertext_modulus);
+
+    format!(
+        "{}_GLWE = LWE.Parameters(\n n = {},\n q = 4294967296,\n Xs=ND.UniformMod(2), \n Xe=ND.DiscreteGaussian({}),\n tag='{}_glwe' \n)\n\n",
+        param.name(), param.glwe_dimension.0*param.polynomial_size.0, modular_std_dev, param.name())
+}
+
+pub fn write_all_param_in_file(vec_boolean_param: &[BooleanParameters]) {
+    let path = "../ci/boolean_parameters_lattice_estimator.sage";
+
+    for params in vec_boolean_param.iter().copied() {
+        fs::write(path, format_lwe_parameters_to_lattice_estimator(params))
+            .expect("Unable to write file");
+        fs::write(path, format_glwe_parameters_to_lattice_estimator(params))
+            .expect("Unable to write file");
+    }
+    fs::write(path, "all_params = [\n");
+    for (i, params) in vec_boolean_param.iter().copied().enumerate() {
+        let param_lwe_name = format!("{}_LWE,", params.name());
+        let param_glwe_name = format!("{}_GLWE,", params.name());
+        fs::write(path, param_lwe_name).expect("Unable to write file");
+        fs::write(path, param_glwe_name).expect("Unable to write file");
+
+        if i < (vec_boolean_param.len() - 1) {
+            fs::write(path, ", ").expect("Unable to write file");
+        }
+    }
+    fs::write(path, "]").expect("Unable to write file");
+}
+
+#[test]
+pub fn test_format_le() {
+    write_all_param_in_file(&VEC_BOOLEAN_PARAM);
+    //panic!();
+}
+impl NamedParam for BooleanParameters {
+    fn name(&self) -> &'static str {
+        if *self == DEFAULT_PARAMETERS {
+            "DEFAULT_PARAMETERS"
+        } else if *self == TFHE_LIB_PARAMETERS {
+            "TFHE_LIB_PARAMETERS"
+        } else {
+            panic!("Unknown parameters, missing name implementation")
+        }
+    }
+}
