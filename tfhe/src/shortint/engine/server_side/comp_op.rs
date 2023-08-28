9b19bd1e8b3d625e8dffc90d8594f36ef5166cdb
Mayeul@Zama <mayeul.debellabre@zama.ai>
2023-08-28 15:17:56 +0200

    refactor(shortint): remove comp_assign operations

--- tfhe/src/shortint/engine/server_side/comp_op.rs
+++ tfhe/src/shortint/engine/server_side/comp_op.rs
@@ -28,32 +28,19 @@ impl ShortintEngine {
         Ok(())
     }
 
-    // by convention smart operations take mut refs to their inputs, even if they do not modify them
-    #[allow(clippy::needless_pass_by_ref_mut)]
     pub(crate) fn smart_greater(
         &mut self,
         server_key: &ServerKey,
         ct_left: &mut Ciphertext,
         ct_right: &mut Ciphertext,
     ) -> EngineResult<Ciphertext> {
-        let mut result = ct_left.clone();
-        self.smart_greater_assign(server_key, &mut result, ct_right)?;
-        Ok(result)
-    }
-
-    pub(crate) fn smart_greater_assign(
-        &mut self,
-        server_key: &ServerKey,
-        ct_left: &mut Ciphertext,
-        ct_right: &mut Ciphertext,
-    ) -> EngineResult<()> {
         if !server_key.is_functional_bivariate_pbs_possible(ct_left, ct_right) {
             self.message_extract_assign(server_key, ct_left)?;
             self.message_extract_assign(server_key, ct_right)?;
         }
-
-        self.unchecked_greater_assign(server_key, ct_left, ct_right)?;
-        Ok(())
+        let mut result = ct_left.clone();
+        self.unchecked_greater_assign(server_key, &mut result, ct_right)?;
+        Ok(result)
     }
 
     pub(crate) fn unchecked_greater_or_equal(
@@ -82,31 +69,19 @@ impl ShortintEngine {
         Ok(())
     }
 
-    // by convention smart operations take mut refs to their inputs, even if they do not modify them
-    #[allow(clippy::needless_pass_by_ref_mut)]
     pub(crate) fn smart_greater_or_equal(
         &mut self,
         server_key: &ServerKey,
         ct_left: &mut Ciphertext,
         ct_right: &mut Ciphertext,
     ) -> EngineResult<Ciphertext> {
-        let mut result = ct_left.clone();
-        self.smart_greater_or_equal_assign(server_key, &mut result, ct_right)?;
-        Ok(result)
-    }
-
-    pub(crate) fn smart_greater_or_equal_assign(
-        &mut self,
-        server_key: &ServerKey,
-        ct_left: &mut Ciphertext,
-        ct_right: &mut Ciphertext,
-    ) -> EngineResult<()> {
         if !server_key.is_functional_bivariate_pbs_possible(ct_left, ct_right) {
             self.message_extract_assign(server_key, ct_left)?;
             self.message_extract_assign(server_key, ct_right)?;
         }
-        self.unchecked_greater_or_equal_assign(server_key, ct_left, ct_right)?;
-        Ok(())
+        let mut result = ct_left.clone();
+        self.unchecked_greater_or_equal_assign(server_key, &mut result, ct_right)?;
+        Ok(result)
     }
 
     pub(crate) fn unchecked_less(
@@ -135,31 +110,19 @@ impl ShortintEngine {
         Ok(())
     }
 
-    // by convention smart operations take mut refs to their inputs, even if they do not modify them
-    #[allow(clippy::needless_pass_by_ref_mut)]
     pub(crate) fn smart_less(
         &mut self,
         server_key: &ServerKey,
         ct_left: &mut Ciphertext,
         ct_right: &mut Ciphertext,
     ) -> EngineResult<Ciphertext> {
-        let mut result = ct_left.clone();
-        self.smart_less_assign(server_key, &mut result, ct_right)?;
-        Ok(result)
-    }
-
-    pub(crate) fn smart_less_assign(
-        &mut self,
-        server_key: &ServerKey,
-        ct_left: &mut Ciphertext,
-        ct_right: &mut Ciphertext,
-    ) -> EngineResult<()> {
         if !server_key.is_functional_bivariate_pbs_possible(ct_left, ct_right) {
             self.message_extract_assign(server_key, ct_left)?;
             self.message_extract_assign(server_key, ct_right)?;
         }
-        self.unchecked_less_assign(server_key, ct_left, ct_right)?;
-        Ok(())
+        let mut result = ct_left.clone();
+        self.unchecked_less_assign(server_key, &mut result, ct_right)?;
+        Ok(result)
     }
 
     pub(crate) fn unchecked_less_or_equal(
@@ -188,31 +151,19 @@ impl ShortintEngine {
         Ok(())
     }
 
-    // by convention smart operations take mut refs to their inputs, even if they do not modify them
-    #[allow(clippy::needless_pass_by_ref_mut)]
     pub(crate) fn smart_less_or_equal(
         &mut self,
         server_key: &ServerKey,
         ct_left: &mut Ciphertext,
         ct_right: &mut Ciphertext,
     ) -> EngineResult<Ciphertext> {
-        let mut result = ct_left.clone();
-        self.smart_less_or_equal_assign(server_key, &mut result, ct_right)?;
-        Ok(result)
-    }
-
-    pub(crate) fn smart_less_or_equal_assign(
-        &mut self,
-        server_key: &ServerKey,
-        ct_left: &mut Ciphertext,
-        ct_right: &mut Ciphertext,
-    ) -> EngineResult<()> {
         if !server_key.is_functional_bivariate_pbs_possible(ct_left, ct_right) {
             self.message_extract_assign(server_key, ct_left)?;
             self.message_extract_assign(server_key, ct_right)?;
         }
-        self.unchecked_less_or_equal_assign(server_key, ct_left, ct_right)?;
-        Ok(())
+        let mut result = ct_left.clone();
+        self.unchecked_less_or_equal_assign(server_key, &mut result, ct_right)?;
+        Ok(result)
     }
 
     pub(crate) fn unchecked_equal(
@@ -241,31 +192,19 @@ impl ShortintEngine {
         Ok(())
     }
 
-    // by convention smart operations take mut refs to their inputs, even if they do not modify them
-    #[allow(clippy::needless_pass_by_ref_mut)]
     pub(crate) fn smart_equal(
         &mut self,
         server_key: &ServerKey,
         ct_left: &mut Ciphertext,
         ct_right: &mut Ciphertext,
     ) -> EngineResult<Ciphertext> {
-        let mut result = ct_left.clone();
-        self.smart_equal_assign(server_key, &mut result, ct_right)?;
-        Ok(result)
-    }
-
-    pub(crate) fn smart_equal_assign(
-        &mut self,
-        server_key: &ServerKey,
-        ct_left: &mut Ciphertext,
-        ct_right: &mut Ciphertext,
-    ) -> EngineResult<()> {
         if !server_key.is_functional_bivariate_pbs_possible(ct_left, ct_right) {
             self.message_extract_assign(server_key, ct_left)?;
             self.message_extract_assign(server_key, ct_right)?;
         }
-        self.unchecked_equal_assign(server_key, ct_left, ct_right)?;
-        Ok(())
+        let mut result = ct_left.clone();
+        self.unchecked_equal_assign(server_key, &mut result, ct_right)?;
+        Ok(result)
     }
 
     pub(crate) fn smart_scalar_equal(
@@ -319,31 +258,19 @@ impl ShortintEngine {
         Ok(())
     }
 
-    // by convention smart operations take mut refs to their inputs, even if they do not modify them
-    #[allow(clippy::needless_pass_by_ref_mut)]
     pub(crate) fn smart_not_equal(
         &mut self,
         server_key: &ServerKey,
         ct_left: &mut Ciphertext,
         ct_right: &mut Ciphertext,
     ) -> EngineResult<Ciphertext> {
-        let mut result = ct_left.clone();
-        self.smart_not_equal_assign(server_key, &mut result, ct_right)?;
-        Ok(result)
-    }
-
-    pub(crate) fn smart_not_equal_assign(
-        &mut self,
-        server_key: &ServerKey,
-        ct_left: &mut Ciphertext,
-        ct_right: &mut Ciphertext,
-    ) -> EngineResult<()> {
         if !server_key.is_functional_bivariate_pbs_possible(ct_left, ct_right) {
             self.message_extract_assign(server_key, ct_left)?;
             self.message_extract_assign(server_key, ct_right)?;
         }
-        self.unchecked_not_equal_assign(server_key, ct_left, ct_right)?;
-        Ok(())
+        let mut result = ct_left.clone();
+        self.unchecked_not_equal_assign(server_key, &mut result, ct_right)?;
+        Ok(result)
     }
 
     pub(crate) fn smart_scalar_not_equal(
