7d7298e6ae2a47b7f7423fa01de82f5857df07de
Arthur Meyre <arthur.meyre@zama.ai>
2023-09-05 19:06:00 +0200

    feat(core): add parallel LWE PFPKS and LWE KS
    - add utility algorithms where applicable

--- tfhe/src/core_crypto/algorithms/lwe_keyswitch.rs
+++ tfhe/src/core_crypto/algorithms/lwe_keyswitch.rs
@@ -1,11 +1,14 @@
 //! Module containing primitives pertaining to [`LWE ciphertext
 //! keyswitch`](`LweKeyswitchKey#lwe-keyswitch`).
 
+use crate::core_crypto::algorithms::misc::divide_ceil;
 use crate::core_crypto::algorithms::slice_algorithms::*;
 use crate::core_crypto::commons::math::decomposition::SignedDecomposer;
 use crate::core_crypto::commons::numeric::UnsignedInteger;
+use crate::core_crypto::commons::parameters::ThreadCount;
 use crate::core_crypto::commons::traits::*;
 use crate::core_crypto::entities::*;
+use rayon::prelude::*;
 
 /// Keyswitch an [`LWE ciphertext`](`LweCiphertext`) encrytped under an
 /// [`LWE secret key`](`LweSecretKey`) to another [`LWE secret key`](`LweSecretKey`).
@@ -164,4 +167,316 @@ pub fn keyswitch_lwe_ciphertext<Scalar, KSKCont, InputCont, OutputCont>(
             );
         }
     }
+}
+
+/// Parallel variant of [`keyswitch_lwe_ciphertext`].
+///
+/// This will use all threads available in the current rayon thread pool.
+///
+/// # Example
+///
+/// ```
+/// use tfhe::core_crypto::prelude::*;
+///
+/// // DISCLAIMER: these toy example parameters are not guaranteed to be secure or yield correct
+/// // computations
+/// // Define parameters for LweKeyswitchKey creation
+/// let input_lwe_dimension = LweDimension(742);
+/// let lwe_modular_std_dev = StandardDev(0.000007069849454709433);
+/// let output_lwe_dimension = LweDimension(2048);
+/// let decomp_base_log = DecompositionBaseLog(3);
+/// let decomp_level_count = DecompositionLevelCount(5);
+/// let ciphertext_modulus = CiphertextModulus::new_native();
+///
+/// // Create the PRNG
+/// let mut seeder = new_seeder();
+/// let seeder = seeder.as_mut();
+/// let mut encryption_generator =
+///     EncryptionRandomGenerator::<ActivatedRandomGenerator>::new(seeder.seed(), seeder);
+/// let mut secret_generator =
+///     SecretRandomGenerator::<ActivatedRandomGenerator>::new(seeder.seed());
+///
+/// // Create the LweSecretKey
+/// let input_lwe_secret_key =
+///     allocate_and_generate_new_binary_lwe_secret_key(input_lwe_dimension, &mut secret_generator);
+/// let output_lwe_secret_key = allocate_and_generate_new_binary_lwe_secret_key(
+///     output_lwe_dimension,
+///     &mut secret_generator,
+/// );
+///
+/// let ksk = allocate_and_generate_new_lwe_keyswitch_key(
+///     &input_lwe_secret_key,
+///     &output_lwe_secret_key,
+///     decomp_base_log,
+///     decomp_level_count,
+///     lwe_modular_std_dev,
+///     ciphertext_modulus,
+///     &mut encryption_generator,
+/// );
+///
+/// // Create the plaintext
+/// let msg = 3u64;
+/// let plaintext = Plaintext(msg << 60);
+///
+/// // Create a new LweCiphertext
+/// let input_lwe = allocate_and_encrypt_new_lwe_ciphertext(
+///     &input_lwe_secret_key,
+///     plaintext,
+///     lwe_modular_std_dev,
+///     ciphertext_modulus,
+///     &mut encryption_generator,
+/// );
+///
+/// let mut output_lwe = LweCiphertext::new(
+///     0,
+///     output_lwe_secret_key.lwe_dimension().to_lwe_size(),
+///     ciphertext_modulus,
+/// );
+///
+/// // Use all threads available in the current rayon thread pool
+/// par_keyswitch_lwe_ciphertext(&ksk, &input_lwe, &mut output_lwe);
+///
+/// let decrypted_plaintext = decrypt_lwe_ciphertext(&output_lwe_secret_key, &output_lwe);
+///
+/// // Round and remove encoding
+/// // First create a decomposer working on the high 4 bits corresponding to our encoding.
+/// let decomposer = SignedDecomposer::new(DecompositionBaseLog(4), DecompositionLevelCount(1));
+///
+/// let rounded = decomposer.closest_representable(decrypted_plaintext.0);
+///
+/// // Remove the encoding
+/// let cleartext = rounded >> 60;
+///
+/// // Check we recovered the original message
+/// assert_eq!(cleartext, msg);
+/// ```
+pub fn par_keyswitch_lwe_ciphertext<Scalar, KSKCont, InputCont, OutputCont>(
+    lwe_keyswitch_key: &LweKeyswitchKey<KSKCont>,
+    input_lwe_ciphertext: &LweCiphertext<InputCont>,
+    output_lwe_ciphertext: &mut LweCiphertext<OutputCont>,
+) where
+    Scalar: UnsignedInteger + Send + Sync,
+    KSKCont: Container<Element = Scalar>,
+    InputCont: Container<Element = Scalar>,
+    OutputCont: ContainerMut<Element = Scalar>,
+{
+    let thread_count = ThreadCount(rayon::current_num_threads());
+    par_keyswitch_lwe_ciphertext_with_thread_count(
+        lwe_keyswitch_key,
+        input_lwe_ciphertext,
+        output_lwe_ciphertext,
+        thread_count,
+    )
+}
+
+/// Parallel variant of [`keyswitch_lwe_ciphertext`].
+///
+/// This will try to use `thread_count` threads for the computation, if this number is bigger than
+/// the available number of threads in the current rayon thread pool then only the number of
+/// available threads will be used. Note that `thread_count` cannot be 0.
+///
+/// # Example
+///
+/// ```
+/// use tfhe::core_crypto::prelude::*;
+///
+/// // DISCLAIMER: these toy example parameters are not guaranteed to be secure or yield correct
+/// // computations
+/// // Define parameters for LweKeyswitchKey creation
+/// let input_lwe_dimension = LweDimension(742);
+/// let lwe_modular_std_dev = StandardDev(0.000007069849454709433);
+/// let output_lwe_dimension = LweDimension(2048);
+/// let decomp_base_log = DecompositionBaseLog(3);
+/// let decomp_level_count = DecompositionLevelCount(5);
+/// let ciphertext_modulus = CiphertextModulus::new_native();
+///
+/// // Create the PRNG
+/// let mut seeder = new_seeder();
+/// let seeder = seeder.as_mut();
+/// let mut encryption_generator =
+///     EncryptionRandomGenerator::<ActivatedRandomGenerator>::new(seeder.seed(), seeder);
+/// let mut secret_generator =
+///     SecretRandomGenerator::<ActivatedRandomGenerator>::new(seeder.seed());
+///
+/// // Create the LweSecretKey
+/// let input_lwe_secret_key =
+///     allocate_and_generate_new_binary_lwe_secret_key(input_lwe_dimension, &mut secret_generator);
+/// let output_lwe_secret_key = allocate_and_generate_new_binary_lwe_secret_key(
+///     output_lwe_dimension,
+///     &mut secret_generator,
+/// );
+///
+/// let ksk = allocate_and_generate_new_lwe_keyswitch_key(
+///     &input_lwe_secret_key,
+///     &output_lwe_secret_key,
+///     decomp_base_log,
+///     decomp_level_count,
+///     lwe_modular_std_dev,
+///     ciphertext_modulus,
+///     &mut encryption_generator,
+/// );
+///
+/// // Create the plaintext
+/// let msg = 3u64;
+/// let plaintext = Plaintext(msg << 60);
+///
+/// // Create a new LweCiphertext
+/// let input_lwe = allocate_and_encrypt_new_lwe_ciphertext(
+///     &input_lwe_secret_key,
+///     plaintext,
+///     lwe_modular_std_dev,
+///     ciphertext_modulus,
+///     &mut encryption_generator,
+/// );
+///
+/// let mut output_lwe = LweCiphertext::new(
+///     0,
+///     output_lwe_secret_key.lwe_dimension().to_lwe_size(),
+///     ciphertext_modulus,
+/// );
+///
+/// // Try to use 4 threads for the keyswitch if enough are available
+/// // in the current rayon thread pool
+/// par_keyswitch_lwe_ciphertext_with_thread_count(
+///     &ksk,
+///     &input_lwe,
+///     &mut output_lwe,
+///     ThreadCount(4),
+/// );
+///
+/// let decrypted_plaintext = decrypt_lwe_ciphertext(&output_lwe_secret_key, &output_lwe);
+///
+/// // Round and remove encoding
+/// // First create a decomposer working on the high 4 bits corresponding to our encoding.
+/// let decomposer = SignedDecomposer::new(DecompositionBaseLog(4), DecompositionLevelCount(1));
+///
+/// let rounded = decomposer.closest_representable(decrypted_plaintext.0);
+///
+/// // Remove the encoding
+/// let cleartext = rounded >> 60;
+///
+/// // Check we recovered the original message
+/// assert_eq!(cleartext, msg);
+/// ```
+pub fn par_keyswitch_lwe_ciphertext_with_thread_count<Scalar, KSKCont, InputCont, OutputCont>(
+    lwe_keyswitch_key: &LweKeyswitchKey<KSKCont>,
+    input_lwe_ciphertext: &LweCiphertext<InputCont>,
+    output_lwe_ciphertext: &mut LweCiphertext<OutputCont>,
+    thread_count: ThreadCount,
+) where
+    Scalar: UnsignedInteger + Send + Sync,
+    KSKCont: Container<Element = Scalar>,
+    InputCont: Container<Element = Scalar>,
+    OutputCont: ContainerMut<Element = Scalar>,
+{
+    assert!(
+        lwe_keyswitch_key.input_key_lwe_dimension()
+            == input_lwe_ciphertext.lwe_size().to_lwe_dimension(),
+        "Mismatched input LweDimension. \
+        LweKeyswitchKey input LweDimension: {:?}, input LweCiphertext LweDimension {:?}.",
+        lwe_keyswitch_key.input_key_lwe_dimension(),
+        input_lwe_ciphertext.lwe_size().to_lwe_dimension(),
+    );
+    assert!(
+        lwe_keyswitch_key.output_key_lwe_dimension()
+            == output_lwe_ciphertext.lwe_size().to_lwe_dimension(),
+        "Mismatched output LweDimension. \
+        LweKeyswitchKey output LweDimension: {:?}, output LweCiphertext LweDimension {:?}.",
+        lwe_keyswitch_key.output_key_lwe_dimension(),
+        output_lwe_ciphertext.lwe_size().to_lwe_dimension(),
+    );
+    assert!(
+        lwe_keyswitch_key.ciphertext_modulus() == input_lwe_ciphertext.ciphertext_modulus(),
+        "Mismatched CiphertextModulus. \
+        LweKeyswitchKey CiphertextModulus: {:?}, input LweCiphertext CiphertextModulus {:?}.",
+        lwe_keyswitch_key.ciphertext_modulus(),
+        input_lwe_ciphertext.ciphertext_modulus()
+    );
+    assert!(
+        lwe_keyswitch_key.ciphertext_modulus() == output_lwe_ciphertext.ciphertext_modulus(),
+        "Mismatched CiphertextModulus. \
+        LweKeyswitchKey CiphertextModulus: {:?}, output LweCiphertext CiphertextModulus {:?}.",
+        lwe_keyswitch_key.ciphertext_modulus(),
+        output_lwe_ciphertext.ciphertext_modulus()
+    );
+    assert!(
+        lwe_keyswitch_key
+            .ciphertext_modulus()
+            .is_compatible_with_native_modulus(),
+        "This operation currently only supports power of 2 moduli"
+    );
+
+    assert!(
+        thread_count.0 != 0,
+        "Got thread_count == 0, this is not supported"
+    );
+
+    // We instantiate a decomposer
+    let decomposer = SignedDecomposer::new(
+        lwe_keyswitch_key.decomposition_base_log(),
+        lwe_keyswitch_key.decomposition_level_count(),
+    );
+
+    let mut intermediate_accumulators: Vec<_> = Vec::with_capacity(thread_count.0);
+
+    let output_lwe_size = output_lwe_ciphertext.lwe_size();
+    let output_ciphertext_modulus = output_lwe_ciphertext.ciphertext_modulus();
+    // Don't go above the current number of threads
+    let thread_count = thread_count.0.min(rayon::current_num_threads());
+
+    // Smallest chunk_size such that thread_count * chunk_size >= input_lwe_size
+    let chunk_size = divide_ceil(input_lwe_ciphertext.lwe_size().0, thread_count);
+
+    lwe_keyswitch_key
+        .par_chunks(chunk_size)
+        .zip(
+            input_lwe_ciphertext
+                .get_mask()
+                .as_ref()
+                .par_chunks(chunk_size),
+        )
+        .map(|(keyswitch_key_block_chunk, input_mask_element_chunk)| {
+            let mut buffer =
+                LweCiphertext::new(Scalar::ZERO, output_lwe_size, output_ciphertext_modulus);
+
+            for (keyswitch_key_block, &input_mask_element) in keyswitch_key_block_chunk
+                .iter()
+                .zip(input_mask_element_chunk.iter())
+            {
+                let decomposition_iter = decomposer.decompose(input_mask_element);
+                // Loop over the levels
+                for (level_key_ciphertext, decomposed) in
+                    keyswitch_key_block.iter().zip(decomposition_iter)
+                {
+                    slice_wrapping_sub_scalar_mul_assign(
+                        buffer.as_mut(),
+                        level_key_ciphertext.as_ref(),
+                        decomposed.value(),
+                    );
+                }
+            }
+            buffer
+        })
+        .collect_into_vec(&mut intermediate_accumulators);
+
+    let reduced = intermediate_accumulators
+        .par_iter_mut()
+        .reduce_with(|lhs, rhs| {
+            lhs.as_mut()
+                .iter_mut()
+                .zip(rhs.as_ref().iter())
+                .for_each(|(dst, &src)| *dst = (*dst).wrapping_add(src));
+
+            lhs
+        })
+        .unwrap();
+
+    output_lwe_ciphertext
+        .get_mut_mask()
+        .as_mut()
+        .copy_from_slice(reduced.get_mask().as_ref());
+    let input_lwe_body = *input_lwe_ciphertext.get_body().data;
+    let reduced_ksed_body = *reduced.get_body().data;
+    // Copy the input body to the output ciphertext
+    *output_lwe_ciphertext.get_mut_body().data = input_lwe_body.wrapping_add(reduced_ksed_body);
 }
---
+++ tfhe/src/core_crypto/algorithms/test/lwe_private_functional_packing_keyswitch.rs
@@ -0,0 +1,205 @@
+use super::*;
+
+fn test_parallel_pfpks_equivalence<Scalar: UnsignedTorus + Send + Sync>(
+    ciphertext_modulus: CiphertextModulus<Scalar>,
+) {
+    // Realistic sizes
+    {
+        let decomp_base_log = DecompositionBaseLog(15);
+        let decomp_level_count = DecompositionLevelCount(2);
+        let input_key_lwe_dimension = LweDimension(769);
+        let output_glwe_dimension = GlweDimension(1);
+        let output_glwe_size = output_glwe_dimension.to_glwe_size();
+        let output_polynomial_size = PolynomialSize(2048);
+        // ~ 2^-20
+        let std_dev = StandardDev(0.0000006791658447437413);
+
+        let mut rsc = TestResources::new();
+
+        let mut lwe_pfpksk = LwePrivateFunctionalPackingKeyswitchKey::new(
+            Scalar::ZERO,
+            decomp_base_log,
+            decomp_level_count,
+            input_key_lwe_dimension,
+            output_glwe_size,
+            output_polynomial_size,
+            ciphertext_modulus,
+        );
+
+        for _ in 0..10 {
+            let input_lwe_secret_key = allocate_and_generate_new_binary_lwe_secret_key(
+                input_key_lwe_dimension,
+                &mut rsc.secret_random_generator,
+            );
+
+            let output_glwe_secret_key = allocate_and_generate_new_binary_glwe_secret_key(
+                output_glwe_dimension,
+                output_polynomial_size,
+                &mut rsc.secret_random_generator,
+            );
+
+            let mut polynomial = Polynomial::new(Scalar::ZERO, output_polynomial_size);
+            rsc.encryption_random_generator
+                .fill_slice_with_random_mask(polynomial.as_mut());
+
+            par_generate_lwe_private_functional_packing_keyswitch_key(
+                &input_lwe_secret_key,
+                &output_glwe_secret_key,
+                &mut lwe_pfpksk,
+                std_dev,
+                &mut rsc.encryption_random_generator,
+                |x| x.wrapping_neg(),
+                &polynomial,
+            );
+
+            let mut random_lwe = LweCiphertext::new(
+                Scalar::ZERO,
+                input_key_lwe_dimension.to_lwe_size(),
+                ciphertext_modulus,
+            );
+            rsc.encryption_random_generator
+                .fill_slice_with_random_mask(random_lwe.as_mut());
+
+            let mut output_glwe_serial = GlweCiphertext::new(
+                Scalar::ZERO,
+                output_glwe_size,
+                output_polynomial_size,
+                ciphertext_modulus,
+            );
+
+            let mut output_glwe_parallel = GlweCiphertext::new(
+                Scalar::ZERO,
+                output_glwe_size,
+                output_polynomial_size,
+                ciphertext_modulus,
+            );
+
+            let start_serial = std::time::Instant::now();
+            private_functional_keyswitch_lwe_ciphertext_into_glwe_ciphertext(
+                &lwe_pfpksk,
+                &mut output_glwe_serial,
+                &random_lwe,
+            );
+            let elapsed_serial = start_serial.elapsed();
+            println!("serial:{elapsed_serial:?}");
+
+            let start_parallel = std::time::Instant::now();
+            par_private_functional_keyswitch_lwe_ciphertext_into_glwe_ciphertext(
+                &lwe_pfpksk,
+                &mut output_glwe_parallel,
+                &random_lwe,
+            );
+            let elapsed_parallel = start_parallel.elapsed();
+            println!("parallel:{elapsed_parallel:?}");
+
+            assert_eq!(output_glwe_serial, output_glwe_parallel);
+        }
+    }
+
+    // Small sizes
+    {
+        for _ in 0..10 {
+            let decomp_base_log = DecompositionBaseLog(
+                crate::core_crypto::commons::test_tools::random_usize_between(2..5),
+            );
+            let decomp_level_count = DecompositionLevelCount(
+                crate::core_crypto::commons::test_tools::random_usize_between(2..5),
+            );
+            let input_key_lwe_dimension =
+                LweDimension(crate::core_crypto::commons::test_tools::random_usize_between(5..10));
+            let output_glwe_dimension =
+                GlweDimension(crate::core_crypto::commons::test_tools::random_usize_between(5..10));
+            let output_glwe_size = output_glwe_dimension.to_glwe_size();
+            let output_polynomial_size = PolynomialSize(
+                crate::core_crypto::commons::test_tools::random_usize_between(5..10),
+            );
+            // ~ 2^-20
+            let std_dev = StandardDev(0.0000006791658447437413);
+
+            let mut rsc = TestResources::new();
+
+            let mut lwe_pfpksk = LwePrivateFunctionalPackingKeyswitchKey::new(
+                Scalar::ZERO,
+                decomp_base_log,
+                decomp_level_count,
+                input_key_lwe_dimension,
+                output_glwe_size,
+                output_polynomial_size,
+                ciphertext_modulus,
+            );
+
+            for _ in 0..10 {
+                let input_lwe_secret_key = allocate_and_generate_new_binary_lwe_secret_key(
+                    input_key_lwe_dimension,
+                    &mut rsc.secret_random_generator,
+                );
+
+                let output_glwe_secret_key = allocate_and_generate_new_binary_glwe_secret_key(
+                    output_glwe_dimension,
+                    output_polynomial_size,
+                    &mut rsc.secret_random_generator,
+                );
+
+                let mut polynomial = Polynomial::new(Scalar::ZERO, output_polynomial_size);
+                rsc.encryption_random_generator
+                    .fill_slice_with_random_mask(polynomial.as_mut());
+
+                par_generate_lwe_private_functional_packing_keyswitch_key(
+                    &input_lwe_secret_key,
+                    &output_glwe_secret_key,
+                    &mut lwe_pfpksk,
+                    std_dev,
+                    &mut rsc.encryption_random_generator,
+                    |x| x.wrapping_neg(),
+                    &polynomial,
+                );
+
+                let mut random_lwe = LweCiphertext::new(
+                    Scalar::ZERO,
+                    input_key_lwe_dimension.to_lwe_size(),
+                    ciphertext_modulus,
+                );
+                rsc.encryption_random_generator
+                    .fill_slice_with_random_mask(random_lwe.as_mut());
+
+                let mut output_glwe_serial = GlweCiphertext::new(
+                    Scalar::ZERO,
+                    output_glwe_size,
+                    output_polynomial_size,
+                    ciphertext_modulus,
+                );
+
+                let mut output_glwe_parallel = GlweCiphertext::new(
+                    Scalar::ZERO,
+                    output_glwe_size,
+                    output_polynomial_size,
+                    ciphertext_modulus,
+                );
+
+                private_functional_keyswitch_lwe_ciphertext_into_glwe_ciphertext(
+                    &lwe_pfpksk,
+                    &mut output_glwe_serial,
+                    &random_lwe,
+                );
+
+                par_private_functional_keyswitch_lwe_ciphertext_into_glwe_ciphertext(
+                    &lwe_pfpksk,
+                    &mut output_glwe_parallel,
+                    &random_lwe,
+                );
+
+                assert_eq!(output_glwe_serial, output_glwe_parallel);
+            }
+        }
+    }
+}
+
+#[test]
+fn test_parallel_pfpks_equivalence_u32_native_mod() {
+    test_parallel_pfpks_equivalence::<u32>(CiphertextModulus::new_native())
+}
+
+#[test]
+fn test_parallel_pfpks_equivalence_u64_native_mod() {
+    test_parallel_pfpks_equivalence::<u64>(CiphertextModulus::new_native())
+}
