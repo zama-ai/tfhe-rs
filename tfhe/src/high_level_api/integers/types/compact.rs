076cc782faa2d9bd921222fddd721cb084fcdb4b
Mayeul@Zama <mayeul.debellabre@zama.ai>
2023-09-29 14:54:52 +0200

    test(hlapi): test compact integer conformance

--- tfhe/src/high_level_api/integers/types/compact.rs
+++ tfhe/src/high_level_api/integers/types/compact.rs
@@ -119,3 +119,240 @@ impl<P: IntegerParameter> ParameterSetConformant for GenericCompactIntegerList<P
         self.list.is_conformant(&params)
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::conformance::ParameterSetConformant;
+    use crate::core_crypto::prelude::UnsignedInteger;
+    use crate::shortint::parameters::PARAM_MESSAGE_2_CARRY_2_KS_PBS;
+    use crate::shortint::{CiphertextModulus, PBSOrder};
+    use crate::{
+        generate_keys, set_server_key, CompactFheUint8, CompactFheUint8List, ConfigBuilder,
+    };
+    use rand::{thread_rng, Rng};
+
+    type ParameterAccessor<Ct, T> = dyn Fn(&mut Ct) -> &mut T;
+
+    type ParameterModifier<'a, Ct> = dyn Fn(&mut Ct) + 'a;
+
+    fn change_parameters<Ct, T: UnsignedInteger>(
+        func: &ParameterAccessor<Ct, T>,
+    ) -> Vec<Box<ParameterModifier<'_, Ct>>> {
+        vec![
+            Box::new(|ct| *func(ct) = T::ZERO),
+            Box::new(|ct| *func(ct) = func(ct).wrapping_add(T::ONE)),
+            Box::new(|ct| *func(ct) = func(ct).wrapping_sub(T::ONE)),
+        ]
+    }
+
+    #[test]
+    fn test_invalid_generic_compact_integer() {
+        type Ct = CompactFheUint8;
+
+        let config = ConfigBuilder::all_disabled()
+            .enable_default_integers()
+            .build();
+
+        let (client_key, _server_key) = generate_keys(config);
+
+        let public_key = CompactPublicKey::new(&client_key);
+
+        let ct = CompactFheUint8::try_encrypt(0, &public_key).unwrap();
+
+        assert!(ct.is_conformant(&PARAM_MESSAGE_2_CARRY_2_KS_PBS.into()));
+
+        let breaker_lists = vec![
+            change_parameters(&|ct: &mut Ct| &mut ct.list.num_blocks),
+            change_parameters(&|ct: &mut Ct| &mut ct.list.ct_list.message_modulus.0),
+            change_parameters(&|ct: &mut Ct| &mut ct.list.ct_list.carry_modulus.0),
+            change_parameters(&|ct: &mut Ct| &mut ct.list.ct_list.degree.0),
+            change_parameters(&|ct: &mut Ct| {
+                &mut ct.list.ct_list.ct_list.get_mut_lwe_ciphertext_count().0
+            }),
+            change_parameters(&|ct: &mut Ct| &mut ct.list.ct_list.ct_list.get_mut_lwe_size().0),
+        ];
+
+        for breaker_list in breaker_lists {
+            for breaker in breaker_list {
+                let mut ct_clone = ct.clone();
+
+                breaker(&mut ct_clone);
+
+                assert!(!ct_clone.is_conformant(&PARAM_MESSAGE_2_CARRY_2_KS_PBS.into()));
+            }
+        }
+
+        let breakers2: Vec<&ParameterModifier<'_, Ct>> = vec![
+            &|ct: &mut Ct| ct.list.ct_list.pbs_order = PBSOrder::BootstrapKeyswitch,
+            &|ct: &mut Ct| {
+                *ct.list.ct_list.ct_list.get_mut_ciphertext_modulus() =
+                    CiphertextModulus::try_new_power_of_2(1).unwrap();
+            },
+            &|ct: &mut Ct| {
+                *ct.list.ct_list.ct_list.get_mut_ciphertext_modulus() =
+                    CiphertextModulus::try_new(3).unwrap();
+            },
+            &|ct: &mut Ct| {
+                ct.list.ct_list.ct_list.get_mut_container().pop();
+            },
+            &|ct: &mut Ct| ct.list.ct_list.ct_list.get_mut_container().push(0),
+        ];
+
+        for breaker in breakers2 {
+            let mut ct_clone = ct.clone();
+
+            breaker(&mut ct_clone);
+
+            assert!(!ct_clone.is_conformant(&PARAM_MESSAGE_2_CARRY_2_KS_PBS.into()));
+        }
+    }
+
+    #[test]
+    fn test_invalid_generic_compact_integer_list() {
+        type Ct = CompactFheUint8List;
+
+        let config = ConfigBuilder::all_disabled()
+            .enable_default_integers()
+            .build();
+
+        let (client_key, _server_key) = generate_keys(config);
+
+        let public_key = CompactPublicKey::new(&client_key);
+
+        let ct = Ct::try_encrypt(&[0, 1], &public_key).unwrap();
+
+        let params = (
+            PARAM_MESSAGE_2_CARRY_2_KS_PBS.into(),
+            ListSizeConstraint::exact_size(2),
+        );
+
+        assert!(ct.is_conformant(&params));
+
+        let breaker_lists = vec![
+            change_parameters(&|ct: &mut Ct| &mut ct.list.num_blocks),
+            change_parameters(&|ct: &mut Ct| &mut ct.list.ct_list.message_modulus.0),
+            change_parameters(&|ct: &mut Ct| &mut ct.list.ct_list.carry_modulus.0),
+            change_parameters(&|ct: &mut Ct| &mut ct.list.ct_list.degree.0),
+            change_parameters(&|ct: &mut Ct| {
+                &mut ct.list.ct_list.ct_list.get_mut_lwe_ciphertext_count().0
+            }),
+            change_parameters(&|ct: &mut Ct| &mut ct.list.ct_list.ct_list.get_mut_lwe_size().0),
+        ];
+
+        for breaker_list in breaker_lists {
+            for breaker in breaker_list {
+                let mut ct_clone = ct.clone();
+
+                breaker(&mut ct_clone);
+
+                assert!(!ct_clone.is_conformant(&params));
+            }
+        }
+
+        let breakers2: Vec<&ParameterModifier<'_, Ct>> = vec![
+            &|ct: &mut Ct| ct.list.ct_list.pbs_order = PBSOrder::BootstrapKeyswitch,
+            &|ct: &mut Ct| {
+                *ct.list.ct_list.ct_list.get_mut_ciphertext_modulus() =
+                    CiphertextModulus::try_new_power_of_2(1).unwrap();
+            },
+            &|ct: &mut Ct| {
+                *ct.list.ct_list.ct_list.get_mut_ciphertext_modulus() =
+                    CiphertextModulus::try_new(3).unwrap();
+            },
+            &|ct: &mut Ct| {
+                ct.list.ct_list.ct_list.get_mut_container().pop();
+            },
+            &|ct: &mut Ct| ct.list.ct_list.ct_list.get_mut_container().push(0),
+        ];
+
+        for breaker in breakers2 {
+            let mut ct_clone = ct.clone();
+
+            breaker(&mut ct_clone);
+
+            assert!(!ct_clone.is_conformant(&params));
+        }
+    }
+
+    #[test]
+    fn test_valid_generic_compact_integer() {
+        let config = ConfigBuilder::all_disabled()
+            .enable_default_integers()
+            .build();
+
+        let (client_key, server_key) = generate_keys(config);
+
+        let public_key = CompactPublicKey::new(&client_key);
+
+        set_server_key(server_key);
+
+        let ct = CompactFheUint8::try_encrypt(0, &public_key).unwrap();
+
+        assert!(ct.is_conformant(&PARAM_MESSAGE_2_CARRY_2_KS_PBS.into()));
+
+        let mut rng = thread_rng();
+
+        for _ in 0..10 {
+            let mut ct_clone = ct.clone();
+
+            ct_clone
+                .list
+                .ct_list
+                .ct_list
+                .get_mut_container()
+                .fill_with(|| rng.gen::<u64>());
+
+            assert!(ct_clone.is_conformant(&PARAM_MESSAGE_2_CARRY_2_KS_PBS.into()));
+
+            let mut ct_clone_expanded = ct_clone.expand();
+
+            ct_clone_expanded += &ct_clone_expanded.clone();
+        }
+    }
+
+    #[test]
+    fn test_valid_generic_compact_integer_list() {
+        let config = ConfigBuilder::all_disabled()
+            .enable_default_integers()
+            .build();
+
+        let (client_key, server_key) = generate_keys(config);
+
+        let public_key = CompactPublicKey::new(&client_key);
+
+        set_server_key(server_key);
+
+        let ct = CompactFheUint8List::try_encrypt(&[0, 1], &public_key).unwrap();
+
+        let params = (
+            PARAM_MESSAGE_2_CARRY_2_KS_PBS.into(),
+            ListSizeConstraint::exact_size(2),
+        );
+
+        assert!(ct.is_conformant(&params));
+
+        let mut rng = thread_rng();
+
+        for _ in 0..10 {
+            let mut ct_clone = ct.clone();
+
+            ct_clone
+                .list
+                .ct_list
+                .ct_list
+                .get_mut_container()
+                .fill_with(|| rng.gen::<u64>());
+
+            assert!(ct_clone.is_conformant(&params));
+
+            let mut ct_clone_expanded = ct_clone.expand();
+
+            // We check that this conformant random ciphertext can be used to do operations without
+            // panicking
+            for i in &mut ct_clone_expanded {
+                *i += i.clone();
+            }
+        }
+    }
+}
