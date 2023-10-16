ecee305340661ad6bf3db258b0e323809a864dc0
Arthur Meyre <arthur.meyre@zama.ai>
2023-10-16 14:41:23 +0200

    chore(core): change prelude algorithms imports

--- tfhe/src/core_crypto/prelude.rs
+++ tfhe/src/core_crypto/prelude.rs
@@ -3,9 +3,7 @@
 //! The TFHE-rs preludes include convenient imports.
 //! Having `tfhe::core_crypto::prelude::*;` should be enough to start using the lib.
 
-pub use super::algorithms::{
-    add_external_product_assign, polynomial_algorithms, slice_algorithms, *,
-};
+pub use super::algorithms::*;
 pub use super::commons::computation_buffers::ComputationBuffers;
 pub use super::commons::dispersion::*;
 pub use super::commons::generators::{EncryptionRandomGenerator, SecretRandomGenerator};
