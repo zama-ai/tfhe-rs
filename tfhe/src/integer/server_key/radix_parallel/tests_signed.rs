fb46b2b47299782f110d36af83fe23e3ae7d5d3a
tmontaigu <thomas.montaigu@laposte.net>
2023-11-06 11:42:43 +0100

    feat(integer): add signed_overflowing_sub

--- tfhe/src/integer/server_key/radix_parallel/tests_signed.rs
+++ tfhe/src/integer/server_key/radix_parallel/tests_signed.rs
@@ -1,5 +1,5 @@
 use crate::integer::keycache::KEY_CACHE;
-use crate::integer::RadixClientKey;
+use crate::integer::{RadixClientKey, ServerKey, SignedRadixCiphertext};
 use crate::shortint::parameters::*;
 use itertools::{iproduct, izip};
 use paste::paste;
@@ -79,15 +79,22 @@ fn signed_neg_under_modulus(lhs: i64, modulus: i64) -> i64 {
 //
 // lhs and rhs must be in [-modulus..modulus[
 fn signed_sub_under_modulus(lhs: i64, rhs: i64, modulus: i64) -> i64 {
+    signed_overflowing_sub_under_modulus(lhs, rhs, modulus).0
+}
+
+fn signed_overflowing_sub_under_modulus(lhs: i64, rhs: i64, modulus: i64) -> (i64, bool) {
     assert!(modulus > 0);
     let mut res = lhs - rhs;
+    let mut overflow = false;
     if res < -modulus {
         // rem_euclid(modulus) would also work
         res = modulus + (res - -modulus);
+        overflow = true;
     } else if res > modulus - 1 {
         res = -modulus + (res - modulus);
+        overflow = true;
     }
-    res
+    (res, overflow)
 }
 
 fn signed_mul_under_modulus(lhs: i64, rhs: i64, modulus: i64) -> i64 {
@@ -366,6 +373,18 @@ fn integer_signed_encrypt_decrypt(param: impl Into<PBSParameters>) {
 create_parametrized_test!(integer_signed_unchecked_add);
 create_parametrized_test!(integer_signed_unchecked_neg);
 create_parametrized_test!(integer_signed_unchecked_sub);
+create_parametrized_test!(integer_signed_unchecked_overflowing_sub);
+create_parametrized_test!(integer_signed_unchecked_overflowing_sub_parallelized {
+    // Requires 4 bits, so 1_1 parameters are not supported
+    // until they get their own version of the algorithm
+    PARAM_MESSAGE_2_CARRY_2_KS_PBS,
+    PARAM_MESSAGE_3_CARRY_3_KS_PBS,
+    PARAM_MESSAGE_4_CARRY_4_KS_PBS,
+    PARAM_MULTI_BIT_MESSAGE_2_CARRY_2_GROUP_2_KS_PBS,
+    PARAM_MULTI_BIT_MESSAGE_3_CARRY_3_GROUP_2_KS_PBS,
+    PARAM_MULTI_BIT_MESSAGE_2_CARRY_2_GROUP_3_KS_PBS,
+    PARAM_MULTI_BIT_MESSAGE_3_CARRY_3_GROUP_3_KS_PBS
+});
 create_parametrized_test!(integer_signed_unchecked_mul);
 create_parametrized_test!(integer_signed_unchecked_bitand);
 create_parametrized_test!(integer_signed_unchecked_bitor);
@@ -544,6 +563,107 @@ fn integer_signed_unchecked_sub(param: impl Into<PBSParameters>) {
         let clear_res = signed_sub_under_modulus(clear_0, clear_1, modulus);
         assert_eq!(clear_res, dec_res);
     }
+}
+
+fn signed_unchecked_overflowing_sub_test_case<P, F>(param: P, signed_overflowing_sub: F)
+where
+    P: Into<PBSParameters>,
+    F: for<'a> Fn(
+        &'a ServerKey,
+        &'a SignedRadixCiphertext,
+        &'a SignedRadixCiphertext,
+    ) -> (SignedRadixCiphertext, crate::shortint::Ciphertext),
+{
+    let (cks, mut sks) = KEY_CACHE.get_from_params(param);
+    let cks = RadixClientKey::from((cks, NB_CTXT));
+
+    sks.set_deterministic_pbs_execution(true);
+
+    let mut rng = rand::thread_rng();
+
+    let modulus = (cks.parameters().message_modulus().0.pow(NB_CTXT as u32) / 2) as i64;
+
+    for _ in 0..NB_TEST {
+        let clear_0 = rng.gen::<i64>() % modulus;
+        let clear_1 = rng.gen::<i64>() % modulus;
+
+        let ctxt_0 = cks.encrypt_signed(clear_0);
+        let ctxt_1 = cks.encrypt_signed(clear_1);
+
+        let (ct_res, result_overflowed) = signed_overflowing_sub(&sks, &ctxt_0, &ctxt_1);
+        let (tmp_ct, tmp_o) = signed_overflowing_sub(&sks, &ctxt_0, &ctxt_1);
+        assert!(ct_res.block_carries_are_empty());
+        assert!(result_overflowed.carry_is_empty());
+        assert_eq!(ct_res, tmp_ct, "Failed determinism check");
+        assert_eq!(tmp_o, result_overflowed, "Failed determinism check");
+
+        let (expected_result, expected_overflowed) =
+            signed_overflowing_sub_under_modulus(clear_0, clear_1, modulus);
+
+        let decrypted_result: i64 = cks.decrypt_signed(&ct_res);
+        let decrypted_overflowed = cks.decrypt_one_block(&result_overflowed) == 1;
+        assert_eq!(
+            decrypted_result, expected_result,
+            "Invalid result for sub, for ({clear_0} - {clear_1}) % {modulus} \
+             expected {expected_result}, got {decrypted_result}"
+        );
+        assert_eq!(
+            decrypted_overflowed,
+            expected_overflowed,
+            "Invalid overflow flag result for overflowing_sub for ({clear_0} - {clear_1}) % {modulus} \
+             expected overflow flag {expected_overflowed}, got {decrypted_overflowed}"
+        );
+    }
+
+    // Test with trivial inputs, as it was bugged at some point
+    for _ in 0..4 {
+        // Reduce maximum value of random number such that at least the last block is a trivial 0
+        // (This is how the reproducing case was found)
+        let clear_0 = rng.gen::<i64>() % modulus;
+        let clear_1 = rng.gen::<i64>() % modulus;
+
+        let a: SignedRadixCiphertext = sks.create_trivial_radix(clear_0, NB_CTXT);
+        let b: SignedRadixCiphertext = sks.create_trivial_radix(clear_1, NB_CTXT);
+
+        let (encrypted_result, encrypted_overflow) = signed_overflowing_sub(&sks, &a, &b);
+
+        let (expected_result, expected_overflowed) =
+            signed_overflowing_sub_under_modulus(clear_0, clear_1, modulus);
+
+        let decrypted_result: i64 = cks.decrypt_signed(&encrypted_result);
+        let decrypted_overflowed = cks.decrypt_one_block(&encrypted_overflow) == 1;
+        assert_eq!(
+            decrypted_result, expected_result,
+            "Invalid result for sub, for ({clear_0} - {clear_1}) % {modulus} \
+                expected {expected_result}, got {decrypted_result}"
+        );
+        assert_eq!(
+            decrypted_overflowed,
+            expected_overflowed,
+            "Invalid overflow flag result for overflowing_sub, for ({clear_0} - {clear_1}) % {modulus} \
+                expected overflow flag {expected_overflowed}, got {decrypted_overflowed}"
+        );
+    }
+}
+
+fn integer_signed_unchecked_overflowing_sub<P>(param: P)
+where
+    P: Into<PBSParameters>,
+{
+    signed_unchecked_overflowing_sub_test_case(param, ServerKey::unchecked_signed_overflowing_sub);
+}
+
+fn integer_signed_unchecked_overflowing_sub_parallelized<P>(param: P)
+where
+    P: Into<PBSParameters>,
+{
+    // Call _impl so we are sure the parallel version is tested
+    //
+    // However this only supports param X_X where X >= 4
+    signed_unchecked_overflowing_sub_test_case(
+        param,
+        ServerKey::unchecked_signed_overflowing_sub_parallelized_impl,
+    );
 }
 
 fn integer_signed_unchecked_mul(param: impl Into<PBSParameters>) {
@@ -1107,6 +1227,7 @@ fn integer_signed_smart_absolute_value(param: impl Into<PBSParameters>) {
 create_parametrized_test!(integer_signed_default_add);
 create_parametrized_test!(integer_signed_default_neg);
 create_parametrized_test!(integer_signed_default_sub);
+create_parametrized_test!(integer_signed_default_overflowing_sub);
 create_parametrized_test!(integer_signed_default_mul);
 create_parametrized_test!(integer_signed_default_bitnot);
 create_parametrized_test!(integer_signed_default_bitand);
@@ -1292,6 +1413,123 @@ where
             // println!("clear = {}, dec_res = {}", clear, dec_res);
             assert_eq!(clear, dec_res);
         }
+    }
+}
+
+fn integer_signed_default_overflowing_sub<P>(param: P)
+where
+    P: Into<PBSParameters>,
+{
+    let (cks, mut sks) = KEY_CACHE.get_from_params(param);
+    let cks = RadixClientKey::from((cks, NB_CTXT));
+
+    sks.set_deterministic_pbs_execution(true);
+
+    let mut rng = rand::thread_rng();
+
+    // message_modulus^vec_length
+    let modulus = (cks.parameters().message_modulus().0.pow(NB_CTXT as u32) / 2) as i64;
+
+    for _ in 0..NB_TEST_SMALLER {
+        let clear_0 = rng.gen::<i64>() % modulus;
+        let clear_1 = rng.gen::<i64>() % modulus;
+
+        let ctxt_0 = cks.encrypt_signed(clear_0);
+        let ctxt_1 = cks.encrypt_signed(clear_1);
+
+        let (ct_res, result_overflowed) = sks.signed_overflowing_sub_parallelized(&ctxt_0, &ctxt_1);
+        let (tmp_ct, tmp_o) = sks.signed_overflowing_sub_parallelized(&ctxt_0, &ctxt_1);
+        assert!(ct_res.block_carries_are_empty());
+        assert!(result_overflowed.carry_is_empty());
+        assert_eq!(ct_res, tmp_ct, "Failed determinism check");
+        assert_eq!(tmp_o, result_overflowed, "Failed determinism check");
+
+        let (expected_result, expected_overflowed) =
+            signed_overflowing_sub_under_modulus(clear_0, clear_1, modulus);
+
+        let decrypted_result: i64 = cks.decrypt_signed(&ct_res);
+        let decrypted_overflowed = cks.decrypt_one_block(&result_overflowed) == 1;
+        assert_eq!(
+            decrypted_result, expected_result,
+            "Invalid result for sub, for ({clear_0} - {clear_1}) % {modulus} \
+             expected {expected_result}, got {decrypted_result}"
+        );
+        assert_eq!(
+            decrypted_overflowed,
+            expected_overflowed,
+            "Invalid overflow flag result for overflowing_suv for ({clear_0} - {clear_1}) % {modulus} \
+             expected overflow flag {expected_overflowed}, got {decrypted_overflowed}"
+        );
+
+        for _ in 0..NB_TEST_SMALLER {
+            // Add non zero scalar to have non clean ciphertexts
+            let clear_2 = random_non_zero_value(&mut rng, modulus);
+            let clear_3 = random_non_zero_value(&mut rng, modulus);
+
+            let ctxt_0 = sks.unchecked_scalar_add(&ctxt_0, clear_2);
+            let ctxt_1 = sks.unchecked_scalar_add(&ctxt_1, clear_3);
+
+            let clear_lhs = signed_add_under_modulus(clear_0, clear_2, modulus);
+            let clear_rhs = signed_add_under_modulus(clear_1, clear_3, modulus);
+
+            let d0: i64 = cks.decrypt_signed(&ctxt_0);
+            assert_eq!(d0, clear_lhs, "Failed sanity decryption check");
+            let d1: i64 = cks.decrypt_signed(&ctxt_1);
+            assert_eq!(d1, clear_rhs, "Failed sanity decryption check");
+
+            let (ct_res, result_overflowed) =
+                sks.signed_overflowing_sub_parallelized(&ctxt_0, &ctxt_1);
+            assert!(ct_res.block_carries_are_empty());
+            assert!(result_overflowed.carry_is_empty());
+
+            let (expected_result, expected_overflowed) =
+                signed_overflowing_sub_under_modulus(clear_lhs, clear_rhs, modulus);
+
+            let decrypted_result: i64 = cks.decrypt_signed(&ct_res);
+            let decrypted_overflowed = cks.decrypt_one_block(&result_overflowed) == 1;
+            assert_eq!(
+                decrypted_result, expected_result,
+                "Invalid result for sub, for ({clear_lhs} - {clear_rhs}) % {modulus} \
+                expected {expected_result}, got {decrypted_result}"
+            );
+            assert_eq!(
+                decrypted_overflowed,
+                expected_overflowed,
+                "Invalid overflow flag result for overflowing_sub, for ({clear_lhs} - {clear_rhs}) % {modulus} \
+                expected overflow flag {expected_overflowed}, got {decrypted_overflowed}"
+            );
+        }
+    }
+
+    // Test with trivial inputs, as it was bugged at some point
+    for _ in 0..4 {
+        // Reduce maximum value of random number such that at least the last block is a trivial 0
+        // (This is how the reproducing case was found)
+        let clear_0 = rng.gen::<i64>() % modulus;
+        let clear_1 = rng.gen::<i64>() % modulus;
+
+        let a: SignedRadixCiphertext = sks.create_trivial_radix(clear_0, NB_CTXT);
+        let b: SignedRadixCiphertext = sks.create_trivial_radix(clear_1, NB_CTXT);
+
+        let (encrypted_result, encrypted_overflow) =
+            sks.signed_overflowing_sub_parallelized(&a, &b);
+
+        let (expected_result, expected_overflowed) =
+            signed_overflowing_sub_under_modulus(clear_0, clear_1, modulus);
+
+        let decrypted_result: i64 = cks.decrypt_signed(&encrypted_result);
+        let decrypted_overflowed = cks.decrypt_one_block(&encrypted_overflow) == 1;
+        assert_eq!(
+            decrypted_result, expected_result,
+            "Invalid result for sub, for ({clear_0} - {clear_1}) % {modulus} \
+                expected {expected_result}, got {decrypted_result}"
+        );
+        assert_eq!(
+            decrypted_overflowed,
+            expected_overflowed,
+            "Invalid overflow flag result for overflowing_sub, for ({clear_0} - {clear_1}) % {modulus} \
+                expected overflow flag {expected_overflowed}, got {decrypted_overflowed}"
+        );
     }
 }
 
