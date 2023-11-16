ee57f5658b2a802045e5e76c94be0680981d127a
Arthur Meyre <arthur.meyre@zama.ai>
2023-11-16 16:48:39 +0100

    chore(ci): refactor integer script and skip div and rem preferring div_rem

--- scripts/integer-tests.sh
+++ scripts/integer-tests.sh
@@ -88,112 +88,86 @@ if [[ $(uname) == "Darwin" ]]; then
     nproc_bin="sysctl -n hw.logicalcpu"
 fi
 
-n_threads="$(${nproc_bin})"
+# TODO autodetect/have a finer CPU count depending on memory
+num_cpu_threads="$(${nproc_bin})"
 
 if uname -a | grep "arm64"; then
     if [[ $(uname) == "Darwin" ]]; then
         # Keys are 4.7 gigs at max, CI M1 macs only has 8 gigs of RAM
-        n_threads=1
+        small_instance_n_threads=1
     fi
 else
     # Keys are 4.7 gigs at max, test machine has 32 gigs of RAM
-    n_threads=6
+    small_instance_n_threads=6
 fi
 
-if [[ "${BIG_TESTS_INSTANCE}" != TRUE ]]; then
-    if [[ "${FAST_TESTS}" != TRUE ]]; then
-        # block pbs are too slow for high params
-        # mul_crt_4_4 is extremely flaky (~80% failure)
-        # test_wopbs_bivariate_crt_wopbs_param_message generate tables that are too big at the moment
-        # test_integer_smart_mul_param_message_4_carry_4_ks_pbs is too slow
-        # so is test_integer_default_add_sequence_multi_thread_param_message_4_carry_4_ks_pbs
-        filter_expression="""\
-test(/^integer::.*${multi_bit}/) \
-${signed:+"and test(/^integer::.*${signed}/)"} \
-${not_multi_bit:+"and not test(~${not_multi_bit})"} \
-${not_signed:+"and not test(~${not_signed})"} \
-and not test(/.*_block_pbs(_base)?_param_message_[34]_carry_[34]_ks_pbs$/) \
-and not test(~mul_crt_param_message_4_carry_4_ks_pbs) \
-and not test(/.*test_wopbs_bivariate_crt_wopbs_param_message_[34]_carry_[34]_ks_pbs$/) \
-and not test(/.*test_integer_smart_mul_param_message_4_carry_4_ks_pbs$/) \
-and not test(/.*test_integer_default_add_sequence_multi_thread_param_message_4_carry_4_ks_pbs$/)"""
-    else
-        # test only fast default operations with only two set of parameters
-        filter_expression="""\
-test(/^integer::.*${multi_bit}/) \
-${signed:+"and test(/^integer::.*${signed}/)"} \
-${not_multi_bit:+"and not test(~${not_multi_bit})"} \
-${not_signed:+"and not test(~${not_signed})"} \
-and test(/.*_default_.*?_param${multi_bit}_message_[2-3]_carry_[2-3]${multi_bit:+"_group_2"}_ks_pbs/) \
-and not test(/.*_param_message_[14]_carry_[14]_ks_pbs$/) \
-and not test(/.*default_add_sequence_multi_thread_param_message_3_carry_3_ks_pbs$/)"""
-    fi
-
-    cargo "${RUST_TOOLCHAIN}" nextest run \
-        --tests \
-        --cargo-profile "${cargo_profile}" \
-        --package tfhe \
-        --profile ci \
-        --features="${ARCH_FEATURE}",integer,internal-keycache,"${avx512_feature}" \
-        --test-threads "${n_threads}" \
-        -E "$filter_expression"
-
-    if [[ "${multi_bit}" == "" ]]; then
-        cargo "${RUST_TOOLCHAIN}" test \
-            --profile "${cargo_profile_doctests}" \
-            --package tfhe \
-            --features="${ARCH_FEATURE}",integer,internal-keycache,"${avx512_feature}" \
-            --doc \
-            -- integer::
-    fi
+if [[ "${BIG_TESTS_INSTANCE}" == TRUE ]]; then
+    test_threads="$((num_cpu_threads * 1 / 2))"
+    doctest_threads="${num_cpu_threads}"
 else
-    if [[ "${FAST_TESTS}" != TRUE ]]; then
-        # block pbs are too slow for high params
-        # mul_crt_4_4 is extremely flaky (~80% failure)
-        # test_wopbs_bivariate_crt_wopbs_param_message generate tables that are too big at the moment
-        # test_integer_smart_mul_param_message_4_carry_4_ks_pbs is too slow
-        # so is test_integer_default_add_sequence_multi_thread_param_message_4_carry_4_ks_pbs
-        filter_expression="""\
+    test_threads="${small_instance_n_threads}"
+    doctest_threads="${num_cpu_threads}"
+fi
+
+# block pbs are too slow for high params
+# mul_crt_4_4 is extremely flaky (~80% failure)
+# test_wopbs_bivariate_crt_wopbs_param_message generate tables that are too big at the moment
+# test_integer_smart_mul_param_message_4_carry_4_ks_pbs is too slow
+# so is test_integer_default_add_sequence_multi_thread_param_message_4_carry_4_ks_pbs
+# we skip smart_div, smart_rem which are already covered by the smar_div_rem test
+# we similarly skip default_div, default_rem which are covered by default_div_rem
+full_test_filter_expression="""\
 test(/^integer::.*${multi_bit}/) \
 ${signed:+"and test(/^integer::.*${signed}/)"} \
 ${not_multi_bit:+"and not test(~${not_multi_bit})"} \
 ${not_signed:+"and not test(~${not_signed})"} \
+and not test(/.*integer_smart_div_param/) \
+and not test(/.*integer_smart_rem_param/) \
+and not test(/.*integer_default_div_param/) \
+and not test(/.*integer_default_rem_param/) \
 and not test(/.*_block_pbs(_base)?_param_message_[34]_carry_[34]_ks_pbs$/) \
 and not test(~mul_crt_param_message_4_carry_4_ks_pbs) \
 and not test(/.*test_wopbs_bivariate_crt_wopbs_param_message_[34]_carry_[34]_ks_pbs$/) \
 and not test(/.*test_integer_smart_mul_param_message_4_carry_4_ks_pbs$/) \
 and not test(/.*test_integer_default_add_sequence_multi_thread_param_message_4_carry_4_ks_pbs$/)"""
-    else
-        # test only fast default operations with only two set of parameters
-        filter_expression="""\
+
+# test only fast default operations with only two set of parameters
+# we skip default_div, default_rem which are covered by default_div_rem
+fast_test_filter_expression="""\
 test(/^integer::.*${multi_bit}/) \
 ${signed:+"and test(/^integer::.*${signed}/)"} \
 ${not_multi_bit:+"and not test(~${not_multi_bit})"} \
 ${not_signed:+"and not test(~${not_signed})"} \
 and test(/.*_default_.*?_param${multi_bit}_message_[2-3]_carry_[2-3]${multi_bit:+"_group_2"}_ks_pbs/) \
+and not test(/.*integer_default_div_param/) \
+and not test(/.*integer_default_rem_param/) \
 and not test(/.*_param_message_[14]_carry_[14]_ks_pbs$/) \
 and not test(/.*default_add_sequence_multi_thread_param_message_3_carry_3_ks_pbs$/)"""
-    fi
 
-    num_cpu_threads="$(${nproc_bin})"
-    num_threads=$((num_cpu_threads * 1 / 2))
-    cargo "${RUST_TOOLCHAIN}" nextest run \
-        --tests \
-        --cargo-profile "${cargo_profile}" \
+if [[ "${FAST_TESTS}" == "TRUE" ]]; then
+    echo "Running 'fast' test set'"
+    filter_expression="${fast_test_filter_expression}"
+else
+    echo "Running 'slow' test set"
+    filter_expression="${full_test_filter_expression}"
+fi
+
+cargo "${RUST_TOOLCHAIN}" nextest run \
+    --tests \
+    --cargo-profile "${cargo_profile}" \
+    --package tfhe \
+    --profile ci \
+    --features="${ARCH_FEATURE}",integer,internal-keycache,"${avx512_feature}" \
+    --test-threads "${test_threads}" \
+    -E "$filter_expression"
+
+if [[ "${multi_bit}" == "" ]]; then
+    cargo "${RUST_TOOLCHAIN}" test \
+        --profile "${cargo_profile_doctests}" \
         --package tfhe \
-        --profile ci \
         --features="${ARCH_FEATURE}",integer,internal-keycache,"${avx512_feature}" \
-        --test-threads $num_threads \
-        -E "$filter_expression"
-
-    if [[ "${multi_bit}" == "" ]]; then
-        cargo "${RUST_TOOLCHAIN}" test \
-            --profile "${cargo_profile_doctests}" \
-            --package tfhe \
-            --features="${ARCH_FEATURE}",integer,internal-keycache,"${avx512_feature}" \
-            --doc \
-            -- --test-threads="$(${nproc_bin})" integer::
-    fi
+        --doc \
+        -- --test-threads="${doctest_threads}" integer::
 fi
 
 echo "Test ran in $SECONDS seconds"
