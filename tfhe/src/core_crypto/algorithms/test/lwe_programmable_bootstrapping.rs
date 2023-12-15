4b7c041082e330bbc25dac222357b99e50437836
Arthur Meyre <arthur.meyre@zama.ai>
2023-12-15 14:21:20 +0100

    test(core): add a many LUT test in core crypto
    - it does not require any new primitive so was made into a test at the core
    crypto level
    - shortint will have a more user friendly API, using the MSBs for selecting
    the function means it should not require too much design as deltas are
    always the same

--- tfhe/src/core_crypto/algorithms/test/lwe_programmable_bootstrapping.rs
+++ tfhe/src/core_crypto/algorithms/test/lwe_programmable_bootstrapping.rs
@@ -165,6 +165,214 @@ where
 
 create_parametrized_test!(lwe_encrypt_pbs_decrypt_custom_mod);
 
+// Here we will define a helper function to generate a many lut accumulator for a PBS
+fn generate_accumulator_many_lut<Scalar: UnsignedTorus + CastFrom<usize>>(
+    polynomial_size: PolynomialSize,
+    glwe_size: GlweSize,
+    message_modulus: usize,
+    ciphertext_modulus: CiphertextModulus<Scalar>,
+    delta: Scalar,
+    funcs: &[&dyn Fn(Scalar) -> Scalar],
+) -> (GlweCiphertextOwned<Scalar>, usize) {
+    // N/(p/2) = size of each block, to correct noise from the input we introduce the
+    // notion of box, which manages redundancy to yield a denoised value
+    // for several noisy values around a true input value.
+    let box_size = polynomial_size.0 / message_modulus;
+
+    let fn_count = funcs.len();
+    let fn_count = if fn_count.is_power_of_two() {
+        fn_count
+    } else {
+        fn_count.next_power_of_two()
+    };
+
+    assert!(fn_count <= (message_modulus / 2));
+
+    let func_chunk_size = polynomial_size.0 / fn_count;
+
+    // Create the accumulator
+    let mut accumulator_scalar = vec![Scalar::ZERO; polynomial_size.0];
+
+    let func_chunks = accumulator_scalar.chunks_exact_mut(func_chunk_size);
+
+    // Fill each box with the encoded denoised value
+    for (func, func_chunk) in funcs.iter().zip(func_chunks) {
+        for (msg_value, box_) in func_chunk.chunks_exact_mut(box_size).enumerate() {
+            let msg_value = Scalar::cast_from(msg_value);
+
+            let func_eval = func(msg_value) * delta;
+            box_.fill(func_eval);
+        }
+    }
+
+    let half_box_size = box_size / 2;
+
+    if ciphertext_modulus.is_compatible_with_native_modulus() {
+        // Negate the first half_box_size coefficients to manage negacyclicity and rotate
+        for a_i in accumulator_scalar[0..half_box_size].iter_mut() {
+            *a_i = (*a_i).wrapping_neg();
+        }
+    } else {
+        let modulus: Scalar = ciphertext_modulus.get_custom_modulus().cast_into();
+        for a_i in accumulator_scalar[0..half_box_size].iter_mut() {
+            *a_i = (*a_i).wrapping_neg_custom_mod(modulus);
+        }
+    }
+
+    // Rotate the accumulator
+    accumulator_scalar.rotate_left(half_box_size);
+
+    let accumulator_plaintext = PlaintextList::from_container(accumulator_scalar);
+
+    (
+        allocate_and_trivially_encrypt_new_glwe_ciphertext(
+            glwe_size,
+            &accumulator_plaintext,
+            ciphertext_modulus,
+        ),
+        func_chunk_size,
+    )
+}
+
+fn lwe_encrypt_pbs_many_lut_decrypt_custom_mod<Scalar>(params: ClassicTestParams<Scalar>)
+where
+    Scalar: UnsignedTorus
+        + Sync
+        + Send
+        + CastFrom<usize>
+        + CastInto<usize>
+        + Serialize
+        + DeserializeOwned,
+    ClassicTestParams<Scalar>: KeyCacheAccess<Keys = ClassicBootstrapKeys<Scalar>>,
+{
+    let lwe_modular_std_dev = params.lwe_modular_std_dev;
+    let ciphertext_modulus = params.ciphertext_modulus;
+    let message_modulus_log = params.message_modulus_log;
+    let msg_modulus = Scalar::ONE.shl(message_modulus_log.0);
+    let encoding_with_padding = get_encoding_with_padding(ciphertext_modulus);
+    let glwe_dimension = params.glwe_dimension;
+    let polynomial_size = params.polynomial_size;
+
+    let total_plaintext_modulus = msg_modulus;
+
+    let mut rsc = TestResources::new();
+
+    // We need at least two bits of space to manage 4 functions, we could make something even more
+    // flexible, but this should be enough
+    assert!(total_plaintext_modulus > Scalar::cast_from(4u128));
+
+    let f1 = |x: Scalar| x % total_plaintext_modulus;
+    let f2 = |x: Scalar| (x.wrapping_mul(Scalar::cast_from(3u128)) % total_plaintext_modulus);
+    let f3 = |x: Scalar| ((x + Scalar::ONE) % total_plaintext_modulus);
+    let f4 = |x: Scalar| ((x / Scalar::TWO) % total_plaintext_modulus);
+
+    let funcs: [&dyn Fn(Scalar) -> Scalar; 4] = [&f1, &f2, &f3, &f4];
+
+    for fn_count in 1..=funcs.len() {
+        let funcs = &funcs[..fn_count];
+
+        let fn_count = funcs.len();
+        // Compute a potentially inflated fn_count to allocate enough bits in the plaintext space
+        let fn_count = if fn_count.is_power_of_two() {
+            fn_count
+        } else {
+            fn_count.next_power_of_two()
+        };
+
+        // Actual bits used for the msg, will allow enough functions to be computed at the same time
+        let msg_modulus = total_plaintext_modulus / Scalar::cast_from(fn_count);
+
+        let mut msg = msg_modulus;
+
+        // Here the separate delta is done on purpose, it's to indicate how a many lut approach
+        // could be dealing with two deltas, one for the input (which has to leave some LSB set to
+        // 0) and this lut delta which can make use of the full plaintext space
+        let delta: Scalar = encoding_with_padding / total_plaintext_modulus;
+
+        // The box size is required to know where to look for the evaluations of the other functions
+        let (accumulator, func_chunk_size) = generate_accumulator_many_lut(
+            polynomial_size,
+            glwe_dimension.to_glwe_size(),
+            total_plaintext_modulus.cast_into(),
+            ciphertext_modulus,
+            delta,
+            funcs,
+        );
+
+        assert!(check_encrypted_content_respects_mod(
+            &accumulator,
+            ciphertext_modulus
+        ));
+
+        while msg != Scalar::ZERO {
+            msg = msg.wrapping_sub(Scalar::ONE);
+
+            let mut keys_gen = |params| generate_keys(params, &mut rsc);
+            let keys = gen_keys_or_get_from_cache_if_enabled(params, &mut keys_gen);
+            let (input_lwe_secret_key, output_lwe_secret_key, fbsk) =
+                (keys.small_lwe_sk, keys.big_lwe_sk, keys.fbsk);
+
+            for _ in 0..NB_TESTS {
+                let plaintext = Plaintext(msg * delta);
+
+                let lwe_ciphertext_in = allocate_and_encrypt_new_lwe_ciphertext(
+                    &input_lwe_secret_key,
+                    plaintext,
+                    lwe_modular_std_dev,
+                    ciphertext_modulus,
+                    &mut rsc.encryption_random_generator,
+                );
+
+                assert!(check_encrypted_content_respects_mod(
+                    &lwe_ciphertext_in,
+                    ciphertext_modulus
+                ));
+
+                let mut tmp_acc = accumulator.clone();
+
+                blind_rotate_assign(&lwe_ciphertext_in, &mut tmp_acc, &fbsk);
+
+                assert!(check_encrypted_content_respects_mod(
+                    &tmp_acc,
+                    ciphertext_modulus
+                ));
+
+                let mut out_pbs_ct = LweCiphertext::new(
+                    Scalar::ZERO,
+                    output_lwe_secret_key.lwe_dimension().to_lwe_size(),
+                    ciphertext_modulus,
+                );
+
+                for (fn_idx, func) in funcs.iter().enumerate() {
+                    extract_lwe_sample_from_glwe_ciphertext(
+                        &tmp_acc,
+                        &mut out_pbs_ct,
+                        MonomialDegree(fn_idx * func_chunk_size),
+                    );
+
+                    assert!(check_encrypted_content_respects_mod(
+                        &out_pbs_ct,
+                        ciphertext_modulus
+                    ));
+
+                    let decrypted = decrypt_lwe_ciphertext(&output_lwe_secret_key, &out_pbs_ct);
+
+                    let decoded = round_decode(decrypted.0, delta) % total_plaintext_modulus;
+
+                    assert_eq!(decoded, func(msg));
+                }
+            }
+
+            // In coverage, we break after one while loop iteration, changing message values does
+            // not yield higher coverage
+            #[cfg(feature = "__coverage")]
+            break;
+        }
+    }
+}
+
+create_parametrized_test!(lwe_encrypt_pbs_many_lut_decrypt_custom_mod);
+
 // DISCLAIMER: all parameters here are not guaranteed to be secure or yield correct computations
 pub const TEST_PARAMS_4_BITS_NATIVE_U128: ClassicTestParams<u128> = ClassicTestParams {
     lwe_dimension: LweDimension(742),
