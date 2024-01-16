zama-ai/tfhe-rs

commit 48405959a4929131566779aba9cf6679a5a141c0

feat(integer): add decrypt_trivial
This adds a decrypt_trivial method to all ciphertext types of
shortint and integer.

This functions tries to "decrypt" the ciphertext if it is a
trivial one, otherwise it return an error.

This is meant to be a debugging 'tool':

To debug a function / circuit, users can call the function
on trivial ciphertexts intead of real ciphertext, that way,
computations are faster _and_ they will now be able to see intermediate
values via these decrypt_trivial, to do some print-debugging or use a
debugger.

tfhe/src/integer/client_key/mod.rs
@@ -50,6 +50,35 @@ impl RecomposableSignedInteger for i128 {}
 
 impl<const N: usize> RecomposableSignedInteger for StaticSignedBigInt<N> {}
 
+/// This function takes a signed integer of type `T` for which `num_bits_set`
+/// have been set.
+///
+/// It will set the most significant bits to the value of the bit
+/// at pos `num_bits_set - 1`.
+///
+/// This is used to correctly decrypt a signed radix ciphertext into a clear type
+/// that has more bits than the original ciphertext.
+///
+/// This is like doing i8 as i16, i16 as i64, i6 as i8, etc
+pub(in crate::integer) fn sign_extend_partial_number<T>(unpadded_value: T, num_bits_set: u32) -> T
+where
+    T: RecomposableSignedInteger,
+{
+    if num_bits_set >= T::BITS as u32 {
+        return unpadded_value;
+    }
+
+    let sign_bit_pos = num_bits_set - 1;
+    let sign_bit_mask = T::cast_from(1u32 << sign_bit_pos);
+    let sign_bit = (unpadded_value & sign_bit_mask) >> sign_bit_pos;
+
+    // Creates a padding mask
+    // where bits above num_bits_set
+    // are 1s if sign bit is `1` else `0`
+    let padding = (T::MAX * sign_bit) << num_bits_set;
+    padding | unpadded_value
+}
+
 /// A structure containing the client key, which must be kept secret.
 ///
 /// This key can be used to encrypt both in Radix and CRT
@@ -391,19 +420,7 @@ impl ClientKey {
 
         let num_bits_in_message = message_modulus.ilog2();
         let num_bits_in_ctxt = num_bits_in_message * ctxt.blocks.len() as u32;
-        if num_bits_in_ctxt >= T::BITS as u32 {
-            return unpadded_value;
-        }
-
-        let sign_bit_pos = num_bits_in_ctxt - 1;
-        let sign_bit_mask = T::cast_from(1u32 << sign_bit_pos);
-        let sign_bit = (unpadded_value & sign_bit_mask) >> sign_bit_pos;
-
-        // Creates a padding mask
-        // where bits above num_bits_in_ctxt
-        // are 1s if sign bit is one else 0
-        let padding = (T::MAX * sign_bit) << num_bits_in_ctxt;
-        padding | unpadded_value
+        sign_extend_partial_number(unpadded_value, num_bits_in_ctxt)
     }
 
     /// Encrypts one block.

tfhe/src/integer/ciphertext/mod.rs
@@ -5,6 +5,10 @@ use super::parameters::{
     RadixCiphertextConformanceParams, RadixCompactCiphertextListConformanceParams,
 };
 use crate::conformance::ParameterSetConformant;
+use crate::core_crypto::prelude::UnsignedNumeric;
+use crate::integer::block_decomposition::{BlockRecomposer, RecomposableFrom};
+use crate::integer::client_key::{sign_extend_partial_number, RecomposableSignedInteger};
+use crate::shortint::ciphertext::NotTrivialCiphertextError;
 use crate::shortint::{Ciphertext, CompressedCiphertext};
 use serde::{Deserialize, Serialize};
 
@@ -128,6 +132,69 @@ impl RadixCiphertext {
     pub fn block_carries_are_empty(&self) -> bool {
         self.blocks.iter().all(Ciphertext::carry_is_empty)
     }
+
+    pub fn is_trivial(&self) -> bool {
+        self.blocks.iter().all(Ciphertext::is_trivial)
+    }
+
+    /// Decrypts a trivial ciphertext
+    ///
+    /// Trivial ciphertexts are ciphertexts which are not encrypted
+    /// meaning they can be decrypted by any key, or even without a key.
+    ///
+    /// For debugging it can be useful to use trivial ciphertext to speed up
+    /// execution, and use [Self::decrypt_trivial] to decrypt temporary values
+    /// and debug.
+    ///
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use tfhe::integer::{gen_keys_radix, RadixCiphertext};
+    /// use tfhe::shortint::parameters::PARAM_MESSAGE_2_CARRY_2_KS_PBS;
+    ///
+    /// // 8 bits
+    /// let (cks, sks) = gen_keys_radix(PARAM_MESSAGE_2_CARRY_2_KS_PBS, 4);
+    ///
+    /// let msg = 124u8;
+    /// let msg2 = 17u8;
+    ///
+    /// // Trivial encryption
+    /// let trivial_ct: RadixCiphertext = sks.create_trivial_radix(msg, 4);
+    /// let non_trivial_ct = cks.encrypt(msg2);
+    ///
+    /// let res = trivial_ct.decrypt_trivial();
+    /// assert_eq!(Ok(msg), res);
+    ///
+    /// let res = non_trivial_ct.decrypt_trivial::<u8>();
+    /// matches!(res, Err(_));
+    ///
+    /// // Doing operations that mixes trivial and non trivial
+    /// // will always return a non trivial
+    /// let ct_res = sks.add_parallelized(&trivial_ct, &non_trivial_ct);
+    /// let res = ct_res.decrypt_trivial::<u8>();
+    /// matches!(res, Err(_));
+    ///
+    /// // Doing operations using only trivial ciphertexts
+    /// // will return a trivial
+    /// let ct_res = sks.add_parallelized(&trivial_ct, &trivial_ct);
+    /// let res = ct_res.decrypt_trivial::<u8>();
+    /// assert_eq!(Ok(msg + msg), res);
+    /// ```
+    pub fn decrypt_trivial<Clear>(&self) -> Result<Clear, NotTrivialCiphertextError>
+    where
+        Clear: UnsignedNumeric + RecomposableFrom<u64>,
+    {
+        let bits_in_block = self.blocks[0].message_modulus.0.ilog2();
+        let mut recomposer = BlockRecomposer::<Clear>::new(bits_in_block);
+
+        for encrypted_block in &self.blocks {
+            let decrypted_block = encrypted_block.decrypt_trivial_message_and_carry()?;
+            recomposer.add_unmasked(decrypted_block);
+        }
+
+        Ok(recomposer.value())
+    }
 }
 
 /// Structure containing a ciphertext in radix decomposition
@@ -178,6 +245,71 @@ impl ParameterSetConformant for CompressedSignedRadixCiphertext {
 impl SignedRadixCiphertext {
     pub fn block_carries_are_empty(&self) -> bool {
         self.blocks.iter().all(Ciphertext::carry_is_empty)
+    }
+
+    pub fn is_trivial(&self) -> bool {
+        self.blocks.iter().all(Ciphertext::is_trivial)
+    }
+
+    /// Decrypts a trivial ciphertext
+    ///
+    /// Trivial ciphertexts are ciphertexts which are not encrypted
+    /// meaning they can be decrypted by any key, or even without a key.
+    ///
+    /// For debugging it can be useful to use trivial ciphertext to speed up
+    /// execution, and use [Self::decrypt_trivial] to decrypt temporary values
+    /// and debug.
+    ///
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use tfhe::integer::{gen_keys_radix, RadixCiphertext, SignedRadixCiphertext};
+    /// use tfhe::shortint::parameters::PARAM_MESSAGE_2_CARRY_2_KS_PBS;
+    ///
+    /// // 8 bits
+    /// let (cks, sks) = gen_keys_radix(PARAM_MESSAGE_2_CARRY_2_KS_PBS, 4);
+    ///
+    /// let msg = -35i8;
+    /// let msg2 = 17i8;
+    ///
+    /// // Trivial encryption
+    /// let trivial_ct: SignedRadixCiphertext = sks.create_trivial_radix(msg, 4);
+    /// let non_trivial_ct = cks.encrypt_signed(msg2);
+    ///
+    /// let res = trivial_ct.decrypt_trivial();
+    /// assert_eq!(Ok(msg), res);
+    ///
+    /// let res = non_trivial_ct.decrypt_trivial::<i8>();
+    /// matches!(res, Err(_));
+    ///
+    /// // Doing operations that mixes trivial and non trivial
+    /// // will always return a non trivial
+    /// let ct_res = sks.add_parallelized(&trivial_ct, &non_trivial_ct);
+    /// let res = ct_res.decrypt_trivial::<i8>();
+    /// matches!(res, Err(_));
+    ///
+    /// // Doing operations using only trivial ciphertexts
+    /// // will return a trivial
+    /// let ct_res = sks.add_parallelized(&trivial_ct, &trivial_ct);
+    /// let res = ct_res.decrypt_trivial::<i8>();
+    /// assert_eq!(Ok(msg + msg), res);
+    /// ```
+    pub fn decrypt_trivial<Clear>(&self) -> Result<Clear, NotTrivialCiphertextError>
+    where
+        Clear: RecomposableSignedInteger,
+    {
+        let bits_in_block = self.blocks[0].message_modulus.0.ilog2();
+        let mut recomposer = BlockRecomposer::<Clear>::new(bits_in_block);
+
+        for encrypted_block in &self.blocks {
+            let decrypted_block = encrypted_block.decrypt_trivial_message_and_carry()?;
+            recomposer.add_unmasked(decrypted_block);
+        }
+
+        let num_bits_in_ctxt = bits_in_block * self.blocks.len() as u32;
+        let unpadded_value = recomposer.value();
+        Ok(sign_extend_partial_number(unpadded_value, num_bits_in_ctxt))
     }
 }
 impl From<CompressedSignedRadixCiphertext> for SignedRadixCiphertext {

tfhe/src/shortint/ciphertext/mod.rs
@@ -10,6 +10,18 @@ use std::fmt::Debug;
 use super::parameters::{CiphertextConformanceParams, CiphertextListConformanceParams};
 use super::CheckError;
 
+/// Error for when a non trivial ciphertext was used when a trivial was expected
+#[derive(Copy, Clone, PartialEq, Eq, Debug)]
+pub struct NotTrivialCiphertextError;
+
+impl std::fmt::Display for NotTrivialCiphertextError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "The ciphertext is a not a trivial ciphertext")
+    }
+}
+
+impl std::error::Error for NotTrivialCiphertextError {}
+
 /// This tracks the number of operations that has been done.
 #[derive(Debug, PartialEq, Eq, Copy, Clone, Serialize, Deserialize)]
 pub struct Degree(pub usize);
@@ -288,6 +300,88 @@ impl Ciphertext {
 
     pub fn set_noise_level(&mut self, noise_level: NoiseLevel) {
         self.noise_level = noise_level;
+    }
+
+    /// Decrypts a trivial ciphertext
+    ///
+    /// Trivial ciphertexts are ciphertexts which are not encrypted
+    /// meaning they can be decrypted by any key, or even without a key.
+    ///
+    /// For debugging it can be useful to use trivial ciphertext to speed up
+    /// execution, and use [Self::decrypt_trivial] to decrypt temporary values
+    /// and debug.
+    ///
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use tfhe::shortint::parameters::PARAM_MESSAGE_2_CARRY_2_KS_PBS;
+    /// use tfhe::shortint::{gen_keys, Ciphertext};
+    ///
+    /// // Generate the client key and the server key:
+    /// let (cks, sks) = gen_keys(PARAM_MESSAGE_2_CARRY_2_KS_PBS);
+    ///
+    /// let msg = 1;
+    /// let msg2 = 2;
+    ///
+    /// // Trivial encryption
+    /// let trivial_ct = sks.create_trivial(msg);
+    /// let non_trivial_ct = cks.encrypt(msg2);
+    ///
+    /// let res = trivial_ct.decrypt_trivial();
+    /// assert_eq!(Ok(1), res);
+    ///
+    /// let res = non_trivial_ct.decrypt_trivial();
+    /// matches!(res, Err(_));
+    ///
+    /// // Doing operations that mixes trivial and non trivial
+    /// // will always return a non trivial
+    /// let ct_res = sks.add(&trivial_ct, &non_trivial_ct);
+    /// let res = ct_res.decrypt_trivial();
+    /// matches!(res, Err(_));
+    ///
+    /// // Doing operations using only trivial ciphertexts
+    /// // will return a trivial
+    /// let ct_res = sks.add(&trivial_ct, &trivial_ct);
+    /// let res = ct_res.decrypt_trivial();
+    /// assert_eq!(Ok(2), res);
+    /// ```
+    pub fn decrypt_trivial(&self) -> Result<u64, NotTrivialCiphertextError> {
+        self.decrypt_trivial_message_and_carry()
+            .map(|x| x % self.message_modulus.0 as u64)
+    }
+
+    /// See [Self::decrypt_trivial].
+    /// # Example
+    ///
+    /// ```rust
+    /// use tfhe::shortint::parameters::PARAM_MESSAGE_2_CARRY_2_KS_PBS;
+    /// use tfhe::shortint::{gen_keys, Ciphertext};
+    ///
+    /// // Generate the client key and the server key:
+    /// let (cks, sks) = gen_keys(PARAM_MESSAGE_2_CARRY_2_KS_PBS);
+    ///
+    /// let msg = 2u64;
+    /// let clear = 3u64;
+    ///
+    /// let mut trivial_ct = sks.create_trivial(msg);
+    ///
+    /// sks.unchecked_scalar_add_assign(&mut trivial_ct, clear as u8);
+    ///
+    /// let res = trivial_ct.decrypt_trivial();
+    /// let expected = (msg + clear) % PARAM_MESSAGE_2_CARRY_2_KS_PBS.message_modulus.0 as u64;
+    /// assert_eq!(Ok(expected), res);
+    ///
+    /// let res = trivial_ct.decrypt_trivial_message_and_carry();
+    /// assert_eq!(Ok(msg + clear), res);
+    /// ```
+    pub fn decrypt_trivial_message_and_carry(&self) -> Result<u64, NotTrivialCiphertextError> {
+        if self.is_trivial() {
+            let delta = (1u64 << 63) / (self.message_modulus.0 * self.carry_modulus.0) as u64;
+            Ok(self.ct.get_body().data / delta)
+        } else {
+            Err(NotTrivialCiphertextError)
+        }
     }
 }
 

tfhe/src/integer/ciphertext/boolean_value.rs
@@ -1,5 +1,6 @@
 use super::{IntegerCiphertext, IntegerRadixCiphertext};
 use crate::integer::{RadixCiphertext, ServerKey};
+use crate::shortint::ciphertext::NotTrivialCiphertextError;
 use crate::shortint::Ciphertext;
 use serde::{Deserialize, Serialize};
 
@@ -123,6 +124,42 @@ impl BooleanBlock {
         let missing_blocks = num_blocks.saturating_sub(1);
         sks.extend_radix_with_trivial_zero_blocks_msb_assign(&mut radix_ct, missing_blocks);
         T::from_blocks(radix_ct.blocks)
+    }
+
+    /// Decrypts a trivial ciphertext
+    ///
+    /// Trivial ciphertexts are ciphertexts which are not encrypted
+    /// meaning they can be decrypted by any key, or even without a key.
+    ///
+    /// For debugging it can be useful to use trivial ciphertext to speed up
+    /// execution, and use [Self::decrypt_trivial] to decrypt temporary values
+    /// and debug.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use tfhe::integer::{gen_keys_radix, RadixCiphertext, SignedRadixCiphertext};
+    /// use tfhe::shortint::parameters::PARAM_MESSAGE_2_CARRY_2_KS_PBS;
+    ///
+    /// // 8 bits
+    /// let (cks, sks) = gen_keys_radix(PARAM_MESSAGE_2_CARRY_2_KS_PBS, 4);
+    ///
+    /// let msg = false;
+    /// let msg2 = true;
+    ///
+    /// // Trivial encryption
+    /// let trivial_ct = sks.create_trivial_boolean_block(msg);
+    /// let non_trivial_ct = cks.encrypt_bool(msg2);
+    ///
+    /// let res = trivial_ct.decrypt_trivial();
+    /// assert_eq!(Ok(msg), res);
+    ///
+    /// let res = non_trivial_ct.decrypt_trivial();
+    /// matches!(res, Err(_));
+    /// ```
+    pub fn decrypt_trivial(&self) -> Result<bool, NotTrivialCiphertextError> {
+        let value = self.0.decrypt_trivial()?;
+        Ok(value != 0)
     }
 }
 

commit 34c41624339c4fb5307c71a38d8ed4abe580a92a

fix(integer): fix decrypting negative value of N >= 32 BITS
Bug was probably introduced in 48405959a4929131566779aba9cf6679a5a141c0

tfhe/src/integer/client_key/mod.rs
@@ -66,10 +66,8 @@ where
     if num_bits_set >= T::BITS as u32 {
         return unpadded_value;
     }
-
     let sign_bit_pos = num_bits_set - 1;
-    let sign_bit_mask = T::cast_from(1u32 << sign_bit_pos);
-    let sign_bit = (unpadded_value & sign_bit_mask) >> sign_bit_pos;
+    let sign_bit = (unpadded_value >> sign_bit_pos) & T::ONE;
 
     // Creates a padding mask
     // where bits above num_bits_set

tfhe/src/integer/server_key/radix/tests.rs
@@ -1,5 +1,6 @@
+use crate::core_crypto::prelude::misc::divide_ceil;
 use crate::integer::keycache::KEY_CACHE;
-use crate::integer::{IntegerKeyKind, ServerKey, U256};
+use crate::integer::{IntegerKeyKind, ServerKey, SignedRadixCiphertext, U256};
 use crate::shortint::parameters::*;
 use crate::shortint::ClassicPBSParameters;
 use rand::Rng;
@@ -67,6 +68,8 @@ create_parametrized_test!(integer_full_propagate {
     PARAM_MESSAGE_3_CARRY_3_KS_PBS,
     PARAM_MESSAGE_4_CARRY_4_KS_PBS
 });
+
+create_parametrized_test!(integer_signed_decryption_correctly_sign_extend);
 
 fn integer_encrypt_decrypt(param: ClassicPBSParameters) {
     let (cks, _) = KEY_CACHE.get_from_params(param, IntegerKeyKind::Radix);
@@ -1135,4 +1138,22 @@ where
 {
     let executor = CpuFunctionExecutor::new(&ServerKey::full_propagate);
     full_propagate_test(param, executor);
+}
+fn integer_signed_decryption_correctly_sign_extend(param: impl Into<PBSParameters>) {
+    // Test that when decrypting a negative SignedRadixCiphertext of N bits to a
+    // clear type of M bits where M > N, the sign extension is correctly done
+    //
+    // Specifically here we take N = 64 bits, M = 128 bits
+    let (cks, sks) = KEY_CACHE.get_from_params(param, IntegerKeyKind::Radix);
+
+    let num_bits_in_one_block = sks.message_modulus().0.ilog2();
+    let num_blocks = divide_ceil(64, num_bits_in_one_block);
+    let value = -1i64;
+
+    let encrypted = cks.encrypt_signed_radix(value, num_blocks as usize);
+    let decrypted: i128 = cks.decrypt_signed_radix(&encrypted);
+    assert_eq!(decrypted, value as i128);
+
+    let trivial: SignedRadixCiphertext = sks.create_trivial_radix(value, num_blocks as usize);
+    assert_eq!(trivial.decrypt_trivial::<i128>().unwrap(), value as i128);
 }