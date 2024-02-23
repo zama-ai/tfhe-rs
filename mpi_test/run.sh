99bbd0ed7d091f0a8300e40d84125f5ef7b58eb4
Mayeul@Zama <69792125+mayeul-zama@users.noreply.github.com>
2024-02-23 11:45:49 +0100

    f

--- mpi_test/run.sh
+++ mpi_test/run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e
 
-cargo build --profile=devo
+cargo build --profile=release
 source /etc/profile.d/modules.sh
 module load mpi/openmpi-x86_64
 # export LD_LIBRARY_PATH="/usr/lib64/mpich/lib/"
 export LD_LIBRARY_PATH="/usr/lib64/openmpi/lib/"
+export RUSTFLAGS="-Ctarget-cpu=native"
+
 mpirun -n 6 ../target/devo/mpi_test
