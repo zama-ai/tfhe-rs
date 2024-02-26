d1abdc081d3849ed10914acf52ad988256bd0c20
Arthur Meyre <arthur.meyre@zama.ai>
2024-02-26 20:56:29 +0100

    chore(ci): fix test runs by moving panic to its own test file
    - investigate what goes wrong in the panic hook leading to multiple use of
    the same object from the js side

--- tfhe/js_on_wasm_tests/test-hlapi-unsigned.js
+++ tfhe/js_on_wasm_tests/test-hlapi-unsigned.js
@@ -36,27 +36,6 @@ const U32_MAX = 4294967295;
 // will be printed and tess will be ok
 init_panic_hook();
 
-// Here integers are not enabled
-// but we try to use them, so an error should be returned
-// as the underlying panic should have been trapped
-test('hlapi_panic', (t) => {
-    let config = TfheConfigBuilder.all_disabled()
-        .build();
-
-    let clientKey = TfheClientKey.generate(config);
-
-    let clear = 73;
-
-    console.log("\nThe following log is an expected error log:\n=======================\n")
-
-    try {
-        let _ = FheUint8.encrypt_with_client_key(clear, clientKey);
-        assert(false);
-    } catch (e) {
-        assert(true);
-    }
-});
-
 test('hlapi_key_gen_big', (t) => {
     let config = TfheConfigBuilder.all_disabled()
         .enable_default_integers()
