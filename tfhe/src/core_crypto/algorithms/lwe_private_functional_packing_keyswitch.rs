902755c33c262b56fcef0ac02da3deef8742e511
David Testé <david.teste@fratris.fr>
2024-02-29 10:58:36 +0100

    feat(core_crypto): add parallelized pfpks with lwe ciphertext list

--- tfhe/src/core_crypto/algorithms/lwe_private_functional_packing_keyswitch.rs
+++ tfhe/src/core_crypto/algorithms/lwe_private_functional_packing_keyswitch.rs
@@ -161,8 +161,8 @@ pub fn par_private_functional_keyswitch_lwe_ciphertext_into_glwe_ciphertext_with
         "This operation currently only supports native moduli"
     );
 
-    assert!(
-        thread_count.0 != 0,
+    assert_ne!(
+        thread_count.0, 0,
         "Got thread_count == 0, this is not supported"
     );
 
@@ -247,31 +247,42 @@ pub fn private_functional_keyswitch_lwe_ciphertext_list_and_pack_in_glwe_ciphert
     OutputCont,
 >(
     lwe_pfpksk: &LwePrivateFunctionalPackingKeyswitchKey<KeyCont>,
-    output: &mut GlweCiphertext<OutputCont>,
-    input: &LweCiphertextList<InputCont>,
+    output_glwe_ciphertext: &mut GlweCiphertext<OutputCont>,
+    input_lwe_ciphertext_list: &LweCiphertextList<InputCont>,
 ) where
     Scalar: UnsignedTorus,
     KeyCont: Container<Element = Scalar>,
     InputCont: Container<Element = Scalar>,
     OutputCont: ContainerMut<Element = Scalar>,
 {
-    assert_eq!(lwe_pfpksk.ciphertext_modulus(), output.ciphertext_modulus());
-    assert_eq!(output.ciphertext_modulus(), input.ciphertext_modulus());
+    assert_eq!(
+        lwe_pfpksk.ciphertext_modulus(),
+        output_glwe_ciphertext.ciphertext_modulus()
+    );
+    assert_eq!(
+        output_glwe_ciphertext.ciphertext_modulus(),
+        input_lwe_ciphertext_list.ciphertext_modulus()
+    );
     assert!(
-        input.ciphertext_modulus().is_native_modulus(),
+        input_lwe_ciphertext_list
+            .ciphertext_modulus()
+            .is_native_modulus(),
         "This operation currently only supports native moduli"
     );
 
-    assert!(input.lwe_ciphertext_count().0 <= output.polynomial_size().0);
-    output.as_mut().fill(Scalar::ZERO);
+    assert!(
+        input_lwe_ciphertext_list.lwe_ciphertext_count().0
+            <= output_glwe_ciphertext.polynomial_size().0
+    );
+    output_glwe_ciphertext.as_mut().fill(Scalar::ZERO);
     let mut buffer = GlweCiphertext::new(
         Scalar::ZERO,
-        output.glwe_size(),
-        output.polynomial_size(),
-        output.ciphertext_modulus(),
+        output_glwe_ciphertext.glwe_size(),
+        output_glwe_ciphertext.polynomial_size(),
+        output_glwe_ciphertext.ciphertext_modulus(),
     );
     // for each ciphertext, call mono_key_switch
-    for (degree, input_ciphertext) in input.iter().enumerate() {
+    for (degree, input_ciphertext) in input_lwe_ciphertext_list.iter().enumerate() {
         private_functional_keyswitch_lwe_ciphertext_into_glwe_ciphertext(
             lwe_pfpksk,
             &mut buffer,
@@ -283,6 +294,175 @@ pub fn private_functional_keyswitch_lwe_ciphertext_list_and_pack_in_glwe_ciphert
             .for_each(|mut poly| {
                 polynomial_wrapping_monic_monomial_mul_assign(&mut poly, MonomialDegree(degree));
             });
-        slice_wrapping_add_assign(output.as_mut(), buffer.as_ref());
+        slice_wrapping_add_assign(output_glwe_ciphertext.as_mut(), buffer.as_ref());
     }
+}
+
+/// Parallel variant of
+/// [`private_functional_keyswitch_lwe_ciphertext_list_and_pack_in_glwe_ciphertext`].
+///
+/// This will use all threads available in the current rayon thread pool.
+pub fn par_private_functional_keyswitch_lwe_ciphertext_list_and_pack_in_glwe_ciphertext<
+    Scalar,
+    KeyCont,
+    InputCont,
+    OutputCont,
+>(
+    lwe_pfpksk: &LwePrivateFunctionalPackingKeyswitchKey<KeyCont>,
+    output_glwe_ciphertext: &mut GlweCiphertext<OutputCont>,
+    input_lwe_ciphertext_list: &LweCiphertextList<InputCont>,
+) where
+    Scalar: UnsignedTorus + Send + Sync,
+    KeyCont: Container<Element = Scalar> + Sync,
+    InputCont: Container<Element = Scalar>,
+    OutputCont: ContainerMut<Element = Scalar>,
+{
+    let thread_count = ThreadCount(rayon::current_num_threads());
+    par_private_functional_keyswitch_lwe_ciphertext_list_and_pack_in_glwe_ciphertext_with_thread_count(
+        lwe_pfpksk,
+        output_glwe_ciphertext,
+        input_lwe_ciphertext_list,
+        thread_count,
+    );
+}
+
+/// Parallel variant of
+/// [`private_functional_keyswitch_lwe_ciphertext_list_and_pack_in_glwe_ciphertext`].
+///
+/// This will try to use `thread_count` threads for the computation, if this number is bigger than
+/// the available number of threads in the current rayon thread pool then only the number of
+/// available threads will be used. Note that `thread_count` cannot be 0.
+pub fn par_private_functional_keyswitch_lwe_ciphertext_list_and_pack_in_glwe_ciphertext_with_thread_count<
+    Scalar,
+    KeyCont,
+    InputCont,
+    OutputCont,
+>(
+    lwe_pfpksk: &LwePrivateFunctionalPackingKeyswitchKey<KeyCont>,
+    output_glwe_ciphertext: &mut GlweCiphertext<OutputCont>,
+    input_lwe_ciphertext_list: &LweCiphertextList<InputCont>,
+    thread_count: ThreadCount,
+) where
+    Scalar: UnsignedTorus + Send + Sync,
+    KeyCont: Container<Element = Scalar> + Sync,
+    InputCont: Container<Element = Scalar>,
+    OutputCont: ContainerMut<Element = Scalar>,
+{
+    assert_eq!(
+        lwe_pfpksk.input_key_lwe_dimension(),
+        input_lwe_ciphertext_list.lwe_size().to_lwe_dimension(),
+        "Mismatched input LweDimension. \
+        LwePackingKeyswitchKey input LweDimension: {:?}, input LweCiphertext LweDimension {:?}.",
+        lwe_pfpksk.input_key_lwe_dimension(),
+        input_lwe_ciphertext_list.lwe_size().to_lwe_dimension()
+    );
+    assert_eq!(
+        lwe_pfpksk.output_key_glwe_dimension(),
+        output_glwe_ciphertext.glwe_size().to_glwe_dimension(),
+        "Mismatched output GlweDimension. \
+        LwePackingKeyswitchKey output GlweDimension: {:?}, \
+        output GlweCiphertext GlweDimension {:?}.",
+        lwe_pfpksk.output_key_glwe_dimension(),
+        output_glwe_ciphertext.glwe_size().to_glwe_dimension()
+    );
+    assert_eq!(
+        lwe_pfpksk.output_polynomial_size(),
+        output_glwe_ciphertext.polynomial_size(),
+        "Mismatched output PolynomialSize. \
+        LwePackingKeyswitchKey output PolynomialSize: {:?}, \
+        output GlweCiphertext PolynomialSize {:?}.",
+        lwe_pfpksk.output_polynomial_size(),
+        output_glwe_ciphertext.polynomial_size()
+    );
+
+    assert_eq!(
+        lwe_pfpksk.ciphertext_modulus(),
+        input_lwe_ciphertext_list.ciphertext_modulus(),
+        "Mismatched CiphertextModulus. \
+        LwePackingKeyswitchKey CiphertextModulus: {:?}, input LweCiphertext CiphertextModulus {:?}.",
+        lwe_pfpksk.ciphertext_modulus(),
+        input_lwe_ciphertext_list.ciphertext_modulus()
+    );
+    assert_eq!(
+        lwe_pfpksk.ciphertext_modulus(),
+        output_glwe_ciphertext.ciphertext_modulus(),
+        "Mismatched CiphertextModulus. \
+        LwePackingKeyswitchKey CiphertextModulus: {:?}, \
+        output LweCiphertext CiphertextModulus {:?}.",
+        lwe_pfpksk.ciphertext_modulus(),
+        output_glwe_ciphertext.ciphertext_modulus()
+    );
+    assert!(
+        input_lwe_ciphertext_list
+            .ciphertext_modulus()
+            .is_compatible_with_native_modulus(),
+        "This operation currently only supports power of 2 moduli"
+    );
+    assert!(
+        input_lwe_ciphertext_list.lwe_ciphertext_count().0
+            <= output_glwe_ciphertext.polynomial_size().0
+    );
+    assert_ne!(
+        thread_count.0, 0,
+        "Got thread_count == 0, this is not supported"
+    );
+
+    let output_glwe_size = output_glwe_ciphertext.glwe_size();
+    let output_polynomial_size = output_glwe_ciphertext.polynomial_size();
+    let output_ciphertext_modulus = output_glwe_ciphertext.ciphertext_modulus();
+
+    // Don't go above the current number of threads
+    let thread_count = thread_count.0.min(rayon::current_num_threads());
+    let mut intermediate_buffers = Vec::with_capacity(thread_count);
+
+    // Smallest chunk_size such that thread_count * chunk_size >= input_lwe_size
+    let chunk_size = input_lwe_ciphertext_list
+        .lwe_ciphertext_count()
+        .0
+        .div_ceil(thread_count);
+
+    // for each ciphertext, call mono_key_switch
+    input_lwe_ciphertext_list
+        .par_chunks(chunk_size)
+        .enumerate()
+        .map(|(chunk_idx, input_ciphertext_list_chunk)| {
+            let mut buffer = GlweCiphertext::new(
+                Scalar::ZERO,
+                output_glwe_size,
+                output_polynomial_size,
+                output_ciphertext_modulus,
+            );
+
+            private_functional_keyswitch_lwe_ciphertext_list_and_pack_in_glwe_ciphertext(
+                lwe_pfpksk,
+                &mut buffer,
+                &input_ciphertext_list_chunk,
+            );
+
+            // Rotate to put the ciphertexts in the right slot
+            buffer
+                .as_mut_polynomial_list()
+                .iter_mut()
+                .for_each(|mut poly| {
+                    polynomial_wrapping_monic_monomial_mul_assign(
+                        &mut poly,
+                        MonomialDegree(chunk_idx * chunk_size),
+                    );
+                });
+
+            buffer
+        })
+        .collect_into_vec(&mut intermediate_buffers);
+
+    let result = intermediate_buffers
+        .par_iter_mut()
+        .reduce_with(|lhs, rhs| {
+            slice_wrapping_add_assign(lhs.as_mut(), rhs.as_ref());
+            lhs
+        })
+        .unwrap();
+
+    output_glwe_ciphertext
+        .as_mut()
+        .copy_from_slice(result.as_ref());
 }
--- tfhe/src/core_crypto/algorithms/test/lwe_private_functional_packing_keyswitch.rs
+++ tfhe/src/core_crypto/algorithms/test/lwe_private_functional_packing_keyswitch.rs
@@ -9,6 +9,7 @@ fn test_parallel_pfpks_equivalence<Scalar: UnsignedTorus + Send + Sync>(
     ciphertext_modulus: CiphertextModulus<Scalar>,
 ) {
     // Realistic sizes
+    #[cfg(not(tarpaulin))]
     {
         let decomp_base_log = DecompositionBaseLog(15);
         let decomp_level_count = DecompositionLevelCount(2);
@@ -79,23 +80,17 @@ fn test_parallel_pfpks_equivalence<Scalar: UnsignedTorus + Send + Sync>(
                 ciphertext_modulus,
             );
 
-            let start_serial = std::time::Instant::now();
             private_functional_keyswitch_lwe_ciphertext_into_glwe_ciphertext(
                 &lwe_pfpksk,
                 &mut output_glwe_serial,
                 &random_lwe,
             );
-            let elapsed_serial = start_serial.elapsed();
-            println!("serial:{elapsed_serial:?}");
 
-            let start_parallel = std::time::Instant::now();
             par_private_functional_keyswitch_lwe_ciphertext_into_glwe_ciphertext(
                 &lwe_pfpksk,
                 &mut output_glwe_parallel,
                 &random_lwe,
             );
-            let elapsed_parallel = start_parallel.elapsed();
-            println!("parallel:{elapsed_parallel:?}");
 
             assert_eq!(output_glwe_serial, output_glwe_parallel);
         }
@@ -207,4 +202,207 @@ fn test_parallel_pfpks_equivalence_u32_native_mod() {
 #[test]
 fn test_parallel_pfpks_equivalence_u64_native_mod() {
     test_parallel_pfpks_equivalence::<u64>(CiphertextModulus::new_native());
+}
+
+fn test_parallel_pfpks_lwe_list_equivalence<Scalar: UnsignedTorus + Send + Sync>(
+    ciphertext_modulus: CiphertextModulus<Scalar>,
+) {
+    #[cfg(not(tarpaulin))]
+    // Realistic sizes
+    {
+        let decomp_base_log = DecompositionBaseLog(15);
+        let decomp_level_count = DecompositionLevelCount(2);
+        let input_key_lwe_dimension = LweDimension(769);
+        let output_glwe_dimension = GlweDimension(1);
+        let output_glwe_size = output_glwe_dimension.to_glwe_size();
+        let output_polynomial_size = PolynomialSize(2048);
+        // ~ 2^-20
+        let std_dev = StandardDev(0.0000006791658447437413);
+
+        let lwe_ciphertext_count = LweCiphertextCount(320); // 32 blocks 2_2 times 10 (thus 10 64bits integer)
+
+        let mut rsc = TestResources::new();
+
+        let mut lwe_pfpksk = LwePrivateFunctionalPackingKeyswitchKey::new(
+            Scalar::ZERO,
+            decomp_base_log,
+            decomp_level_count,
+            input_key_lwe_dimension,
+            output_glwe_size,
+            output_polynomial_size,
+            ciphertext_modulus,
+        );
+
+        for _ in 0..NB_TESTS {
+            let input_lwe_secret_key = allocate_and_generate_new_binary_lwe_secret_key(
+                input_key_lwe_dimension,
+                &mut rsc.secret_random_generator,
+            );
+
+            let output_glwe_secret_key = allocate_and_generate_new_binary_glwe_secret_key(
+                output_glwe_dimension,
+                output_polynomial_size,
+                &mut rsc.secret_random_generator,
+            );
+
+            let mut polynomial = Polynomial::new(Scalar::ZERO, output_polynomial_size);
+            rsc.encryption_random_generator
+                .fill_slice_with_random_uniform_mask(polynomial.as_mut());
+
+            par_generate_lwe_private_functional_packing_keyswitch_key(
+                &input_lwe_secret_key,
+                &output_glwe_secret_key,
+                &mut lwe_pfpksk,
+                std_dev,
+                &mut rsc.encryption_random_generator,
+                UnsignedInteger::wrapping_neg,
+                &polynomial,
+            );
+
+            let mut lwe_list = LweCiphertextList::new(
+                Scalar::ZERO,
+                input_key_lwe_dimension.to_lwe_size(),
+                lwe_ciphertext_count,
+                ciphertext_modulus,
+            );
+            rsc.encryption_random_generator
+                .fill_slice_with_random_uniform_mask(lwe_list.as_mut());
+
+            let mut output_glwe_serial = GlweCiphertext::new(
+                Scalar::ZERO,
+                output_glwe_size,
+                output_polynomial_size,
+                ciphertext_modulus,
+            );
+
+            let mut output_glwe_parallel = GlweCiphertext::new(
+                Scalar::ZERO,
+                output_glwe_size,
+                output_polynomial_size,
+                ciphertext_modulus,
+            );
+
+            private_functional_keyswitch_lwe_ciphertext_list_and_pack_in_glwe_ciphertext(
+                &lwe_pfpksk,
+                &mut output_glwe_serial,
+                &lwe_list,
+            );
+
+            par_private_functional_keyswitch_lwe_ciphertext_list_and_pack_in_glwe_ciphertext(
+                &lwe_pfpksk,
+                &mut output_glwe_parallel,
+                &lwe_list,
+            );
+
+            assert_eq!(output_glwe_serial, output_glwe_parallel);
+        }
+    }
+
+    // Small sizes
+    {
+        for _ in 0..NB_TESTS {
+            let decomp_base_log = DecompositionBaseLog(
+                crate::core_crypto::commons::test_tools::random_usize_between(2..5),
+            );
+            let decomp_level_count = DecompositionLevelCount(
+                crate::core_crypto::commons::test_tools::random_usize_between(2..5),
+            );
+            let input_key_lwe_dimension =
+                LweDimension(crate::core_crypto::commons::test_tools::random_usize_between(5..10));
+            let output_glwe_dimension =
+                GlweDimension(crate::core_crypto::commons::test_tools::random_usize_between(5..10));
+            let output_glwe_size = output_glwe_dimension.to_glwe_size();
+            let output_polynomial_size = PolynomialSize(
+                crate::core_crypto::commons::test_tools::random_usize_between(5..10),
+            );
+            // ~ 2^-20
+            let std_dev = StandardDev(0.0000006791658447437413);
+
+            let mut rsc = TestResources::new();
+
+            let mut lwe_pfpksk = LwePrivateFunctionalPackingKeyswitchKey::new(
+                Scalar::ZERO,
+                decomp_base_log,
+                decomp_level_count,
+                input_key_lwe_dimension,
+                output_glwe_size,
+                output_polynomial_size,
+                ciphertext_modulus,
+            );
+
+            for _ in 0..NB_TESTS {
+                let input_lwe_secret_key = allocate_and_generate_new_binary_lwe_secret_key(
+                    input_key_lwe_dimension,
+                    &mut rsc.secret_random_generator,
+                );
+
+                let output_glwe_secret_key = allocate_and_generate_new_binary_glwe_secret_key(
+                    output_glwe_dimension,
+                    output_polynomial_size,
+                    &mut rsc.secret_random_generator,
+                );
+
+                let mut polynomial = Polynomial::new(Scalar::ZERO, output_polynomial_size);
+                rsc.encryption_random_generator
+                    .fill_slice_with_random_uniform_mask(polynomial.as_mut());
+
+                par_generate_lwe_private_functional_packing_keyswitch_key(
+                    &input_lwe_secret_key,
+                    &output_glwe_secret_key,
+                    &mut lwe_pfpksk,
+                    std_dev,
+                    &mut rsc.encryption_random_generator,
+                    UnsignedInteger::wrapping_neg,
+                    &polynomial,
+                );
+
+                let mut lwe_list = LweCiphertextList::new(
+                    Scalar::ZERO,
+                    input_key_lwe_dimension.to_lwe_size(),
+                    LweCiphertextCount(output_polynomial_size.0),
+                    ciphertext_modulus,
+                );
+                rsc.encryption_random_generator
+                    .fill_slice_with_random_uniform_mask(lwe_list.as_mut());
+
+                let mut output_glwe_serial = GlweCiphertext::new(
+                    Scalar::ZERO,
+                    output_glwe_size,
+                    output_polynomial_size,
+                    ciphertext_modulus,
+                );
+
+                let mut output_glwe_parallel = GlweCiphertext::new(
+                    Scalar::ZERO,
+                    output_glwe_size,
+                    output_polynomial_size,
+                    ciphertext_modulus,
+                );
+
+                private_functional_keyswitch_lwe_ciphertext_list_and_pack_in_glwe_ciphertext(
+                    &lwe_pfpksk,
+                    &mut output_glwe_serial,
+                    &lwe_list,
+                );
+
+                par_private_functional_keyswitch_lwe_ciphertext_list_and_pack_in_glwe_ciphertext(
+                    &lwe_pfpksk,
+                    &mut output_glwe_parallel,
+                    &lwe_list,
+                );
+
+                assert_eq!(output_glwe_serial, output_glwe_parallel);
+            }
+        }
+    }
+}
+
+#[test]
+fn test_parallel_pfpks_lwe_list_equivalence_u32_native_mod() {
+    test_parallel_pfpks_lwe_list_equivalence::<u32>(CiphertextModulus::new_native());
+}
+
+#[test]
+fn test_parallel_pfpks_lwe_list_equivalence_u64_native_mod() {
+    test_parallel_pfpks_lwe_list_equivalence::<u64>(CiphertextModulus::new_native());
 }
