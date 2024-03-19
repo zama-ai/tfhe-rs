f911af6e1875c7996532dadcaa6a1fbedf2dd49e
Mayeul@Zama <69792125+mayeul-zama@users.noreply.github.com>
2024-03-19 11:49:27 +0100

    chore(c_api): remove useless feature flags

--- tfhe/src/c_api/high_level_api/mod.rs
+++ tfhe/src/c_api/high_level_api/mod.rs
@@ -3,15 +3,10 @@ mod utils;
 #[cfg(feature = "boolean")]
 pub mod booleans;
 pub mod config;
-#[cfg(feature = "integer")]
 pub mod i128;
-#[cfg(feature = "integer")]
 pub mod i256;
-#[cfg(feature = "integer")]
 pub mod integers;
 pub mod keys;
 mod threading;
-#[cfg(feature = "integer")]
 pub mod u128;
-#[cfg(feature = "integer")]
 pub mod u256;
