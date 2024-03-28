4225ee0b869522f8f6e8d54316eac577e36de6d6
Arthur Meyre <arthur.meyre@zama.ai>
2024-03-28 11:06:09 +0100

    test(core): add stair KS tests
    - StairKS 4 parameters were optimized for norm2 = 2^4, we don't apply the
    dot product, so keeping the loops is fine as the noise grows less

--- tfhe/src/core_crypto/algorithms/test/mod.rs
+++ tfhe/src/core_crypto/algorithms/test/mod.rs
@@ -1,11 +1,11 @@
 pub mod params;
 pub(crate) use params::*;
 
-pub use super::misc::check_encrypted_content_respects_mod;
-use crate::core_crypto::algorithms::misc::divide_round;
-use crate::core_crypto::keycache::KeyCacheAccess;
-use crate::core_crypto::prelude::*;
-use std::fmt::Debug;
+pub(crate) use super::misc::check_encrypted_content_respects_mod;
+pub(crate) use crate::core_crypto::algorithms::misc::divide_round;
+pub(crate) use crate::core_crypto::keycache::KeyCacheAccess;
+pub(crate) use crate::core_crypto::prelude::*;
+pub(crate) use std::fmt::Debug;
 
 mod ggsw_encryption;
 mod glwe_encryption;
--- tfhe/src/core_crypto/experimental/algorithms/mod.rs
+++ tfhe/src/core_crypto/experimental/algorithms/mod.rs
@@ -1,7 +1,12 @@
 pub mod lwe_shrinking_keyswitch;
 pub mod lwe_shrinking_keyswitch_key_generation;
+pub mod partial_glwe_secret_key_generation;
 pub mod shared_lwe_secret_key_generation;
 
 pub use lwe_shrinking_keyswitch::*;
 pub use lwe_shrinking_keyswitch_key_generation::*;
+pub use partial_glwe_secret_key_generation::*;
 pub use shared_lwe_secret_key_generation::*;
+
+#[cfg(test)]
+mod test;
