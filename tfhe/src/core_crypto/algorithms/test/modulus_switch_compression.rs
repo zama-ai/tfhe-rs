b6bc8e9b592093e710699ec2c6877dde68b4083f
Mayeul@Zama <69792125+mayeul-zama@users.noreply.github.com>
2024-05-02 13:15:16 +0200

    feat(core): add ms compression multi bit

--- tfhe/src/core_crypto/algorithms/test/modulus_switch_compression.rs
+++ tfhe/src/core_crypto/algorithms/test/modulus_switch_compression.rs
@@ -1,5 +1,6 @@
 use super::*;
-use crate::core_crypto::prelude::compressed_modulus_switched_lwe_ciphertext::CompressedModulusSwitchedLweCiphertext;
+use crate::core_crypto::fft_impl::common::modulus_switch;
+use itertools::Itertools;
 
 #[cfg(not(tarpaulin))]
 const NB_TESTS: usize = 10;
@@ -59,6 +60,81 @@ fn encryption_ms_decryption<Scalar: UnsignedTorus + Sync + Send>(
         // not yield higher coverage
         #[cfg(tarpaulin)]
         break;
+    }
+}
+
+fn assert_ms_compression<Scalar: UnsignedTorus + CastInto<usize> + CastFrom<usize>>(
+    ct: &LweCiphertext<Vec<Scalar>>,
+    log_modulus: CiphertextModulusLog,
+) {
+    let a = CompressedModulusSwitchedLweCiphertext::compress(ct, log_modulus);
+
+    let b = a.extract();
+
+    for (i, j) in ct.as_ref().iter().zip_eq(b.as_ref().iter()) {
+        assert_eq!(modulus_switch(*i, log_modulus) << (64 - log_modulus.0), *j);
+    }
+}
+
+fn assert_ms_multi_bit_compression<Scalar: UnsignedTorus + CastInto<usize> + CastFrom<usize>>(
+    ct: &LweCiphertext<Vec<Scalar>>,
+    log_modulus: CiphertextModulusLog,
+    grouping_factor: LweBskGroupingFactor,
+) {
+    let a = StandardMultiBitModulusSwitchedCt {
+        input: ct,
+        grouping_factor,
+        log_modulus,
+    };
+
+    let b =
+        CompressedModulusSwitchedMultiBitLweCiphertext::compress(ct, log_modulus, grouping_factor);
+
+    let c = b.extract();
+
+    assert_eq!(
+        a.switched_modulus_input_lwe_body(),
+        c.switched_modulus_input_lwe_body()
+    );
+
+    for i in 0..ct.lwe_size().to_lwe_dimension().0 / grouping_factor.0 {
+        for (j, k) in a
+            .switched_modulus_input_mask_per_group(i)
+            .zip_eq(c.switched_modulus_input_mask_per_group(i))
+        {
+            assert_eq!(j, k);
+        }
+    }
+}
+
+#[test]
+fn test_ms_with_packing() {
+    for ciphertext_modulus in [
+        CiphertextModulus::new(1 << 63),
+        CiphertextModulus::new_native(),
+    ] {
+        for lwe_dimension in (10..1025).map(LweDimension) {
+            for log_modulus in (10..15).map(CiphertextModulusLog) {
+                let mut lwe_ciphertext_in =
+                    LweCiphertextOwned::new(0_u64, lwe_dimension.to_lwe_size(), ciphertext_modulus);
+
+                for j in lwe_ciphertext_in.as_mut() {
+                    *j = rand::random();
+                }
+
+                assert_ms_compression(&lwe_ciphertext_in, log_modulus);
+
+                for grouping_factor in (1..6).map(LweBskGroupingFactor) {
+                    if lwe_dimension.0 % grouping_factor.0 == 0 {
+                        assert_ms_multi_bit_compression(
+                            &lwe_ciphertext_in,
+                            log_modulus,
+                            grouping_factor,
+                        );
+                    }
+                }
+            }
+        }
     }
 }
 
