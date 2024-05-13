44683026307bca814e283704a39a774cca72d625
Mayeul@Zama <69792125+mayeul-zama@users.noreply.github.com>
2024-05-13 12:03:33 +0200

    feat(integer): improve compression

--- tfhe/src/integer/server_key/radix_parallel/modulus_switch_compression.rs
+++ tfhe/src/integer/server_key/radix_parallel/modulus_switch_compression.rs
@@ -1,8 +1,10 @@
 use crate::integer::ciphertext::{
     BaseRadixCiphertext, BaseSignedRadixCiphertext, CompressedModulusSwitchedRadixCiphertext,
+    CompressedModulusSwitchedRadixCiphertextGeneric,
     CompressedModulusSwitchedSignedRadixCiphertext,
 };
 use crate::integer::{RadixCiphertext, ServerKey, SignedRadixCiphertext};
+use crate::shortint::Ciphertext;
 use rayon::prelude::*;
 
 impl ServerKey {
@@ -13,13 +15,9 @@ impl ServerKey {
         &self,
         ct: &RadixCiphertext,
     ) -> CompressedModulusSwitchedRadixCiphertext {
-        let blocks = ct
-            .blocks
-            .par_iter()
-            .map(|a| self.key.switch_modulus_and_compress(a))
-            .collect();
-
-        BaseRadixCiphertext { blocks }
+        CompressedModulusSwitchedRadixCiphertext(
+            self.switch_modulus_and_compress_generic_parallelized(&ct.blocks),
+        )
     }
     /// Decompresses a compressed ciphertext
     /// This operation costs a PBS
@@ -29,13 +27,9 @@ impl ServerKey {
         &self,
         compressed_ct: &CompressedModulusSwitchedRadixCiphertext,
     ) -> RadixCiphertext {
-        let blocks = compressed_ct
-            .blocks
-            .par_iter()
-            .map(|a| self.key.decompress(a))
-            .collect();
-
-        BaseRadixCiphertext { blocks }
+        BaseRadixCiphertext {
+            blocks: self.decompress_generic_parallelized(&compressed_ct.0),
+        }
     }
 
     /// Compresses a signed ciphertext to have a smaller serialization size
@@ -45,13 +39,9 @@ impl ServerKey {
         &self,
         ct: &SignedRadixCiphertext,
     ) -> CompressedModulusSwitchedSignedRadixCiphertext {
-        let blocks = ct
-            .blocks
-            .par_iter()
-            .map(|a| self.key.switch_modulus_and_compress(a))
-            .collect();
-
-        BaseSignedRadixCiphertext { blocks }
+        CompressedModulusSwitchedSignedRadixCiphertext(
+            self.switch_modulus_and_compress_generic_parallelized(&ct.blocks),
+        )
     }
     /// Decompresses a signed compressed ciphertext
     /// This operation costs a PBS
@@ -61,12 +51,94 @@ impl ServerKey {
         &self,
         compressed_ct: &CompressedModulusSwitchedSignedRadixCiphertext,
     ) -> SignedRadixCiphertext {
-        let blocks = compressed_ct
-            .blocks
-            .par_iter()
-            .map(|a| self.key.decompress(a))
+        BaseSignedRadixCiphertext {
+            blocks: self.decompress_generic_parallelized(&compressed_ct.0),
+        }
+    }
+
+    #[allow(clippy::int_plus_one)]
+    fn switch_modulus_and_compress_generic_parallelized(
+        &self,
+        blocks: &[Ciphertext],
+    ) -> CompressedModulusSwitchedRadixCiphertextGeneric {
+        assert!(
+            self.message_modulus().0 <= self.carry_modulus().0,
+            "Compression does not support message_modulus > carry_modulus"
+        );
+        assert!(
+            self.key.max_noise_level.get() >= self.message_modulus().0 + 1,
+            "Compression does not support max_noise_level < message_modulus + 1"
+        );
+
+        let paired_blocks;
+
+        let last_block;
+
+        let len = blocks.len();
+
+        if len % 2 == 0 {
+            paired_blocks = blocks;
+            last_block = None;
+        } else {
+            paired_blocks = &blocks[..len - 1];
+            last_block = Some(blocks.last().unwrap());
+        }
+
+        let paired_blocks = paired_blocks
+            .par_chunks_exact(2)
+            .map(|pair| {
+                let mut packed = pair[0].clone();
+
+                let scaled = self
+                    .key
+                    .unchecked_scalar_mul(&pair[1], self.message_modulus().0 as u8);
+
+                self.key.unchecked_add_assign(&mut packed, &scaled);
+
+                self.key.switch_modulus_and_compress(&packed)
+            })
             .collect();
 
-        BaseSignedRadixCiphertext { blocks }
+        let last_block = last_block.map(|a| self.key.switch_modulus_and_compress(a));
+
+        CompressedModulusSwitchedRadixCiphertextGeneric {
+            paired_blocks,
+            last_block,
+        }
+    }
+
+    fn decompress_generic_parallelized(
+        &self,
+        compressed_ct: &CompressedModulusSwitchedRadixCiphertextGeneric,
+    ) -> Vec<Ciphertext> {
+        let message_extract = self
+            .key
+            .generate_lookup_table(|x| x % self.message_modulus().0 as u64);
+
+        let carry_extract = self
+            .key
+            .generate_lookup_table(|x| x / self.message_modulus().0 as u64);
+
+        let mut blocks: Vec<Ciphertext> = compressed_ct
+            .paired_blocks
+            .par_iter()
+            .flat_map(|a| {
+                [
+                    self.key
+                        .decompress_and_apply_lookup_table(a, &message_extract),
+                    self.key
+                        .decompress_and_apply_lookup_table(a, &carry_extract),
+                ]
+            })
+            .collect();
+
+        if let Some(last_block) = compressed_ct.last_block.as_ref() {
+            blocks.push(
+                self.key
+                    .decompress_and_apply_lookup_table(last_block, &message_extract),
+            );
+        }
+
+        blocks
     }
 }
