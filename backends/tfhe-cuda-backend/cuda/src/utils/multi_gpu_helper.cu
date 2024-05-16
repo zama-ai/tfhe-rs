73eaef71c71ea7baa835b53708d4a80c1d31e8d8
Agnes Leroy <agnes.leroy@zama.ai>
2024-05-16 10:50:25 +0200

    chore(gpu): simplify multi-gpu setup

--- backends/tfhe-cuda-backend/cuda/src/utils/multi_gpu_helper.cu
+++ backends/tfhe-cuda-backend/cuda/src/utils/multi_gpu_helper.cu
@@ -6,66 +6,50 @@ int cuda_setup_multi_gpu() {
   int num_gpus = cuda_get_number_of_gpus();
   if (num_gpus == 0)
     PANIC("GPU error: the number of GPUs should be > 0.")
+  int num_used_gpus = 1;
   if (num_gpus > 1) {
-    int can_access_peer_ij;
-    int can_access_peer_ji;
+    int has_peer_access_to_device_0;
     for (int i = 1; i < num_gpus; i++) {
-      check_cuda_error(cudaDeviceCanAccessPeer(&can_access_peer_ij, i, 0));
-      check_cuda_error(cudaDeviceCanAccessPeer(&can_access_peer_ji, 0, i));
-      if (can_access_peer_ij && can_access_peer_ji) {
+      check_cuda_error(
+          cudaDeviceCanAccessPeer(&has_peer_access_to_device_0, i, 0));
+      if (has_peer_access_to_device_0) {
         cudaMemPool_t mempool;
         cudaMemAccessDesc desc = {};
-
         // Enable P2P Access and mempool access
         check_cuda_error(cudaSetDevice(i));
-        check_cuda_error(cudaDeviceGetDefaultMemPool(&mempool, i));
-        desc.location.type = cudaMemLocationTypeDevice;
-        desc.location.id = 0;
-        desc.flags = cudaMemAccessFlagsProtReadWrite;
-        check_cuda_error(
-            cudaMemPoolSetAccess(mempool, &desc, 1 /* numDescs */));
         check_cuda_error_ignore_specific(cudaDeviceEnablePeerAccess(0, 0),
                                          cudaErrorPeerAccessAlreadyEnabled);
 
-        check_cuda_error(cudaSetDevice(0));
         check_cuda_error(cudaDeviceGetDefaultMemPool(&mempool, 0));
         desc.location.type = cudaMemLocationTypeDevice;
         desc.location.id = i;
         desc.flags = cudaMemAccessFlagsProtReadWrite;
         check_cuda_error(
             cudaMemPoolSetAccess(mempool, &desc, 1 /* numDescs */));
-        check_cuda_error_ignore_specific(cudaDeviceEnablePeerAccess(i, 0),
-                                         cudaErrorPeerAccessAlreadyEnabled);
 
+        num_used_gpus += 1;
       } else {
-        PANIC("Multi GPU error: all GPUs should have peer access to GPU 0")
+        printf("Number of GPUs with p2p access used: %d\n", num_used_gpus);
+        break;
       }
     }
   }
-  return num_gpus;
+  return num_used_gpus;
 }
 
 void cuda_cleanup_multi_gpu() {
-
   int num_gpus = cuda_get_number_of_gpus();
   if (num_gpus == 0)
     PANIC("GPU error: the number of GPUs should be > 0.")
   if (num_gpus > 1) {
-    int can_access_peer_ij;
-    int can_access_peer_ji;
+    int has_peer_access_to_device_0;
     for (int i = 1; i < num_gpus; i++) {
-      check_cuda_error(cudaDeviceCanAccessPeer(&can_access_peer_ij, i, 0));
-      check_cuda_error(cudaDeviceCanAccessPeer(&can_access_peer_ji, 0, i));
-      if (can_access_peer_ij && can_access_peer_ji) {
+      check_cuda_error(
+          cudaDeviceCanAccessPeer(&has_peer_access_to_device_0, i, 0));
+      if (has_peer_access_to_device_0) {
         //// Disable access to memory pool
         cudaMemPool_t mempool;
-        cudaDeviceGetDefaultMemPool(&mempool, i);
         cudaMemAccessDesc desc = {};
-        desc.location.type = cudaMemLocationTypeDevice;
-        desc.location.id = 0;
-        desc.flags = cudaMemAccessFlagsProtNone;
-        cudaMemPoolSetAccess(mempool, &desc, 1 /* numDescs */);
-
         cudaDeviceGetDefaultMemPool(&mempool, 0);
         desc.location.type = cudaMemLocationTypeDevice;
         desc.location.id = i;
@@ -74,10 +58,8 @@ void cuda_cleanup_multi_gpu() {
         //  Disable P2P Access
         cudaSetDevice(i);
         cudaDeviceDisablePeerAccess(0);
-        cudaSetDevice(0);
-        cudaDeviceDisablePeerAccess(i);
       } else {
-        PANIC("Multi GPU error: all GPUs should have peer access to GPU 0")
+        break;
       }
     }
   }
