e97fdb4c1087b04c5548d9bad9a7881e5d488b64
Daniel Demmler <daniel.demmler@gmail.com>
2024-05-28 13:35:48 +0200

    fix: comment fmt

--- tfhe/src/high_level_api/zk.rs
+++ tfhe/src/high_level_api/zk.rs
@@ -3,7 +3,8 @@ use crate::Config;
 
 impl CompactPkeCrs {
     /// Create a new `CompactPkeCrs` from a `Config` object.
-    /// max_bit_size is the maximum number of bits that can be proven, e.g. 64 for a single FheUint64 or 8 x FheUint8 values.
+    /// max_bit_size is the maximum number of bits that can be proven, e.g. 64 for a single
+    /// FheUint64 or 8 x FheUint8 values.
     pub fn from_config(config: Config, max_bit_size: usize) -> crate::Result<Self> {
         let max_num_message =
             max_bit_size / config.inner.block_parameters.message_modulus().0.ilog2() as usize;
