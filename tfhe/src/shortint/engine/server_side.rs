e62d0f47da88ad6bee7614e31d88bf7336b17aa4
Guillermo Oyarzun <guillermo.oyarzun@zama.ai>
2024-06-14 11:40:37 +0200

    jumping assert for testing purposes

--- tfhe/src/shortint/engine/server_side.rs
+++ tfhe/src/shortint/engine/server_side.rs
@@ -44,6 +44,7 @@ impl ShortintEngine {
         match grouping_factor.0 {
             2 => ThreadCount(5),
             3 => ThreadCount(7),
+            4 => ThreadCount(7), // just to test
             _ => {
                 todo!("Currently shortint only supports grouping factor 2 and 3 for multi bit PBS")
             }
