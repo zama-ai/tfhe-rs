dc7081fd989911449aff1855ac7ef98d02bbf613
Nicolas Sarlin <nicolas.sarlin@zama.ai>
2024-06-17 15:56:59 +0200

    feat(all): add `safe_serialize_versioned`

--- tfhe/src/safe_deserialization.rs
+++ tfhe/src/safe_deserialization.rs
@@ -1,8 +1,11 @@
+use std::borrow::Cow;
+
 use crate::conformance::ParameterSetConformant;
 use crate::named::Named;
 use bincode::Options;
 use serde::de::DeserializeOwned;
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
+use tfhe_versionable::{Unversionize, Versionize};
 
 // The `SERIALIZATION_VERSION` is serialized along objects serialized with `safe_serialize`.
 // This `SERIALIZATION_VERSION` should be changed on each release where any object serialization
@@ -15,12 +18,77 @@ use serde::Serialize;
 // deserialization error or worse, a garbage object.
 const SERIALIZATION_VERSION: &str = "0.3";
 
+/// Tells if this serialized object is versioned or not
+#[derive(Serialize, Deserialize, PartialEq, Eq)]
+enum SerializationMode {
+    /// Serialize with type versioning for backward compatibility
+    Versioned,
+    /// Directly serialize the type as it is provided
+    Direct,
+}
+
+/// Header with global metadata about the serialized object.
+#[derive(Serialize, Deserialize)]
+struct SerializationHeader {
+    mode: SerializationMode,
+    version: Cow<'static, str>,
+    name: Cow<'static, str>,
+}
+
+impl SerializationHeader {
+    /// Creates a new header for a versioned message
+    fn new_versioned<T: Named>() -> Self {
+        Self {
+            mode: SerializationMode::Versioned,
+            version: Cow::Borrowed(VERSIONING_VERSION),
+            name: Cow::Borrowed(T::NAME),
+        }
+    }
+
+    /// Checks the validity of a versioned message
+    fn check_versioned<T: Named>(&self) -> Result<(), String> {
+        if self.mode != SerializationMode::Versioned {
+            return Err(
+                "On deserialization, expected versioned type but got unversioned one".to_string(),
+            );
+        }
+
+        // Since there is only one "VERSIONING_VERSION", a message with a different value than the
+        // expected one is clearly invalid, so we return an error. In the future, we want to
+        // be able to upgrade it to the new versioning scheme.
+        if self.version != VERSIONING_VERSION {
+            return Err(format!(
+                "On deserialization, expected versioning scheme version {VERSIONING_VERSION}, \
+                got version {}",
+                self.version
+            ));
+        }
+
+        if self.name != T::NAME {
+            return Err(format!(
+                "On deserialization, expected type {}, got type {}",
+                T::NAME,
+                self.name
+            ));
+        }
+
+        Ok(())
+    }
+}
+
+// This is the version of the versioning scheme used to add backward compatibibility on tfhe-rs
+// types. Similar to SERIALIZATION_VERSION, this number should be increased when the versioning
+// scheme is upgraded.
+const VERSIONING_VERSION: &str = "0.1";
+
 // `VERSION_LENGTH_LIMIT` is the maximum `SERIALIZATION_VERSION` size which `safe_deserialization`
 // is going to try to read (it returns an error if it's too big).
 // It helps prevent an attacker passing a very long `SERIALIZATION_VERSION` to exhaust memory.
 const VERSION_LENGTH_LIMIT: u64 = 100;
 
 const TYPE_NAME_LENGTH_LIMIT: u64 = 1000;
+
+const HEADER_LENGTH_LIMIT: u64 = 1000;
 
 /// Serializes an object into a [writer](std::io::Write).
 /// The result contains a version of the serialization and the name of the
@@ -47,6 +115,29 @@ pub fn safe_serialize<T: Serialize + Named>(
     options
         .with_limit(serialized_size_limit)
         .serialize_into(&mut writer, object)?;
+
+    Ok(())
+}
+
+/// Serializes an object into a [writer](std::io::Write) like [`safe_serialize`] does,
+/// but adds versioning information before.
+pub fn safe_serialize_versioned<T: Versionize + Named>(
+    object: &T,
+    mut writer: impl std::io::Write,
+    serialized_size_limit: u64,
+) -> bincode::Result<()> {
+    let options = bincode::DefaultOptions::new()
+        .with_fixint_encoding()
+        .with_limit(0);
+
+    let header = SerializationHeader::new_versioned::<T>();
+    options
+        .with_limit(HEADER_LENGTH_LIMIT)
+        .serialize_into(&mut writer, &header)?;
+
+    options
+        .with_limit(serialized_size_limit)
+        .serialize_into(&mut writer, &object.versionize())?;
 
     Ok(())
 }
@@ -113,16 +204,65 @@ pub fn safe_deserialize_conformant<T: DeserializeOwned + Named + ParameterSetCon
     Ok(deser)
 }
 
+/// Deserializes an object serialized by `safe_serialize_versioned` from a [reader](std::io::Read).
+/// Checks that the serialization version and the name of the
+/// deserialized type are correct.
+/// `serialized_size_limit` is the size limit (in number of byte) of the serialized object
+/// (excluding version and name serialization).
+pub fn safe_deserialize_versioned<T: Unversionize + Named>(
+    mut reader: impl std::io::Read,
+    serialized_size_limit: u64,
+) -> Result<T, String> {
+    let options = bincode::DefaultOptions::new()
+        .with_fixint_encoding()
+        .with_limit(0);
+
+    let deserialized_header: SerializationHeader = options
+        .with_limit(HEADER_LENGTH_LIMIT)
+        .deserialize_from(&mut reader)
+        .map_err(|err| err.to_string())?;
+
+    deserialized_header.check_versioned::<T>()?;
+
+    options
+        .with_limit(serialized_size_limit)
+        .deserialize_from(&mut reader)
+        .map_err(|err| err.to_string())
+        .and_then(|val| T::unversionize(val).map_err(|err| err.to_string()))
+}
+
+/// Deserializes an object with [safe_deserialize] and checks than it is conformant with the given
+/// parameter set
+pub fn safe_deserialize_conformant_versioned<T: Unversionize + Named + ParameterSetConformant>(
+    reader: impl std::io::Read,
+    serialized_size_limit: u64,
+    parameter_set: &T::ParameterSet,
+) -> Result<T, String> {
+    let deser: T = safe_deserialize_versioned(reader, serialized_size_limit)?;
+
+    if !deser.is_conformant(parameter_set) {
+        return Err(format!(
+            "Deserialized object of type {} not conformant with given parameter set",
+            T::NAME
+        ));
+    }
+
+    Ok(deser)
+}
+
 #[cfg(all(test, feature = "shortint"))]
 mod test_shortint {
-    use crate::safe_deserialization::{safe_deserialize_conformant, safe_serialize};
+    use crate::safe_deserialization::{
+        safe_deserialize_conformant, safe_deserialize_conformant_versioned, safe_serialize,
+        safe_serialize_versioned,
+    };
     use crate::shortint::parameters::{
         PARAM_MESSAGE_2_CARRY_2_KS_PBS, PARAM_MESSAGE_3_CARRY_3_KS_PBS,
     };
     use crate::shortint::{gen_keys, Ciphertext};
 
     #[test]
-    fn safe_desererialization_ct() {
+    fn safe_deserialization_ct() {
         let (ck, _sk) = gen_keys(PARAM_MESSAGE_2_CARRY_2_KS_PBS);
 
         let msg = 2_u64;
@@ -150,6 +290,36 @@ mod test_shortint {
         let dec = ck.decrypt(&ct2);
         assert_eq!(msg, dec);
     }
+
+    #[test]
+    fn safe_deserialization_ct_versioned() {
+        let (ck, _sk) = gen_keys(PARAM_MESSAGE_2_CARRY_2_KS_PBS);
+
+        let msg = 2_u64;
+
+        let ct = ck.encrypt(msg);
+
+        let mut buffer = vec![];
+
+        safe_serialize_versioned(&ct, &mut buffer, 1 << 40).unwrap();
+
+        assert!(safe_deserialize_conformant_versioned::<Ciphertext>(
+            buffer.as_slice(),
+            1 << 20,
+            &PARAM_MESSAGE_3_CARRY_3_KS_PBS.to_shortint_conformance_param(),
+        )
+        .is_err());
+
+        let ct2 = safe_deserialize_conformant_versioned(
+            buffer.as_slice(),
+            1 << 20,
+            &PARAM_MESSAGE_2_CARRY_2_KS_PBS.to_shortint_conformance_param(),
+        )
+        .unwrap();
+
+        let dec = ck.decrypt(&ct2);
+        assert_eq!(msg, dec);
+    }
 }
 
 #[cfg(all(test, feature = "integer"))]
@@ -157,7 +327,10 @@ mod test_integer {
     use crate::conformance::ListSizeConstraint;
     use crate::high_level_api::{generate_keys, ConfigBuilder};
     use crate::prelude::{FheDecrypt, FheTryEncrypt};
-    use crate::safe_deserialization::{safe_deserialize_conformant, safe_serialize};
+    use crate::safe_deserialization::{
+        safe_deserialize_conformant, safe_deserialize_conformant_versioned, safe_serialize,
+        safe_serialize_versioned,
+    };
     use crate::shortint::parameters::{
         PARAM_MESSAGE_1_CARRY_1_KS_PBS, PARAM_MESSAGE_2_CARRY_2_KS_PBS,
         PARAM_MESSAGE_3_CARRY_3_KS_PBS,
@@ -166,8 +339,9 @@ mod test_integer {
         CompactFheUint8, CompactFheUint8List, CompactFheUint8ListConformanceParams,
         CompactPublicKey, FheUint8ConformanceParams,
     };
+
     #[test]
-    fn safe_desererialization_ct() {
+    fn safe_deserialization_ct() {
         let (client_key, server_key) = generate_keys(ConfigBuilder::default().build());
 
         let public_key = CompactPublicKey::new(&client_key);
@@ -198,7 +372,41 @@ mod test_integer {
     }
 
     #[test]
-    fn safe_desererialization_ct_list() {
+    fn safe_deserialization_ct_versioned() {
+        let (client_key, server_key) = generate_keys(ConfigBuilder::default().build());
+
+        let public_key = CompactPublicKey::new(&client_key);
+
+        let msg = 27u8;
+
+        let ct = CompactFheUint8::try_encrypt(msg, &public_key).unwrap();
+
+        let mut buffer = vec![];
+
+        safe_serialize_versioned(&ct, &mut buffer, 1 << 40).unwrap();
+
+        let params = FheUint8ConformanceParams::from(PARAM_MESSAGE_1_CARRY_1_KS_PBS);
+        assert!(safe_deserialize_conformant_versioned::<CompactFheUint8>(
+            buffer.as_slice(),
+            1 << 20,
+            &params
+        )
+        .is_err());
+
+        let params = FheUint8ConformanceParams::from(&server_key);
+        let ct2 = safe_deserialize_conformant_versioned::<CompactFheUint8>(
+            buffer.as_slice(),
+            1 << 20,
+            &params,
+        )
+        .unwrap();
+
+        let dec: u8 = ct2.expand().decrypt(&client_key);
+        assert_eq!(msg, dec);
+    }
+
+    #[test]
+    fn safe_deserialization_ct_list() {
         let (client_key, server_key) = generate_keys(ConfigBuilder::default().build());
 
         let public_key = CompactPublicKey::new(&client_key);
