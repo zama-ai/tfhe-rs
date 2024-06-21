e854823233c93ee5c6e1deabfcb9e01230a2a391
Guillermo Oyarzun <guillermo.oyarzun@zama.ai>
2024-06-21 16:30:15 +0200

    refactor(gpu): speedup twiddles reads

--- backends/tfhe-cuda-backend/cuda/src/fft/twiddles.cuh
+++ backends/tfhe-cuda-backend/cuda/src/fft/twiddles.cuh
@@ -2,12 +2,7 @@
 #define GPU_BOOTSTRAP_TWIDDLES_CUH
 
 /*
- * 'negtwiddles' are stored in constant memory for faster access times
- * because of it's limited size, only twiddles for up to 2^12 polynomial size
- * can be stored there, twiddles for 2^13 are stored in device memory
- * 'negtwiddles13'
+ * 'negtwiddles' are stored in device memory to profit caching
  */
-
-extern __constant__ double2 negtwiddles[4096];
-extern __device__ double2 negtwiddles13[4096];
+extern __device__ double2 negtwiddles[8192];
 #endif
