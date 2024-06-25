13b984d554b7eb24305ca3b54b091fe92d32a748
Agnes Leroy <agnes.leroy@zama.ai>
2024-06-25 09:53:55 +0200

    chore(gpu): restrict the number of active gpus

--- backends/tfhe-cuda-backend/cuda/src/utils/helper_multi_gpu.cu
+++ backends/tfhe-cuda-backend/cuda/src/utils/helper_multi_gpu.cu
@@ -56,9 +56,15 @@ int cuda_setup_multi_gpu() {
 }
 
 int get_active_gpu_count(int num_inputs, int gpu_count) {
-  int active_gpu_count = gpu_count;
-  if (gpu_count > num_inputs) {
-    active_gpu_count = num_inputs;
+  int active_gpu_count = 1;
+  int num_sms = 0;
+  check_cuda_error(
+      cudaDeviceGetAttribute(&num_sms, cudaDevAttrMultiProcessorCount, 0));
+  for (uint gpus = gpu_count; gpus > 0; gpus--) {
+    if (num_inputs / gpus >= num_sms) {
+      active_gpu_count = gpus;
+      break;
+    }
   }
   return active_gpu_count;
 }
@@ -72,32 +78,25 @@ int get_gpu_offset(int total_num_inputs, int gpu_index, int gpu_count) {
 
 int get_num_inputs_on_gpu(int total_num_inputs, int gpu_index, int gpu_count) {
 
-  int num_inputs = 0;
-  // If there are fewer inputs than GPUs, not all GPUs are active and GPU 0
-  // handles everything
-  if (gpu_count > total_num_inputs) {
-    if (gpu_index < total_num_inputs) {
-      num_inputs = 1;
-    }
+  int num_inputs;
+  // If there are more inputs than GPUs, all GPUs are active and compute over
+  // a chunk of the total inputs. The chunk size is smaller on the last GPUs.
+  int small_input_num, large_input_num, cutoff;
+  if (total_num_inputs % gpu_count == 0) {
+    small_input_num = total_num_inputs / gpu_count;
+    large_input_num = small_input_num;
+    cutoff = 0;
   } else {
-    // If there are more inputs than GPUs, all GPUs are active and compute over
-    // a chunk of the total inputs. The chunk size is smaller on the last GPUs.
-    int small_input_num, large_input_num, cutoff;
-    if (total_num_inputs % gpu_count == 0) {
-      small_input_num = total_num_inputs / gpu_count;
-      large_input_num = small_input_num;
-      cutoff = 0;
-    } else {
-      int y = ceil((double)total_num_inputs / (double)gpu_count) * gpu_count -
-              total_num_inputs;
-      cutoff = gpu_count - y;
-      small_input_num = total_num_inputs / gpu_count;
-      large_input_num = (int)ceil((double)total_num_inputs / (double)gpu_count);
-    }
-    if (gpu_index < cutoff)
-      num_inputs = large_input_num;
-    else
-      num_inputs = small_input_num;
+    int y = ceil((double)total_num_inputs / (double)gpu_count) * gpu_count -
+            total_num_inputs;
+    cutoff = gpu_count - y;
+    small_input_num = total_num_inputs / gpu_count;
+    large_input_num = (int)ceil((double)total_num_inputs / (double)gpu_count);
   }
+  if (gpu_index < cutoff)
+    num_inputs = large_input_num;
+  else
+    num_inputs = small_input_num;
+
   return num_inputs;
 }
