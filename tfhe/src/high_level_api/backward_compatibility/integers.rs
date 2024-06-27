223dd2a023b3c9095adaa6a5f7829f63d3d72a40
Arthur Meyre <arthur.meyre@zama.ai>
2024-06-27 11:19:51 +0200

    chore(tfhe): bump version to 0.7.1

--- tfhe/src/high_level_api/backward_compatibility/integers.rs
+++ tfhe/src/high_level_api/backward_compatibility/integers.rs
@@ -160,7 +160,7 @@ pub enum CompactFheUintListVersions<Id: FheUintId> {
 
 #[derive(Clone, Versionize)]
 #[versionize(CompactFheIntVersions)]
-#[deprecated(since = "0.7.0", note = "Use CompactCiphertextList instead")]
+#[deprecated(since = "0.7.1", note = "Use CompactCiphertextList instead")]
 pub struct CompactFheInt<Id: FheIntId> {
     list: CompactCiphertextList,
     id: Id,
@@ -190,7 +190,7 @@ where
 
 #[derive(Clone, Versionize)]
 #[versionize(CompactFheIntListVersions)]
-#[deprecated(since = "0.7.0", note = "Use CompactCiphertextList instead")]
+#[deprecated(since = "0.7.1", note = "Use CompactCiphertextList instead")]
 pub struct CompactFheIntList<Id: FheIntId> {
     list: CompactCiphertextList,
     id: Id,
@@ -227,7 +227,7 @@ where
 
 #[derive(Clone, Versionize)]
 #[versionize(CompactFheUintVersions)]
-#[deprecated(since = "0.7.0", note = "Use CompactCiphertextList instead")]
+#[deprecated(since = "0.7.1", note = "Use CompactCiphertextList instead")]
 pub struct CompactFheUint<Id: FheUintId> {
     list: CompactCiphertextList,
     id: Id,
@@ -258,7 +258,7 @@ where
 
 #[derive(Clone, Versionize)]
 #[versionize(CompactFheUintListVersions)]
-#[deprecated(since = "0.7.0", note = "Use CompactCiphertextList instead")]
+#[deprecated(since = "0.7.1", note = "Use CompactCiphertextList instead")]
 pub struct CompactFheUintList<Id: FheUintId> {
     list: CompactCiphertextList,
     id: Id,
