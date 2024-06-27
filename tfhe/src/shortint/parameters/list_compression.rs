a44be90a440dd5883d5df45cb5f3382df2cfddd3
Arthur Meyre <arthur.meyre@zama.ai>
2024-06-27 11:04:31 +0200

    chore(shortint): add gaussian compression parameters

--- tfhe/src/shortint/parameters/list_compression.rs
+++ tfhe/src/shortint/parameters/list_compression.rs
@@ -1,6 +1,6 @@
 use tfhe_versionable::Versionize;
 
-use crate::core_crypto::prelude::{CiphertextModulusLog, LweCiphertextCount};
+use crate::core_crypto::prelude::{CiphertextModulusLog, LweCiphertextCount, StandardDev};
 use crate::shortint::backward_compatibility::parameters::list_compression::CompressionParametersVersions;
 use crate::shortint::parameters::{
     DecompositionBaseLog, DecompositionLevelCount, DynamicDistribution, GlweDimension,
@@ -33,4 +33,19 @@ pub const COMP_PARAM_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M64: CompressionParamete
         lwe_per_glwe: LweCiphertextCount(256),
         storage_log_modulus: CiphertextModulusLog(11),
         packing_ks_key_noise_distribution: DynamicDistribution::new_t_uniform(36),
+    };
+
+pub const COMP_PARAM_MESSAGE_2_CARRY_2_KS_PBS_GAUSSIAN_2M64: CompressionParameters =
+    CompressionParameters {
+        br_level: DecompositionLevelCount(1),
+        br_base_log: DecompositionBaseLog(25),
+        packing_ks_level: DecompositionLevelCount(2),
+        packing_ks_base_log: DecompositionBaseLog(8),
+        packing_ks_polynomial_size: PolynomialSize(256),
+        packing_ks_glwe_dimension: GlweDimension(5),
+        lwe_per_glwe: LweCiphertextCount(256),
+        storage_log_modulus: CiphertextModulusLog(11),
+        packing_ks_key_noise_distribution: DynamicDistribution::new_gaussian_from_std_dev(
+            StandardDev(1.6173527465097522e-09),
+        ),
     };
