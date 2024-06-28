ef390b79c539569d6e29504c616a611980e723ed
Nicolas Sarlin <nicolas.sarlin@zama.ai>
2024-06-28 16:47:57 +0200

    chore(backward): use shallow clone for backward compat tests

--- scripts/clone_backward_compat_data.sh
+++ scripts/clone_backward_compat_data.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-if [ $# -lt 2 ]; then
-		echo "$0 git_url dest_path"
+if [ $# -lt 3 ]; then
+		echo "$0 git_url branch dest_path"
 		exit 1
 fi
 
@@ -13,7 +13,8 @@ if ! git lfs env 2>/dev/null >/dev/null; then
 fi
 
 if [ -d $2 ]; then
-		cd $2 && git pull
+		cd $2 && git fetch --depth 1 && git reset --hard origin/$2 && git clean -dfx
+
 else
-		git clone $1 $2
+		git clone $1 -b $2 --depth 1
 fi
