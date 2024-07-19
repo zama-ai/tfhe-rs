90f9ff6226f19330d6101d90bed92ae1ed883faa
Beka Barbakadze <beka.barbakadze@zama.ai>
2024-07-19 13:28:21 +0400

    feat(gpu): Add signed_overflowing_scalar_add and signed_overflowing_scalar_sub

--- tfhe/src/high_level_api/integers/signed/overflowing_ops.rs
+++ tfhe/src/high_level_api/integers/signed/overflowing_ops.rs
@@ -140,9 +140,14 @@ where
                 (FheInt::new(result), FheBool::new(overflow))
             }
             #[cfg(feature = "gpu")]
-            InternalServerKey::Cuda(_) => {
-                todo!("Cuda devices do not support signed integer");
-            }
+            InternalServerKey::Cuda(cuda_key) => with_thread_local_cuda_streams(|streams| {
+                let (result, overflow) = cuda_key.key.signed_overflowing_scalar_add(
+                    &self.ciphertext.on_gpu(),
+                    other,
+                    streams,
+                );
+                (FheInt::new(result), FheBool::new(overflow))
+            }),
         })
     }
 }
@@ -349,9 +354,14 @@ where
                 (FheInt::new(result), FheBool::new(overflow))
             }
             #[cfg(feature = "gpu")]
-            InternalServerKey::Cuda(_) => {
-                todo!("Cuda devices do not support signed integer");
-            }
+            InternalServerKey::Cuda(cuda_key) => with_thread_local_cuda_streams(|streams| {
+                let (result, overflow) = cuda_key.key.signed_overflowing_scalar_sub(
+                    &self.ciphertext.on_gpu(),
+                    other,
+                    streams,
+                );
+                (FheInt::new(result), FheBool::new(overflow))
+            }),
         })
     }
 }
