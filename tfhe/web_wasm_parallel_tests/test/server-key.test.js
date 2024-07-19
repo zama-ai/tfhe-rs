7479cc826ba575538dfbc219df3c96336f611614
Arthur Meyre <arthur.meyre@zama.ai>
2024-07-19 10:24:02 +0200

    chore(bench): increase wasm bench timeout

--- tfhe/web_wasm_parallel_tests/test/server-key.test.js
+++ tfhe/web_wasm_parallel_tests/test/server-key.test.js
@@ -2,8 +2,8 @@ import { runTestAttachedToButton } from "./common.mjs";
 
 it("Server Key Bench 1_1", async () => {
   await runTestAttachedToButton("compressedServerKeyBenchMessage1Carry1");
-}, 300000); // Five minutes timeout
+}, 600000); // 10 minutes timeout
 
 it("Server Key Bench 2_2", async () => {
   await runTestAttachedToButton("compressedServerKeyBenchMessage2Carry2");
-}, 300000); // Five minutes timeout
+}, 600000); // 10 minutes timeout
