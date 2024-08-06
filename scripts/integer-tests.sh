50d5ed3fbcb91d41fee715b0d7e7b1d48aef38af
Agnes Leroy <agnes.leroy@zama.ai>
2024-08-06 14:05:41 +0200

    chore(ci): transfer all GPU CI to hyperstack

--- scripts/integer-tests.sh
+++ scripts/integer-tests.sh
@@ -130,8 +130,8 @@ fi
 # Override test-threads number to avoid Out-of-memory issues on GPU instances
 if [[ "${backend}" == "gpu" ]]; then
     if [[ "${BIG_TESTS_INSTANCE}" == TRUE ]]; then
-        test_threads=5
-        doctest_threads=5
+        test_threads=8
+        doctest_threads=8
     else
         test_threads=3
         doctest_threads=3
