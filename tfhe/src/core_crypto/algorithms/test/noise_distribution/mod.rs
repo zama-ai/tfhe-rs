56c8cae8338433a0098293e67fba0d4857101911
Arthur Meyre <arthur.meyre@zama.ai>
2024-08-30 18:06:05 +0200

    test(core): add noise formulas and variance tests for KS and PBS

--- tfhe/src/core_crypto/algorithms/test/noise_distribution/mod.rs
+++ tfhe/src/core_crypto/algorithms/test/noise_distribution/mod.rs
@@ -1,3 +1,30 @@
 use super::*;
 
 mod lwe_encryption_noise;
+mod lwe_keyswitch_noise;
+mod lwe_programmable_boostrapping_noise;
+
+#[allow(clippy::excessive_precision)]
+pub const NOISE_TEST_PARAMS_4_BITS_NATIVE_U64_132_BITS_GAUSSIAN: ClassicTestParams<u64> =
+    ClassicTestParams {
+        lwe_dimension: LweDimension(841),
+        glwe_dimension: GlweDimension(1),
+        polynomial_size: PolynomialSize(2048),
+        lwe_noise_distribution: DynamicDistribution::new_gaussian_from_std_dev(StandardDev(
+            3.1496674685772435e-06,
+        )),
+        glwe_noise_distribution: DynamicDistribution::new_gaussian_from_std_dev(StandardDev(
+            2.845267479601915e-15,
+        )),
+        pbs_base_log: DecompositionBaseLog(22),
+        pbs_level: DecompositionLevelCount(1),
+        ks_level: DecompositionLevelCount(5),
+        ks_base_log: DecompositionBaseLog(3),
+        pfks_level: DecompositionLevelCount(0),
+        pfks_base_log: DecompositionBaseLog(0),
+        pfks_noise_distribution: DynamicDistribution::new_gaussian_from_std_dev(StandardDev(0.0)),
+        cbs_level: DecompositionLevelCount(0),
+        cbs_base_log: DecompositionBaseLog(0),
+        message_modulus_log: MessageModulusLog(4),
+        ciphertext_modulus: CiphertextModulus::new_native(),
+    };
