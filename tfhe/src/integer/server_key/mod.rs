62858b0c6037f9416ec2568ea4f0eaa58efba2b3
tmontaigu <thomas.montaigu@laposte.net>
2024-09-20 10:52:58 +0200

    feat(integer): add count_ones/zeros
    The non naive version made for 2_2 parameters
    only bring slight (10-15%) for some small sizes like (64, 128, 256 bits)
    but reduces number of PBS. The place where it brings the best
    improvements it for very large numbers (e.g 6400 blocks 1.8s for naive,
    1.1 sec for non-naive)

--- tfhe/src/integer/server_key/mod.rs
+++ tfhe/src/integer/server_key/mod.rs
@@ -212,19 +212,25 @@ impl ServerKey {
         self.key.carry_modulus
     }
 
+    pub fn num_bits_to_represent_unsigned_value<Clear>(&self, clear: Clear) -> usize
+    where
+        Clear: UnsignedInteger,
+    {
+        if clear == Clear::MAX {
+            Clear::BITS
+        } else {
+            (clear + Clear::ONE).ceil_ilog2() as usize
+        }
+    }
+
     /// Returns how many blocks a radix ciphertext should have to
     /// be able to represent the given unsigned integer
     pub fn num_blocks_to_represent_unsigned_value<Clear>(&self, clear: Clear) -> usize
     where
         Clear: UnsignedInteger,
     {
+        let num_bits_to_represent_output_value = self.num_bits_to_represent_unsigned_value(clear);
         let num_bits_in_message = self.message_modulus().0.ilog2();
-        let num_bits_to_represent_output_value = if clear == Clear::MAX {
-            Clear::BITS
-        } else {
-            (clear + Clear::ONE).ceil_ilog2() as usize
-        };
-
         num_bits_to_represent_output_value.div_ceil(num_bits_in_message as usize)
     }
 
---
+++ tfhe/src/integer/server_key/radix_parallel/tests_unsigned/test_count_zeros_ones.rs
@@ -0,0 +1,153 @@
+use crate::integer::keycache::KEY_CACHE;
+use crate::integer::server_key::radix_parallel::tests_cases_unsigned::FunctionExecutor;
+use crate::integer::server_key::radix_parallel::tests_unsigned::{
+    nb_tests_smaller_for_params, CpuFunctionExecutor, MAX_NB_CTXT, NB_CTXT,
+};
+use crate::integer::tests::create_parametrized_test;
+use crate::integer::{IntegerKeyKind, RadixCiphertext, RadixClientKey, ServerKey};
+use crate::shortint::PBSParameters;
+use rand::Rng;
+use std::sync::Arc;
+
+use crate::shortint::parameters::*;
+
+create_parametrized_test!(integer_extensive_trivial_default_count_zeros_ones);
+create_parametrized_test!(integer_default_count_zeros_ones);
+
+fn integer_extensive_trivial_default_count_zeros_ones<P>(param: P)
+where
+    P: Into<PBSParameters>,
+{
+    let count_zeros_executor = CpuFunctionExecutor::new(&ServerKey::count_zeros_parallelized);
+    let count_ones_executor = CpuFunctionExecutor::new(&ServerKey::count_ones_parallelized);
+    extensive_trivial_default_count_zeros_ones_test(
+        param,
+        count_zeros_executor,
+        count_ones_executor,
+    );
+}
+
+fn integer_default_count_zeros_ones<P>(param: P)
+where
+    P: Into<PBSParameters>,
+{
+    let count_zeros_executor = CpuFunctionExecutor::new(&ServerKey::count_zeros_parallelized);
+    let count_ones_executor = CpuFunctionExecutor::new(&ServerKey::count_ones_parallelized);
+    default_count_zeros_ones_test(param, count_zeros_executor, count_ones_executor);
+}
+
+pub(crate) fn default_count_zeros_ones_test<P, E1, E2>(
+    param: P,
+    mut count_zeros_executor: E1,
+    mut count_ones_executor: E2,
+) where
+    P: Into<PBSParameters>,
+    E1: for<'a> FunctionExecutor<&'a RadixCiphertext, RadixCiphertext>,
+    E2: for<'a> FunctionExecutor<&'a RadixCiphertext, RadixCiphertext>,
+{
+    let param = param.into();
+    let nb_tests = nb_tests_smaller_for_params(param);
+    let (cks, mut sks) = KEY_CACHE.get_from_params(param, IntegerKeyKind::Radix);
+    let cks = RadixClientKey::from((cks, NB_CTXT));
+
+    sks.set_deterministic_pbs_execution(true);
+    let sks = Arc::new(sks);
+
+    let mut rng = rand::thread_rng();
+
+    count_zeros_executor.setup(&cks, sks.clone());
+    count_ones_executor.setup(&cks, sks);
+
+    let cks: crate::integer::ClientKey = cks.into();
+
+    for num_blocks in 1..=MAX_NB_CTXT {
+        let Some(modulus) =
+            (cks.parameters().message_modulus().0 as u128).checked_pow(num_blocks as u32)
+        else {
+            break;
+        };
+        for _ in 0..nb_tests {
+            let clear_a = rng.gen::<u128>() % modulus;
+
+            let a: RadixCiphertext = cks.encrypt_radix(clear_a, num_blocks);
+
+            let encrypted = count_ones_executor.execute(&a);
+            let decrypted: u32 = cks.decrypt_radix(&encrypted);
+            assert_eq!(
+                decrypted,
+                clear_a.count_ones(),
+                "Invalid count_ones for input {clear_a}"
+            );
+
+            // Set all bits above the modulus to 1, so the count_zeros does no count them
+            // mask looks like `111111111111110000000`
+            //                                ^ modulus.ilog2()
+            let mask = u128::MAX.wrapping_mul(modulus);
+            let clear_a = mask | clear_a;
+            let encrypted = count_zeros_executor.execute(&a);
+            let decrypted: u32 = cks.decrypt_radix(&encrypted);
+            assert_eq!(
+                decrypted,
+                clear_a.count_zeros(),
+                "Invalid count_zeros for input {clear_a}"
+            );
+        }
+    }
+}
+
+pub(crate) fn extensive_trivial_default_count_zeros_ones_test<P, E1, E2>(
+    param: P,
+    mut count_zeros_executor: E1,
+    mut count_ones_executor: E2,
+) where
+    P: Into<PBSParameters>,
+    E1: for<'a> FunctionExecutor<&'a RadixCiphertext, RadixCiphertext>,
+    E2: for<'a> FunctionExecutor<&'a RadixCiphertext, RadixCiphertext>,
+{
+    let param = param.into();
+    let (cks, mut sks) = KEY_CACHE.get_from_params(param, IntegerKeyKind::Radix);
+    let cks = RadixClientKey::from((cks, NB_CTXT));
+
+    sks.set_deterministic_pbs_execution(true);
+    let sks = Arc::new(sks);
+
+    let mut rng = rand::thread_rng();
+
+    count_zeros_executor.setup(&cks, sks.clone());
+    count_ones_executor.setup(&cks, sks.clone());
+
+    let cks: crate::integer::ClientKey = cks.into();
+
+    for num_blocks in 1..=64 {
+        let Some(modulus) = (cks.parameters().message_modulus().0 as u128).checked_pow(num_blocks)
+        else {
+            break;
+        };
+        for _ in 0..50 {
+            let clear_a = rng.gen::<u128>() % modulus;
+
+            let a: RadixCiphertext = sks.create_trivial_radix(clear_a, num_blocks as usize);
+
+            let encrypted = count_ones_executor.execute(&a);
+            let decrypted: u32 = cks.decrypt_radix(&encrypted);
+            assert_eq!(
+                decrypted,
+                clear_a.count_ones(),
+                "Invalid count_ones for input {clear_a}"
+            );
+
+            // Set all bits above the modulus to 1, so the count_zeros does no count them
+            // mask looks like `111111111111110000000`
+            //                                ^ modulus.ilog2()
+            let mask = u128::MAX.wrapping_mul(modulus);
+            let clear_a = mask | clear_a;
+            let encrypted = count_zeros_executor.execute(&a);
+            let decrypted: u32 = cks.decrypt_radix(&encrypted);
+            assert_eq!(
+                decrypted,
+                clear_a.count_zeros(),
+                "Invalid count_zeros for input {clear_a}"
+            );
+        }
+    }
+}
