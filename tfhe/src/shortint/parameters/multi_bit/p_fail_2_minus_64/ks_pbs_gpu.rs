55cf8e8cda1de345b6ef260114d74d7a5e56c558
Guillermo Oyarzun <guillermo.oyarzun@zama.ai>
2024-09-24 16:36:07 +0000

    simple test gf4 + fft

--- tfhe/src/shortint/parameters/multi_bit/p_fail_2_minus_64/ks_pbs_gpu.rs
+++ tfhe/src/shortint/parameters/multi_bit/p_fail_2_minus_64/ks_pbs_gpu.rs
@@ -111,28 +111,55 @@ pub const PARAM_GPU_MULTI_BIT_GROUP_3_MESSAGE_1_CARRY_1_KS_PBS_GAUSSIAN_2M64:
 // p-fail = 2^-65.005, algorithmic cost ~ 79, 2-norm = 5
 pub const PARAM_GPU_MULTI_BIT_GROUP_3_MESSAGE_2_CARRY_2_KS_PBS_GAUSSIAN_2M64:
     MultiBitPBSParameters = MultiBitPBSParameters {
-    lwe_dimension: LweDimension(843),
+    lwe_dimension: LweDimension(844),
     glwe_dimension: GlweDimension(1),
     polynomial_size: PolynomialSize(2048),
     lwe_noise_distribution: DynamicDistribution::new_gaussian_from_std_dev(StandardDev(
-        3.0428360625436423e-06,
+        2.9907869683339986e-06,
     )),
     glwe_noise_distribution: DynamicDistribution::new_gaussian_from_std_dev(StandardDev(
         2.845267479601915e-15,
     )),
-    pbs_base_log: DecompositionBaseLog(22),
+    pbs_base_log: DecompositionBaseLog(21),
     pbs_level: DecompositionLevelCount(1),
     ks_base_log: DecompositionBaseLog(3),
     ks_level: DecompositionLevelCount(5),
     message_modulus: MessageModulus(4),
     carry_modulus: CarryModulus(4),
     max_noise_level: MaxNoiseLevel::new(5),
-    log2_p_fail: -65.005,
+    log2_p_fail: -65.196,
     ciphertext_modulus: CiphertextModulus::new_native(),
     encryption_key_choice: EncryptionKeyChoice::Big,
-    grouping_factor: LweBskGroupingFactor(3),
+    grouping_factor: LweBskGroupingFactor(4),
     deterministic_execution: false,
 };
+
+
+//// p-fail = 2^-65.005, algorithmic cost ~ 79, 2-norm = 5
+//pub const PARAM_GPU_MULTI_BIT_GROUP_3_MESSAGE_2_CARRY_2_KS_PBS_GAUSSIAN_2M64:
+//    MultiBitPBSParameters = MultiBitPBSParameters {
+//    lwe_dimension: LweDimension(843),
+//    glwe_dimension: GlweDimension(1),
+//    polynomial_size: PolynomialSize(2048),
+//    lwe_noise_distribution: DynamicDistribution::new_gaussian_from_std_dev(StandardDev(
+//        3.0428360625436423e-06,
+//    )),
+//    glwe_noise_distribution: DynamicDistribution::new_gaussian_from_std_dev(StandardDev(
+//        2.845267479601915e-15,
+//    )),
+//    pbs_base_log: DecompositionBaseLog(22),
+//    pbs_level: DecompositionLevelCount(1),
+//    ks_base_log: DecompositionBaseLog(3),
+//    ks_level: DecompositionLevelCount(5),
+//    message_modulus: MessageModulus(4),
+//    carry_modulus: CarryModulus(4),
+//    max_noise_level: MaxNoiseLevel::new(5),
+//    log2_p_fail: -65.005,
+//    ciphertext_modulus: CiphertextModulus::new_native(),
+//    encryption_key_choice: EncryptionKeyChoice::Big,
+//    grouping_factor: LweBskGroupingFactor(3),
+//    deterministic_execution: false,
+//};
 
 // p-fail = 2^-64.173, algorithmic cost ~ 645, 2-norm = 9
 pub const PARAM_GPU_MULTI_BIT_GROUP_3_MESSAGE_3_CARRY_3_KS_PBS_GAUSSIAN_2M64:
