0877bd0353699718753f47e324b2bd73e1582148
Jakub Klemsa <jakub.klemsa@zama.ai>
2024-09-28 22:30:01 +0200

    some shitty prints

--- tfhe-rs-cost-model/src/external_product_correction.py
+++ tfhe-rs-cost-model/src/external_product_correction.py
@@ -420,7 +420,7 @@ def test(x_values, y_values, weights, fft_noise_fun):
             pred_out = max(fft_noise_fun(params, *list(weights))[0], 0.000001)  #TODO make sure this const is OK
             if params[0,0] == big_N:
                 mse += (log_var(real_out) - log_var(pred_out)) ** 2
-                # ~ print(f"{log_var(real_out) - log_var(pred_out)}, {real_out}, {pred_out}") # log_var(real_out) - log_var(pred_out) == log_var(real_out/pred_out)
+                print(f"{log_var(real_out) - log_var(pred_out)}, {params}, {real_out}, {pred_out}") # log_var(real_out) - log_var(pred_out) == log_var(real_out/pred_out)
                 # print(
                 #     f"th: {log_var(params[0, -1])}, pred_fft: {log_var(pred_out)}, "
                 #     f"real: {log_var(real_out)}"
@@ -430,12 +430,14 @@ def test(x_values, y_values, weights, fft_noise_fun):
             # print(log_var(params[0, -1]))
             # mse_without_correction += (log_var(real_out) ) ** 2
 
+        print()
         count = max(count, 1)
 
         mse /= count  # len(x_values)
         mse = .5 * mse ** .5
         mse_without_correction /= count  # len(x_values)
-        print(f"½ √mse (N = {big_N}): {mse}") #  \nMSE without correction: {mse_without_correction}
+        mse_without_correction = .5 * mse_without_correction ** .5
+        # ~ print(f"½ √mse (N = {big_N}): {mse} .. {2 ** (2*mse)}") #  \nMSE without correction: {mse_without_correction}
 
     mse = 0.0
     mse_without_correction = 0.0
@@ -460,7 +462,7 @@ def test(x_values, y_values, weights, fft_noise_fun):
     mse = .5 * mse ** .5
     mse_without_correction /= count  # len(x_values)
     mse_without_correction = .5 * mse_without_correction ** .5
-    print(f"½ √mse (all N): {mse} \n½ √MSE without correction: {mse_without_correction}")
+    print(f"½ √mse (all N): {mse} .. {2 ** (2*mse)} \n½ √MSE without correction: {mse_without_correction} .. {2 ** (2*mse_without_correction)}")
     return mse, mse_without_correction
 
 
