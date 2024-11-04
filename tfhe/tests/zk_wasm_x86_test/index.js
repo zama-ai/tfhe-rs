6ee3eb17b9444aac2b6b6fe29b1766fa355e8a36
Nicolas Sarlin <nicolas.sarlin@zama.ai>
2024-11-04 17:57:39 +0100

    chore(zk): add a proof compat test between x86_64 and wasm

---
+++ tfhe/tests/zk_wasm_x86_test/index.js
@@ -0,0 +1,38 @@
+const {
+    TfheCompactPublicKey,
+    CompactCiphertextList,
+    CompactPkePublicParams,
+    ZkComputeLoad,
+} = require('node-tfhe');
+
+const fs = require('fs');
+
+const SIZE_LIMIT = BigInt(1024) * BigInt(1024) * BigInt(1024);
+
+const tfhe_proof = async () => {
+    const publicKeyBuf = fs.readFileSync(`${__dirname}/public_key.bin`);
+    const publicParamsBuf = fs.readFileSync(`${__dirname}/crs.bin`);
+    const publicKey = TfheCompactPublicKey.safe_deserialize(publicKeyBuf, SIZE_LIMIT);
+    const publicParams = CompactPkePublicParams.safe_deserialize(publicParamsBuf, SIZE_LIMIT);
+
+    const builder = CompactCiphertextList.builder(publicKey);
+    builder.push_u4(1);
+
+		builder.push_u8(0xff);
+
+    const encrypted = builder.build_with_proof_packed(
+        publicParams,
+        new Uint8Array(),
+        ZkComputeLoad.Proof,
+    );
+
+    const ciphertext = encrypted.safe_serialize(SIZE_LIMIT);
+    let ciphertext_hex = Buffer.from(ciphertext);
+
+
+    fs.writeFile('proof.bin', ciphertext_hex, (err) => {
+
+        if (err) throw err;
+    });
+}
+tfhe_proof();
