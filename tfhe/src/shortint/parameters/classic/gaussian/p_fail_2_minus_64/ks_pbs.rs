697b426ccad8fa3d86eb458da0fe39bcd9334f33
David Testé <david.teste@zama.ai>
2024-11-15 16:42:47 +0100

    BENCH: params update

--- tfhe/src/shortint/parameters/classic/gaussian/p_fail_2_minus_64/ks_pbs.rs
+++ tfhe/src/shortint/parameters/classic/gaussian/p_fail_2_minus_64/ks_pbs.rs
@@ -234,11 +234,11 @@ pub const PARAM_MESSAGE_2_CARRY_1_KS_PBS_GAUSSIAN_2M64: ClassicPBSParameters =
 // p-fail = 2^-64.074, algorithmic cost ~ 106, 2-norm = 5
 pub const PARAM_MESSAGE_2_CARRY_2_KS_PBS_GAUSSIAN_2M64: ClassicPBSParameters =
     ClassicPBSParameters {
-        lwe_dimension: LweDimension(516),
+        lwe_dimension: LweDimension(515),
         glwe_dimension: GlweDimension(1),
         polynomial_size: PolynomialSize(2048),
-        lwe_noise_distribution: DynamicDistribution::new_gaussian_from_std_dev(StandardDev(1.709417788421641e-06)),
-        glwe_noise_distribution: DynamicDistribution::new_gaussian_from_std_dev(StandardDev(2.168404344974257e-19)),
+        lwe_noise_distribution: DynamicDistribution::new_gaussian_from_std_dev(StandardDev(1.7213526167799302e-06)),
+        glwe_noise_distribution: DynamicDistribution::new_gaussian_from_std_dev(StandardDev(2.168404344971752e-19)),
         pbs_base_log: DecompositionBaseLog(23),
         pbs_level: DecompositionLevelCount(1),
         ks_base_log: DecompositionBaseLog(5),
@@ -246,7 +246,7 @@ pub const PARAM_MESSAGE_2_CARRY_2_KS_PBS_GAUSSIAN_2M64: ClassicPBSParameters =
         message_modulus: MessageModulus(4),
         carry_modulus: CarryModulus(4),
         max_noise_level: MaxNoiseLevel::new(5),
-        log2_p_fail: -63.753,
+        log2_p_fail: -63.36,
         ciphertext_modulus: CiphertextModulus::new_native(),
         encryption_key_choice: EncryptionKeyChoice::Big,
     };
