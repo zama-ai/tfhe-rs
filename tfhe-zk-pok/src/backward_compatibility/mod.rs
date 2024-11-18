1242a435dababb829bc68728d6346d1d87bedf74
Nicolas Sarlin <nicolas.sarlin@zama.ai>
2024-11-18 17:27:57 +0100

    fix(zk): recompute B according to k in proof and use squared bounds
    This removes the need for sqrt operations
    also fix a proof slack was too big in v2

--- tfhe-zk-pok/src/backward_compatibility/mod.rs
+++ tfhe-zk-pok/src/backward_compatibility/mod.rs
@@ -1,12 +1,18 @@
+// to follow the notation of the paper
+#![allow(non_snake_case)]
+
 pub mod pke;
 pub mod pke_v2;
 
+use std::convert::Infallible;
 use std::error::Error;
 use std::fmt::Display;
 
-use tfhe_versionable::VersionsDispatch;
+use tfhe_versionable::{Upgrade, Version, VersionsDispatch};
 
 use crate::curve_api::Curve;
+use crate::four_squares::{isqrt, sqr};
+use crate::proofs::pke_v2::Bound;
 use crate::proofs::GroupElements;
 use crate::serialization::{
     SerializableAffine, SerializableCubicExtField, SerializableFp, SerializableFp2,
@@ -65,6 +71,65 @@ pub(crate) enum SerializableGroupElementsVersions {
     V0(SerializableGroupElements),
 }
 
+#[derive(Version)]
+pub struct SerializablePKEv2PublicParamsV0 {
+    pub(crate) g_lists: SerializableGroupElements,
+    pub(crate) D: usize,
+    pub n: usize,
+    pub d: usize,
+    pub k: usize,
+    pub B: u64,
+    pub B_r: u64,
+    pub B_bound: u64,
+    pub m_bound: usize,
+    pub q: u64,
+    pub t: u64,
+    pub msbs_zero_padding_bit_count: u64,
+    // We use Vec<u8> since serde does not support fixed size arrays of 256 elements
+    pub(crate) hash: Vec<u8>,
+    pub(crate) hash_R: Vec<u8>,
+    pub(crate) hash_t: Vec<u8>,
+    pub(crate) hash_w: Vec<u8>,
+    pub(crate) hash_agg: Vec<u8>,
+    pub(crate) hash_lmap: Vec<u8>,
+    pub(crate) hash_phi: Vec<u8>,
+    pub(crate) hash_xi: Vec<u8>,
+    pub(crate) hash_z: Vec<u8>,
+    pub(crate) hash_chi: Vec<u8>,
+}
+
+impl Upgrade<SerializablePKEv2PublicParams> for SerializablePKEv2PublicParamsV0 {
+    type Error = Infallible;
+
+    fn upgrade(self) -> Result<SerializablePKEv2PublicParams, Self::Error> {
+        let slack_factor = isqrt((self.d + self.k) as u128) as u64;
+        let B_inf = self.B / slack_factor;
+        Ok(SerializablePKEv2PublicParams {
+            g_lists: self.g_lists,
+            D: self.D,
+            n: self.n,
+            d: self.d,
+            k: self.k,
+            B_bound_squared: sqr(self.B_bound as u128),
+            B_inf,
+            q: self.q,
+            t: self.t,
+            msbs_zero_padding_bit_count: self.msbs_zero_padding_bit_count,
+            bound_type: Bound::CS,
+            hash: self.hash,
+            hash_R: self.hash_R,
+            hash_t: self.hash_t,
+            hash_w: self.hash_w,
+            hash_agg: self.hash_agg,
+            hash_lmap: self.hash_lmap,
+            hash_phi: self.hash_phi,
+            hash_xi: self.hash_xi,
+            hash_z: self.hash_z,
+            hash_chi: self.hash_chi,
+        })
+    }
+}
+
 #[derive(VersionsDispatch)]
 pub enum SerializablePKEv2PublicParamsVersions {
     V0(SerializablePKEv2PublicParams),
@@ -73,4 +138,9 @@ pub enum SerializablePKEv2PublicParamsVersions {
 #[derive(VersionsDispatch)]
 pub enum SerializablePKEv1PublicParamsVersions {
     V0(SerializablePKEv1PublicParams),
+}
+
+#[derive(VersionsDispatch)]
+pub enum BoundVersions {
+    V0(Bound),
 }
