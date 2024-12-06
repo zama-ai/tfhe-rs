f6cb2e109e4716ccbf8e2a33b51d3db3eede285d
Agnes Leroy <agnes.leroy@zama.ai>
2024-12-06 10:31:12 +0100

    fix(gpu): fix single gpu on device other than 0

--- tfhe/src/integer/server_key/radix_parallel/tests_long_run/mod.rs
+++ tfhe/src/integer/server_key/radix_parallel/tests_long_run/mod.rs
@@ -1,4 +1,4 @@
 pub(crate) mod test_erc20;
 pub(crate) mod test_random_op_sequence;
 pub(crate) const NB_CTXT_LONG_RUN: usize = 32;
-pub(crate) const NB_TESTS_LONG_RUN: usize = 2000;
+pub(crate) const NB_TESTS_LONG_RUN: usize = 1500;
