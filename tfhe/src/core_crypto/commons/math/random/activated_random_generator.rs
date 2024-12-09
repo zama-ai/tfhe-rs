d4809de54258e0a47be721553b2a4f019bce9b2a
Nicolas Sarlin <nicolas.sarlin@zama.ai>
2024-12-09 15:39:03 +0100

    chore(tfhe)!: remove arch specific features
    BREAKING_CHANGE:
    -	The x86_64, x86_64-unix, aarch64, aarch64-unix have been removed, the target
    architecture and os family are now automatically detected. A `software_prng`
    feature has been added to force the use of a software implementation on older
    CPUs

--- tfhe/src/core_crypto/commons/math/random/activated_random_generator.rs
+++ tfhe/src/core_crypto/commons/math/random/activated_random_generator.rs
@@ -1,19 +1,13 @@
-#[cfg(all(target_arch = "x86_64", target_feature = "aes"))]
+#[cfg(all(target_arch = "x86_64", not(feature = "software_prng")))]
 use tfhe_csprng::generators::AesniRandomGenerator;
-#[cfg(all(target_arch = "aarch64", target_feature = "neon"))]
+#[cfg(all(target_arch = "aarch64", not(feature = "software_prng")))]
 use tfhe_csprng::generators::NeonAesRandomGenerator;
-#[cfg(all(
-    not(all(target_arch = "x86_64", target_feature = "aes")),
-    not(all(target_arch = "aarch64", target_feature = "neon"))
-))]
+#[cfg(feature = "software_prng")]
 use tfhe_csprng::generators::SoftwareRandomGenerator;
 
-#[cfg(all(target_arch = "x86_64", target_feature = "aes"))]
+#[cfg(all(target_arch = "x86_64", not(feature = "software_prng")))]
 pub type ActivatedRandomGenerator = AesniRandomGenerator;
-#[cfg(all(target_arch = "aarch64", target_feature = "neon"))]
+#[cfg(all(target_arch = "aarch64", not(feature = "software_prng")))]
 pub type ActivatedRandomGenerator = NeonAesRandomGenerator;
-#[cfg(all(
-    not(all(target_arch = "x86_64", target_feature = "aes")),
-    not(all(target_arch = "aarch64", target_feature = "neon"))
-))]
+#[cfg(feature = "software_prng")]
 pub type ActivatedRandomGenerator = SoftwareRandomGenerator;
