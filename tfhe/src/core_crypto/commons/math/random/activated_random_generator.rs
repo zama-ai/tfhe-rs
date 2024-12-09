ba2ba66d54cb3487517b65ba51750b9d833d8118
Nicolas Sarlin <nicolas.sarlin@zama.ai>
2024-12-09 15:05:09 +0100

    chore(csprng)!: remove generator_aarch64_aes feature
    BREAKING_CHANGE:
    - The `generator_aarch64_aes` feature is no longer supported for tfhe-csprng

--- tfhe/src/core_crypto/commons/math/random/activated_random_generator.rs
+++ tfhe/src/core_crypto/commons/math/random/activated_random_generator.rs
@@ -1,19 +1,19 @@
 #[cfg(all(target_arch = "x86_64", target_feature = "aes"))]
 use tfhe_csprng::generators::AesniRandomGenerator;
-#[cfg(feature = "generator_aarch64_aes")]
+#[cfg(all(target_arch = "aarch64", target_feature = "neon"))]
 use tfhe_csprng::generators::NeonAesRandomGenerator;
 #[cfg(all(
     not(all(target_arch = "x86_64", target_feature = "aes")),
-    not(feature = "generator_aarch64_aes")
+    not(all(target_arch = "aarch64", target_feature = "neon"))
 ))]
 use tfhe_csprng::generators::SoftwareRandomGenerator;
 
 #[cfg(all(target_arch = "x86_64", target_feature = "aes"))]
 pub type ActivatedRandomGenerator = AesniRandomGenerator;
-#[cfg(feature = "generator_aarch64_aes")]
+#[cfg(all(target_arch = "aarch64", target_feature = "neon"))]
 pub type ActivatedRandomGenerator = NeonAesRandomGenerator;
 #[cfg(all(
     not(all(target_arch = "x86_64", target_feature = "aes")),
-    not(feature = "generator_aarch64_aes")
+    not(all(target_arch = "aarch64", target_feature = "neon"))
 ))]
 pub type ActivatedRandomGenerator = SoftwareRandomGenerator;
