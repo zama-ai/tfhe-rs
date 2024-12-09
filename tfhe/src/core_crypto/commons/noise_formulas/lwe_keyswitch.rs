e5f4291a9f19492a6b2e17c8f11e0acdeeeeb978
Arthur Meyre <arthur.meyre@zama.ai>
2024-12-09 10:47:04 +0000

    feat: add noise formulas for TUniform 132 bits of security

--- tfhe/src/core_crypto/commons/noise_formulas/lwe_keyswitch.rs
+++ tfhe/src/core_crypto/commons/noise_formulas/lwe_keyswitch.rs
@@ -42,3 +42,44 @@ pub fn keyswitch_additive_variance_132_bits_security_gaussian_impl(
             * (0.0208333333333333 * modulus.powf(-2.0)
                 + 0.0416666666666667 * decomposition_base.powf(-2.0 * decomposition_level_count))
 }
+
+/// This formula is only valid if the proper noise distributions are used and
+/// if the keys used are encrypted using secure noise given by the
+/// [`minimal_glwe_variance`](`super::secure_noise`)
+/// and [`minimal_lwe_variance`](`super::secure_noise`) family of functions.
+pub fn keyswitch_additive_variance_132_bits_security_tuniform(
+    input_lwe_dimension: LweDimension,
+    output_lwe_dimension: LweDimension,
+    decomposition_base_log: DecompositionBaseLog,
+    decomposition_level_count: DecompositionLevelCount,
+    modulus: f64,
+) -> Variance {
+    Variance(keyswitch_additive_variance_132_bits_security_tuniform_impl(
+        input_lwe_dimension.0 as f64,
+        output_lwe_dimension.0 as f64,
+        2.0f64.powi(decomposition_base_log.0 as i32),
+        decomposition_level_count.0 as f64,
+        modulus,
+    ))
+}
+
+/// This formula is only valid if the proper noise distributions are used and
+/// if the keys used are encrypted using secure noise given by the
+/// [`minimal_glwe_variance`](`super::secure_noise`)
+/// and [`minimal_lwe_variance`](`super::secure_noise`) family of functions.
+pub fn keyswitch_additive_variance_132_bits_security_tuniform_impl(
+    input_lwe_dimension: f64,
+    output_lwe_dimension: f64,
+    decomposition_base: f64,
+    decomposition_level_count: f64,
+    modulus: f64,
+) -> f64 {
+    decomposition_level_count
+        * input_lwe_dimension
+        * ((4.0 - 2.88539008177793 * modulus.ln()).exp2()
+            + (7.31469187675068 - 0.0497829131652661 * output_lwe_dimension).exp2())
+        * ((1_f64 / 12.0) * decomposition_base.powf(2.0) + 0.166666666666667)
+        + input_lwe_dimension
+            * (0.0208333333333333 * modulus.powf(-2.0)
+                + 0.0416666666666667 * decomposition_base.powf(-2.0 * decomposition_level_count))
+}
