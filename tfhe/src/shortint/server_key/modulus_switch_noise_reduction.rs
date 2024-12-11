136b4f933b92db0a3c8ff86af123c5cd57f5fda1
Mayeul@Zama <69792125+mayeul-zama@users.noreply.github.com>
2024-12-11 16:47:18 +0100

    feat(shortint): add modulus switch noise reduction params

---
+++ tfhe/src/shortint/server_key/modulus_switch_noise_reduction.rs
@@ -0,0 +1,14 @@
+use crate::core_crypto::prelude::modulus_switch_noise_reduction::{
+    NoiseEstimationMeasureBound, RSigmaFactor,
+};
+use crate::core_crypto::prelude::LweCiphertextCount;
+use serde::{Deserialize, Serialize};
+use std::fmt::Debug;
+use tfhe_versionable::NotVersioned;
+
+#[derive(Copy, Clone, Debug, PartialEq, Serialize, Deserialize, NotVersioned)]
+pub struct ModulusSwitchNoiseReductionParams {
+    pub modulus_switch_zeros_count: LweCiphertextCount,
+    pub ms_bound: NoiseEstimationMeasureBound,
+    pub ms_r_sigma_factor: RSigmaFactor,
+}
