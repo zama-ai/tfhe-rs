f7b68a0fa9d6b709e6d46a0b66033a5ad0a5e0fd
pgardratzama <pierre.gardrat@zama.ai>
2024-12-12 16:57:58 +0100

    feat(hpu-noise) move hpu noise measure into its own file as it has kind of nothing to do with encryption noise test

--- tfhe/src/core_crypto/algorithms/test/noise_distribution/mod.rs
+++ tfhe/src/core_crypto/algorithms/test/noise_distribution/mod.rs
@@ -1,6 +1,7 @@
 use super::*;
 
 mod lwe_encryption_noise;
+mod lwe_hpu_noise;
 mod lwe_keyswitch_noise;
 mod lwe_multi_bit_programmable_bootstrapping_noise;
 mod lwe_programmable_bootstrapping_noise;
