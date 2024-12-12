f95eb2cf2cfce456aa281fd2ceb34a2fd800df6a
Arthur Meyre <arthur.meyre@zama.ai>
2024-12-12 12:36:38 +0000

    wip: re-exported pbs 128 with symbolic mantissa
    - add pbs 128 params with a mantissa setting
    - noise with this settings is in line with RO prediction

--- tfhe/src/core_crypto/commons/noise_formulas/lwe_programmable_bootstrap_128.rs
+++ tfhe/src/core_crypto/commons/noise_formulas/lwe_programmable_bootstrap_128.rs
@@ -12,6 +12,7 @@ pub fn pbs_128_variance_132_bits_security_gaussian(
     output_polynomial_size: PolynomialSize,
     decomposition_base_log: DecompositionBaseLog,
     decomposition_level_count: DecompositionLevelCount,
+    mantissa_size: f64,
     modulus: f64,
 ) -> Variance {
     Variance(pbs_128_variance_132_bits_security_gaussian_impl(
@@ -20,6 +21,7 @@ pub fn pbs_128_variance_132_bits_security_gaussian(
         output_polynomial_size.0 as f64,
         2.0f64.powi(decomposition_base_log.0 as i32),
         decomposition_level_count.0 as f64,
+        mantissa_size,
         modulus,
     ))
 }
@@ -28,29 +30,24 @@ pub fn pbs_128_variance_132_bits_security_gaussian(
 /// if the keys used are encrypted using secure noise given by the
 /// [`minimal_glwe_variance`](`super::secure_noise`)
 /// and [`minimal_lwe_variance`](`super::secure_noise`) family of functions.
-#[allow(clippy::suspicious_operation_groupings)]
 pub fn pbs_128_variance_132_bits_security_gaussian_impl(
     input_lwe_dimension: f64,
     output_glwe_dimension: f64,
     output_polynomial_size: f64,
     decomposition_base: f64,
     decomposition_level_count: f64,
+    mantissa_size: f64,
     modulus: f64,
 ) -> f64 {
     input_lwe_dimension
-        * (2.54576848986251e-65
-            * decomposition_base.powf(2.0)
-            * decomposition_level_count
-            * output_polynomial_size.powf(2.0)
+        * (decomposition_level_count
+            * output_polynomial_size
+            * ((4.0 - 2.88539008177793 * modulus.ln()).exp2()
+                + (-0.0497829131652661 * output_glwe_dimension * output_polynomial_size
+                    + 5.31469187675068)
+                    .exp2())
+            * ((1_f64 / 12.0) * decomposition_base.powf(2.0) + 0.166666666666667)
             * (output_glwe_dimension + 1.0)
-            + decomposition_level_count
-                * output_polynomial_size
-                * ((4.0 - 2.88539008177793 * modulus.ln()).exp2()
-                    + (-0.0497829131652661 * output_glwe_dimension * output_polynomial_size
-                        + 5.31469187675068)
-                        .exp2())
-                * ((1_f64 / 12.0) * decomposition_base.powf(2.0) + 0.166666666666667)
-                * (output_glwe_dimension + 1.0)
             + (1_f64 / 12.0) * modulus.powf(-2.0)
             + (1_f64 / 2.0)
                 * output_glwe_dimension
@@ -58,7 +55,13 @@ pub fn pbs_128_variance_132_bits_security_gaussian_impl(
                 * (0.0208333333333333 * modulus.powf(-2.0)
                     + 0.0416666666666667
                         * decomposition_base.powf(-2.0 * decomposition_level_count))
-            + (1_f64 / 24.0) * decomposition_base.powf(-2.0 * decomposition_level_count))
+            + (1_f64 / 24.0) * decomposition_base.powf(-2.0 * decomposition_level_count)
+            + 0.16756294607814
+                * (-2.0 * mantissa_size).exp2()
+                * decomposition_base.powf(2.0)
+                * decomposition_level_count
+                * output_polynomial_size.powf(2.0)
+                * (output_glwe_dimension + 1.0))
 }
 
 /// This formula is only valid if the proper noise distributions are used and
@@ -71,6 +74,7 @@ pub fn pbs_128_variance_132_bits_security_tuniform(
     output_polynomial_size: PolynomialSize,
     decomposition_base_log: DecompositionBaseLog,
     decomposition_level_count: DecompositionLevelCount,
+    mantissa_size: f64,
     modulus: f64,
 ) -> Variance {
     Variance(pbs_128_variance_132_bits_security_tuniform_impl(
@@ -79,6 +83,7 @@ pub fn pbs_128_variance_132_bits_security_tuniform(
         output_polynomial_size.0 as f64,
         2.0f64.powi(decomposition_base_log.0 as i32),
         decomposition_level_count.0 as f64,
+        mantissa_size,
         modulus,
     ))
 }
@@ -87,29 +92,24 @@ pub fn pbs_128_variance_132_bits_security_tuniform(
 /// if the keys used are encrypted using secure noise given by the
 /// [`minimal_glwe_variance`](`super::secure_noise`)
 /// and [`minimal_lwe_variance`](`super::secure_noise`) family of functions.
-#[allow(clippy::suspicious_operation_groupings)]
 pub fn pbs_128_variance_132_bits_security_tuniform_impl(
     input_lwe_dimension: f64,
     output_glwe_dimension: f64,
     output_polynomial_size: f64,
     decomposition_base: f64,
     decomposition_level_count: f64,
+    mantissa_size: f64,
     modulus: f64,
 ) -> f64 {
     input_lwe_dimension
-        * (2.54576848986251e-65
-            * decomposition_base.powf(2.0)
-            * decomposition_level_count
-            * output_polynomial_size.powf(2.0)
+        * (decomposition_level_count
+            * output_polynomial_size
+            * ((4.0 - 2.88539008177793 * modulus.ln()).exp2()
+                + (-0.0497829131652661 * output_glwe_dimension * output_polynomial_size
+                    + 7.31469187675068)
+                    .exp2())
+            * ((1_f64 / 12.0) * decomposition_base.powf(2.0) + 0.166666666666667)
             * (output_glwe_dimension + 1.0)
-            + decomposition_level_count
-                * output_polynomial_size
-                * ((4.0 - 2.88539008177793 * modulus.ln()).exp2()
-                    + (-0.0497829131652661 * output_glwe_dimension * output_polynomial_size
-                        + 7.31469187675068)
-                        .exp2())
-                * ((1_f64 / 12.0) * decomposition_base.powf(2.0) + 0.166666666666667)
-                * (output_glwe_dimension + 1.0)
             + (1_f64 / 12.0) * modulus.powf(-2.0)
             + (1_f64 / 2.0)
                 * output_glwe_dimension
@@ -117,5 +117,11 @@ pub fn pbs_128_variance_132_bits_security_tuniform_impl(
                 * (0.0208333333333333 * modulus.powf(-2.0)
                     + 0.0416666666666667
                         * decomposition_base.powf(-2.0 * decomposition_level_count))
-            + (1_f64 / 24.0) * decomposition_base.powf(-2.0 * decomposition_level_count))
+            + (1_f64 / 24.0) * decomposition_base.powf(-2.0 * decomposition_level_count)
+            + 0.16756294607814
+                * (-2.0 * mantissa_size).exp2()
+                * decomposition_base.powf(2.0)
+                * decomposition_level_count
+                * output_polynomial_size.powf(2.0)
+                * (output_glwe_dimension + 1.0))
 }
