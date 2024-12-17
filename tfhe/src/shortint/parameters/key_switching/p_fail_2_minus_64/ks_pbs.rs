3432b941b681768e509f6a45bd4605a6c23caf6e
Nicolas Sarlin <nicolas.sarlin@zama.ai>
2024-12-17 10:59:47 +0100

    chore(shortint): new parameters for tuniform

--- tfhe/src/shortint/parameters/key_switching/p_fail_2_minus_64/ks_pbs.rs
+++ tfhe/src/shortint/parameters/key_switching/p_fail_2_minus_64/ks_pbs.rs
@@ -20,6 +20,10 @@ pub const V0_11_PARAM_KEYSWITCH_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M64:
 // Parameters to keyswitch from input PKE 2_2 TUniform parameters to 2_2 KS_PBS compute parameters
 // arriving under the small key, requires a PBS to get to the big key
 pub const V0_11_PARAM_KEYSWITCH_PKE_TO_SMALL_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M64:
+    ShortintKeySwitchingParameters =
+    V0_11_PARAM_KEYSWITCH_PKE_TO_SMALL_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M64_ZKV2;
+
+pub const V0_11_PARAM_KEYSWITCH_PKE_TO_SMALL_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M64_ZKV2:
     ShortintKeySwitchingParameters = ShortintKeySwitchingParameters {
     ks_level: DecompositionLevelCount(5),
     ks_base_log: DecompositionBaseLog(3),
@@ -29,9 +33,31 @@ pub const V0_11_PARAM_KEYSWITCH_PKE_TO_SMALL_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2
 // Parameters to keyswitch from input PKE 2_2 TUniform parameters to 2_2 KS_PBS compute parameters
 // arriving under the big key, requires a PBS to get to the big key
 pub const V0_11_PARAM_KEYSWITCH_PKE_TO_BIG_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M64:
+    ShortintKeySwitchingParameters =
+    V0_11_PARAM_KEYSWITCH_PKE_TO_BIG_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M64_ZKV2;
+
+pub const V0_11_PARAM_KEYSWITCH_PKE_TO_BIG_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M64_ZKV2:
     ShortintKeySwitchingParameters = ShortintKeySwitchingParameters {
     ks_level: DecompositionLevelCount(1),
-    ks_base_log: DecompositionBaseLog(23),
+    ks_base_log: DecompositionBaseLog(24),
+    destination_key: EncryptionKeyChoice::Big,
+};
+
+// Parameters to keyswitch from input PKE 2_2 TUniform parameters to 2_2 KS_PBS compute parameters
+// arriving under the small key, requires a PBS to get to the big key
+pub const V0_11_PARAM_KEYSWITCH_PKE_TO_SMALL_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M64_ZKV1:
+    ShortintKeySwitchingParameters = ShortintKeySwitchingParameters {
+    ks_level: DecompositionLevelCount(5),
+    ks_base_log: DecompositionBaseLog(3),
+    destination_key: EncryptionKeyChoice::Small,
+};
+
+// Parameters to keyswitch from input PKE 2_2 TUniform parameters to 2_2 KS_PBS compute parameters
+// arriving under the big key, requires a PBS to get to the big key
+pub const V0_11_PARAM_KEYSWITCH_PKE_TO_BIG_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M64_ZKV1:
+    ShortintKeySwitchingParameters = ShortintKeySwitchingParameters {
+    ks_level: DecompositionLevelCount(1),
+    ks_base_log: DecompositionBaseLog(24),
     destination_key: EncryptionKeyChoice::Big,
 };
 
