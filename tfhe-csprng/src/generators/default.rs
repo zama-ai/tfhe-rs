7dc9bdc32466f44e870a8425e3fce72b11cefd93
Mayeul@Zama <69792125+mayeul-zama@users.noreply.github.com>
2024-12-18 15:12:19 +0100

    chore(csprng): cleanup conditional imports

--- tfhe-csprng/src/generators/default.rs
+++ tfhe-csprng/src/generators/default.rs
@@ -1,19 +1,9 @@
 #[cfg(all(target_arch = "x86_64", not(feature = "software-prng")))]
-use super::AesniRandomGenerator;
+pub type DefaultRandomGenerator = super::AesniRandomGenerator;
 #[cfg(all(target_arch = "aarch64", not(feature = "software-prng")))]
-use super::NeonAesRandomGenerator;
+pub type DefaultRandomGenerator = super::NeonAesRandomGenerator;
 #[cfg(any(
     feature = "software-prng",
     not(any(target_arch = "x86_64", target_arch = "aarch64"))
 ))]
-use super::SoftwareRandomGenerator;
-
-#[cfg(all(target_arch = "x86_64", not(feature = "software-prng")))]
-pub type DefaultRandomGenerator = AesniRandomGenerator;
-#[cfg(all(target_arch = "aarch64", not(feature = "software-prng")))]
-pub type DefaultRandomGenerator = NeonAesRandomGenerator;
-#[cfg(any(
-    feature = "software-prng",
-    not(any(target_arch = "x86_64", target_arch = "aarch64"))
-))]
-pub type DefaultRandomGenerator = SoftwareRandomGenerator;
+pub type DefaultRandomGenerator = super::SoftwareRandomGenerator;
