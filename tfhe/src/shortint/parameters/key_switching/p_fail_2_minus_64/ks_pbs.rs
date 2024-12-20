f1eb6753844e34326aabd7e3495d625b08f85e16
Nicolas Sarlin <nicolas.sarlin@zama.ai>
2024-12-20 14:59:52 +0100

    chore(shortint): move tuniform 0.10 parameters into their own folder

--- tfhe/src/shortint/parameters/key_switching/p_fail_2_minus_64/ks_pbs.rs
+++ tfhe/src/shortint/parameters/key_switching/p_fail_2_minus_64/ks_pbs.rs
@@ -60,22 +60,3 @@ pub const V0_11_PARAM_KEYSWITCH_PKE_TO_BIG_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M6
     ks_base_log: DecompositionBaseLog(24),
     destination_key: EncryptionKeyChoice::Big,
 };
-
-// These are the same parameters as they where defined in TFHE-rs 0.10 and before
-pub const V0_10_PARAM_KEYSWITCH_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M64:
-    ShortintKeySwitchingParameters =
-    V0_10_PARAM_KEYSWITCH_PKE_TO_SMALL_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M64;
-
-pub const V0_10_PARAM_KEYSWITCH_PKE_TO_SMALL_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M64:
-    ShortintKeySwitchingParameters = ShortintKeySwitchingParameters {
-    ks_level: DecompositionLevelCount(5),
-    ks_base_log: DecompositionBaseLog(3),
-    destination_key: EncryptionKeyChoice::Small,
-};
-
-pub const V0_10_PARAM_KEYSWITCH_PKE_TO_BIG_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M64:
-    ShortintKeySwitchingParameters = ShortintKeySwitchingParameters {
-    ks_level: DecompositionLevelCount(1),
-    ks_base_log: DecompositionBaseLog(27),
-    destination_key: EncryptionKeyChoice::Big,
-};
