c19683a320a81d4e163d3fdaa92d44b18315d43d
Nicolas Sarlin <nicolas.sarlin@zama.ai>
2024-12-20 14:59:52 +0100

    chore(shortint): move tuniform 0.10 parameters into their own folder

--- tfhe/src/shortint/parameters/compact_public_key_only/p_fail_2_minus_64/ks_pbs.rs
+++ tfhe/src/shortint/parameters/compact_public_key_only/p_fail_2_minus_64/ks_pbs.rs
@@ -48,15 +48,3 @@ pub const V0_11_PARAM_PKE_TO_BIG_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M64_ZKV1:
     expansion_kind: CompactCiphertextListExpansionKind::RequiresCasting,
 }
 .validate();
-
-/// This legacy parameter set were used with the v1 pke zk scheme on TFHE-rs v0.10 and lower
-pub const V0_10_PARAM_PKE_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M64_ZKV1:
-    CompactPublicKeyEncryptionParameters = CompactPublicKeyEncryptionParameters {
-    encryption_lwe_dimension: LweDimension(1024),
-    encryption_noise_distribution: DynamicDistribution::new_t_uniform(42),
-    message_modulus: MessageModulus(4),
-    carry_modulus: CarryModulus(4),
-    ciphertext_modulus: CiphertextModulus::new_native(),
-    expansion_kind: CompactCiphertextListExpansionKind::RequiresCasting,
-}
-.validate();
