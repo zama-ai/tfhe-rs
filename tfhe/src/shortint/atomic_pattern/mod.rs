4d8d68366c443f18f43e017144e360184b451b27
Nicolas Sarlin <nicolas.sarlin@zama.ai>
2025-02-10 17:18:21 +0100

    feat(shortint): add the dynamic ap

--- tfhe/src/shortint/atomic_pattern/mod.rs
+++ tfhe/src/shortint/atomic_pattern/mod.rs
@@ -6,6 +6,8 @@
 
 pub mod classical;
 
+use std::any::Any;
+
 use serde::{Deserialize, Serialize};
 use tfhe_csprng::seeders::Seed;
 use tfhe_versionable::Versionize;
@@ -113,6 +115,66 @@ pub trait AtomicPatternMut: AtomicPattern {
     fn set_deterministic_execution(&mut self, new_deterministic_execution: bool);
 }
 
+// Prevent user implementation of this trait
+mod private {
+    use super::*;
+    /// This trait allow the use of [`AtomicPatternOperations`] in a dynamic context.
+    ///
+    /// It should be automatically derived for types that implement "PartialEq + Clone +
+    /// AtomicPatternMut"
+    pub trait DynamicAtomicPattern:
+        AtomicPatternMut
+        + Send
+        + Sync
+        + std::panic::UnwindSafe
+        + std::panic::RefUnwindSafe
+        + std::fmt::Debug
+    {
+        fn as_any(&self) -> &dyn Any;
+        fn dyn_eq(&self, other: &dyn DynamicAtomicPattern) -> bool;
+        fn dyn_clone(&self) -> Box<dyn DynamicAtomicPattern>;
+    }
+
+    impl<AP> DynamicAtomicPattern for AP
+    where
+        AP: 'static
+            + PartialEq
+            + Clone
+            + AtomicPatternMut
+            + Send
+            + Sync
+            + std::panic::UnwindSafe
+            + std::panic::RefUnwindSafe
+            + std::fmt::Debug,
+    {
+        fn as_any(&self) -> &dyn Any {
+            self
+        }
+
+        fn dyn_eq(&self, other: &dyn DynamicAtomicPattern) -> bool {
+            // Do a type-safe casting. If the types are different,
+            // return false, otherwise test the values for equality.
+            other.as_any().downcast_ref::<AP>() == Some(self)
+        }
+
+        fn dyn_clone(&self) -> Box<dyn DynamicAtomicPattern> {
+            Box::new(self.clone())
+        }
+    }
+
+    impl Clone for Box<dyn DynamicAtomicPattern> {
+        fn clone(&self) -> Self {
+            self.dyn_clone()
+        }
+    }
+
+    impl PartialEq for Box<dyn DynamicAtomicPattern> {
+        fn eq(&self, other: &Self) -> bool {
+            self.dyn_eq(other.as_ref())
+        }
+    }
+}
+
 // This blancket impl is used to allow "views" of server keys, without having to re-implement the
 // trait
 impl<T: AtomicPattern> AtomicPattern for &T {
@@ -181,8 +243,11 @@ impl<T: AtomicPattern> AtomicPattern for &T {
 /// The server key materials for all the supported Atomic Patterns
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize, Versionize)]
 #[versionize(AtomicPatternServerKeyVersions)]
+#[allow(clippy::large_enum_variant)] // The most common variant should be `Classical` so we optimize for it
 pub enum AtomicPatternServerKey {
     Classical(ClassicalAtomicPatternServerKey),
+    #[serde(skip)]
+    Dynamic(Box<dyn private::DynamicAtomicPattern>),
 }
 
 impl AtomicPatternServerKey {
@@ -201,24 +266,28 @@ impl AtomicPattern for AtomicPatternServerKey {
     fn ciphertext_lwe_dimension(&self) -> LweDimension {
         match self {
             Self::Classical(ap) => ap.ciphertext_lwe_dimension(),
+            Self::Dynamic(ap) => ap.ciphertext_lwe_dimension(),
         }
     }
 
     fn ciphertext_modulus(&self) -> CiphertextModulus {
         match self {
             Self::Classical(ap) => ap.ciphertext_modulus(),
+            Self::Dynamic(ap) => ap.ciphertext_modulus(),
         }
     }
 
     fn ciphertext_decompression_method(&self) -> MsDecompressionType {
         match self {
             Self::Classical(ap) => ap.ciphertext_decompression_method(),
+            Self::Dynamic(ap) => ap.ciphertext_decompression_method(),
         }
     }
 
     fn apply_lookup_table_assign(&self, ct: &mut Ciphertext, acc: &LookupTableOwned) {
         match self {
             Self::Classical(ap) => ap.apply_lookup_table_assign(ct, acc),
+            Self::Dynamic(ap) => ap.apply_lookup_table_assign(ct, acc),
         }
     }
 
@@ -229,24 +298,28 @@ impl AtomicPattern for AtomicPatternServerKey {
     ) -> Vec<Ciphertext> {
         match self {
             Self::Classical(ap) => ap.apply_many_lookup_table(ct, lut),
+            Self::Dynamic(ap) => ap.apply_many_lookup_table(ct, lut),
         }
     }
 
     fn lookup_table_size(&self) -> LookupTableSize {
         match self {
             Self::Classical(ap) => ap.lookup_table_size(),
+            Self::Dynamic(ap) => ap.lookup_table_size(),
         }
     }
 
     fn kind(&self) -> AtomicPatternKind {
         match self {
             Self::Classical(ap) => ap.kind(),
+            Self::Dynamic(ap) => ap.kind(),
         }
     }
 
     fn deterministic_execution(&self) -> bool {
         match self {
             Self::Classical(ap) => ap.deterministic_execution(),
+            Self::Dynamic(ap) => ap.deterministic_execution(),
         }
     }
 
@@ -260,12 +333,16 @@ impl AtomicPattern for AtomicPatternServerKey {
             Self::Classical(ap) => {
                 ap.generate_oblivious_pseudo_random(seed, random_bits_count, full_bits_count)
             }
+            Self::Dynamic(ap) => {
+                ap.generate_oblivious_pseudo_random(seed, random_bits_count, full_bits_count)
+            }
         }
     }
 
     fn switch_modulus_and_compress(&self, ct: &Ciphertext) -> CompressedModulusSwitchedCiphertext {
         match self {
             Self::Classical(ap) => ap.switch_modulus_and_compress(ct),
+            Self::Dynamic(ap) => ap.switch_modulus_and_compress(ct),
         }
     }
 
@@ -276,12 +353,14 @@ impl AtomicPattern for AtomicPatternServerKey {
     ) -> Ciphertext {
         match self {
             Self::Classical(ap) => ap.decompress_and_apply_lookup_table(compressed_ct, lut),
+            Self::Dynamic(ap) => ap.decompress_and_apply_lookup_table(compressed_ct, lut),
         }
     }
 
     fn prepare_for_noise_squashing(&self, ct: &Ciphertext) -> LweCiphertextOwned<u64> {
         match self {
             Self::Classical(ap) => ap.prepare_for_noise_squashing(ct),
+            Self::Dynamic(ap) => ap.prepare_for_noise_squashing(ct),
         }
     }
 }
@@ -290,6 +369,7 @@ impl AtomicPatternMut for AtomicPatternServerKey {
     fn set_deterministic_execution(&mut self, new_deterministic_execution: bool) {
         match self {
             Self::Classical(ap) => ap.set_deterministic_execution(new_deterministic_execution),
+            Self::Dynamic(ap) => ap.set_deterministic_execution(new_deterministic_execution),
         }
     }
 }
