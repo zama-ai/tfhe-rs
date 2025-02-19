4033f857e49dfd04d881c989cc1fc26621b7e9ea
Andrei Stoian <andrei.stoian@zama.ai>
2025-02-19 17:46:27 +0100

    fix: add more checks, change sample extract assert

--- tfhe/src/core_crypto/gpu/algorithms/glwe_linear_algebra.rs
+++ tfhe/src/core_crypto/gpu/algorithms/glwe_linear_algebra.rs
@@ -2,7 +2,7 @@ use crate::core_crypto::gpu::vec::CudaVec;
 use crate::core_crypto::gpu::{
     CudaStreams,
 };
-use crate::core_crypto::prelude::{MonomialDegree, UnsignedInteger, UnsignedTorus};
+use crate::core_crypto::prelude::{GlweCiphertextCount, MonomialDegree, UnsignedInteger, UnsignedTorus};
 use tfhe_cuda_backend::bindings::cuda_wrapping_polynomial_mul_one_to_many_64;
 use tfhe_cuda_backend::bindings::cuda_glwe_wrapping_polynomial_mul_one_to_many_64;
 use crate::core_crypto::gpu::glwe_ciphertext_list::CudaGlweCiphertextList;
@@ -90,16 +90,27 @@ pub fn cuda_glwe_dot_product_with_clear_one_to_many<Scalar>(
 ) where
     Scalar: UnsignedInteger + UnsignedTorus
 {
+    assert_eq!(rhs.len() % lhs.polynomial_size().0, 0,
+            "GLWE dot product rhs must have size a multiple of the polynomial size of the lhs"
+    );
     let n_polys = rhs.len() / lhs.polynomial_size().0;
+    assert_eq!(out.lwe_ciphertext_count().0, n_polys, "GLWE dot product output LWE list must have size equal to the number of clear polys");
+    assert_eq!(lhs.glwe_ciphertext_count().0, 1, "GLWE dot product implemented only for a single GLWE in the lhs list");
+
     let mut glwe_list = CudaGlweCiphertextList::<Scalar>::new(
         lhs.glwe_dimension(),
         lhs.polynomial_size(),
-        lhs.glwe_ciphertext_count(),
+        GlweCiphertextCount(n_polys),
         lhs.ciphertext_modulus(),
         &stream
     );
 
-    let degree = MonomialDegree(lhs.polynomial_size().0 - 1);
+    let nths: Vec<MonomialDegree> = (0usize..n_polys)
+        .map(|i| MonomialDegree(i * lhs.polynomial_size().0 + lhs.polynomial_size().0 - 1))
+        .collect();
+
+    assert_eq!(nths.len(), n_polys, "Nths vector has the wrong size");
+
     cuda_glwe_wrapping_polynomial_mul_one_to_many(lhs, rhs, &mut glwe_list, &stream);
-    cuda_extract_lwe_samples_from_glwe_ciphertext_list(&glwe_list, out, std::vec::from_elem(degree, n_polys).as_slice(), stream);
+    cuda_extract_lwe_samples_from_glwe_ciphertext_list(&glwe_list, out, nths.as_slice(), stream);
 }
