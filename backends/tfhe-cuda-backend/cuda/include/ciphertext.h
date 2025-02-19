1a0e19bb9bf0b48ced489501fdf803cd5d9d801c
Pedro Alves <pedro.alves@zama.ai>
2025-02-19 16:47:56 -0300

    feat(gpu): refactor the sample extract entry point so the user can pass how many LWEs should be extracted per GLWE

--- backends/tfhe-cuda-backend/cuda/include/ciphertext.h
+++ backends/tfhe-cuda-backend/cuda/include/ciphertext.h
@@ -18,7 +18,7 @@ void cuda_convert_lwe_ciphertext_vector_to_cpu_64(void *stream,
 void cuda_glwe_sample_extract_64(void *stream, uint32_t gpu_index,
                                  void *lwe_array_out, void const *glwe_array_in,
                                  uint32_t const *nth_array, uint32_t num_nths,
-                                 uint32_t glwe_dimension,
+                                 uint32_t lwe_per_glwe, uint32_t glwe_dimension,
                                  uint32_t polynomial_size);
 }
 #endif
