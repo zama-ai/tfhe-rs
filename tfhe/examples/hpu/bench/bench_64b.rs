7554e735da44e47ce14daa688ee0b2abcfba0a8e
Baptiste Roux <baptiste.roux@zama.ai>
2025-03-06 10:34:20 +0100

    refactor(hpu): Replace hpu example with all-in-one
    All previous Hpu example application coud be supersed by a all-in-one
    application and thus reduce maintance work.
    Delete old showcase application of the explicit xfer API (Replaced by hlapi showcase)
    NB: Explicit API is currently kept for bench/test purpose only.

--- tfhe/examples/hpu/bench/bench_64b.rs
+++
@@ -1,8 +0,0 @@
-use tfhe::prelude::*;
-mod bench_macro;
-use bench_macro::*;
-
-use tfhe::core_crypto::commons::generators::DeterministicSeeder;
-use tfhe::core_crypto::prelude::DefaultRandomGenerator;
-
-impl_hpu_bench!(FheUint64, FheUint64Id, u64, 64);
