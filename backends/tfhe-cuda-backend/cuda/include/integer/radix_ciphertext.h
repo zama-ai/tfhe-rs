43e1510f249293b11eb0bf9371b168484200ed51
Pedro Alves <pedro.alves@zama.ai>
2025-03-10 12:58:58 -0300

    feat(gpu): implement ZK's expand

--- backends/tfhe-cuda-backend/cuda/include/integer/radix_ciphertext.h
+++ backends/tfhe-cuda-backend/cuda/include/integer/radix_ciphertext.h
@@ -8,4 +8,7 @@ void release_radix_ciphertext_async(cudaStream_t const stream,
 void reset_radix_ciphertext_blocks(CudaRadixCiphertextFFI *data,
                                    uint32_t new_num_blocks);
 
+void into_radix_ciphertext(CudaRadixCiphertextFFI *radix, void *lwe_array,
+                           const uint32_t num_radix_blocks,
+                           const uint32_t lwe_dimension);
 #endif
