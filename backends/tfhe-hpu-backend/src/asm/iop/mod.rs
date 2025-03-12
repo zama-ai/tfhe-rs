78bd734badcae79056e848d8887c49cecad89fd7
Baptiste Roux <baptiste.roux@zama.ai>
2025-03-12 12:48:14 +0100

    feat(hpu): Add IOP_IF_THEN_ZERO and IOP_ERC_20
    IF_THEN_ZERO is an altered version of IF_THEN_ELSE than take 0 as default value.
    ERC_20 is a custom iop dedicated to erc_20 computation. Its a first attempt and mainly a placeholder for future work.
    It will be use to test various way to call custom iop from HighLevelApi.
    
    Change test macro to support multi-output IOp correctly.

--- backends/tfhe-hpu-backend/src/asm/iop/mod.rs
+++ backends/tfhe-hpu-backend/src/asm/iop/mod.rs
@@ -51,18 +51,23 @@ impl<const D: usize, const S: usize> From<ConstIOpProto<D, S>> for IOpProto {
 }
 
 // Define some common iop format
-pub const IOP_CT_CT: ConstIOpProto<1, 2> = ConstIOpProto {
+pub const IOP_CT_F_2CT: ConstIOpProto<1, 2> = ConstIOpProto {
     dst: [VarMode::Native; 1],
     src: [VarMode::Native; 2],
     imm: 0,
 };
-pub const IOP_CT_CT_BOOL: ConstIOpProto<1, 3> = ConstIOpProto {
+pub const IOP_CT_F_2CT_BOOL: ConstIOpProto<1, 3> = ConstIOpProto {
     dst: [VarMode::Native; 1],
     src: [VarMode::Native, VarMode::Native, VarMode::Bool],
     imm: 0,
 };
+pub const IOP_CT_F_CT_BOOL: ConstIOpProto<1, 2> = ConstIOpProto {
+    dst: [VarMode::Native; 1],
+    src: [VarMode::Native, VarMode::Bool],
+    imm: 0,
+};
 
-pub const IOP_CT_SCALAR: ConstIOpProto<1, 1> = ConstIOpProto {
+pub const IOP_CT_F_CT_SCALAR: ConstIOpProto<1, 1> = ConstIOpProto {
     dst: [VarMode::Native; 1],
     src: [VarMode::Native; 1],
     imm: 1,
@@ -74,28 +79,38 @@ pub const IOP_CMP: ConstIOpProto<1, 2> = ConstIOpProto {
     imm: 0,
 };
 
+pub const IOP_2CT_F_3CT: ConstIOpProto<2, 3> = ConstIOpProto {
+    dst: [VarMode::Native; 2],
+    src: [VarMode::Native; 3],
+    imm: 0,
+};
+
 use crate::iop;
 use arg::IOpFormat;
 use lazy_static::lazy_static;
 use std::collections::HashMap;
 iop!(
-    [IOP_CT_SCALAR -> "ADDS", opcode::ADDS],
-    [IOP_CT_SCALAR -> "SUBS", opcode::SUBS],
-    [IOP_CT_SCALAR -> "SSUB", opcode::SSUB],
-    [IOP_CT_SCALAR -> "MULS", opcode::MULS],
-    [IOP_CT_CT -> "ADD", opcode::ADD],
-    [IOP_CT_CT -> "ADDK", opcode::ADDK],
-    [IOP_CT_CT -> "SUB", opcode::SUB],
-    [IOP_CT_CT -> "SUBK", opcode::SUBK],
-    [IOP_CT_CT -> "MUL", opcode::MUL],
-    [IOP_CT_CT -> "BW_AND", opcode::BW_AND],
-    [IOP_CT_CT -> "BW_OR", opcode::BW_OR],
-    [IOP_CT_CT -> "BW_XOR", opcode::BW_XOR],
+    [IOP_CT_F_CT_SCALAR -> "ADDS", opcode::ADDS],
+    [IOP_CT_F_CT_SCALAR -> "SUBS", opcode::SUBS],
+    [IOP_CT_F_CT_SCALAR -> "SSUB", opcode::SSUB],
+    [IOP_CT_F_CT_SCALAR -> "MULS", opcode::MULS],
+    [IOP_CT_F_CT_SCALAR -> "MULSF", opcode::MULSF],
+    [IOP_CT_F_2CT -> "ADD", opcode::ADD],
+    [IOP_CT_F_2CT -> "ADDK", opcode::ADDK],
+    [IOP_CT_F_2CT -> "SUB", opcode::SUB],
+    [IOP_CT_F_2CT -> "SUBK", opcode::SUBK],
+    [IOP_CT_F_2CT -> "MUL", opcode::MUL],
+    [IOP_CT_F_2CT -> "MULF", opcode::MULF],
+    [IOP_CT_F_2CT -> "BW_AND", opcode::BW_AND],
+    [IOP_CT_F_2CT -> "BW_OR", opcode::BW_OR],
+    [IOP_CT_F_2CT -> "BW_XOR", opcode::BW_XOR],
     [IOP_CMP -> "CMP_GT", opcode::CMP_GT],
     [IOP_CMP -> "CMP_GTE", opcode::CMP_GTE],
     [IOP_CMP -> "CMP_LT", opcode::CMP_LT],
     [IOP_CMP -> "CMP_LTE", opcode::CMP_LTE],
     [IOP_CMP -> "CMP_EQ", opcode::CMP_EQ],
     [IOP_CMP -> "CMP_NEQ", opcode::CMP_NEQ],
-    [IOP_CT_CT_BOOL -> "IF_THEN_ELSE", opcode::IF_THEN_ELSE],
+    [IOP_CT_F_CT_BOOL -> "IF_THEN_ZERO", opcode::IF_THEN_ZERO],
+    [IOP_CT_F_2CT_BOOL -> "IF_THEN_ELSE", opcode::IF_THEN_ELSE],
+    [IOP_2CT_F_3CT -> "ERC_20", opcode::ERC_20],
 );
