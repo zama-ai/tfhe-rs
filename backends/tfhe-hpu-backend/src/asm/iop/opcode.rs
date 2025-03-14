7daa8e7d7deee95b66d5516690e1d45c07798e91
Helder Campos <helder.campos@zama.ai>
2025-03-14 15:08:09 +0000

    chore(hpu): Dividing Ilp and Llt firmware
    - Llt stands for low latency. All instructions there should be optimize
      to have the least latency possible, at the expense of throughput.

--- backends/tfhe-hpu-backend/src/asm/iop/opcode.rs
+++ backends/tfhe-hpu-backend/src/asm/iop/opcode.rs
@@ -23,16 +23,12 @@ pub const ADDS: u8 = 0xA0;
 pub const SUBS: u8 = 0xA1;
 pub const SSUB: u8 = 0xA2;
 pub const MULS: u8 = 0xA3;
-pub const MULSF: u8 = 0xA4;
 
 // Ct x Ct -------------------------------------------------------------------
 // Arith operations
 pub const ADD: u8 = 0xE0;
-pub const ADDK: u8 = 0xE1;
 pub const SUB: u8 = 0xE2;
-pub const SUBK: u8 = 0xE3;
 pub const MUL: u8 = 0xE4;
-pub const MULF: u8 = 0xE5;
 
 // BW operations
 pub const BW_AND: u8 = 0xD0;
