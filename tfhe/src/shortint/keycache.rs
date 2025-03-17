6a6c3f41c59cda286b108fdddc9a6b4be1999cca
Baptiste Roux <baptiste.roux@zama.ai>
2025-03-17 10:56:33 +0100

    feat(hpu): Benches add Hpu parameters in KeyCache
    Also update hlapi benches to use the NamedParams traits with Hpu

--- tfhe/src/shortint/keycache.rs
+++ tfhe/src/shortint/keycache.rs
@@ -12,7 +12,7 @@ use crate::shortint::{ClientKey, KeySwitchingKey, ServerKey};
 use serde::{Deserialize, Serialize};
 // TODO correct version for HPU parameters set
 // and thus remove this stmt
-use v1_0::V1_0_PARAM_MESSAGE_2_CARRY_2_HPU_PK_KS_PBS_GAUSSIAN_2M64;
+use crate::shortint::parameters::v1_0::V1_0_HPU_PARAM_MESSAGE_2_CARRY_2_KS_PBS_GAUSSIAN_2M64;
 
 use super::atomic_pattern::AtomicPatternParameters;
 
@@ -396,8 +396,6 @@ named_params_impl!( ShortintParameterSet =>
     LEGACY_WOPBS_ONLY_2_BLOCKS_PARAM_MESSAGE_7_CARRY_0_KS_PBS,
     LEGACY_WOPBS_ONLY_2_BLOCKS_PARAM_MESSAGE_7_CARRY_1_KS_PBS,
     LEGACY_WOPBS_ONLY_2_BLOCKS_PARAM_MESSAGE_8_CARRY_0_KS_PBS,
-    // HPU
-    V1_0_PARAM_MESSAGE_2_CARRY_2_HPU_PK_KS_PBS_GAUSSIAN_2M64,
 
     // Coverage
     #[cfg(tarpaulin)]
@@ -412,6 +410,9 @@ named_params_impl!( ShortintParameterSet =>
     COVERAGE_PARAM_MESSAGE_2_CARRY_2_COMPACT_PK_PBS_KS_GAUSSIAN_2M64,
     #[cfg(tarpaulin)]
     COVERAGE_PARAM_MESSAGE_2_CARRY_2_COMPACT_PK_KS_PBS_GAUSSIAN_2M64,
+
+    #[cfg(feature = "hpu")]
+    V1_0_HPU_PARAM_MESSAGE_2_CARRY_2_KS_PBS_GAUSSIAN_2M64,
 );
 
 impl NamedParam for ClassicPBSParameters {
