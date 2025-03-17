e3ee2f2c8da8a70a7fb4106454b3a768c40a004b
Baptiste Roux <baptiste.roux@zama.ai>
2025-03-17 10:56:33 +0100

    feat(hpu): Benches add Hpu parameters in KeyCache
    Also update hlapi benches to use the NamedParams traits with Hpu

--- tfhe/src/shortint/parameters/v1_0/classic/compact_pk/gaussian/p_fail_2_minus_64/ks_pbs.rs
+++ tfhe/src/shortint/parameters/v1_0/classic/compact_pk/gaussian/p_fail_2_minus_64/ks_pbs.rs
@@ -868,28 +868,3 @@ pub const V1_0_PARAM_MESSAGE_8_CARRY_0_COMPACT_PK_KS_PBS_GAUSSIAN_2M64: ClassicP
         encryption_key_choice: EncryptionKeyChoice::Big,
         modulus_switch_noise_reduction_params: None,
     };
-
-// HPU parameters set
-pub const V1_0_PARAM_MESSAGE_2_CARRY_2_HPU_PK_KS_PBS_GAUSSIAN_2M64: ClassicPBSParameters =
-    ClassicPBSParameters {
-        lwe_dimension: LweDimension(804),
-        glwe_dimension: GlweDimension(1),
-        polynomial_size: PolynomialSize(2048),
-        lwe_noise_distribution: DynamicDistribution::new_gaussian_from_std_dev(StandardDev(
-            5.963599673924788e-6,
-        )),
-        glwe_noise_distribution: DynamicDistribution::new_gaussian_from_std_dev(StandardDev(
-            2.8452674713391114e-15,
-        )),
-        pbs_base_log: DecompositionBaseLog(23),
-        pbs_level: DecompositionLevelCount(1),
-        ks_base_log: DecompositionBaseLog(2),
-        ks_level: DecompositionLevelCount(8),
-        message_modulus: MessageModulus(4),
-        carry_modulus: CarryModulus(4),
-        max_noise_level: MaxNoiseLevel::new(5),
-        log2_p_fail: -64.014,
-        ciphertext_modulus: CiphertextModulus::new_native(),
-        encryption_key_choice: EncryptionKeyChoice::Big,
-        modulus_switch_noise_reduction_params: None,
-    };
