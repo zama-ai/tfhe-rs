191f10585f05c7e9463865bef37850218e2206e5
Baptiste Roux <baptiste.roux@zama.ai>
2025-03-24 13:40:43 +0100

    feat(hpu): Move opportunistic config in RtlConfig
    Rename opportunistic to flush_opportunism and retrieved the value from
    RtlConfig instead of HpuParameters

--- backends/tfhe-hpu-backend/src/entities/parameters.rs
+++ backends/tfhe-hpu-backend/src/entities/parameters.rs
@@ -18,7 +18,6 @@ pub struct HpuPBSParameters {
     pub message_width: usize,
     pub carry_width: usize,
     pub ciphertext_width: usize,
-    pub opportunistic: bool,
 }
 // Manual implementation of Eq trait
 // Indeed, we can handle strict comparaison of f64
