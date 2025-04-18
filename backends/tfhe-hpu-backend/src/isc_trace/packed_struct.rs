ac3b4414ea31169fd34c30c82b134041af517d71
Baptiste Roux <baptiste.roux@zama.ai>
2025-04-18 13:47:29 +0200

    refactor(hpu): move packed_struct inside isc_trace
    There is no need to have a separated crate, thus fuse code with isc_trace, the only module that used it

--- backends/tfhe-hpu-backend/src/isc_trace/packed_struct.rs
+++ backends/tfhe-hpu-backend/src/isc_trace/packed_struct.rs
@@ -3,7 +3,38 @@ use std::error::Error;
 use std::fmt;
 use std::fmt::{Debug, Display, Formatter};
 
-mod macros;
+/// Macro used to define packed struct
+macro_rules! integer_packed_struct {
+    ($x: ident) => {
+        impl<O> PackedStructLsb<O> for $x
+        where
+            O: bitvec::store::BitStore,
+        {
+            fn from_bit_slice_le(slice: &BitSlice<O, Lsb0>) -> Result<Self, Box<dyn Error>> {
+                if slice.len() != 0 {
+                    Ok(slice[0..($x::BITS as usize).min(slice.len())].load::<$x>())
+                } else {
+                    Err(NoMoreBits)?
+                }
+            }
+            fn to_bit_slice_le(&self, dst: &mut BitSlice<O, Lsb0>) -> Result<(), Box<dyn Error>> {
+                if dst.len() == 0 {
+                    Err(NoMoreBits)?
+                } else {
+                    let size = dst.len().min($x::len());
+                    dst[0..size].clone_from_bitslice(&self.try_view_bits::<Lsb0>()?[0..size]);
+                    Ok(())
+                }
+            }
+        }
+
+        impl Len for $x {
+            fn len() -> usize {
+                $x::BITS as usize
+            }
+        }
+    };
+}
 
 #[derive(Debug)]
 pub struct NoMoreBits;
