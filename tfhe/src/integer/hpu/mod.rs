5193a1cb03014ea044884c070d366bfc19f045ee
tmontaigu <thomas.montaigu@laposte.net>
2025-04-22 15:41:30 +0200

    chore: fix typos

--- tfhe/src/integer/hpu/mod.rs
+++ tfhe/src/integer/hpu/mod.rs
@@ -1,4 +1,5 @@
-use crate::{core_crypto::hpu::from_with::IntoWith, shortint::ClassicPBSParameters};
+use crate::core_crypto::hpu::from_with::IntoWith;
+use crate::shortint::ClassicPBSParameters;
 use tfhe_hpu_backend::prelude::HpuDevice;
 
 use super::CompressedServerKey;
@@ -19,22 +20,22 @@ pub fn init_device(device: &HpuDevice, server_key: CompressedServerKey) -> crate
 
             // Check that given key is compliant with current device configuration
             if tfhe_params.lwe_dimension != bsk.input_lwe_dimension() {
-                return Err("BootstrappingKey has imcompatible input_lwe_dimension".into());
+                return Err("BootstrappingKey has incompatible input_lwe_dimension".into());
             }
             if tfhe_params.glwe_dimension.to_glwe_size() != bsk.glwe_size() {
-                return Err("BootstrappingKey has imcompatible glwe_size".into());
+                return Err("BootstrappingKey has incompatible glwe_size".into());
             }
             if tfhe_params.polynomial_size != bsk.polynomial_size() {
-                return Err("BootstrappingKey has imcompatible polynomial size".into());
+                return Err("BootstrappingKey has incompatible polynomial size".into());
             }
             if tfhe_params.pbs_base_log != bsk.decomposition_base_log() {
-                return Err("BootstrappingKey has imcompatible decomposition_base_log".into());
+                return Err("BootstrappingKey has incompatible decomposition_base_log".into());
             }
             if tfhe_params.pbs_level != bsk.decomposition_level_count() {
-                return Err("BootstrappingKey has imcompatible decomposition_level_count".into());
+                return Err("BootstrappingKey has incompatible decomposition_level_count".into());
             }
             if tfhe_params.ciphertext_modulus != bsk.ciphertext_modulus() {
-                return Err("BootstrappingKey has imcompatible ciphertext_modulus".into());
+                return Err("BootstrappingKey has incompatible ciphertext_modulus".into());
             }
             Ok(bsk)
         }
@@ -50,10 +51,10 @@ pub fn init_device(device: &HpuDevice, server_key: CompressedServerKey) -> crate
         .decompress_into_lwe_keyswitch_key();
     // Check that given key is compliant with current device configuration
     if tfhe_params.ks_base_log != ksk.decomposition_base_log() {
-        return Err("KeyswitchingKey has imcompatible decomposition_base_log".into());
+        return Err("KeyswitchingKey has incompatible decomposition_base_log".into());
     }
     if tfhe_params.ks_level != ksk.decomposition_level_count() {
-        return Err("KeyswitchingKey has imcompatible decomposition_level_count".into());
+        return Err("KeyswitchingKey has incompatible decomposition_level_count".into());
     }
     let hpu_ksk = ksk.as_view().into_with(params);
 
