36cd9a99e7243dcf3cd1da2a52c73cefb92fb4c6
Nicolas Sarlin <nicolas.sarlin@zama.ai>
2025-04-23 18:40:00 +0200

    fix(core): remove additional body coeff in multi bit ms compression

--- tfhe/src/core_crypto/backward_compatibility/entities/compressed_modulus_switched_multi_bit_lwe_ciphertext.rs
+++ tfhe/src/core_crypto/backward_compatibility/entities/compressed_modulus_switched_multi_bit_lwe_ciphertext.rs
@@ -1,12 +1,56 @@
-use tfhe_versionable::VersionsDispatch;
+use std::convert::Infallible;
 
-use crate::core_crypto::prelude::{
-    CastFrom, CastInto, CompressedModulusSwitchedMultiBitLweCiphertext, UnsignedInteger,
-};
+use packed_integers::PackedIntegers;
+use tfhe_versionable::{Upgrade, Version, VersionsDispatch};
+
+use crate::core_crypto::prelude::*;
+
+#[derive(Version)]
+pub struct CompressedModulusSwitchedMultiBitLweCiphertextV0<
+    Scalar: UnsignedInteger + CastInto<usize> + CastFrom<usize>,
+> {
+    body: usize,
+    packed_mask: PackedIntegers<usize>,
+    packed_diffs: Option<PackedIntegers<usize>>,
+    lwe_dimension: LweDimension,
+    uncompressed_ciphertext_modulus: CiphertextModulus<Scalar>,
+    grouping_factor: LweBskGroupingFactor,
+}
+
+impl<Scalar: UnsignedInteger + CastInto<usize> + CastFrom<usize>>
+    Upgrade<CompressedModulusSwitchedMultiBitLweCiphertext<Scalar>>
+    for CompressedModulusSwitchedMultiBitLweCiphertextV0<Scalar>
+{
+    type Error = Infallible;
+
+    fn upgrade(
+        self,
+    ) -> Result<CompressedModulusSwitchedMultiBitLweCiphertext<Scalar>, Self::Error> {
+        // In the previous version a last coefficient for the body was stored along the mask
+        // elements. This was a duplicate, so we remove it.
+
+        let mask_without_body = PackedIntegers::from_raw_parts(
+            self.packed_mask.packed_coeffs()[..self.lwe_dimension.0].to_vec(),
+            self.packed_mask.log_modulus(),
+            self.packed_mask.initial_len(),
+        );
+        Ok(
+            CompressedModulusSwitchedMultiBitLweCiphertext::from_raw_parts(
+                self.body,
+                mask_without_body,
+                self.packed_diffs,
+                self.lwe_dimension,
+                self.uncompressed_ciphertext_modulus,
+                self.grouping_factor,
+            ),
+        )
+    }
+}
 
 #[derive(VersionsDispatch)]
 pub enum CompressedModulusSwitchedMultiBitLweCiphertextVersions<
     Scalar: UnsignedInteger + CastInto<usize> + CastFrom<usize>,
 > {
-    V0(CompressedModulusSwitchedMultiBitLweCiphertext<Scalar>),
+    V0(CompressedModulusSwitchedMultiBitLweCiphertextV0<Scalar>),
+    V1(CompressedModulusSwitchedMultiBitLweCiphertext<Scalar>),
 }
