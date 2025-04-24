acd136af3081d80b3b1daf0647f07d96c9f1130a
tmontaigu <thomas.montaigu@laposte.net>
2025-04-24 10:08:17 +0000

    chore: other fixes

--- tfhe/src/high_level_api/global_state.rs
+++ tfhe/src/high_level_api/global_state.rs
@@ -160,6 +160,7 @@ where
             .as_ref()
             .ok_or(UninitializedServerKey)
             .unwrap_display();
+        #[allow(irrefutable_let_patterns, reason = "It depends on hardware features")]
         let InternalServerKey::Cpu(cpu_key) = key else {
             panic!(
                 "Cpu key requested but only the key for {:?} is available",
