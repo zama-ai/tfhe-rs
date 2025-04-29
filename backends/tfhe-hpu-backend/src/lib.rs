e28f66aa02f7914e3ccff05fa14e618de5b24122
Baptiste Roux <baptiste.roux@zama.ai>
2025-04-29 18:07:15 +0200

    fix(hpu): Add clear compile error with hw-v80/hw-xrt
    Only one feature could be enabled at a time

--- backends/tfhe-hpu-backend/src/lib.rs
+++ backends/tfhe-hpu-backend/src/lib.rs
@@ -1,3 +1,6 @@
+#[cfg(all(feature = "hw-v80", feature = "hw-xrt"))]
+compile_error! {"hw-v80 and hw-xrt features are used to select the targeted fpga family. Only one fpga family could be used at a time thus these features are mutually exclusive. Only enable one of them at a time. "}
+
 mod entities;
 
 #[cfg(not(feature = "utils"))]
