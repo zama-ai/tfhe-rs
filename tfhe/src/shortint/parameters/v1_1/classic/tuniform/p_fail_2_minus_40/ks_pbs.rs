68ce859abff9c8b15476ddc392643cae361e43d4
Arthur Meyre <arthur.meyre@zama.ai>
2025-05-02 17:38:11 +0200

    chore: update v1_1 parameters so that comments are doc comments
    - this allows to keep relevant information with param_dedup, as param_dedup
    uses syn, comments are lost as syn does not preserver comments in the AST

--- tfhe/src/shortint/parameters/v1_1/classic/tuniform/p_fail_2_minus_40/ks_pbs.rs
+++ tfhe/src/shortint/parameters/v1_1/classic/tuniform/p_fail_2_minus_40/ks_pbs.rs
@@ -4,7 +4,7 @@ use crate::shortint::parameters::{
     MaxNoiseLevel, MessageModulus, PolynomialSize,
 };
 
-// p-fail = 2^-40.525, algorithmic cost ~ 49, 2-norm = 3
+/// p-fail = 2^-40.525, algorithmic cost ~ 49, 2-norm = 3
 pub const V1_1_PARAM_MESSAGE_1_CARRY_1_KS_PBS_TUNIFORM_2M40: ClassicPBSParameters =
     ClassicPBSParameters {
         lwe_dimension: LweDimension(799),
@@ -25,7 +25,7 @@ pub const V1_1_PARAM_MESSAGE_1_CARRY_1_KS_PBS_TUNIFORM_2M40: ClassicPBSParameter
         modulus_switch_noise_reduction_params: None,
     };
 
-// p-fail = 2^-57.015, algorithmic cost ~ 107, 2-norm = 5
+/// p-fail = 2^-57.015, algorithmic cost ~ 107, 2-norm = 5
 pub const V1_1_PARAM_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M40: ClassicPBSParameters =
     ClassicPBSParameters {
         lwe_dimension: LweDimension(839),
@@ -46,7 +46,7 @@ pub const V1_1_PARAM_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M40: ClassicPBSParameter
         modulus_switch_noise_reduction_params: None,
     };
 
-// p-fail = 2^-50.002, algorithmic cost ~ 816, 2-norm = 9
+/// p-fail = 2^-50.002, algorithmic cost ~ 816, 2-norm = 9
 pub const V1_1_PARAM_MESSAGE_3_CARRY_3_KS_PBS_TUNIFORM_2M40: ClassicPBSParameters =
     ClassicPBSParameters {
         lwe_dimension: LweDimension(958),
@@ -67,7 +67,7 @@ pub const V1_1_PARAM_MESSAGE_3_CARRY_3_KS_PBS_TUNIFORM_2M40: ClassicPBSParameter
         modulus_switch_noise_reduction_params: None,
     };
 
-// p-fail = 2^-41.009, algorithmic cost ~ 4165, 2-norm = 17
+/// p-fail = 2^-41.009, algorithmic cost ~ 4165, 2-norm = 17
 pub const V1_1_PARAM_MESSAGE_4_CARRY_4_KS_PBS_TUNIFORM_2M40: ClassicPBSParameters =
     ClassicPBSParameters {
         lwe_dimension: LweDimension(1077),
