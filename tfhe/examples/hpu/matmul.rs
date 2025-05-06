27e93ebad8bca3fe850f74de89fa3b5160bb8960
lsainati <lucas.sainati@zama.ai>
2025-05-06 13:43:38 +0200

    feat(hpu): Update matmul example to perform asymmetric matrix multiplications

--- tfhe/examples/hpu/matmul.rs
+++ tfhe/examples/hpu/matmul.rs
@@ -4,8 +4,6 @@ use tfhe::prelude::*;
 use crate::tfhe_hpu_backend::prelude::ShellString;
 use crate::tfhe_hpu_backend::prelude::*;
 use tfhe::{set_server_key, FheUint64, *};
-// cpu
-// use tfhe::{set_server_key, generate_keys, FheUint64, ConfigBuilder, *};
 // misc
 pub use clap::Parser;
 use rand::Rng;
@@ -25,8 +23,13 @@ fn main() {
         .init();
 
     println!("\n----------------------------------------------");
-    println!("- hpu demo: matrix x vector multiplication -");
+    println!("- hpu demo: matrix multiplication -");
     println!("----------------------------------------------");
+    // This examples performs a matrix multiplication between matrix_a and matrix_b
+    // matrix_a as m rows and n columns
+    // matrix_b as n rows and p columns
+    // m=3, n=2 and p=2 can be set using CLI by adding: -- --m=3 --n=2 --p=2
+
     /// Define CLI arguments
     #[derive(clap::Parser, Debug, Clone, serde::Serialize)]
     #[clap(long_about = "HPU example that shows the use of the HighLevelAPI.")]
@@ -39,45 +42,61 @@ fn main() {
         )]
         pub config: ShellString,
 
-        /// Dimension of the matrix
-        #[clap(long, value_parser, default_value_t = 5)]
-        pub dimension: usize,
+        /// Number of rows in matrix A
+        #[clap(long, value_parser, default_value_t = 2)]
+        pub m: usize,
+        /// Number of columns in matrix A and Number of rows in matrix B
+        #[clap(long, value_parser, default_value_t = 2)]
+        pub n: usize,
+        /// Number of columns in matrix B
+        #[clap(long, value_parser, default_value_t = 2)]
+        pub p: usize,
     }
     let args = Args::parse();
     let hpu_device = HpuDevice::from_config(&args.config.expand());
 
     println!("\n 1. Key generation");
-    // let config = ConfigBuilder::default().build(); // CPU
-    // let (client_key, server_key) = generate_keys(config); // CPU
-    //
     println!("   Generate client and server keys...");
+    // println!("   -> targeting CPU");
+    // let config = ConfigBuilder::default().build();
+    // let (client_key, server_key) = generate_keys(config);
+    println!("   -> targeting HPU");
     let config = Config::from_hpu_device(&hpu_device);
     let client_key = ClientKey::generate(config);
     let server_key = CompressedServerKey::new(&client_key);
 
+    // println!("   Upload keys-material on CPU...");
+    // set_server_key(server_key);
     println!("   Upload keys-material on HPU...");
     set_server_key((hpu_device, server_key));
-    // set_server_key(server_key); // CPU
 
-    println!("\n 2. Matrix and vector definition");
-    let random_vector = (0..args.dimension)
-        .map(|_| rand::thread_rng().gen::<u64>())
-        .collect::<Vec<_>>();
-    let random_matrix = (0..args.dimension)
+    println!("\n 2. Matrices definition");
+    let random_matrix_a = (0..args.m)
         .map(|_| {
-            (0..args.dimension)
+            (0..args.n)
+                .map(|_| rand::thread_rng().gen::<u64>())
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<Vec<_>>>();
+    let random_matrix_b = (0..args.n)
+        .map(|_| {
+            (0..args.p)
                 .map(|_| rand::thread_rng().gen::<u64>())
                 .collect::<Vec<_>>()
         })
         .collect::<Vec<Vec<_>>>();
 
-    println!("\n 3. Encrypting matrix and vector");
-    let encrypted_vector = random_vector
+    println!("\n 3. Encrypting the two matrices");
+    let encrypted_matrix_a: Vec<Vec<FheUint64>> = random_matrix_a
         .iter()
-        .map(|&val| FheUint64::encrypt(val, &client_key))
-        .collect::<Vec<FheUint64>>();
+        .map(|row| {
+            row.iter()
+                .map(|&val| FheUint64::encrypt(val, &client_key))
+                .collect()
+        })
+        .collect();
 
-    let encrypted_matrix: Vec<Vec<FheUint64>> = random_matrix
+    let encrypted_matrix_b: Vec<Vec<FheUint64>> = random_matrix_b
         .iter()
         .map(|row| {
             row.iter()
@@ -88,41 +107,53 @@ fn main() {
 
     println!("\n 4. Triggering operations through hl_api");
     // Iterate over matrix row and do a cartesian product with the vector
-    let fhe_result = encrypted_matrix
-        .iter()
-        .map(|row| {
-            row.iter().enumerate().fold(
-                FheUint64::try_encrypt(0u64, &client_key).unwrap(),
-                |acc, (j, x)| acc + x * &encrypted_vector[j],
-            )
+    let fhe_result = (0..args.m)
+        .map(|i| {
+            (0..args.p)
+                .map(|j| {
+                    (0..args.n).fold(
+                        FheUint64::try_encrypt(0u64, &client_key).unwrap(),
+                        |acc, k| acc + &encrypted_matrix_a[i][k] * &encrypted_matrix_b[k][j],
+                    )
+                })
+                .collect::<Vec<FheUint64>>()
         })
-        .collect::<Vec<_>>();
+        .collect::<Vec<Vec<FheUint64>>>();
 
     println!("\n 5. Wait for computation");
     fhe_result
-        .last()
-        .expect("Compute over empty matrix or vector")
+        .last() // last row
+        .expect("Compute over empty row matrix ")
+        .last() // last coef of the row
+        .expect("Compute over empty column matrix")
         .wait();
 
     println!("\n 6. Decrypting result");
     let dec_result = fhe_result
         .iter()
-        .map(|ct| ct.decrypt(&client_key))
-        .collect::<Vec<u64>>();
+        .map(|row| {
+            row.iter()
+                .map(|x| x.decrypt(&client_key))
+                .collect::<Vec<u64>>()
+        })
+        .collect::<Vec<Vec<u64>>>();
 
     println!("\n----------------------------------------------");
     println!("- checker: cleartext computation -");
     println!("----------------------------------------------");
-    let clear_result = random_matrix
-        .iter()
-        .map(|row| {
-            row.iter().enumerate().fold(0_u64, |acc, (j, x)| {
-                acc.wrapping_add(x.wrapping_mul(random_vector[j]))
-            })
+    let clear_result = (0..args.m)
+        .map(|i| {
+            (0..args.p)
+                .map(|j| {
+                    (0..args.n).fold(0_u64, |acc, k| {
+                        acc.wrapping_add(random_matrix_a[i][k].wrapping_mul(random_matrix_b[k][j]))
+                    })
+                })
+                .collect::<Vec<u64>>()
         })
-        .collect::<Vec<_>>();
+        .collect::<Vec<Vec<u64>>>();
     println!("\n> decrypted result {:?}", dec_result);
     println!("> cleartext result {:?}", clear_result);
 
-    assert!(clear_result == dec_result, "vectors are not the same");
+    assert!(clear_result == dec_result, "matrices are not the same");
 }
