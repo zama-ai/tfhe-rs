89712d3b08e8d766094bd5c4cb75e0a7e02a19d4
Enzo Di Maria <enzo.dimaria@zama.ai>
2025-06-06 14:21:27 +0200

    refactor(gpu): moving unchecked_scalar_div_async to backend

--- backends/tfhe-cuda-backend/src/bindings.rs
+++ backends/tfhe-cuda-backend/src/bindings.rs
@@ -1333,53 +1333,6 @@ unsafe extern "C" {
     );
 }
 unsafe extern "C" {
-    pub fn scratch_cuda_integer_radix_scalar_mul_high_kb_64(
-        streams: *const *mut ffi::c_void,
-        gpu_indexes: *const u32,
-        gpu_count: u32,
-        mem_ptr: *mut *mut i8,
-        glwe_dimension: u32,
-        polynomial_size: u32,
-        lwe_dimension: u32,
-        ks_level: u32,
-        ks_base_log: u32,
-        pbs_level: u32,
-        pbs_base_log: u32,
-        grouping_factor: u32,
-        num_blocks: u32,
-        message_modulus: u32,
-        carry_modulus: u32,
-        pbs_type: PBS_TYPE,
-        anticipated_buffer_drop: bool,
-        allocate_gpu_memory: bool,
-        allocate_ms_array: bool,
-    ) -> u64;
-}
-unsafe extern "C" {
-    pub fn cuda_integer_radix_scalar_mul_high_kb_64(
-        streams: *const *mut ffi::c_void,
-        gpu_indexes: *const u32,
-        gpu_count: u32,
-        ct: *mut CudaRadixCiphertextFFI,
-        mem_ptr: *mut i8,
-        ksks: *const *mut ffi::c_void,
-        rhs: u64,
-        decomposed_scalar: *const u64,
-        has_at_least_one_set: *const u64,
-        ms_noise_reduction_key: *const CudaModulusSwitchNoiseReductionKeyFFI,
-        bsks: *const *mut ffi::c_void,
-        num_scalars: u32,
-    );
-}
-unsafe extern "C" {
-    pub fn cleanup_cuda_integer_radix_scalar_mul_high_kb_64(
-        streams: *const *mut ffi::c_void,
-        gpu_indexes: *const u32,
-        gpu_count: u32,
-        mem_ptr_void: *mut *mut i8,
-    );
-}
-unsafe extern "C" {
     pub fn scratch_cuda_apply_noise_squashing_kb(
         streams: *const *mut ffi::c_void,
         gpu_indexes: *const u32,
@@ -1475,7 +1428,7 @@ unsafe extern "C" {
     );
 }
 unsafe extern "C" {
-    pub fn scratch_cuda_integer_radix_scalar_mul_high_kb_64(
+    pub fn scratch_cuda_integer_unsigned_scalar_div_radix_kb_64(
         streams: *const *mut ffi::c_void,
         gpu_indexes: *const u32,
         gpu_count: u32,
@@ -1492,29 +1445,38 @@ unsafe extern "C" {
         message_modulus: u32,
         carry_modulus: u32,
         pbs_type: PBS_TYPE,
-        anticipated_buffer_drop: bool,
         allocate_gpu_memory: bool,
+        is_divisor_power_of_two: bool,
+        log2_divisor_exceeds_threshold: bool,
+        multiplier_exceeds_threshold: bool,
+        ilog2_divisor: u32,
         allocate_ms_array: bool,
     ) -> u64;
 }
 unsafe extern "C" {
-    pub fn cuda_integer_radix_scalar_mul_high_kb_64(
+    pub fn cuda_integer_unsigned_scalar_div_radix_kb_64(
         streams: *const *mut ffi::c_void,
         gpu_indexes: *const u32,
         gpu_count: u32,
-        ct: *mut CudaRadixCiphertextFFI,
+        numerator_ct: *mut CudaRadixCiphertextFFI,
         mem_ptr: *mut i8,
         ksks: *const *mut ffi::c_void,
-        rhs: u64,
         decomposed_scalar: *const u64,
         has_at_least_one_set: *const u64,
         ms_noise_reduction_key: *const CudaModulusSwitchNoiseReductionKeyFFI,
         bsks: *const *mut ffi::c_void,
         num_scalars: u32,
+        multiplier_exceeds_threshold: bool,
+        is_divisor_power_of_two: bool,
+        log2_divisor_exceeds_threshold: bool,
+        ilog2_divisor: u32,
+        shift_pre: u64,
+        shift_post: u32,
+        rhs: u64,
     );
 }
 unsafe extern "C" {
-    pub fn cleanup_cuda_integer_radix_scalar_mul_high_kb_64(
+    pub fn cleanup_cuda_integer_unsigned_scalar_div_radix_kb_64(
         streams: *const *mut ffi::c_void,
         gpu_indexes: *const u32,
         gpu_count: u32,
