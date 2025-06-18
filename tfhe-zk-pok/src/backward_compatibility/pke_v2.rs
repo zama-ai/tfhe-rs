9f2cb538b60f25651f0f3e454642e430bb1136c0
Nicolas Sarlin <nicolas.sarlin@zama.ai>
2025-06-18 18:29:30 +0200

    feat(zk): add compact hash mode for zkv2

--- tfhe-zk-pok/src/backward_compatibility/pke_v2.rs
+++ tfhe-zk-pok/src/backward_compatibility/pke_v2.rs
@@ -1,11 +1,13 @@
 // to follow the notation of the paper
 #![allow(non_snake_case)]
 
+use std::convert::Infallible;
+
 use tfhe_versionable::{Upgrade, Version, VersionsDispatch};
 
 use crate::curve_api::{CompressedG1, CompressedG2, Compressible, Curve};
 use crate::proofs::pke_v2::{
-    CompressedComputeLoadProofFields, CompressedProof, ComputeLoadProofFields, Proof,
+    CompressedComputeLoadProofFields, CompressedProof, ComputeLoadProofFields, PkeV2HashMode, Proof,
 };
 
 use super::IncompleteProof;
@@ -28,17 +30,17 @@ pub struct ProofV0<G: Curve> {
     C_hat_w: Option<G::G2>,
 }
 
-impl<G: Curve> Upgrade<Proof<G>> for ProofV0<G> {
+impl<G: Curve> Upgrade<ProofV1<G>> for ProofV0<G> {
     type Error = IncompleteProof;
 
-    fn upgrade(self) -> Result<Proof<G>, Self::Error> {
+    fn upgrade(self) -> Result<ProofV1<G>, Self::Error> {
         let compute_load_proof_fields = match (self.C_hat_h3, self.C_hat_w) {
             (None, None) => None,
             (Some(C_hat_h3), Some(C_hat_w)) => Some(ComputeLoadProofFields { C_hat_h3, C_hat_w }),
             _ => return Err(IncompleteProof),
         };
 
-        Ok(Proof {
+        Ok(ProofV1 {
             C_hat_e: self.C_hat_e,
             C_e: self.C_e,
             C_r_tilde: self.C_r_tilde,
@@ -55,10 +57,49 @@ impl<G: Curve> Upgrade<Proof<G>> for ProofV0<G> {
     }
 }
 
+#[derive(Version)]
+pub struct ProofV1<G: Curve> {
+    C_hat_e: G::G2,
+    C_e: G::G1,
+    C_r_tilde: G::G1,
+    C_R: G::G1,
+    C_hat_bin: G::G2,
+    C_y: G::G1,
+    C_h1: G::G1,
+    C_h2: G::G1,
+    C_hat_t: G::G2,
+    pi: G::G1,
+    pi_kzg: G::G1,
+    compute_load_proof_fields: Option<ComputeLoadProofFields<G>>,
+}
+
+impl<G: Curve> Upgrade<Proof<G>> for ProofV1<G> {
+    type Error = Infallible;
+
+    fn upgrade(self) -> Result<Proof<G>, Self::Error> {
+        Ok(Proof {
+            C_hat_e: self.C_hat_e,
+            C_e: self.C_e,
+            C_r_tilde: self.C_r_tilde,
+            C_R: self.C_R,
+            C_hat_bin: self.C_hat_bin,
+            C_y: self.C_y,
+            C_h1: self.C_h1,
+            C_h2: self.C_h2,
+            C_hat_t: self.C_hat_t,
+            pi: self.pi,
+            pi_kzg: self.pi_kzg,
+            compute_load_proof_fields: self.compute_load_proof_fields,
+            hash_mode: PkeV2HashMode::BackwardCompat,
+        })
+    }
+}
+
 #[derive(VersionsDispatch)]
 pub enum ProofVersions<G: Curve> {
     V0(ProofV0<G>),
-    V1(Proof<G>),
+    V1(ProofV1<G>),
+    V2(Proof<G>),
 }
 
 #[derive(VersionsDispatch)]
@@ -67,6 +108,7 @@ pub(crate) enum ComputeLoadProofFieldsVersions<G: Curve> {
     V0(ComputeLoadProofFields<G>),
 }
 
+#[derive(Version)]
 pub struct CompressedProofV0<G: Curve>
 where
     G::G1: Compressible,
@@ -88,14 +130,14 @@ where
     C_hat_w: Option<CompressedG2<G>>,
 }
 
-impl<G: Curve> Upgrade<CompressedProof<G>> for CompressedProofV0<G>
+impl<G: Curve> Upgrade<CompressedProofV1<G>> for CompressedProofV0<G>
 where
     G::G1: Compressible,
     G::G2: Compressible,
 {
     type Error = IncompleteProof;
 
-    fn upgrade(self) -> Result<CompressedProof<G>, Self::Error> {
+    fn upgrade(self) -> Result<CompressedProofV1<G>, Self::Error> {
         let compute_load_proof_fields = match (self.C_hat_h3, self.C_hat_w) {
             (None, None) => None,
             (Some(C_hat_h3), Some(C_hat_w)) => {
@@ -104,7 +146,7 @@ where
             _ => return Err(IncompleteProof),
         };
 
-        Ok(CompressedProof {
+        Ok(CompressedProofV1 {
             C_hat_e: self.C_hat_e,
             C_e: self.C_e,
             C_r_tilde: self.C_r_tilde,
@@ -117,6 +159,52 @@ where
             pi: self.pi,
             pi_kzg: self.pi_kzg,
             compute_load_proof_fields,
+        })
+    }
+}
+
+#[derive(Version)]
+pub struct CompressedProofV1<G: Curve>
+where
+    G::G1: Compressible,
+    G::G2: Compressible,
+{
+    C_hat_e: CompressedG2<G>,
+    C_e: CompressedG1<G>,
+    C_r_tilde: CompressedG1<G>,
+    C_R: CompressedG1<G>,
+    C_hat_bin: CompressedG2<G>,
+    C_y: CompressedG1<G>,
+    C_h1: CompressedG1<G>,
+    C_h2: CompressedG1<G>,
+    C_hat_t: CompressedG2<G>,
+    pi: CompressedG1<G>,
+    pi_kzg: CompressedG1<G>,
+    compute_load_proof_fields: Option<CompressedComputeLoadProofFields<G>>,
+}
+
+impl<G: Curve> Upgrade<CompressedProof<G>> for CompressedProofV1<G>
+where
+    G::G1: Compressible,
+    G::G2: Compressible,
+{
+    type Error = Infallible;
+
+    fn upgrade(self) -> Result<CompressedProof<G>, Self::Error> {
+        Ok(CompressedProof {
+            C_hat_e: self.C_hat_e,
+            C_e: self.C_e,
+            C_r_tilde: self.C_r_tilde,
+            C_R: self.C_R,
+            C_hat_bin: self.C_hat_bin,
+            C_y: self.C_y,
+            C_h1: self.C_h1,
+            C_h2: self.C_h2,
+            C_hat_t: self.C_hat_t,
+            pi: self.pi,
+            pi_kzg: self.pi_kzg,
+            compute_load_proof_fields: self.compute_load_proof_fields,
+            hash_mode: PkeV2HashMode::BackwardCompat,
         })
     }
 }
@@ -139,4 +227,10 @@ where
 {
     #[allow(dead_code)]
     V0(CompressedComputeLoadProofFields<G>),
+}
+
+#[derive(VersionsDispatch)]
+pub(crate) enum PkeV2HashModeVersions {
+    #[allow(dead_code)]
+    V0(PkeV2HashMode),
 }
