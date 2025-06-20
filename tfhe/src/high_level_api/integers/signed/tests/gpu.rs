e7c0f0d2ff9a34e3a893d09dc46d82895ca1f74d
Andrei Stoian <andrei.stoian@zama.ai>
2025-06-20 14:39:13 +0200

    chore(gpu): refactor the gpu oom checker

--- tfhe/src/high_level_api/integers/signed/tests/gpu.rs
+++ tfhe/src/high_level_api/integers/signed/tests/gpu.rs
@@ -3,11 +3,11 @@ use crate::high_level_api::integers::signed::tests::{
 };
 use crate::high_level_api::integers::unsigned::tests::gpu::setup_gpu;
 use crate::prelude::{
-    check_valid_cuda_malloc, AddSizeOnGpu, BitAndSizeOnGpu, BitNotSizeOnGpu, BitOrSizeOnGpu,
-    BitXorSizeOnGpu, DivRemSizeOnGpu, DivSizeOnGpu, FheEncrypt, FheEqSizeOnGpu, FheMaxSizeOnGpu,
-    FheMinSizeOnGpu, FheOrdSizeOnGpu, FheTryEncrypt, IfThenElseSizeOnGpu, MulSizeOnGpu,
-    NegSizeOnGpu, RemSizeOnGpu, RotateLeftSizeOnGpu, RotateRightSizeOnGpu, ShlSizeOnGpu,
-    ShrSizeOnGpu, SubSizeOnGpu,
+    check_valid_cuda_malloc_assert_oom, AddSizeOnGpu, BitAndSizeOnGpu, BitNotSizeOnGpu,
+    BitOrSizeOnGpu, BitXorSizeOnGpu, DivRemSizeOnGpu, DivSizeOnGpu, FheEncrypt, FheEqSizeOnGpu,
+    FheMaxSizeOnGpu, FheMinSizeOnGpu, FheOrdSizeOnGpu, FheTryEncrypt, IfThenElseSizeOnGpu,
+    MulSizeOnGpu, NegSizeOnGpu, RemSizeOnGpu, RotateLeftSizeOnGpu, RotateRightSizeOnGpu,
+    ShlSizeOnGpu, ShrSizeOnGpu, SubSizeOnGpu,
 };
 use crate::shortint::parameters::PARAM_GPU_MULTI_BIT_GROUP_4_MESSAGE_2_CARRY_2_KS_PBS;
 use crate::{FheBool, FheInt32, FheUint32, GpuIndex};
@@ -94,30 +94,15 @@ fn test_gpu_get_add_sub_size_on_gpu() {
     let sub_tmp_buffer_size = a.get_sub_size_on_gpu(b);
     let scalar_add_tmp_buffer_size = clear_a.get_add_size_on_gpu(b);
     let scalar_sub_tmp_buffer_size = clear_a.get_sub_size_on_gpu(b);
-    assert!(check_valid_cuda_malloc(
-        add_tmp_buffer_size,
-        GpuIndex::new(0)
-    ));
-    assert!(check_valid_cuda_malloc(
-        sub_tmp_buffer_size,
-        GpuIndex::new(0)
-    ));
-    assert!(check_valid_cuda_malloc(
-        scalar_add_tmp_buffer_size,
-        GpuIndex::new(0)
-    ));
-    assert!(check_valid_cuda_malloc(
-        scalar_sub_tmp_buffer_size,
-        GpuIndex::new(0)
-    ));
+    check_valid_cuda_malloc_assert_oom(add_tmp_buffer_size, GpuIndex::new(0));
+    check_valid_cuda_malloc_assert_oom(sub_tmp_buffer_size, GpuIndex::new(0));
+    check_valid_cuda_malloc_assert_oom(scalar_add_tmp_buffer_size, GpuIndex::new(0));
+    check_valid_cuda_malloc_assert_oom(scalar_sub_tmp_buffer_size, GpuIndex::new(0));
     assert_eq!(add_tmp_buffer_size, sub_tmp_buffer_size);
     assert_eq!(add_tmp_buffer_size, scalar_add_tmp_buffer_size);
     assert_eq!(add_tmp_buffer_size, scalar_sub_tmp_buffer_size);
     let neg_tmp_buffer_size = a.get_neg_size_on_gpu();
-    assert!(check_valid_cuda_malloc(
-        neg_tmp_buffer_size,
-        GpuIndex::new(0)
-    ));
+    check_valid_cuda_malloc_assert_oom(neg_tmp_buffer_size, GpuIndex::new(0));
 }
 
 #[test]
@@ -135,39 +120,18 @@ fn test_gpu_get_bitops_size_on_gpu() {
 
     let bitand_tmp_buffer_size = a.get_bitand_size_on_gpu(b);
     let scalar_bitand_tmp_buffer_size = clear_a.get_bitand_size_on_gpu(b);
-    assert!(check_valid_cuda_malloc(
-        bitand_tmp_buffer_size,
-        GpuIndex::new(0)
-    ));
-    assert!(check_valid_cuda_malloc(
-        scalar_bitand_tmp_buffer_size,
-        GpuIndex::new(0)
-    ));
+    check_valid_cuda_malloc_assert_oom(bitand_tmp_buffer_size, GpuIndex::new(0));
+    check_valid_cuda_malloc_assert_oom(scalar_bitand_tmp_buffer_size, GpuIndex::new(0));
     let bitor_tmp_buffer_size = a.get_bitor_size_on_gpu(b);
     let scalar_bitor_tmp_buffer_size = clear_a.get_bitor_size_on_gpu(b);
-    assert!(check_valid_cuda_malloc(
-        bitor_tmp_buffer_size,
-        GpuIndex::new(0)
-    ));
-    assert!(check_valid_cuda_malloc(
-        scalar_bitor_tmp_buffer_size,
-        GpuIndex::new(0)
-    ));
+    check_valid_cuda_malloc_assert_oom(bitor_tmp_buffer_size, GpuIndex::new(0));
+    check_valid_cuda_malloc_assert_oom(scalar_bitor_tmp_buffer_size, GpuIndex::new(0));
     let bitxor_tmp_buffer_size = a.get_bitxor_size_on_gpu(b);
     let scalar_bitxor_tmp_buffer_size = clear_a.get_bitxor_size_on_gpu(b);
-    assert!(check_valid_cuda_malloc(
-        bitxor_tmp_buffer_size,
-        GpuIndex::new(0)
-    ));
-    assert!(check_valid_cuda_malloc(
-        scalar_bitxor_tmp_buffer_size,
-        GpuIndex::new(0)
-    ));
+    check_valid_cuda_malloc_assert_oom(bitxor_tmp_buffer_size, GpuIndex::new(0));
+    check_valid_cuda_malloc_assert_oom(scalar_bitxor_tmp_buffer_size, GpuIndex::new(0));
     let bitnot_tmp_buffer_size = a.get_bitnot_size_on_gpu();
-    assert!(check_valid_cuda_malloc(
-        bitnot_tmp_buffer_size,
-        GpuIndex::new(0)
-    ));
+    check_valid_cuda_malloc_assert_oom(bitnot_tmp_buffer_size, GpuIndex::new(0));
 }
 #[test]
 fn test_gpu_get_comparisons_size_on_gpu() {
@@ -184,84 +148,36 @@ fn test_gpu_get_comparisons_size_on_gpu() {
 
     let gt_tmp_buffer_size = a.get_gt_size_on_gpu(b);
     let scalar_gt_tmp_buffer_size = a.get_gt_size_on_gpu(clear_b);
-    assert!(check_valid_cuda_malloc(
-        gt_tmp_buffer_size,
-        GpuIndex::new(0)
-    ));
-    assert!(check_valid_cuda_malloc(
-        scalar_gt_tmp_buffer_size,
-        GpuIndex::new(0)
-    ));
+    check_valid_cuda_malloc_assert_oom(gt_tmp_buffer_size, GpuIndex::new(0));
+    check_valid_cuda_malloc_assert_oom(scalar_gt_tmp_buffer_size, GpuIndex::new(0));
     let ge_tmp_buffer_size = a.get_ge_size_on_gpu(b);
     let scalar_ge_tmp_buffer_size = a.get_ge_size_on_gpu(clear_b);
-    assert!(check_valid_cuda_malloc(
-        ge_tmp_buffer_size,
-        GpuIndex::new(0)
-    ));
-    assert!(check_valid_cuda_malloc(
-        scalar_ge_tmp_buffer_size,
-        GpuIndex::new(0)
-    ));
+    check_valid_cuda_malloc_assert_oom(ge_tmp_buffer_size, GpuIndex::new(0));
+    check_valid_cuda_malloc_assert_oom(scalar_ge_tmp_buffer_size, GpuIndex::new(0));
     let lt_tmp_buffer_size = a.get_lt_size_on_gpu(b);
     let scalar_lt_tmp_buffer_size = a.get_lt_size_on_gpu(clear_b);
-    assert!(check_valid_cuda_malloc(
-        lt_tmp_buffer_size,
-        GpuIndex::new(0)
-    ));
-    assert!(check_valid_cuda_malloc(
-        scalar_lt_tmp_buffer_size,
-        GpuIndex::new(0)
-    ));
+    check_valid_cuda_malloc_assert_oom(lt_tmp_buffer_size, GpuIndex::new(0));
+    check_valid_cuda_malloc_assert_oom(scalar_lt_tmp_buffer_size, GpuIndex::new(0));
     let le_tmp_buffer_size = a.get_le_size_on_gpu(b);
     let scalar_le_tmp_buffer_size = a.get_le_size_on_gpu(clear_b);
-    assert!(check_valid_cuda_malloc(
-        le_tmp_buffer_size,
-        GpuIndex::new(0)
-    ));
-    assert!(check_valid_cuda_malloc(
-        scalar_le_tmp_buffer_size,
-        GpuIndex::new(0)
-    ));
+    check_valid_cuda_malloc_assert_oom(le_tmp_buffer_size, GpuIndex::new(0));
+    check_valid_cuda_malloc_assert_oom(scalar_le_tmp_buffer_size, GpuIndex::new(0));
     let max_tmp_buffer_size = a.get_max_size_on_gpu(b);
     let scalar_max_tmp_buffer_size = a.get_max_size_on_gpu(clear_b);
-    assert!(check_valid_cuda_malloc(
-        max_tmp_buffer_size,
-        GpuIndex::new(0)
-    ));
-    assert!(check_valid_cuda_malloc(
-        scalar_max_tmp_buffer_size,
-        GpuIndex::new(0)
-    ));
+    check_valid_cuda_malloc_assert_oom(max_tmp_buffer_size, GpuIndex::new(0));
+    check_valid_cuda_malloc_assert_oom(scalar_max_tmp_buffer_size, GpuIndex::new(0));
     let min_tmp_buffer_size = a.get_min_size_on_gpu(b);
     let scalar_min_tmp_buffer_size = a.get_min_size_on_gpu(clear_b);
-    assert!(check_valid_cuda_malloc(
-        min_tmp_buffer_size,
-        GpuIndex::new(0)
-    ));
-    assert!(check_valid_cuda_malloc(
-        scalar_min_tmp_buffer_size,
-        GpuIndex::new(0)
-    ));
+    check_valid_cuda_malloc_assert_oom(min_tmp_buffer_size, GpuIndex::new(0));
+    check_valid_cuda_malloc_assert_oom(scalar_min_tmp_buffer_size, GpuIndex::new(0));
     let eq_tmp_buffer_size = a.get_eq_size_on_gpu(b);
     let scalar_eq_tmp_buffer_size = a.get_eq_size_on_gpu(clear_b);
-    assert!(check_valid_cuda_malloc(
-        eq_tmp_buffer_size,
-        GpuIndex::new(0)
-    ));
-    assert!(check_valid_cuda_malloc(
-        scalar_eq_tmp_buffer_size,
-        GpuIndex::new(0)
-    ));
+    check_valid_cuda_malloc_assert_oom(eq_tmp_buffer_size, GpuIndex::new(0));
+    check_valid_cuda_malloc_assert_oom(scalar_eq_tmp_buffer_size, GpuIndex::new(0));
     let ne_tmp_buffer_size = a.get_ne_size_on_gpu(b);
     let scalar_ne_tmp_buffer_size = a.get_ne_size_on_gpu(clear_b);
-    assert!(check_valid_cuda_malloc(
-        ne_tmp_buffer_size,
-        GpuIndex::new(0)
-    ));
-    assert!(check_valid_cuda_malloc(
-        scalar_ne_tmp_buffer_size,
-        GpuIndex::new(0)
-    ));
+    check_valid_cuda_malloc_assert_oom(ne_tmp_buffer_size, GpuIndex::new(0));
+    check_valid_cuda_malloc_assert_oom(scalar_ne_tmp_buffer_size, GpuIndex::new(0));
 }
 
 #[test]
@@ -279,44 +195,20 @@ fn test_gpu_get_shift_rotate_size_on_gpu() {
 
     let left_shift_tmp_buffer_size = a.get_left_shift_size_on_gpu(b);
     let scalar_left_shift_tmp_buffer_size = a.get_left_shift_size_on_gpu(clear_b);
-    assert!(check_valid_cuda_malloc(
-        left_shift_tmp_buffer_size,
-        GpuIndex::new(0)
-    ));
-    assert!(check_valid_cuda_malloc(
-        scalar_left_shift_tmp_buffer_size,
-        GpuIndex::new(0)
-    ));
+    check_valid_cuda_malloc_assert_oom(left_shift_tmp_buffer_size, GpuIndex::new(0));
+    check_valid_cuda_malloc_assert_oom(scalar_left_shift_tmp_buffer_size, GpuIndex::new(0));
     let right_shift_tmp_buffer_size = a.get_right_shift_size_on_gpu(b);
     let scalar_right_shift_tmp_buffer_size = a.get_right_shift_size_on_gpu(clear_b);
-    assert!(check_valid_cuda_malloc(
-        right_shift_tmp_buffer_size,
-        GpuIndex::new(0)
-    ));
-    assert!(check_valid_cuda_malloc(
-        scalar_right_shift_tmp_buffer_size,
-        GpuIndex::new(0)
-    ));
+    check_valid_cuda_malloc_assert_oom(right_shift_tmp_buffer_size, GpuIndex::new(0));
+    check_valid_cuda_malloc_assert_oom(scalar_right_shift_tmp_buffer_size, GpuIndex::new(0));
     let rotate_left_tmp_buffer_size = a.get_rotate_left_size_on_gpu(b);
     let scalar_rotate_left_tmp_buffer_size = a.get_rotate_left_size_on_gpu(clear_b);
-    assert!(check_valid_cuda_malloc(
-        rotate_left_tmp_buffer_size,
-        GpuIndex::new(0)
-    ));
-    assert!(check_valid_cuda_malloc(
-        scalar_rotate_left_tmp_buffer_size,
-        GpuIndex::new(0)
-    ));
+    check_valid_cuda_malloc_assert_oom(rotate_left_tmp_buffer_size, GpuIndex::new(0));
+    check_valid_cuda_malloc_assert_oom(scalar_rotate_left_tmp_buffer_size, GpuIndex::new(0));
     let rotate_right_tmp_buffer_size = a.get_rotate_right_size_on_gpu(b);
     let scalar_rotate_right_tmp_buffer_size = a.get_rotate_right_size_on_gpu(clear_b);
-    assert!(check_valid_cuda_malloc(
-        rotate_right_tmp_buffer_size,
-        GpuIndex::new(0)
-    ));
-    assert!(check_valid_cuda_malloc(
-        scalar_rotate_right_tmp_buffer_size,
-        GpuIndex::new(0)
-    ));
+    check_valid_cuda_malloc_assert_oom(rotate_right_tmp_buffer_size, GpuIndex::new(0));
+    check_valid_cuda_malloc_assert_oom(scalar_rotate_right_tmp_buffer_size, GpuIndex::new(0));
 }
 
 #[test]
@@ -335,20 +227,11 @@ fn test_gpu_get_if_then_else_size_on_gpu() {
     let b = &b;
 
     let if_then_else_tmp_buffer_size = c.get_if_then_else_size_on_gpu(a, b);
-    assert!(check_valid_cuda_malloc(
-        if_then_else_tmp_buffer_size,
-        GpuIndex::new(0)
-    ));
+    check_valid_cuda_malloc_assert_oom(if_then_else_tmp_buffer_size, GpuIndex::new(0));
     let select_tmp_buffer_size = c.get_select_size_on_gpu(a, b);
-    assert!(check_valid_cuda_malloc(
-        select_tmp_buffer_size,
-        GpuIndex::new(0)
-    ));
+    check_valid_cuda_malloc_assert_oom(select_tmp_buffer_size, GpuIndex::new(0));
     let cmux_tmp_buffer_size = c.get_cmux_size_on_gpu(a, b);
-    assert!(check_valid_cuda_malloc(
-        cmux_tmp_buffer_size,
-        GpuIndex::new(0)
-    ));
+    check_valid_cuda_malloc_assert_oom(cmux_tmp_buffer_size, GpuIndex::new(0));
 }
 #[test]
 fn test_gpu_get_mul_size_on_gpu() {
@@ -365,14 +248,8 @@ fn test_gpu_get_mul_size_on_gpu() {
 
     let mul_tmp_buffer_size = a.get_mul_size_on_gpu(b);
     let scalar_mul_tmp_buffer_size = b.get_mul_size_on_gpu(clear_a);
-    assert!(check_valid_cuda_malloc(
-        mul_tmp_buffer_size,
-        GpuIndex::new(0)
-    ));
-    assert!(check_valid_cuda_malloc(
-        scalar_mul_tmp_buffer_size,
-        GpuIndex::new(0)
-    ));
+    check_valid_cuda_malloc_assert_oom(mul_tmp_buffer_size, GpuIndex::new(0));
+    check_valid_cuda_malloc_assert_oom(scalar_mul_tmp_buffer_size, GpuIndex::new(0));
 }
 #[test]
 fn test_gpu_get_div_size_on_gpu() {
@@ -390,31 +267,13 @@ fn test_gpu_get_div_size_on_gpu() {
     let div_tmp_buffer_size = a.get_div_size_on_gpu(b);
     let rem_tmp_buffer_size = a.get_rem_size_on_gpu(b);
     let div_rem_tmp_buffer_size = a.get_div_rem_size_on_gpu(b);
-    assert!(check_valid_cuda_malloc(
-        div_tmp_buffer_size,
-        GpuIndex::new(0)
-    ));
-    assert!(check_valid_cuda_malloc(
-        rem_tmp_buffer_size,
-        GpuIndex::new(0)
-    ));
-    assert!(check_valid_cuda_malloc(
-        div_rem_tmp_buffer_size,
-        GpuIndex::new(0)
-    ));
+    check_valid_cuda_malloc_assert_oom(div_tmp_buffer_size, GpuIndex::new(0));
+    check_valid_cuda_malloc_assert_oom(rem_tmp_buffer_size, GpuIndex::new(0));
+    check_valid_cuda_malloc_assert_oom(div_rem_tmp_buffer_size, GpuIndex::new(0));
     let scalar_div_tmp_buffer_size = a.get_div_size_on_gpu(b);
     let scalar_rem_tmp_buffer_size = a.get_rem_size_on_gpu(b);
     let scalar_div_rem_tmp_buffer_size = a.get_div_rem_size_on_gpu(b);
-    assert!(check_valid_cuda_malloc(
-        scalar_div_tmp_buffer_size,
-        GpuIndex::new(0)
-    ));
-    assert!(check_valid_cuda_malloc(
-        scalar_rem_tmp_buffer_size,
-        GpuIndex::new(0)
-    ));
-    assert!(check_valid_cuda_malloc(
-        scalar_div_rem_tmp_buffer_size,
-        GpuIndex::new(0)
-    ));
+    check_valid_cuda_malloc_assert_oom(scalar_div_tmp_buffer_size, GpuIndex::new(0));
+    check_valid_cuda_malloc_assert_oom(scalar_rem_tmp_buffer_size, GpuIndex::new(0));
+    check_valid_cuda_malloc_assert_oom(scalar_div_rem_tmp_buffer_size, GpuIndex::new(0));
 }
