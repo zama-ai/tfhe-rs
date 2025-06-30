4325da72cf3788283d6aa5af03926fe89863e19e
Arthur Meyre <arthur.meyre@zama.ai>
2025-06-30 15:44:32 +0200

    chore: allow to not perform the half case correction for mean compensation

--- tfhe/src/core_crypto/hpu/algorithms/modswitch.rs
+++ tfhe/src/core_crypto/hpu/algorithms/modswitch.rs
@@ -1,4 +1,7 @@
-use crate::core_crypto::prelude::UnsignedInteger;
+use crate::core_crypto::prelude::{
+    centered_binary_ms_body_correction_to_add, CastInto, CiphertextModulusLog, Container,
+    LazyStandardModulusSwitchedLweCiphertext, LweCiphertext, UnsignedInteger,
+};
 use tfhe_hpu_backend::prelude::HpuParameters;
 
 /// This function change information position in value
@@ -34,4 +37,29 @@ pub fn lsb2msb_align<Scalar: UnsignedInteger>(params: &HpuParameters, data: &mut
     for val in data.iter_mut() {
         *val <<= storage_width - ct_width;
     }
+}
+
+// Temporary implementation without half case correction which is not currently implemented
+// Once implemented revert back to CPU implem
+pub fn hpu_lwe_ciphertext_centered_binary_modulus_switch<Scalar, SwitchedScalar, Cont>(
+    lwe_in: LweCiphertext<Cont>,
+    log_modulus: CiphertextModulusLog,
+) -> LazyStandardModulusSwitchedLweCiphertext<Scalar, SwitchedScalar, Cont>
+where
+    Scalar: UnsignedInteger + CastInto<SwitchedScalar>,
+    SwitchedScalar: UnsignedInteger,
+    Cont: Container<Element = Scalar>,
+{
+    assert!(log_modulus.0 <= Scalar::BITS);
+    assert!(log_modulus.0 <= SwitchedScalar::BITS);
+
+    let body_correction_to_add =
+        // false here disables the half case correction
+        centered_binary_ms_body_correction_to_add(&lwe_in, log_modulus, false);
+
+    LazyStandardModulusSwitchedLweCiphertext::from_raw_parts(
+        lwe_in,
+        body_correction_to_add,
+        log_modulus,
+    )
 }
