8af2cbe722a9fd8325940d2dac2cb25039c8f858
Andrei Stoian <andrei.stoian@zama.ai>
2025-07-04 17:19:28 +0200

    fix: sample generator

--- tfhe/src/integer/server_key/radix_parallel/tests_long_run/mod.rs
+++ tfhe/src/integer/server_key/radix_parallel/tests_long_run/mod.rs
@@ -3,8 +3,7 @@ use tfhe_csprng::generators::DefaultRandomGenerator;
 use tfhe_csprng::seeders::Seed;
 use crate::core_crypto::commons::generators::DeterministicSeeder;
 use crate::integer::RadixClientKey;
-use crate::integer::RadixCiphertext;
-use crate::integer::RadixCiphertext::RadixSignedCiphertext;
+use crate::integer::{RadixCiphertext, SignedRadixCiphertext};
 use crate::integer::block_decomposition::DecomposableInto;
 use tfhe_csprng::seeders::Seeder;
 
@@ -38,29 +37,35 @@ pub(crate) struct TestDataSample<P, C> {
     pub(crate) producer: String,
 }
 
-trait EncryptSample {
-    fn encrypt(&self, cks: &RadixClientKey) -> RadixCiphertext
-    where
-        Self: Sized + DecomposableInto<u64>;
+pub trait RadixEncryptable {
+    type Output;
+
+    fn encrypt(&self, key: &RadixClientKey) -> Self::Output;
 }
 
-impl EncryptSample for i64 {
-    fn encrypt(&self, cks: &RadixClientKey) -> RadixSignedCiphertext {
-        cks.encrypt_signed(*self)
+impl RadixEncryptable for u64 {
+    type Output = RadixCiphertext;
+
+
+    fn encrypt(&self, key: &RadixClientKey) -> Self::Output {
+        key.encrypt(*self)
     }
 }
 
-impl EncryptSample for u64 {
-    fn encrypt(&self, cks: &RadixClientKey) -> RadixCiphertext {
-        cks.encrypt(*self)
+impl RadixEncryptable for i64 {
+    type Output = SignedRadixCiphertext;
+
+
+    fn encrypt(&self, key: &RadixClientKey) -> Self::Output {
+        key.encrypt_signed(*self)
     }
 }
 
-impl EncryptSample for bool {
+/*impl EncryptSample for bool {
     fn encrypt(&self, cks: &RadixClientKey) -> RadixCiphertext {
         cks.encrypt_bool(*self)
     }
-}
+}*/
 
 struct RandomOpSequenceDataGenerator<P, C> {
     pub(crate) clear_left_vec: Vec<TestDataSample<P, C>>,
@@ -70,7 +75,7 @@ struct RandomOpSequenceDataGenerator<P, C> {
     cks: RadixClientKey,
 }
 
-impl<P:EncryptSample + integer::block_decomposition::DecomposableInto<u64>, C> RandomOpSequenceDataGenerator<P, C> {
+impl<P:RadixEncryptable<Output = C> + DecomposableInto<u64> + From<u128>, C> RandomOpSequenceDataGenerator<P, C> {
     fn new(total_num_ops: usize, cks: &RadixClientKey) -> Self {
         let mut rng = rand::thread_rng();
 
@@ -78,25 +83,25 @@ impl<P:EncryptSample + integer::block_decomposition::DecomposableInto<u64>, C> R
         Self::new_with_seed(total_num_ops, Seed(seed), cks)
     }
 
-    fn make_data_sample_vector(deterministic_seeder: DeterministicSeeder<DefaultRandomGenerator>, total_num_ops: usize, cks: &RadixClientKey) -> Vec<TestDataSample<P, C>> {
+    fn make_data_sample_vector(deterministic_seeder: &mut DeterministicSeeder<DefaultRandomGenerator>, total_num_ops: usize, cks: &RadixClientKey) -> Vec<TestDataSample<P, C>> {
         (0..total_num_ops)
             .map(|_| {
-                let p = deterministic_seeder.seed().0 as P;
-                let c = p.encrypt(&cks);
+                let plain = P::from(deterministic_seeder.seed().0);
+                let cipher: C = plain.encrypt(&cks);
                 TestDataSample {
-                    clear_value: p,
-                    encrypted_value: c,
+                    clear_value: plain,
+                    encrypted_value: cipher,
                     producer: "encrypt".to_string(),
                 }
             }) // Generate random i64 values and encrypt them
             .collect()
     }
     fn new_with_seed(total_num_ops: usize, seed: Seed, cks: &RadixClientKey) -> Self {
-        let deterministic_seeder = DeterministicSeeder::<DefaultRandomGenerator>::new(seed);
+        let mut deterministic_seeder = DeterministicSeeder::<DefaultRandomGenerator>::new(seed);
 
         RandomOpSequenceDataGenerator {
-            clear_left_vec: Self::make_data_sample_vector(deterministic_seeder, total_num_ops, cks),
-            clear_right_vec: Self::make_data_sample_vector(deterministic_seeder, total_num_ops, cks),
+            clear_left_vec: Self::make_data_sample_vector(&mut deterministic_seeder, total_num_ops, cks),
+            clear_right_vec: Self::make_data_sample_vector(&mut deterministic_seeder, total_num_ops, cks),
             deterministic_seeder,
             seed,
             cks: RadixClientKey::from(cks.clone())
