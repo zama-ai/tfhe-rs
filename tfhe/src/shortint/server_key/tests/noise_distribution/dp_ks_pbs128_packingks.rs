810e9fdfdd2ea379cebee0a3ff95089f3ed5f530
Arthur Meyre <arthur.meyre@zama.ai>
2025-07-09 18:31:14 +0200

    wip BR DP KS MS

--- tfhe/src/shortint/server_key/tests/noise_distribution/dp_ks_pbs128_packingks.rs
+++ tfhe/src/shortint/server_key/tests/noise_distribution/dp_ks_pbs128_packingks.rs
@@ -74,7 +74,7 @@ where
         >,
     // The accumulator has the information about the output size and modulus, therefore it is the
     // one to allocate the blind rotation result
-    Accumulator: AllocateBlindRotationResult<Output = PbsResult, SideResources = Resources>,
+    Accumulator: AllocateBootstrapResult<Output = PbsResult, SideResources = Resources>,
     // We need to be able to apply the PBS
     Bsk: StandardFft128Bootstrap<MsResult, PbsResult, Accumulator, SideResources = Resources>,
 {
@@ -91,7 +91,7 @@ where
         side_resources,
     );
 
-    let mut pbs_result = accumulator.allocated_blind_rotation_result(side_resources);
+    let mut pbs_result = accumulator.allocate_bootstrap_result(side_resources);
     bsk_128.standard_fft_128_pbs(&ms_result, &mut pbs_result, accumulator, side_resources);
     (
         input,
@@ -161,7 +161,7 @@ where
         > + Sync,
     // The accumulator has the information about the output size and modulus, therefore it is the
     // one to allocate the blind rotation result
-    Accumulator: AllocateBlindRotationResult<Output = PbsResult, SideResources = Resources> + Sync,
+    Accumulator: AllocateBootstrapResult<Output = PbsResult, SideResources = Resources> + Sync,
     // We need to be able to apply the PBS
     Bsk:
         StandardFft128Bootstrap<MsResult, PbsResult, Accumulator, SideResources = Resources> + Sync,
