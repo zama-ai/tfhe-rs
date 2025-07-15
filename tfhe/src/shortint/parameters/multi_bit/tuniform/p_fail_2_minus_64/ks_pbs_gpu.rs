b519902b8ed880213189dc8960bb89834f2cb414
David Testé <david.teste@zama.ai>
2025-07-15 17:46:21 +0200

    WIP: set up to measure small subset of benchmarks

--- tfhe/src/shortint/parameters/multi_bit/tuniform/p_fail_2_minus_64/ks_pbs_gpu.rs
+++ tfhe/src/shortint/parameters/multi_bit/tuniform/p_fail_2_minus_64/ks_pbs_gpu.rs
@@ -25,10 +25,10 @@ pub const V0_10_PARAM_GPU_MULTI_BIT_GROUP_3_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M
 
 pub const V0_11_PARAM_GPU_MULTI_BIT_GROUP_3_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M64:
     MultiBitPBSParameters = MultiBitPBSParameters {
-    lwe_dimension: LweDimension(879),
+    lwe_dimension: LweDimension(920),
     glwe_dimension: GlweDimension(1),
     polynomial_size: PolynomialSize(2048),
-    lwe_noise_distribution: DynamicDistribution::new_t_uniform(46),
+    lwe_noise_distribution: DynamicDistribution::new_t_uniform(45),
     glwe_noise_distribution: DynamicDistribution::new_t_uniform(17),
     pbs_base_log: DecompositionBaseLog(22),
     pbs_level: DecompositionLevelCount(1),
@@ -37,11 +37,11 @@ pub const V0_11_PARAM_GPU_MULTI_BIT_GROUP_3_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M
     message_modulus: MessageModulus(4),
     carry_modulus: CarryModulus(4),
     max_noise_level: MaxNoiseLevel::new(5),
-    log2_p_fail: -64.59,
+    log2_p_fail: -134.345,
     ciphertext_modulus: CiphertextModulus::new_native(),
     encryption_key_choice: EncryptionKeyChoice::Big,
-    grouping_factor: LweBskGroupingFactor(3),
-    deterministic_execution: true,
+    grouping_factor: LweBskGroupingFactor(4),
+    deterministic_execution: false,
 };
 pub const PARAM_GPU_MULTI_BIT_GROUP_3_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M64:
     MultiBitPBSParameters =
