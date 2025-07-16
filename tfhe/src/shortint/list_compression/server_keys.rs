a4cfc12941f94e1c3de2480f620d6b67e2cece22
tmontaigu <thomas.montaigu@laposte.net>
2025-07-16 11:55:50 +0200

    chore: add missing into/from_raw_parts for SQCompression

--- tfhe/src/shortint/list_compression/server_keys.rs
+++ tfhe/src/shortint/list_compression/server_keys.rs
@@ -246,6 +246,39 @@ pub struct NoiseSquashingCompressionKey {
     pub(super) lwe_per_glwe: LweCiphertextCount,
 }
 
+impl NoiseSquashingCompressionKey {
+    /// Construct from raw parts
+    ///
+    /// # Panics
+    ///
+    /// Panics if lwe_per_glwe is greater than the output polynomial size of the packing key
+    /// switching key
+    pub fn from_raw_parts(
+        packing_key_switching_key: LwePackingKeyswitchKey<Vec<u128>>,
+        lwe_per_glwe: LweCiphertextCount,
+    ) -> Self {
+        assert!(
+            lwe_per_glwe.0 <= packing_key_switching_key.output_polynomial_size().0,
+            "Cannot pack more than polynomial_size(={}) elements per glwe, {} requested",
+            packing_key_switching_key.output_polynomial_size().0,
+            lwe_per_glwe.0,
+        );
+        Self {
+            packing_key_switching_key,
+            lwe_per_glwe,
+        }
+    }
+
+    pub fn into_raw_parts(self) -> (LwePackingKeyswitchKey<Vec<u128>>, LweCiphertextCount) {
+        let Self {
+            packing_key_switching_key,
+            lwe_per_glwe,
+        } = self;
+
+        (packing_key_switching_key, lwe_per_glwe)
+    }
+}
+
 impl NoiseSquashingPrivateKey {
     pub fn new_noise_squashing_compression_key(
         &self,
