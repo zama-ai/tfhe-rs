ff370a671b4ffc026ecfb1323ef7f9455a7fe759
Agnes Leroy <agnes.leroy@zama.ai>
2025-07-30 17:13:46 +0200

    chore(gpu): add corner case test for mul

--- tfhe/src/integer/gpu/server_key/radix/tests_unsigned/test_mul.rs
+++ tfhe/src/integer/gpu/server_key/radix/tests_unsigned/test_mul.rs
@@ -3,12 +3,13 @@ use crate::integer::gpu::server_key::radix::tests_unsigned::{
 };
 use crate::integer::gpu::CudaServerKey;
 use crate::integer::server_key::radix_parallel::tests_cases_unsigned::{
-    default_mul_test, unchecked_mul_test,
+    default_mul_test, unchecked_mul_corner_cases_test, unchecked_mul_test,
 };
 use crate::shortint::parameters::test_params::*;
 use crate::shortint::parameters::*;
 
 create_gpu_parameterized_test!(integer_unchecked_mul);
+create_gpu_parameterized_test!(integer_unchecked_mul_corner_cases);
 create_gpu_parameterized_test!(integer_mul {
     PARAM_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M128,
     TEST_PARAM_MESSAGE_3_CARRY_3_KS_PBS_GAUSSIAN_2M128,
@@ -22,6 +23,14 @@ where
 {
     let executor = GpuFunctionExecutor::new(&CudaServerKey::unchecked_mul);
     unchecked_mul_test(param, executor);
+}
+
+fn integer_unchecked_mul_corner_cases<P>(param: P)
+where
+    P: Into<TestParameters>,
+{
+    let executor = GpuFunctionExecutor::new(&CudaServerKey::unchecked_mul);
+    unchecked_mul_corner_cases_test(param, executor);
 }
 
 fn integer_mul<P>(param: P)
