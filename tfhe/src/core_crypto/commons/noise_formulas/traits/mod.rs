0229293275af06ace956e0d0442b206bc4645819
Arthur Meyre <arthur.meyre@zama.ai>
2025-08-04 15:05:52 +0200

    chore: uniformize noise check tools naming

--- tfhe/src/core_crypto/commons/noise_formulas/traits/mod.rs
+++ tfhe/src/core_crypto/commons/noise_formulas/traits/mod.rs
@@ -4,13 +4,13 @@ pub mod lwe_programmable_bootstrap;
 pub mod modulus_switch;
 pub mod scalar_mul;
 
-pub use lwe_keyswitch::{AllocateKeyswtichResult, Keyswitch};
-pub use lwe_packing_keyswitch::{AllocatePackingKeyswitchResult, LwePackingKeyswitch};
+pub use lwe_keyswitch::{AllocateLweKeyswitchResult, LweKeyswitch};
+pub use lwe_packing_keyswitch::{AllocateLwePackingKeyswitchResult, LwePackingKeyswitch};
 pub use lwe_programmable_bootstrap::{
-    AllocateBootstrapResult, StandardFft128Bootstrap, StandardFftBootstrap,
+    AllocateLweBootstrapResult, LweStandardFft128Bootstrap, LweStandardFftBootstrap,
 };
 pub use modulus_switch::{
-    AllocateDriftTechniqueStandardModSwitchResult, AllocateStandardPBSModSwitchResult,
-    DriftTechniqueStandardModSwitch, StandardPBSModSwitch,
+    AllocateDriftTechniqueStandardModSwitchResult, AllocateStandardModSwitchResult,
+    DriftTechniqueStandardModSwitch, StandardModSwitch,
 };
 pub use scalar_mul::{ScalarMul, ScalarMulAssign};
