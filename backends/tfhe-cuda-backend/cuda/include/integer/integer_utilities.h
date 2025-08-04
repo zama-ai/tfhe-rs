834009ab7261e3dfee55f0cdec67b6919f79c9f9
Andrei Stoian <andrei.stoian@zama.ai>
2025-08-04 15:14:43 +0200

    fix: leak fix

--- backends/tfhe-cuda-backend/cuda/include/integer/integer_utilities.h
+++ backends/tfhe-cuda-backend/cuda/include/integer/integer_utilities.h
@@ -4920,12 +4920,22 @@ template <typename Torus> struct int_scalar_mul_buffer {
     sc_prop_mem->release(streams, gpu_indexes, gpu_count);
     delete sc_prop_mem;
     delete all_shifted_buffer;
-    if (!anticipated_buffers_drop) {
+    release_buffers(streams, gpu_indexes, gpu_count);
+  }
+
+  void release_buffers(cudaStream_t const *streams, uint32_t const *gpu_indexes,
+                       uint32_t gpu_count) {
+    if (preshifted_buffer) {
       release_radix_ciphertext_async(streams[0], gpu_indexes[0],
                                      preshifted_buffer, gpu_memory_allocated);
+      delete preshifted_buffer;
+      preshifted_buffer = nullptr;
+    }
+
+    if (logical_scalar_shift_buffer) {
       logical_scalar_shift_buffer->release(streams, gpu_indexes, gpu_count);
       delete logical_scalar_shift_buffer;
-      delete preshifted_buffer;
+      logical_scalar_shift_buffer = nullptr;
     }
   }
 };
@@ -5215,6 +5225,7 @@ template <typename Torus> struct int_scalar_mul_high_buffer {
 
     scalar_mul_mem->release(streams, gpu_indexes, gpu_count);
     delete scalar_mul_mem;
+    scalar_mul_mem = nullptr;
 
     release_radix_ciphertext_async(streams[0], gpu_indexes[0], tmp,
                                    allocate_gpu_memory);
@@ -5736,23 +5747,29 @@ template <typename Torus> struct int_signed_scalar_div_rem_buffer {
     release_radix_ciphertext_async(streams[0], gpu_indexes[0], numerator_ct,
                                    allocate_gpu_memory);
     delete numerator_ct;
+    numerator_ct = nullptr;
 
     signed_div_mem->release(streams, gpu_indexes, gpu_count);
     delete signed_div_mem;
+    signed_div_mem = nullptr;
 
     scp_mem->release(streams, gpu_indexes, gpu_count);
     delete scp_mem;
+    scp_mem = nullptr;
 
     if (logical_scalar_shift_mem != nullptr) {
       logical_scalar_shift_mem->release(streams, gpu_indexes, gpu_count);
       delete logical_scalar_shift_mem;
+      logical_scalar_shift_mem = nullptr;
     }
     if (scalar_mul_mem != nullptr) {
       scalar_mul_mem->release(streams, gpu_indexes, gpu_count);
       delete scalar_mul_mem;
+      scalar_mul_mem = nullptr;
     }
     sub_and_propagate_mem->release(streams, gpu_indexes, gpu_count);
     delete sub_and_propagate_mem;
+    sub_and_propagate_mem = nullptr;
   }
 };
 
