bee062169e2553e9ee1b187fb7f595d8a9e0e96e
Pedro Alves <pedro.alves@zama.ai>
2025-08-18 10:57:03 -0300

    feat(gpu): implement 128-bit compression and add it to the integer API

--- backends/tfhe-cuda-backend/cuda/include/integer/integer_utilities.h
+++ backends/tfhe-cuda-backend/cuda/include/integer/integer_utilities.h
@@ -6,7 +6,6 @@
 #include "integer/radix_ciphertext.h"
 #include "keyswitch/keyswitch.h"
 #include "pbs/programmable_bootstrap.cuh"
-#include "pbs/programmable_bootstrap_128.cuh"
 #include "utils/helper_multi_gpu.cuh"
 #include <cmath>
 #include <functional>
@@ -354,7 +353,7 @@ template <typename Torus> struct int_radix_lut {
           streams[i], gpu_indexes[i], &gpu_pbs_buffer, params.glwe_dimension,
           params.small_lwe_dimension, params.polynomial_size, params.pbs_level,
           params.grouping_factor, num_blocks_on_gpu, params.pbs_type,
-          allocate_gpu_memory, params.allocate_ms_array, size);
+          allocate_gpu_memory, params.allocate_ms_array, &size);
       if (i == 0) {
         size_tracker += size;
       }
@@ -571,7 +570,7 @@ template <typename Torus> struct int_radix_lut {
           streams[i], gpu_indexes[i], &gpu_pbs_buffer, params.glwe_dimension,
           params.small_lwe_dimension, params.polynomial_size, params.pbs_level,
           params.grouping_factor, num_blocks_on_gpu, params.pbs_type,
-          allocate_gpu_memory, params.allocate_ms_array, size);
+          allocate_gpu_memory, params.allocate_ms_array, &size);
       if (i == 0) {
         size_tracker += size;
       }
@@ -876,11 +875,11 @@ template <typename InputTorus> struct int_noise_squashing_lut {
           get_num_inputs_on_gpu(num_radix_blocks, i, active_gpu_count));
       int8_t *gpu_pbs_buffer;
       uint64_t size = 0;
-      execute_scratch_pbs_128(streams[i], gpu_indexes[i], &gpu_pbs_buffer,
-                              params.small_lwe_dimension, params.glwe_dimension,
-                              params.polynomial_size, params.pbs_level,
-                              num_radix_blocks_on_gpu, allocate_gpu_memory,
-                              params.allocate_ms_array, size);
+      execute_scratch_pbs<__uint128_t>(
+          streams[i], gpu_indexes[i], &gpu_pbs_buffer, params.glwe_dimension,
+          params.small_lwe_dimension, params.polynomial_size, params.pbs_level,
+          params.grouping_factor, num_radix_blocks_on_gpu, params.pbs_type,
+          allocate_gpu_memory, params.allocate_ms_array, &size);
       cuda_synchronize_stream(streams[i], gpu_indexes[i]);
       if (i == 0) {
         size_tracker += size;
