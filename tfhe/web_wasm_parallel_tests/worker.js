4f6919ee680788dbcc45c35ce30e7675f8d4f93c
David Testé <david.teste@zama.ai>
2025-08-19 16:13:49 +0200

    WIP: modify wasm benchmarks for zkpok

--- tfhe/web_wasm_parallel_tests/worker.js
+++ tfhe/web_wasm_parallel_tests/worker.js
@@ -641,96 +641,207 @@ async function compressedServerKeyBenchMessage2Carry2() {
   );
 }
 
+// async function compactPublicKeyZeroKnowledgeBench() {
+//   let params_to_bench = [
+//     {
+//       name: "PARAM_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M64",
+//       block_params: new ShortintParameters(
+//         ShortintParametersName.PARAM_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M64,
+//       ),
+//       casting_params: new ShortintCompactPublicKeyEncryptionParameters(
+//         ShortintCompactPublicKeyEncryptionParametersName.SHORTINT_PARAM_PKE_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M64,
+//       ),
+//     },
+//   ];
+//
+//   let bench_results = {};
+//
+//   for (const params of params_to_bench) {
+//     let block_params_name = params.name;
+//     let block_params = params.block_params;
+//     let casting_params = params.casting_params;
+//
+//     let config = TfheConfigBuilder.default()
+//       .use_custom_parameters(block_params)
+//       .use_dedicated_compact_public_key_parameters(casting_params)
+//       .build();
+//
+//     let clientKey = TfheClientKey.generate(config);
+//     let publicKey = TfheCompactPublicKey.new(clientKey);
+//
+//     const bench_loops = 5; // The computation is expensive
+//     let load_choices = [ZkComputeLoad.Proof, ZkComputeLoad.Verify];
+//     const load_to_str = {
+//       [ZkComputeLoad.Proof]: "compute_load_proof",
+//       [ZkComputeLoad.Verify]: "compute_load_verify",
+//     };
+//
+//     let bits_to_encrypt = [64, 640, 1280, 4096];
+//
+//     let encrypt_counts = bits_to_encrypt.map((v) => v / 64);
+//
+//     for (const encrypt_count of encrypt_counts) {
+//       console.log("Start CRS generation");
+//       console.time("CRS generation");
+//       let crs = CompactPkeCrs.from_config(config, encrypt_count * 64);
+//       console.timeEnd("CRS generation");
+//
+//       // 320 bits is a use case we have, 8 bits per byte
+//       const metadata = new Uint8Array(320 / 8);
+//       crypto.getRandomValues(metadata);
+//
+//       let inputs = Array.from(Array(encrypt_count).keys()).map((_) => U64_MAX);
+//       for (const loadChoice of load_choices) {
+//         let serialized_size = 0;
+//         let timing = 0;
+//         for (let i = 0; i < bench_loops; i++) {
+//           console.time("Loop " + i);
+//           let compact_list_builder =
+//             ProvenCompactCiphertextList.builder(publicKey);
+//           for (let j = 0; j < encrypt_count; j++) {
+//             compact_list_builder.push_u64(inputs[j]);
+//           }
+//           const start = performance.now();
+//           let list = compact_list_builder.build_with_proof_packed(
+//             crs,
+//             metadata,
+//             loadChoice,
+//           );
+//           const end = performance.now();
+//           console.timeEnd("Loop " + i);
+//           timing += end - start;
+//           serialized_size = list.safe_serialize(BigInt(10000000)).length;
+//         }
+//         const mean = timing / bench_loops;
+//         const common_bench_str =
+//           "compact_fhe_uint_proven_encryption_" +
+//           encrypt_count * 64 +
+//           "_bits_packed_" +
+//           load_to_str[loadChoice];
+//         const bench_str_1 = common_bench_str + "_mean_" + block_params_name;
+//         console.log(bench_str_1, ": ", mean, " ms");
+//         const bench_str_2 =
+//           common_bench_str + "_serialized_size_mean_" + block_params_name;
+//         console.log(bench_str_2, ": ", serialized_size, " bytes");
+//
+//         bench_results[bench_str_1] = mean;
+//         bench_results[bench_str_2] = serialized_size;
+//       }
+//     }
+//   }
+//
+//   return bench_results;
+// }
+
 async function compactPublicKeyZeroKnowledgeBench() {
-  let params_to_bench = [
-    {
-      name: "PARAM_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M64",
-      block_params: new ShortintParameters(
-        ShortintParametersName.PARAM_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M64,
-      ),
-      casting_params: new ShortintCompactPublicKeyEncryptionParameters(
-        ShortintCompactPublicKeyEncryptionParametersName.SHORTINT_PARAM_PKE_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M64,
-      ),
-    },
-  ];
-
-  let bench_results = {};
-
-  for (const params of params_to_bench) {
-    let block_params_name = params.name;
-    let block_params = params.block_params;
-    let casting_params = params.casting_params;
-
-    let config = TfheConfigBuilder.default()
-      .use_custom_parameters(block_params)
-      .use_dedicated_compact_public_key_parameters(casting_params)
-      .build();
-
-    let clientKey = TfheClientKey.generate(config);
-    let publicKey = TfheCompactPublicKey.new(clientKey);
-
-    const bench_loops = 5; // The computation is expensive
-    let load_choices = [ZkComputeLoad.Proof, ZkComputeLoad.Verify];
-    const load_to_str = {
-      [ZkComputeLoad.Proof]: "compute_load_proof",
-      [ZkComputeLoad.Verify]: "compute_load_verify",
-    };
-
-    let bits_to_encrypt = [64, 640, 1280, 4096];
-
-    let encrypt_counts = bits_to_encrypt.map((v) => v / 64);
-
-    for (const encrypt_count of encrypt_counts) {
-      console.log("Start CRS generation");
-      console.time("CRS generation");
-      let crs = CompactPkeCrs.from_config(config, encrypt_count * 64);
-      console.timeEnd("CRS generation");
-
-      // 320 bits is a use case we have, 8 bits per byte
-      const metadata = new Uint8Array(320 / 8);
-      crypto.getRandomValues(metadata);
-
-      let inputs = Array.from(Array(encrypt_count).keys()).map((_) => U64_MAX);
-      for (const loadChoice of load_choices) {
-        let serialized_size = 0;
-        let timing = 0;
-        for (let i = 0; i < bench_loops; i++) {
-          console.time("Loop " + i);
-          let compact_list_builder =
-            ProvenCompactCiphertextList.builder(publicKey);
-          for (let j = 0; j < encrypt_count; j++) {
-            compact_list_builder.push_u64(inputs[j]);
-          }
-          const start = performance.now();
-          let list = compact_list_builder.build_with_proof_packed(
-            crs,
-            metadata,
-            loadChoice,
-          );
-          const end = performance.now();
-          console.timeEnd("Loop " + i);
-          timing += end - start;
-          serialized_size = list.safe_serialize(BigInt(10000000)).length;
+    let params_to_bench = [
+        {
+            zk_scheme: "ZKV2",
+            name: shortint_params_name(
+                ShortintParametersName.PARAM_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M128,
+            ),
+            block_params: new ShortintParameters(
+                ShortintParametersName.PARAM_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M64,
+            ),
+            casting_params: new ShortintCompactPublicKeyEncryptionParameters(
+                ShortintCompactPublicKeyEncryptionParametersName.SHORTINT_PARAM_PKE_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M64,
+            ),
         }
-        const mean = timing / bench_loops;
-        const common_bench_str =
-          "compact_fhe_uint_proven_encryption_" +
-          encrypt_count * 64 +
-          "_bits_packed_" +
-          load_to_str[loadChoice];
-        const bench_str_1 = common_bench_str + "_mean_" + block_params_name;
-        console.log(bench_str_1, ": ", mean, " ms");
-        const bench_str_2 =
-          common_bench_str + "_serialized_size_mean_" + block_params_name;
-        console.log(bench_str_2, ": ", serialized_size, " bytes");
+    ];
 
-        bench_results[bench_str_1] = mean;
-        bench_results[bench_str_2] = serialized_size;
-      }
+    let bench_results = {};
+
+    for (const params of params_to_bench) {
+        let block_params_name = params.name;
+        let block_params = params.block_params;
+        let casting_params = params.casting_params;
+
+        let config = TfheConfigBuilder.default()
+            .use_custom_parameters(block_params)
+            .use_dedicated_compact_public_key_parameters(casting_params)
+            .build();
+
+        let clientKey = TfheClientKey.generate(config);
+        let publicKey = TfheCompactPublicKey.new(clientKey);
+
+        const bench_loops = 5; // The computation is expensive
+        let load_choices = [ZkComputeLoad.Proof, ZkComputeLoad.Verify];
+        const load_to_str = {
+            [ZkComputeLoad.Proof]: "compute_load_proof",
+            [ZkComputeLoad.Verify]: "compute_load_verify",
+        };
+
+        // Proof configuration:
+        // [
+        //    [crs_size, [bits_to_encrypt_1, bits_to_encrypt_2, ...]],
+        //    ...
+        // ]
+        let proof_configs = [
+            // [64, [64]],
+            // [640, [640]],
+            [2048, [2048, 64 * 4]],
+            // [4096, [4096]],
+        ];
+
+        for (const proof_config of proof_configs) {
+            console.log("Start CRS generation");
+            console.time("CRS generation");
+            let crs = CompactPkeCrs.from_config(config, proof_config[0]);
+            console.timeEnd("CRS generation");
+
+            // 320 bits is a use case we have, 8 bits per byte
+            const metadata = new Uint8Array(320 / 8);
+            crypto.getRandomValues(metadata);
+
+            for (const bits_to_encrypt of proof_config[1]) {
+                let encrypt_count = bits_to_encrypt / 64;
+
+                let inputs = Array.from(Array(encrypt_count).keys()).map(
+                    (_) => U64_MAX,
+                );
+                for (const loadChoice of load_choices) {
+                    let serialized_size = 0;
+                    let timing = 0;
+                    for (let i = 0; i < bench_loops; i++) {
+                        console.time("Loop " + i);
+                        let compact_list_builder =
+                            ProvenCompactCiphertextList.builder(publicKey);
+                        for (let j = 0; j < encrypt_count; j++) {
+                            compact_list_builder.push_u64(inputs[j]);
+                        }
+                        const start = performance.now();
+                        let list = compact_list_builder.build_with_proof_packed(
+                            crs,
+                            metadata,
+                            loadChoice,
+                        );
+                        const end = performance.now();
+                        console.timeEnd("Loop " + i);
+                        timing += end - start;
+                        serialized_size = list.safe_serialize(BigInt(10000000)).length;
+                    }
+                    const mean = timing / bench_loops;
+                    const common_bench_str =
+                        "compact_fhe_uint_proven_encryption_" +
+                        params.zk_scheme +
+                        "_" +
+                        encrypt_count * 64 +
+                        "_bits_packed_" +
+                        load_to_str[loadChoice];
+                    const bench_str_1 = common_bench_str + "_mean_" + block_params_name;
+                    console.log(bench_str_1, ": ", mean, " ms");
+                    const bench_str_2 =
+                        common_bench_str + "_serialized_size_mean_" + block_params_name;
+                    console.log(bench_str_2, ": ", serialized_size, " bytes");
+
+                    bench_results[bench_str_1] = mean;
+                    bench_results[bench_str_2] = serialized_size;
+                }
+            }
+        }
     }
-  }
 
-  return bench_results;
+    return bench_results;
 }
 
 async function main() {
@@ -739,19 +850,19 @@ async function main() {
   await init_panic_hook();
 
   return Comlink.proxy({
-    publicKeyTest,
-    compressedPublicKeyTest,
-    compressedCompactPublicKeyTest256BitSmall,
-    compressedCompactPublicKeyTest256BitBig,
-    compactPublicKeyZeroKnowledgeTest,
-    compactPublicKeyBench32BitBig,
-    compactPublicKeyBench32BitSmall,
-    compactPublicKeyBench256BitBig,
-    compactPublicKeyBench256BitSmall,
-    compactPublicKeyWithCastingTest256Bit,
-    compressedCompactPublicKeyWithCastingTest256Bit,
-    compressedServerKeyBenchMessage1Carry1,
-    compressedServerKeyBenchMessage2Carry2,
+    // publicKeyTest,
+    // compressedPublicKeyTest,
+    // compressedCompactPublicKeyTest256BitSmall,
+    // compressedCompactPublicKeyTest256BitBig,
+    // compactPublicKeyZeroKnowledgeTest,
+    // compactPublicKeyBench32BitBig,
+    // compactPublicKeyBench32BitSmall,
+    // compactPublicKeyBench256BitBig,
+    // compactPublicKeyBench256BitSmall,
+    // compactPublicKeyWithCastingTest256Bit,
+    // compressedCompactPublicKeyWithCastingTest256Bit,
+    // compressedServerKeyBenchMessage1Carry1,
+    // compressedServerKeyBenchMessage2Carry2,
     compactPublicKeyZeroKnowledgeBench,
   });
 }
