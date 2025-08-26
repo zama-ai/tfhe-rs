3faa3c0646f01941a428add780cf660572030d77
Pedro Alves <pedro.alves@zama.ai>
2025-08-26 13:37:54 -0300

    feat(gpu): add support to the 128-bit multi-bit PBS on HL's noise squashing

--- tfhe/src/integer/gpu/noise_squashing/noise_squashing_keys.rs
+++ tfhe/src/integer/gpu/noise_squashing/noise_squashing_keys.rs
@@ -3,18 +3,20 @@ use crate::core_crypto::gpu::lwe_bootstrap_key::{
     CudaLweBootstrapKey, CudaModulusSwitchNoiseReductionConfiguration,
 };
 use crate::core_crypto::gpu::CudaStreams;
+use crate::core_crypto::gpu::lwe_multi_bit_bootstrap_key::CudaLweMultiBitBootstrapKey;
+use crate::integer::gpu::server_key::CudaBootstrappingKey;
 use crate::integer::noise_squashing::CompressedNoiseSquashingKey;
 use crate::shortint::noise_squashing::CompressedShortint128BootstrappingKey;
 use crate::shortint::server_key::CompressedModulusSwitchConfiguration;
 
 impl CompressedNoiseSquashingKey {
     pub fn decompress_to_cuda(&self, streams: &CudaStreams) -> CudaNoiseSquashingKey {
-        if let CompressedShortint128BootstrappingKey::Classic {
-            bsk,
-            modulus_switch_noise_reduction_key,
-        } = self.key.bootstrapping_key()
-        {
-            let std_bsk = bsk.as_view().par_decompress_into_lwe_bootstrap_key();
+        let bootstrapping_key = match self.key.bootstrapping_key() {
+            CompressedShortint128BootstrappingKey::Classic {
+                bsk: seeded_bsk,
+                modulus_switch_noise_reduction_key,
+            } => {
+                let std_bsk = seeded_bsk.as_view().par_decompress_into_lwe_bootstrap_key();
 
             let modulus_switch_noise_reduction_configuration = match modulus_switch_noise_reduction_key {
             CompressedModulusSwitchConfiguration::Standard => None,
@@ -26,20 +28,36 @@ impl CompressedNoiseSquashingKey {
             }
         };
 
-            let bootstrapping_key = CudaLweBootstrapKey::from_lwe_bootstrap_key(
+            let bsk = CudaLweBootstrapKey::from_lwe_bootstrap_key(
                 &std_bsk,
                 modulus_switch_noise_reduction_configuration,
                 streams,
             );
-
-            CudaNoiseSquashingKey {
-                bootstrapping_key,
-                message_modulus: self.key.message_modulus(),
-                carry_modulus: self.key.carry_modulus(),
-                output_ciphertext_modulus: self.key.output_ciphertext_modulus(),
+                CudaBootstrappingKey::Classic(bsk)
             }
-        } else {
-            panic!("Multi bit noise squashing PBS currently not supported on GPU");
+            CompressedShortint128BootstrappingKey::MultiBit {
+                bsk: seeded_mb_bsk,
+                thread_count: _thread_count,
+                deterministic_execution: _deterministic_execution,
+            } => {
+                let std_mb_bsk = seeded_mb_bsk
+                    .as_view()
+                    .par_decompress_into_lwe_multi_bit_bootstrap_key();
+
+                let mb_bsk = CudaLweMultiBitBootstrapKey::from_lwe_multi_bit_bootstrap_key(
+                    &std_mb_bsk,
+                    streams,
+                );
+
+                CudaBootstrappingKey::MultiBit(mb_bsk)
+            }
+        };
+
+        CudaNoiseSquashingKey {
+            bootstrapping_key,
+            message_modulus: self.key.message_modulus(),
+            carry_modulus: self.key.carry_modulus(),
+            output_ciphertext_modulus: self.key.output_ciphertext_modulus(),
         }
     }
 }
