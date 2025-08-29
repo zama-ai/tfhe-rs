4a10d8136c977ee18ff016afc494280290813015
David Testé <david.teste@zama.ai>
2025-08-29 18:49:24 +0200

    WIP: set classical 2_2 tuniform 2-128 params with no drift mitigation

--- tfhe/src/shortint/parameters/classic/tuniform/p_fail_2_minus_64/ks_pbs.rs
+++ tfhe/src/shortint/parameters/classic/tuniform/p_fail_2_minus_64/ks_pbs.rs
@@ -1,10 +1,13 @@
-// security = 132 bits, p-fail = 2^-64.138, algorithmic cost ~ 113, 2-norm = 5
+use crate::core_crypto::prelude::*;
+use crate::shortint::ciphertext::MaxNoiseLevel;
+use crate::shortint::parameters::{CarryModulus, ClassicPBSParameters, MessageModulus};
+
 pub const PARAM_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M64: ClassicPBSParameters =
     ClassicPBSParameters {
         lwe_dimension: LweDimension(879),
         glwe_dimension: GlweDimension(1),
         polynomial_size: PolynomialSize(4096),
-        lwe_noise_distribution: DynamicDistribution::new_t_uniform(46),
+        lwe_noise_distribution: DynamicDistribution::new_t_uniform(45),
         glwe_noise_distribution: DynamicDistribution::new_t_uniform(17),
         pbs_base_log: DecompositionBaseLog(23),
         pbs_level: DecompositionLevelCount(1),
@@ -17,7 +20,3 @@ pub const PARAM_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M64: ClassicPBSParameters =
         ciphertext_modulus: CiphertextModulus::new_native(),
         encryption_key_choice: EncryptionKeyChoice::Big,
     };
-
-use crate::core_crypto::prelude::*;
-use crate::shortint::ciphertext::MaxNoiseLevel;
-use crate::shortint::parameters::{CarryModulus, ClassicPBSParameters, MessageModulus};
