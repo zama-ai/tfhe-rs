a0256850d018ccfdc7880891caf6e5fad9b9856e
Pedro Alves <pedro.alves@zama.ai>
2025-09-09 14:44:33 -0300

    chore(gpu): refactor the indexing logic for the LWE expand

--- backends/tfhe-cuda-backend/cuda/include/zk/zk.h
+++ backends/tfhe-cuda-backend/cuda/include/zk/zk.h
@@ -5,14 +5,13 @@
 #include "../pbs/pbs_enums.h"
 #include <stdint.h>
 
-extern "C" {
+#include "zk_utilities.h"
 
 void cuda_lwe_expand_64(void *const stream, uint32_t gpu_index,
-                        void *lwe_array_out, const void *lwe_compact_array_in,
-                        uint32_t lwe_dimension, uint32_t num_lwe,
-                        const uint32_t *lwe_compact_input_indexes,
-                        const uint32_t *output_body_id_per_compact_list);
+                        void *lwe_array_out, expand_job<uint64_t> *d_jobs,
+                        uint32_t num_lwes, uint32_t lwe_dimension);
 
+extern "C" {
 uint64_t scratch_cuda_expand_without_verification_64(
     void *const *streams, uint32_t const *gpu_indexes, uint32_t gpu_count,
     int8_t **mem_ptr, uint32_t glwe_dimension, uint32_t polynomial_size,
