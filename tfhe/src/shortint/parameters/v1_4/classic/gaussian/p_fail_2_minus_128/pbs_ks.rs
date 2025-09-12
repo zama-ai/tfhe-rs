df0b3f5e91c70914362d3ce9deba4cf8506fee87
Arthur Meyre <arthur.meyre@zama.ai>
2025-09-12 13:51:28 +0200

    chore: update parameters with mean reduction technique
    - parameters checked visually with 1.1 for those who differ, all seems ok

--- tfhe/src/shortint/parameters/v1_4/classic/gaussian/p_fail_2_minus_128/pbs_ks.rs
+++ tfhe/src/shortint/parameters/v1_4/classic/gaussian/p_fail_2_minus_128/pbs_ks.rs
@@ -1,17 +1,105 @@
-use crate::shortint::parameters::ClassicPBSParameters;
-/// p-fail = 2^-128.181, algorithmic cost ~ 70, 2-norm = 3
-/// Average number of encryptions of 0s ~ 17, peak noise ~ Variance(0.0000226994502138943)
+use crate::shortint::parameters::{
+    CarryModulus, CiphertextModulus, ClassicPBSParameters, DecompositionBaseLog,
+    DecompositionLevelCount, DynamicDistribution, EncryptionKeyChoice, GlweDimension, LweDimension,
+    MaxNoiseLevel, MessageModulus, ModulusSwitchType, PolynomialSize, StandardDev,
+};
+
+/// p-fail = 2^-128.4, algorithmic cost ~ 70, 2-norm = 3
 pub const V1_4_PARAM_MESSAGE_1_CARRY_1_PBS_KS_GAUSSIAN_2M128: ClassicPBSParameters =
-    crate::shortint::parameters::v1_1::V1_1_PARAM_MESSAGE_1_CARRY_1_PBS_KS_GAUSSIAN_2M128;
-/// p-fail = 2^-128.163, algorithmic cost ~ 128, 2-norm = 5
-/// Average number of encryptions of 0s ~ 17, peak noise ~ Variance(0.00000141892645707080)
+    ClassicPBSParameters {
+        lwe_dimension: LweDimension(914),
+        glwe_dimension: GlweDimension(4),
+        polynomial_size: PolynomialSize(512),
+        lwe_noise_distribution: DynamicDistribution::new_gaussian_from_std_dev(StandardDev(
+            8.938614855855138e-07,
+        )),
+        glwe_noise_distribution: DynamicDistribution::new_gaussian_from_std_dev(StandardDev(
+            2.845267479601915e-15,
+        )),
+        pbs_base_log: DecompositionBaseLog(23),
+        pbs_level: DecompositionLevelCount(1),
+        ks_base_log: DecompositionBaseLog(5),
+        ks_level: DecompositionLevelCount(3),
+        message_modulus: MessageModulus(2),
+        carry_modulus: CarryModulus(2),
+        max_noise_level: MaxNoiseLevel::new(3),
+        log2_p_fail: -128.4,
+        ciphertext_modulus: CiphertextModulus::new_native(),
+        encryption_key_choice: EncryptionKeyChoice::Small,
+        modulus_switch_noise_reduction_params: ModulusSwitchType::CenteredMeanNoiseReduction,
+    };
+
+/// p-fail = 2^-128.05, algorithmic cost ~ 128, 2-norm = 5
 pub const V1_4_PARAM_MESSAGE_2_CARRY_2_PBS_KS_GAUSSIAN_2M128: ClassicPBSParameters =
-    crate::shortint::parameters::v1_1::V1_1_PARAM_MESSAGE_2_CARRY_2_PBS_KS_GAUSSIAN_2M128;
-/// p-fail = 2^-128.674, algorithmic cost ~ 2030, 2-norm = 9
-/// Average number of encryptions of 0s ~ 34, peak noise ~ Variance(8.83211431719384E-8)
+    ClassicPBSParameters {
+        lwe_dimension: LweDimension(978),
+        glwe_dimension: GlweDimension(1),
+        polynomial_size: PolynomialSize(2048),
+        lwe_noise_distribution: DynamicDistribution::new_gaussian_from_std_dev(StandardDev(
+            2.962875621642539e-07,
+        )),
+        glwe_noise_distribution: DynamicDistribution::new_gaussian_from_std_dev(StandardDev(
+            2.845267479601915e-15,
+        )),
+        pbs_base_log: DecompositionBaseLog(23),
+        pbs_level: DecompositionLevelCount(1),
+        ks_base_log: DecompositionBaseLog(3),
+        ks_level: DecompositionLevelCount(6),
+        message_modulus: MessageModulus(4),
+        carry_modulus: CarryModulus(4),
+        max_noise_level: MaxNoiseLevel::new(5),
+        log2_p_fail: -128.05,
+        ciphertext_modulus: CiphertextModulus::new_native(),
+        encryption_key_choice: EncryptionKeyChoice::Small,
+        modulus_switch_noise_reduction_params: ModulusSwitchType::CenteredMeanNoiseReduction,
+    };
+
+/// p-fail = 2^-128.729, algorithmic cost ~ 2030, 2-norm = 9
 pub const V1_4_PARAM_MESSAGE_3_CARRY_3_PBS_KS_GAUSSIAN_2M128: ClassicPBSParameters =
-    crate::shortint::parameters::v1_1::V1_1_PARAM_MESSAGE_3_CARRY_3_PBS_KS_GAUSSIAN_2M128;
-/// p-fail = 2^-129.799, algorithmic cost ~ 13785, 2-norm = 17
-/// Average number of encryptions of 0s ~ 34, peak noise ~ Variance(5.47094548750703E-9)
+    ClassicPBSParameters {
+        lwe_dimension: LweDimension(1106),
+        glwe_dimension: GlweDimension(1),
+        polynomial_size: PolynomialSize(16384),
+        lwe_noise_distribution: DynamicDistribution::new_gaussian_from_std_dev(StandardDev(
+            3.255365915886752e-08,
+        )),
+        glwe_noise_distribution: DynamicDistribution::new_gaussian_from_std_dev(StandardDev(
+            2.168404344971009e-19,
+        )),
+        pbs_base_log: DecompositionBaseLog(15),
+        pbs_level: DecompositionLevelCount(2),
+        ks_base_log: DecompositionBaseLog(3),
+        ks_level: DecompositionLevelCount(7),
+        message_modulus: MessageModulus(8),
+        carry_modulus: CarryModulus(8),
+        max_noise_level: MaxNoiseLevel::new(9),
+        log2_p_fail: -128.729,
+        ciphertext_modulus: CiphertextModulus::new_native(),
+        encryption_key_choice: EncryptionKeyChoice::Small,
+        modulus_switch_noise_reduction_params: ModulusSwitchType::CenteredMeanNoiseReduction,
+    };
+
+/// p-fail = 2^-129.855, algorithmic cost ~ 13785, 2-norm = 17
 pub const V1_4_PARAM_MESSAGE_4_CARRY_4_PBS_KS_GAUSSIAN_2M128: ClassicPBSParameters =
-    crate::shortint::parameters::v1_1::V1_1_PARAM_MESSAGE_4_CARRY_4_PBS_KS_GAUSSIAN_2M128;
+    ClassicPBSParameters {
+        lwe_dimension: LweDimension(1267),
+        glwe_dimension: GlweDimension(1),
+        polynomial_size: PolynomialSize(65536),
+        lwe_noise_distribution: DynamicDistribution::new_gaussian_from_std_dev(StandardDev(
+            2.0240196581361536e-09,
+        )),
+        glwe_noise_distribution: DynamicDistribution::new_gaussian_from_std_dev(StandardDev(
+            2.168404344971009e-19,
+        )),
+        pbs_base_log: DecompositionBaseLog(11),
+        pbs_level: DecompositionLevelCount(3),
+        ks_base_log: DecompositionBaseLog(3),
+        ks_level: DecompositionLevelCount(9),
+        message_modulus: MessageModulus(16),
+        carry_modulus: CarryModulus(16),
+        max_noise_level: MaxNoiseLevel::new(17),
+        log2_p_fail: -129.855,
+        ciphertext_modulus: CiphertextModulus::new_native(),
+        encryption_key_choice: EncryptionKeyChoice::Small,
+        modulus_switch_noise_reduction_params: ModulusSwitchType::CenteredMeanNoiseReduction,
+    };
