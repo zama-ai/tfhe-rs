2fb93cc5af3e6db49b4e0d157bda4c0d4c7a30e7
Arthur Meyre <arthur.meyre@zama.ai>
2025-09-18 13:32:23 +0200

    test: add multi bit modswitch noise simulation traits

--- tfhe/src/core_crypto/commons/noise_formulas/noise_simulation/modulus_switch.rs
+++ tfhe/src/core_crypto/commons/noise_formulas/noise_simulation/modulus_switch.rs
@@ -1,12 +1,14 @@
 use crate::core_crypto::commons::dispersion::Variance;
 use crate::core_crypto::commons::noise_formulas::modulus_switch::modulus_switch_additive_variance;
+use crate::core_crypto::commons::noise_formulas::multi_bit_modulus_switch::multi_bit_modulus_switch_additive_variance;
 use crate::core_crypto::commons::noise_formulas::noise_simulation::traits::{
-    AllocateStandardModSwitchResult, StandardModSwitch,
+    AllocateMultiBitModSwitchResult, AllocateStandardModSwitchResult, MultiBitModSwitch,
+    StandardModSwitch,
 };
 use crate::core_crypto::commons::noise_formulas::noise_simulation::{
     NoiseSimulationLwe, NoiseSimulationModulus,
 };
-use crate::core_crypto::commons::parameters::CiphertextModulusLog;
+use crate::core_crypto::commons::parameters::{CiphertextModulusLog, LweBskGroupingFactor};
 
 impl AllocateStandardModSwitchResult for NoiseSimulationLwe {
     type Output = Self;
@@ -43,6 +45,59 @@ impl StandardModSwitch<Self> for NoiseSimulationLwe {
 
         let mod_switch_additive_variance = modulus_switch_additive_variance(
             self.lwe_dimension,
+            input_modulus_f64,
+            output_modulus_f64,
+        );
+
+        *output = Self::new(
+            self.lwe_dimension,
+            Variance(self.variance.0 + mod_switch_additive_variance.0),
+            // Mod switched but the noise is to be interpreted with respect to the input modulus,
+            // as strictly the operation adding the noise is the rounding under the
+            // original modulus
+            self.modulus,
+        );
+    }
+}
+
+impl AllocateMultiBitModSwitchResult for NoiseSimulationLwe {
+    type Output = Self;
+    type SideResources = ();
+
+    fn allocate_multi_bit_mod_switch_result(
+        &self,
+        _side_resources: &mut Self::SideResources,
+    ) -> Self::Output {
+        Self {
+            lwe_dimension: self.lwe_dimension,
+            variance: Variance(f64::INFINITY),
+            modulus: self.modulus(),
+        }
+    }
+}
+
+impl MultiBitModSwitch<Self> for NoiseSimulationLwe {
+    type SideResources = ();
+
+    fn multi_bit_mod_switch(
+        &self,
+        grouping_factor: LweBskGroupingFactor,
+        output_modulus_log: CiphertextModulusLog,
+        output: &mut Self,
+        _side_resources: &mut Self::SideResources,
+    ) {
+        let simulation_after_mod_switch_modulus =
+            NoiseSimulationModulus::from_ciphertext_modulus_log(output_modulus_log);
+
+        let grouping_factor_f64 = grouping_factor.0 as f64;
+        let input_modulus_f64 = self.modulus().as_f64();
+        let output_modulus_f64 = simulation_after_mod_switch_modulus.as_f64();
+
+        assert!(output_modulus_f64 < input_modulus_f64);
+
+        let mod_switch_additive_variance = multi_bit_modulus_switch_additive_variance(
+            self.lwe_dimension,
+            grouping_factor_f64,
             input_modulus_f64,
             output_modulus_f64,
         );
