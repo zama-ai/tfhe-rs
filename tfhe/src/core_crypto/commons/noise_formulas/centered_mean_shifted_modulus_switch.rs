d9d7b74ca869eb4efc6d73a694718360e1d63a96
Arthur Meyre <arthur.meyre@zama.ai>
2025-09-19 12:59:48 +0200

    chore: update noise formulas with latest automated code gen

--- tfhe/src/core_crypto/commons/noise_formulas/centered_mean_shifted_modulus_switch.rs
+++ tfhe/src/core_crypto/commons/noise_formulas/centered_mean_shifted_modulus_switch.rs
@@ -7,26 +7,28 @@ use crate::core_crypto::commons::dispersion::Variance;
 use crate::core_crypto::commons::parameters::*;
 
 /// This formula is only valid when going from a larger to a smaller modulus
-pub fn modulus_switch_additive_variance(
+/// This formula is based on a heuristic, so may not always be valid
+pub fn centered_binary_shifted_modulus_switch_additive_variance(
     input_lwe_dimension: LweDimension,
     modulus: f64,
     new_modulus: f64,
 ) -> Variance {
-    Variance(modulus_switch_additive_variance_impl(
-        input_lwe_dimension.0 as f64,
-        modulus,
-        new_modulus,
-    ))
+    Variance(
+        centered_binary_shifted_modulus_switch_additive_variance_impl(
+            input_lwe_dimension.0 as f64,
+            modulus,
+            new_modulus,
+        ),
+    )
 }
 
 /// This formula is only valid when going from a larger to a smaller modulus
-pub fn modulus_switch_additive_variance_impl(
+/// This formula is based on a heuristic, so may not always be valid
+pub fn centered_binary_shifted_modulus_switch_additive_variance_impl(
     input_lwe_dimension: f64,
     modulus: f64,
     new_modulus: f64,
 ) -> f64 {
     input_lwe_dimension
-        * (0.0208333333333333 * modulus.powf(-2.0) + 0.0416666666666667 * new_modulus.powf(-2.0))
-        - 1_f64 / 12.0 * modulus.powf(-2.0)
-        + (1_f64 / 12.0) * new_modulus.powf(-2.0)
+        * (0.0416666666666667 * modulus.powf(-2.0) + 0.0208333333333333 * new_modulus.powf(-2.0))
 }
