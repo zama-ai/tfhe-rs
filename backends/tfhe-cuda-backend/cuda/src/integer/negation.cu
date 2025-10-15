zama-ai/tfhe-rs

commit cf3f25efddfabbc212aa484159a0beabafd0cb00

chore(gpu): add missing syncs in linearalgebra functions and aes

backends/tfhe-cuda-backend/cuda/src/integer/scalar_addition.cuh
@@ -53,6 +53,7 @@ __host__ void host_scalar_addition_inplace(
   for (uint i = 0; i < num_scalars; i++) {
     lwe_array->degrees[i] = lwe_array->degrees[i] + h_scalar_input[i];
   }
+  cuda_synchronize_stream(streams.stream(0), streams.gpu_index(0));
 }
 
 template <typename Torus>
@@ -93,6 +94,7 @@ __host__ void host_add_scalar_one_inplace(CudaStreams streams,
   for (uint i = 0; i < lwe_array->num_radix_blocks; i++) {
     lwe_array->degrees[i] = lwe_array->degrees[i] + 1;
   }
+  cuda_synchronize_stream(streams.stream(0), streams.gpu_index(0));
 }
 
 template <typename Torus>
@@ -134,5 +136,6 @@ __host__ void host_scalar_subtraction_inplace(
                                              input_lwe_ciphertext_count,
                                              lwe_dimension, delta);
   check_cuda_error(cudaGetLastError());
+  cuda_synchronize_stream(streams.stream(0), streams.gpu_index(0));
 }
 #endif

backends/tfhe-cuda-backend/cuda/src/linearalgebra/addition.cuh
@@ -2,15 +2,12 @@
 #define CUDA_ADD_CUH
 
 #ifdef __CDT_PARSER__
-#undef __CUDA_RUNTIME_H__
-#include <cuda_runtime.h>
 #endif
 
 #include "device.h"
 #include "helper_multi_gpu.h"
 #include "integer/integer.h"
 #include "integer/integer_utilities.h"
-#include "linear_algebra.h"
 #include "utils/kernel_dimensions.cuh"
 #include <stdio.h>
 
@@ -65,6 +62,7 @@ __host__ void host_addition_plaintext(cudaStream_t stream, uint32_t gpu_index,
   plaintext_addition<T><<<grid, thds, 0, stream>>>(
       output, lwe_input, plaintext_input, lwe_dimension, num_entries);
   check_cuda_error(cudaGetLastError());
+  cuda_synchronize_stream(stream, gpu_index);
 }
 
 template <typename T>
@@ -86,6 +84,7 @@ __host__ void host_addition_plaintext_scalar(
   plaintext_addition_scalar<T><<<grid, thds, 0, stream>>>(
       output, lwe_input, plaintext_input, lwe_dimension, num_entries);
   check_cuda_error(cudaGetLastError());
+  cuda_synchronize_stream(stream, gpu_index);
 }
 
 template <typename T>
@@ -139,6 +138,7 @@ host_addition(cudaStream_t stream, uint32_t gpu_index,
         input_1->noise_levels[i] + input_2->noise_levels[i];
     CHECK_NOISE_LEVEL(output->noise_levels[i], message_modulus, carry_modulus);
   }
+  cuda_synchronize_stream(stream, gpu_index);
 }
 
 template <typename T>
@@ -201,46 +201,6 @@ __host__ void host_add_the_same_block_to_all_blocks(
 }
 
 template <typename T>
-__global__ void pack_for_overflowing_ops(T *output, T const *input_1,
-                                         T const *input_2, uint32_t num_entries,
-                                         uint32_t message_modulus) {
-
-  int tid = threadIdx.x;
-  int index = blockIdx.x * blockDim.x + tid;
-  if (index < num_entries) {
-    // Here we take advantage of the wrapping behaviour of uint
-    output[index] = input_1[index] * message_modulus + input_2[index];
-  }
-}
-
-template <typename T>
-__host__ void host_pack_for_overflowing_ops(cudaStream_t stream,
-                                            uint32_t gpu_index, T *output,
-                                            T const *input_1, T const *input_2,
-                                            uint32_t input_lwe_dimension,
-                                            uint32_t input_lwe_ciphertext_count,
-                                            uint32_t message_modulus) {
-
-  cuda_set_device(gpu_index);
-  // lwe_size includes the presence of the body
-  // whereas lwe_dimension is the number of elements in the mask
-  int lwe_size = input_lwe_dimension + 1;
-  // Create a 1-dimensional grid of threads
-  int num_blocks = 0, num_threads = 0;
-  int num_entries = lwe_size;
-  getNumBlocksAndThreads(num_entries, 512, num_blocks, num_threads);
-  dim3 grid(num_blocks, 1, 1);
-  dim3 thds(num_threads, 1, 1);
-
-  pack_for_overflowing_ops<T><<<grid, thds, 0, stream>>>(
-      &output[(input_lwe_ciphertext_count - 1) * lwe_size],
-      &input_1[(input_lwe_ciphertext_count - 1) * lwe_size],
-      &input_2[(input_lwe_ciphertext_count - 1) * lwe_size], lwe_size,
-      message_modulus);
-  check_cuda_error(cudaGetLastError());
-}
-
-template <typename T>
 __global__ void subtraction(T *output, T const *input_1, T const *input_2,
                             uint32_t num_entries) {
 
@@ -273,6 +233,7 @@ __host__ void host_subtraction(cudaStream_t stream, uint32_t gpu_index,
   subtraction<T>
       <<<grid, thds, 0, stream>>>(output, input_1, input_2, num_entries);
   check_cuda_error(cudaGetLastError());
+  cuda_synchronize_stream(stream, gpu_index);
 }
 
 template <typename T>
@@ -312,6 +273,7 @@ __host__ void host_subtraction_plaintext(cudaStream_t stream,
   radix_body_subtraction_inplace<T><<<grid, thds, 0, stream>>>(
       output, plaintext_input, input_lwe_dimension, num_entries);
   check_cuda_error(cudaGetLastError());
+  cuda_synchronize_stream(stream, gpu_index);
 }
 
 template <typename T>

backends/tfhe-cuda-backend/cuda/src/linearalgebra/multiplication.cuh
@@ -50,6 +50,7 @@ __host__ void host_cleartext_vec_multiplication(
   cleartext_vec_multiplication<T><<<grid, thds, 0, stream>>>(
       output, lwe_input, cleartext_input, input_lwe_dimension, num_entries);
   check_cuda_error(cudaGetLastError());
+  cuda_synchronize_stream(stream, gpu_index);
 }
 
 template <typename T>

backends/tfhe-cuda-backend/cuda/src/linearalgebra/negation.cuh
@@ -39,6 +39,7 @@ __host__ void host_negation(cudaStream_t stream, uint32_t gpu_index, T *output,
 
   negation<T><<<grid, thds, 0, stream>>>(output, input, num_entries);
   check_cuda_error(cudaGetLastError());
+  cuda_synchronize_stream(stream, gpu_index);
 }
 
 #endif // CUDA_NEGATE_H

backends/tfhe-cuda-backend/cuda/include/aes/aes_utilities.h
@@ -79,6 +79,7 @@ template <typename Torus> struct int_aes_lut_buffers {
     this->carry_lut->release(streams);
     delete this->carry_lut;
     this->carry_lut = nullptr;
+    cuda_synchronize_stream(streams.stream(0), streams.gpu_index(0));
   }
 };
 
@@ -140,6 +141,7 @@ template <typename Torus> struct int_aes_round_workspaces {
                                    allocate_gpu_memory);
     delete this->vec_tmp_bit_buffer;
     this->vec_tmp_bit_buffer = nullptr;
+    cuda_synchronize_stream(streams.stream(0), streams.gpu_index(0));
   }
 };
 
@@ -206,12 +208,12 @@ template <typename Torus> struct int_aes_counter_workspaces {
     delete this->vec_trivial_b_bits_buffer;
     this->vec_trivial_b_bits_buffer = nullptr;
 
-    free(this->h_counter_bits_buffer);
     if (allocate_gpu_memory) {
       cuda_drop_async(this->d_counter_bits_buffer, streams.stream(0),
                       streams.gpu_index(0));
-      streams.synchronize();
     }
+    cuda_synchronize_stream(streams.stream(0), streams.gpu_index(0));
+    free(this->h_counter_bits_buffer);
   }
 };
 
@@ -303,6 +305,7 @@ template <typename Torus> struct int_aes_main_workspaces {
                                    allocate_gpu_memory);
     delete this->batch_processing_buffer;
     this->batch_processing_buffer = nullptr;
+    cuda_synchronize_stream(streams.stream(0), streams.gpu_index(0));
   }
 };
 
@@ -366,6 +369,7 @@ template <typename Torus> struct int_aes_encrypt_buffer {
     main_workspaces->release(streams, allocate_gpu_memory);
     delete main_workspaces;
     main_workspaces = nullptr;
+    cuda_synchronize_stream(streams.stream(0), streams.gpu_index(0));
   }
 };
 
@@ -434,6 +438,7 @@ template <typename Torus> struct int_key_expansion_buffer {
 
     this->aes_encrypt_buffer->release(streams);
     delete this->aes_encrypt_buffer;
+    cuda_synchronize_stream(streams.stream(0), streams.gpu_index(0));
   }
 };
 

commit 73688636ccfc17ab44065d240a164a3bdbb693d2

fix(gpu): fix perf regression introduced in cf3f25efddf

backends/tfhe-cuda-backend/cuda/src/integer/negation.cu
@@ -6,6 +6,8 @@ void cuda_negate_ciphertext_64(CudaStreamsFFI streams,
                                uint32_t message_modulus, uint32_t carry_modulus,
                                uint32_t num_radix_blocks) {
 
-  host_negation<uint64_t>(CudaStreams(streams), lwe_array_out, lwe_array_in,
+  auto cuda_streams = CudaStreams(streams);
+  host_negation<uint64_t>(cuda_streams, lwe_array_out, lwe_array_in,
                           message_modulus, carry_modulus, num_radix_blocks);
+  cuda_synchronize_stream(cuda_streams.stream(0), cuda_streams.gpu_index(0));
 }

backends/tfhe-cuda-backend/cuda/src/integer/scalar_addition.cu
@@ -5,9 +5,10 @@ void cuda_scalar_addition_ciphertext_64_inplace(
     void const *scalar_input, void const *h_scalar_input, uint32_t num_scalars,
     uint32_t message_modulus, uint32_t carry_modulus) {
 
+  auto cuda_streams = CudaStreams(streams);
   host_scalar_addition_inplace<uint64_t>(
-      CudaStreams(streams), lwe_array,
-      static_cast<const uint64_t *>(scalar_input),
+      cuda_streams, lwe_array, static_cast<const uint64_t *>(scalar_input),
       static_cast<const uint64_t *>(h_scalar_input), num_scalars,
       message_modulus, carry_modulus);
+  cuda_synchronize_stream(cuda_streams.stream(0), cuda_streams.gpu_index(0));
 }

backends/tfhe-cuda-backend/cuda/src/integer/scalar_addition.cuh
@@ -53,7 +53,6 @@ __host__ void host_scalar_addition_inplace(
   for (uint i = 0; i < num_scalars; i++) {
     lwe_array->degrees[i] = lwe_array->degrees[i] + h_scalar_input[i];
   }
-  cuda_synchronize_stream(streams.stream(0), streams.gpu_index(0));
 }
 
 template <typename Torus>
@@ -94,7 +93,6 @@ __host__ void host_add_scalar_one_inplace(CudaStreams streams,
   for (uint i = 0; i < lwe_array->num_radix_blocks; i++) {
     lwe_array->degrees[i] = lwe_array->degrees[i] + 1;
   }
-  cuda_synchronize_stream(streams.stream(0), streams.gpu_index(0));
 }
 
 template <typename Torus>
@@ -136,6 +134,5 @@ __host__ void host_scalar_subtraction_inplace(
                                              input_lwe_ciphertext_count,
                                              lwe_dimension, delta);
   check_cuda_error(cudaGetLastError());
-  cuda_synchronize_stream(streams.stream(0), streams.gpu_index(0));
 }
 #endif

backends/tfhe-cuda-backend/cuda/src/linearalgebra/addition.cu
@@ -11,6 +11,7 @@ void cuda_add_lwe_ciphertext_vector_32(void *stream, uint32_t gpu_index,
     PANIC("Cuda error: input and output num radix blocks must be the same")
   host_addition<uint32_t>(static_cast<cudaStream_t>(stream), gpu_index, output,
                           input_1, input_2, output->num_radix_blocks, 0, 0);
+  cuda_synchronize_stream(static_cast<cudaStream_t>(stream), gpu_index);
 }
 
 /*
@@ -49,6 +50,7 @@ void cuda_add_lwe_ciphertext_vector_64(void *stream, uint32_t gpu_index,
     PANIC("Cuda error: input and output num radix blocks must be the same")
   host_addition<uint64_t>(static_cast<cudaStream_t>(stream), gpu_index, output,
                           input_1, input_2, output->num_radix_blocks, 0, 0);
+  cuda_synchronize_stream(static_cast<cudaStream_t>(stream), gpu_index);
 }
 
 /*
@@ -67,6 +69,7 @@ void cuda_add_lwe_ciphertext_vector_plaintext_vector_32(
       static_cast<const uint32_t *>(lwe_array_in),
       static_cast<const uint32_t *>(plaintext_array_in), input_lwe_dimension,
       input_lwe_ciphertext_count);
+  cuda_synchronize_stream(static_cast<cudaStream_t>(stream), gpu_index);
 }
 /*
  * Perform the addition of a u64 input LWE ciphertext vector with a u64 input
@@ -108,6 +111,7 @@ void cuda_add_lwe_ciphertext_vector_plaintext_vector_64(
       static_cast<const uint64_t *>(lwe_array_in),
       static_cast<const uint64_t *>(plaintext_array_in), input_lwe_dimension,
       input_lwe_ciphertext_count);
+  cuda_synchronize_stream(static_cast<cudaStream_t>(stream), gpu_index);
 }
 
 /*
@@ -146,4 +150,5 @@ void cuda_add_lwe_ciphertext_vector_plaintext_64(
       static_cast<uint64_t *>(lwe_array_out),
       static_cast<const uint64_t *>(lwe_array_in), plaintext_in,
       input_lwe_dimension, input_lwe_ciphertext_count);
+  cuda_synchronize_stream(static_cast<cudaStream_t>(stream), gpu_index);
 }

backends/tfhe-cuda-backend/cuda/src/linearalgebra/addition.cuh
@@ -62,7 +62,6 @@ __host__ void host_addition_plaintext(cudaStream_t stream, uint32_t gpu_index,
   plaintext_addition<T><<<grid, thds, 0, stream>>>(
       output, lwe_input, plaintext_input, lwe_dimension, num_entries);
   check_cuda_error(cudaGetLastError());
-  cuda_synchronize_stream(stream, gpu_index);
 }
 
 template <typename T>
@@ -84,7 +83,6 @@ __host__ void host_addition_plaintext_scalar(
   plaintext_addition_scalar<T><<<grid, thds, 0, stream>>>(
       output, lwe_input, plaintext_input, lwe_dimension, num_entries);
   check_cuda_error(cudaGetLastError());
-  cuda_synchronize_stream(stream, gpu_index);
 }
 
 template <typename T>
@@ -138,7 +136,6 @@ host_addition(cudaStream_t stream, uint32_t gpu_index,
         input_1->noise_levels[i] + input_2->noise_levels[i];
     CHECK_NOISE_LEVEL(output->noise_levels[i], message_modulus, carry_modulus);
   }
-  cuda_synchronize_stream(stream, gpu_index);
 }
 
 template <typename T>
@@ -233,7 +230,6 @@ __host__ void host_subtraction(cudaStream_t stream, uint32_t gpu_index,
   subtraction<T>
       <<<grid, thds, 0, stream>>>(output, input_1, input_2, num_entries);
   check_cuda_error(cudaGetLastError());
-  cuda_synchronize_stream(stream, gpu_index);
 }
 
 template <typename T>
@@ -273,7 +269,6 @@ __host__ void host_subtraction_plaintext(cudaStream_t stream,
   radix_body_subtraction_inplace<T><<<grid, thds, 0, stream>>>(
       output, plaintext_input, input_lwe_dimension, num_entries);
   check_cuda_error(cudaGetLastError());
-  cuda_synchronize_stream(stream, gpu_index);
 }
 
 template <typename T>

backends/tfhe-cuda-backend/cuda/src/linearalgebra/multiplication.cu
@@ -17,6 +17,7 @@ void cuda_mult_lwe_ciphertext_vector_cleartext_vector_32(
       static_cast<const uint32_t *>(lwe_array_in),
       static_cast<const uint32_t *>(cleartext_array_in), input_lwe_dimension,
       input_lwe_ciphertext_count);
+  cuda_synchronize_stream(static_cast<cudaStream_t>(stream), gpu_index);
 }
 /*
  * Perform the multiplication of a u64 input LWE ciphertext vector with a u64
@@ -58,6 +59,7 @@ void cuda_mult_lwe_ciphertext_vector_cleartext_vector_64(
       static_cast<const uint64_t *>(lwe_array_in),
       static_cast<const uint64_t *>(cleartext_array_in), input_lwe_dimension,
       input_lwe_ciphertext_count);
+  cuda_synchronize_stream(static_cast<cudaStream_t>(stream), gpu_index);
 }
 
 void scratch_wrapping_polynomial_mul_one_to_many_64(void *stream,

backends/tfhe-cuda-backend/cuda/src/linearalgebra/multiplication.cuh
@@ -50,7 +50,6 @@ __host__ void host_cleartext_vec_multiplication(
   cleartext_vec_multiplication<T><<<grid, thds, 0, stream>>>(
       output, lwe_input, cleartext_input, input_lwe_dimension, num_entries);
   check_cuda_error(cudaGetLastError());
-  cuda_synchronize_stream(stream, gpu_index);
 }
 
 template <typename T>

backends/tfhe-cuda-backend/cuda/src/linearalgebra/negation.cu
@@ -13,6 +13,7 @@ void cuda_negate_lwe_ciphertext_vector_32(
                           static_cast<uint32_t *>(lwe_array_out),
                           static_cast<const uint32_t *>(lwe_array_in),
                           input_lwe_dimension, input_lwe_ciphertext_count);
+  cuda_synchronize_stream(static_cast<cudaStream_t>(stream), gpu_index);
 }
 
 /*
@@ -46,4 +47,5 @@ void cuda_negate_lwe_ciphertext_vector_64(
                           static_cast<uint64_t *>(lwe_array_out),
                           static_cast<const uint64_t *>(lwe_array_in),
                           input_lwe_dimension, input_lwe_ciphertext_count);
+  cuda_synchronize_stream(static_cast<cudaStream_t>(stream), gpu_index);
 }

backends/tfhe-cuda-backend/cuda/src/linearalgebra/negation.cuh
@@ -39,7 +39,6 @@ __host__ void host_negation(cudaStream_t stream, uint32_t gpu_index, T *output,
 
   negation<T><<<grid, thds, 0, stream>>>(output, input, num_entries);
   check_cuda_error(cudaGetLastError());
-  cuda_synchronize_stream(stream, gpu_index);
 }
 
 #endif // CUDA_NEGATE_H