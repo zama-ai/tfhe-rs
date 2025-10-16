59b81e9e8bc0c7ca4c0f30e86703c1b80cf0b672
Mayeul@Zama <69792125+mayeul-zama@users.noreply.github.com>
2025-10-16 10:30:46 +0200

    style(shortint): reorder fields

--- tfhe/src/shortint/parameters/list_compression.rs
+++ tfhe/src/shortint/parameters/list_compression.rs
@@ -10,13 +10,13 @@ use tfhe_versionable::Versionize;
 #[derive(Copy, Clone, Debug, PartialEq, serde::Serialize, serde::Deserialize, Versionize)]
 #[versionize(CompressionParametersVersions)]
 pub struct CompressionParameters {
-    pub br_level: DecompositionLevelCount,
-    pub br_base_log: DecompositionBaseLog,
     pub packing_ks_level: DecompositionLevelCount,
     pub packing_ks_base_log: DecompositionBaseLog,
     pub packing_ks_polynomial_size: PolynomialSize,
     pub packing_ks_glwe_dimension: GlweDimension,
+    pub packing_ks_key_noise_distribution: DynamicDistribution<u64>,
     pub lwe_per_glwe: LweCiphertextCount,
     pub storage_log_modulus: CiphertextModulusLog,
-    pub packing_ks_key_noise_distribution: DynamicDistribution<u64>,
+    pub br_level: DecompositionLevelCount,
+    pub br_base_log: DecompositionBaseLog,
 }
