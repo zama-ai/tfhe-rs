cb44233e13f811c402b2b28b0a66e7fe7619deb7
Enzo Di Maria <enzo.dimaria@zama.ai>
2025-10-29 14:00:25 +0100

    refactor(gpu): delete useless GPU params

--- tfhe/src/shortint/parameters/v1_5/list_compression/p_fail_2_minus_64/mod.rs
+++ tfhe/src/shortint/parameters/v1_5/list_compression/p_fail_2_minus_64/mod.rs
@@ -18,20 +18,6 @@ pub const V1_5_COMP_PARAM_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M64: CompressionPar
         packing_ks_key_noise_distribution: DynamicDistribution::new_t_uniform(43),
     });
 
-/// p-fail = 2^-72.052, algorithmic cost ~ 42700
-pub const V1_5_COMP_PARAM_GPU_MULTI_BIT_GROUP_4_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M64:
-    CompressionParameters = CompressionParameters::Classic(ClassicCompressionParameters {
-    br_level: DecompositionLevelCount(1),
-    br_base_log: DecompositionBaseLog(23),
-    packing_ks_level: DecompositionLevelCount(2),
-    packing_ks_base_log: DecompositionBaseLog(6),
-    packing_ks_polynomial_size: PolynomialSize(256),
-    packing_ks_glwe_dimension: GlweDimension(4),
-    lwe_per_glwe: LweCiphertextCount(256),
-    storage_log_modulus: CiphertextModulusLog(12),
-    packing_ks_key_noise_distribution: DynamicDistribution::new_t_uniform(43),
-});
-
 /// p-fail = 2^-64.174, algorithmic cost ~ 58234
 pub const V1_5_COMP_PARAM_MESSAGE_2_CARRY_2_KS_PBS_GAUSSIAN_2M64: CompressionParameters =
     CompressionParameters::Classic(ClassicCompressionParameters {
@@ -47,19 +33,3 @@ pub const V1_5_COMP_PARAM_MESSAGE_2_CARRY_2_KS_PBS_GAUSSIAN_2M64: CompressionPar
             StandardDev(1.339775301998614e-07),
         ),
     });
-
-/// p-fail = 2^-64.174, algorithmic cost ~ 58234
-pub const V1_5_COMP_PARAM_GPU_MULTI_BIT_GROUP_4_MESSAGE_2_CARRY_2_KS_PBS_GAUSSIAN_2M64:
-    CompressionParameters = CompressionParameters::Classic(ClassicCompressionParameters {
-    br_level: DecompositionLevelCount(1),
-    br_base_log: DecompositionBaseLog(23),
-    packing_ks_level: DecompositionLevelCount(2),
-    packing_ks_base_log: DecompositionBaseLog(6),
-    packing_ks_polynomial_size: PolynomialSize(256),
-    packing_ks_glwe_dimension: GlweDimension(4),
-    lwe_per_glwe: LweCiphertextCount(256),
-    storage_log_modulus: CiphertextModulusLog(12),
-    packing_ks_key_noise_distribution: DynamicDistribution::new_gaussian_from_std_dev(StandardDev(
-        1.339775301998614e-07,
-    )),
-});
