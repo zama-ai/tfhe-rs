77ecc395e9e07a785207ee3839b9b0de3b247011
Nicolas Sarlin <nicolas.sarlin@zama.ai>
2025-11-17 14:29:10 +0100

    chore: test vector generation tool for core algorithms

---
+++ apps/test-vectors/data/README.md
@@ -0,0 +1,102 @@
+# Test vectors for TFHE
+These test vectors are generated using [TFHE-rs](https://github.com/zama-ai/tfhe-rs), with the git tag `tfhe-test-vectors-0.1.0`.
+
+They are TFHE-rs objects serialized in the [cbor format](https://cbor.io/). You can deserialize them using any cbor library for the language of your choice. For example, using the [cbor2](https://pypi.org/project/cbor2/) program, run: `cbor2 --pretty toy_params/lwe_a.cbor`.
+
+You will find 2 folders with test vectors for different parameter sets:
+- `valid_params_128`: valid classical PBS parameters using a gaussian noise distribution, providing 128bits of security in the IND-CPA model and a bootstrapping probability of failure of 2^{-64}.
+- `toy_params`: insecure parameters that yield smaller values
+
+The values are generated for the keyswitch -> bootstrap (KS-PBS) atomic pattern. The cleartext inputs are 2 values, A and B defined below.
+
+All the random values are generated from a fixed seed, that can be found in the `RAND_SEED` constant below. The PRNG used is the one based on the AES block cipher in counter mode, from tfhe `tfhe-csprng` crate.
+
+The programmable bootstrap is applied twice, with 2 different lut, the identity lut and a specific one (currently a x2 operation)
+
+## Vectors
+The following values are generated:
+
+### Keys
+| name                   | description                                                                           | TFHE-rs type                |
+|------------------------|---------------------------------------------------------------------------------------|-----------------------------|
+| `large_lwe_secret_key` | Encryption secret key, before the KS and after the PBS                                | `LweSecretKey<Vec<u64>>`    |
+| `small_lwe_secret_key` | Secret key encrypting ciphertexts between the KS and the PBS                          | `LweSecretKey<Vec<u64>>`    |
+| `ksk`                  | The keyswitching key to convert a ct from the large key to the small one              | `LweKeyswitchKey<Vec<u64>>` |
+| `bsk`                  | the bootstrapping key to perform a programmable bootstrap on the keyswitched ciphertext | `LweBootstrapKey<Vec<u64>>` |
+
+
+### Ciphertexts
+| name                 | description                                                                                                  | TFHE-rs type               | Cleartext    |
+|----------------------|--------------------------------------------------------------------------------------------------------------|----------------------------|--------------|
+| `lwe_a`              | Lwe encryption of A                                                                                          | `LweCiphertext<Vec<u64>>`  | `A`          |
+| `lwe_b`              | Lwe encryption of B                                                                                          | `LweCiphertext<Vec<u64>>`  | `B`          |
+| `lwe_sum`            | Lwe encryption of A plus lwe encryption of B                                                                 | `LweCiphertext<Vec<u64>>`  | `A+B`        |
+| `lwe_prod`           | Lwe encryption of A times cleartext B                                                                        | `LweCiphertext<Vec<u64>>`  | `A*B`        |
+| `lwe_ms`             | The lwe ciphertext after the modswitch part of the PBS ([note](#non-native-encoding))                        | `LweCiphertext<Vec<u64>>`  | `A`          |
+| `lwe_ks`             | The lwe ciphertext after the keyswitch                                                                       | `LweCiphertext<Vec<u64>>`  | `A`          |
+| `glwe_after_id_br`   | The glwe returned by the application of the identity blind rotation on the mod switched ciphertexts.         | `GlweCiphertext<Vec<u64>>` | rot id LUT   |
+| `lwe_after_id_pbs`   | The lwe returned by the application of the sample extract operation on the output of the id blind rotation   | `LweCiphertext<Vec<u64>>`  | `A`          |
+| `glwe_after_spec_br` | The glwe returned by the application of the spec blind rotation on the mod switched ciphertexts.             | `GlweCiphertext<Vec<u64>>` | rot spec LUT |
+| `lwe_after_spec_pbs` | The lwe returned by the application of the sample extract operation on the output of the spec blind rotation | `LweCiphertext<Vec<u64>>`  | `spec(A)`    |
+
+### Encodings
+#### Non native encoding
+Warning: TFHE-rs uses a specific encoding for non native (ie: u32, u64) power of two ciphertext modulus. This encoding puts the encoded value in the high bits of the native integer.
+For example, the value 37 with a modulus of 64 will be encoded as `0b1001010000000000000000000000000000000000000000000000000000000000`. This matters for the post modswitch lwe ciphertext.
+
+#### Ciphertext modulus
+The ciphertext modulus encoding use a specific value for the native modulus: 0. For example, if values are stored on u64 integers, 0 means a ciphertext modulus of 2^64.
+
+## Operations
+
+| name                    | inputs                                                            | outputs                |
+|-------------------------|-------------------------------------------------------------------|------------------------|
+| large secret key gen    | PARAMS, RAND_SEED                                                 | `large_lwe_secret_key` |
+| small secret key gen    | PARAMS, RAND_SEED                                                 | `small_lwe_secret_key` |
+| keyswitch key gen       | PARAMS, RAND_SEED, `large_lwe_secret_key`, `small_lwe_secret_key` | `ksk`                  |
+| bootstrap key gen       | PARAMS, RAND_SEED, `small_lwe_secret_key`, `large_lwe_secret_key` | `bsk`                  |
+| encryption A            | A, `large_lwe_secret_key`                                         | `lwe_a`                |
+| encryption B            | B, `large_lwe_secret_key`                                         | `lwe_b`                |
+| `E(A)+E(B)`             | `lwe_a`, `lwe_b`                                                  | `lwe_sum`              |
+| `E(A)*B`                | `lwe_a`, B                                                        | `lwe_prod`             |
+| keyswitch               | `lwe_a`, `ksk`                                                    | `lwe_ks`               |
+| modulus switch          | `lwe_ks`                                                          | `lwe_ms`               |
+| blind rotation id lut   | ID_LUT, `lwe_ms`, `bsk`                                           | `glwe_after_id_br`     |
+| sample extract id lut   | `glwe_after_id_br`                                                | `lwe_after_id_pbs`     |
+| blind rotation spec lut | SPEC_LUT, `lwe_ms`, `bsk`                                         | `glwe_after_spec_br`   |
+| sample extract spec lut | `glwe_after_spec_br`                                              | `lwe_after_spec_pbs`   |
+
+## Parameters
+
+```rust
+const RAND_SEED: u128 = 0x74666865;
+
+const MSG_A: u64 = 4;
+const MSG_B: u64 = 3;
+
+const VALID_LWE_DIMENSION: LweDimension = LweDimension(833);
+const VALID_GLWE_DIMENSION: GlweDimension = GlweDimension(1);
+const VALID_POLYNOMIAL_SIZE: PolynomialSize = PolynomialSize(2048);
+const VALID_GAUSSIAN_LWE_NOISE_STDDEV: f64 = 3.6158408373309336e-06;
+const VALID_GAUSSIAN_GLWE_NOISE_STDDEV: f64 = 2.845267479601915e-15;
+const VALID_PBS_DECOMPOSITION_BASE_LOG: DecompositionBaseLog = DecompositionBaseLog(23);
+const VALID_PBS_DECOMPOSITION_LEVEL_COUNT: DecompositionLevelCount = DecompositionLevelCount(1);
+const VALID_KS_DECOMPOSITION_BASE_LOG: DecompositionBaseLog = DecompositionBaseLog(3);
+const VALID_KS_DECOMPOSITION_LEVEL_COUNT: DecompositionLevelCount = DecompositionLevelCount(5);
+
+const TOY_LWE_DIMENSION: LweDimension = LweDimension(10);
+const TOY_GLWE_DIMENSION: GlweDimension = GlweDimension(1);
+const TOY_POLYNOMIAL_SIZE: PolynomialSize = PolynomialSize(256);
+const TOY_GAUSSIAN_LWE_NOISE_STDDEV: f64 = 0.;
+const TOY_GAUSSIAN_GLWE_NOISE_STDDEV: f64 = 0.;
+const TOY_PBS_DECOMPOSITION_BASE_LOG: DecompositionBaseLog = DecompositionBaseLog(24);
+const TOY_PBS_DECOMPOSITION_LEVEL_COUNT: DecompositionLevelCount = DecompositionLevelCount(1);
+const TOY_KS_DECOMPOSITION_BASE_LOG: DecompositionBaseLog = DecompositionBaseLog(37);
+const TOY_KS_DECOMPOSITION_LEVEL_COUNT: DecompositionLevelCount = DecompositionLevelCount(1);
+
+const CIPHERTEXT_MODULUS: CiphertextModulus<u64> = CiphertextModulus::new_native();
+const MSG_BITS: usize = 4;
+
+const SPEC_LUT: fn(u64) -> u64 = |x| (x * 2) % (1u64 << MSG_BITS);
+const ID_LUT: fn(u64) -> u64 = |x| x;
+```
---
+++ apps/test-vectors/src/main.rs
@@ -0,0 +1,339 @@
+use serde::Serialize;
+use std::fs::{File, create_dir_all, remove_dir_all};
+use std::panic;
+use std::path::{Path, PathBuf};
+
+use tfhe::core_crypto::commons::generators::DeterministicSeeder;
+use tfhe::core_crypto::commons::math::random::Seed;
+use tfhe::core_crypto::prelude::*;
+use tfhe_csprng::generators::SoftwareRandomGenerator;
+
+const RAND_SEED: u128 = 0x74666865;
+
+const MSG_A: u64 = 4;
+const MSG_B: u64 = 3;
+
+const VALID_LWE_DIMENSION: LweDimension = LweDimension(833);
+const VALID_GLWE_DIMENSION: GlweDimension = GlweDimension(1);
+const VALID_POLYNOMIAL_SIZE: PolynomialSize = PolynomialSize(2048);
+const VALID_GAUSSIAN_LWE_NOISE_STDDEV: f64 = 3.6158408373309336e-06;
+const VALID_GAUSSIAN_GLWE_NOISE_STDDEV: f64 = 2.845267479601915e-15;
+const VALID_PBS_DECOMPOSITION_BASE_LOG: DecompositionBaseLog = DecompositionBaseLog(23);
+const VALID_PBS_DECOMPOSITION_LEVEL_COUNT: DecompositionLevelCount = DecompositionLevelCount(1);
+const VALID_KS_DECOMPOSITION_BASE_LOG: DecompositionBaseLog = DecompositionBaseLog(3);
+const VALID_KS_DECOMPOSITION_LEVEL_COUNT: DecompositionLevelCount = DecompositionLevelCount(5);
+
+const TOY_LWE_DIMENSION: LweDimension = LweDimension(10);
+const TOY_GLWE_DIMENSION: GlweDimension = GlweDimension(1);
+const TOY_POLYNOMIAL_SIZE: PolynomialSize = PolynomialSize(256);
+const TOY_GAUSSIAN_LWE_NOISE_STDDEV: f64 = 0.;
+const TOY_GAUSSIAN_GLWE_NOISE_STDDEV: f64 = 0.;
+const TOY_PBS_DECOMPOSITION_BASE_LOG: DecompositionBaseLog = DecompositionBaseLog(24);
+const TOY_PBS_DECOMPOSITION_LEVEL_COUNT: DecompositionLevelCount = DecompositionLevelCount(1);
+const TOY_KS_DECOMPOSITION_BASE_LOG: DecompositionBaseLog = DecompositionBaseLog(37);
+const TOY_KS_DECOMPOSITION_LEVEL_COUNT: DecompositionLevelCount = DecompositionLevelCount(1);
+
+const ENCODING: Encoding = Encoding {
+    ciphertext_modulus: CiphertextModulus::new_native(),
+    msg_bits: 4,
+};
+
+const SPEC_LUT: fn(u64) -> u64 = |x| (x * 2) & (1u64 << ENCODING.msg_bits);
+const ID_LUT: fn(u64) -> u64 = |x| x;
+
+const DATA_DIR: &str = "./data";
+
+struct Encoding {
+    ciphertext_modulus: CiphertextModulus<u64>,
+    msg_bits: usize,
+}
+
+impl Encoding {
+    fn log_delta(&self) -> usize {
+        self.ciphertext_modulus.into_modulus_log().0 - self.msg_bits - 1
+    }
+
+    const fn msg_modulus(&self) -> usize {
+        1 << self.msg_bits
+    }
+
+    fn encode(&self, msg: u64) -> Plaintext<u64> {
+        Plaintext(msg << self.log_delta())
+    }
+
+    fn decode(&self, plaintext: Plaintext<u64>) -> u64 {
+        let decomposer = SignedDecomposer::new(
+            DecompositionBaseLog(self.msg_bits + 1),
+            DecompositionLevelCount(1),
+        );
+        let decoded = decomposer.decode_plaintext(plaintext);
+
+        decoded.0
+    }
+
+    fn encode_lut(
+        &self,
+        glwe_dimension: GlweDimension,
+        polynomial_size: PolynomialSize,
+        f: impl Fn(u64) -> u64,
+    ) -> GlweCiphertext<Vec<u64>> {
+        generate_programmable_bootstrap_glwe_lut(
+            polynomial_size,
+            glwe_dimension.to_glwe_size(),
+            self.msg_modulus(),
+            self.ciphertext_modulus,
+            1 << self.log_delta(),
+            f,
+        )
+    }
+}
+
+fn modswitched_to_lwe(
+    modswitched: &LazyStandardModulusSwitchedLweCiphertext<u64, usize, Vec<u64>>,
+) -> LweCiphertextOwned<usize> {
+    let cont = modswitched
+        .mask()
+        .chain(std::iter::once(modswitched.body()))
+        // The coefficients are converted to use the power of two encoding
+        .map(|coeff| coeff << (usize::BITS as usize - modswitched.log_modulus().0))
+        .collect();
+
+    LweCiphertext::from_container(
+        cont,
+        CiphertextModulus::new(1 << modswitched.log_modulus().0),
+    )
+}
+
+fn store_data<Data: Serialize, P: AsRef<Path>>(path: P, data: &Data, name: &str) {
+    let mut path = path.as_ref().to_path_buf();
+    path.push(format!("{}.cbor", name));
+
+    let mut file = File::create(path).unwrap();
+    ciborium::ser::into_writer(data, &mut file).unwrap();
+}
+
+#[allow(clippy::too_many_arguments)]
+fn generate_test_vectors<P: AsRef<Path>>(
+    path: P,
+    lwe_dimension: LweDimension,
+    glwe_dimension: GlweDimension,
+    polynomial_size: PolynomialSize,
+    lwe_noise_stddev: f64,
+    glwe_noise_stddev: f64,
+    pbs_decomp_base_log: DecompositionBaseLog,
+    pbs_decomp_level_count: DecompositionLevelCount,
+    ks_decomp_base_log: DecompositionBaseLog,
+    ks_decomp_level_count: DecompositionLevelCount,
+    encoding: Encoding,
+) {
+    let path = path.as_ref();
+    create_dir_all(path).unwrap();
+
+    let mut secret_generator =
+        SecretRandomGenerator::<SoftwareRandomGenerator>::new(Seed(RAND_SEED));
+    let mut encryption_generator = EncryptionRandomGenerator::<SoftwareRandomGenerator>::new(
+        Seed(RAND_SEED),
+        &mut DeterministicSeeder::<SoftwareRandomGenerator>::new(Seed(RAND_SEED)),
+    );
+
+    let glwe_secret_key: GlweSecretKey<Vec<u64>> =
+        GlweSecretKey::generate_new_binary(glwe_dimension, polynomial_size, &mut secret_generator);
+    let large_lwe_secret_key = glwe_secret_key.as_lwe_secret_key();
+    store_data(path, &large_lwe_secret_key, "large_lwe_secret_key");
+
+    let small_lwe_secret_key: LweSecretKey<Vec<u64>> =
+        LweSecretKey::generate_new_binary(lwe_dimension, &mut secret_generator);
+    store_data(path, &small_lwe_secret_key, "small_lwe_secret_key");
+
+    let lwe_noise_distribution = Gaussian::from_standard_dev(StandardDev(lwe_noise_stddev), 0.);
+    let glwe_noise_distribution = Gaussian::from_standard_dev(StandardDev(glwe_noise_stddev), 0.);
+
+    let plaintext_a = encoding.encode(MSG_A);
+    let lwe_a = allocate_and_encrypt_new_lwe_ciphertext(
+        &large_lwe_secret_key,
+        plaintext_a,
+        glwe_noise_distribution,
+        encoding.ciphertext_modulus,
+        &mut encryption_generator,
+    );
+    store_data(path, &lwe_a, "lwe_a");
+
+    let plaintext_b = encoding.encode(MSG_B);
+    let lwe_b = allocate_and_encrypt_new_lwe_ciphertext(
+        &large_lwe_secret_key,
+        plaintext_b,
+        glwe_noise_distribution,
+        encoding.ciphertext_modulus,
+        &mut encryption_generator,
+    );
+    store_data(path, &lwe_b, "lwe_b");
+
+    let mut lwe_sum = LweCiphertext::new(
+        0u64,
+        glwe_dimension
+            .to_equivalent_lwe_dimension(polynomial_size)
+            .to_lwe_size(),
+        encoding.ciphertext_modulus,
+    );
+    lwe_ciphertext_add(&mut lwe_sum, &lwe_a, &lwe_b);
+    let decrypted_sum = decrypt_lwe_ciphertext(&large_lwe_secret_key, &lwe_sum);
+    let res = encoding.decode(decrypted_sum);
+
+    assert_eq!(res, MSG_A + MSG_B);
+    store_data(path, &lwe_sum, "lwe_sum");
+
+    let mut lwe_prod = LweCiphertext::new(
+        0u64,
+        glwe_dimension
+            .to_equivalent_lwe_dimension(polynomial_size)
+            .to_lwe_size(),
+        encoding.ciphertext_modulus,
+    );
+    lwe_ciphertext_cleartext_mul(&mut lwe_prod, &lwe_a, Cleartext(MSG_B));
+    let decrypted_prod = decrypt_lwe_ciphertext(&large_lwe_secret_key, &lwe_prod);
+    let res = encoding.decode(decrypted_prod);
+
+    assert_eq!(res, MSG_A * MSG_B);
+    store_data(path, &lwe_prod, "lwe_prod");
+
+    let ksk = allocate_and_generate_new_lwe_keyswitch_key(
+        &large_lwe_secret_key,
+        &small_lwe_secret_key,
+        ks_decomp_base_log,
+        ks_decomp_level_count,
+        lwe_noise_distribution,
+        encoding.ciphertext_modulus,
+        &mut encryption_generator,
+    );
+    store_data(path, &ksk, "ksk");
+
+    let mut lwe_ks = LweCiphertext::new(
+        0u64,
+        lwe_dimension.to_lwe_size(),
+        encoding.ciphertext_modulus,
+    );
+    keyswitch_lwe_ciphertext(&ksk, &lwe_a, &mut lwe_ks);
+
+    let decrypted_ks = decrypt_lwe_ciphertext(&small_lwe_secret_key, &lwe_ks);
+    let res = encoding.decode(decrypted_ks);
+
+    assert_eq!(res, MSG_A);
+    store_data(path, &lwe_ks, "lwe_ks");
+
+    let bsk = par_allocate_and_generate_new_lwe_bootstrap_key(
+        &small_lwe_secret_key,
+        &glwe_secret_key,
+        pbs_decomp_base_log,
+        pbs_decomp_level_count,
+        glwe_noise_distribution,
+        encoding.ciphertext_modulus,
+        &mut encryption_generator,
+    );
+    store_data(path, &bsk, "bsk");
+
+    let mut fourier_bsk = FourierLweBootstrapKey::new(
+        bsk.input_lwe_dimension(),
+        bsk.glwe_size(),
+        bsk.polynomial_size(),
+        bsk.decomposition_base_log(),
+        bsk.decomposition_level_count(),
+    );
+    par_convert_standard_lwe_bootstrap_key_to_fourier(&bsk, &mut fourier_bsk);
+
+    let lwe_in_ms =
+        LweCiphertext::from_container(lwe_ks.as_ref().to_vec(), lwe_ks.ciphertext_modulus());
+    let log_modulus = polynomial_size.to_blind_rotation_input_modulus_log();
+
+    let modswitched = lwe_ciphertext_modulus_switch(lwe_in_ms, log_modulus);
+    let lwe_ms = modswitched_to_lwe(&modswitched);
+    store_data(path, &lwe_ms, "lwe_ms");
+
+    let mut id_lut = encoding.encode_lut(glwe_dimension, polynomial_size, ID_LUT);
+
+    blind_rotate_assign(&modswitched, &mut id_lut, &fourier_bsk);
+    store_data(path, &id_lut, "glwe_after_id_br");
+
+    let mut lwe_pbs_id = LweCiphertext::new(
+        0u64,
+        glwe_dimension
+            .to_equivalent_lwe_dimension(polynomial_size)
+            .to_lwe_size(),
+        encoding.ciphertext_modulus,
+    );
+
+    extract_lwe_sample_from_glwe_ciphertext(&id_lut, &mut lwe_pbs_id, MonomialDegree(0));
+
+    let decrypted_pbs_id = decrypt_lwe_ciphertext(&large_lwe_secret_key, &lwe_pbs_id);
+    let res = encoding.decode(decrypted_pbs_id);
+
+    assert_eq!(res, MSG_A);
+    store_data(path, &lwe_pbs_id, "lwe_after_id_pbs");
+
+    let mut spec_lut = encoding.encode_lut(glwe_dimension, polynomial_size, SPEC_LUT);
+
+    blind_rotate_assign(&modswitched, &mut spec_lut, &fourier_bsk);
+    store_data(path, &spec_lut, "glwe_after_spec_br");
+
+    let mut lwe_pbs_spec = LweCiphertext::new(
+        0u64,
+        glwe_dimension
+            .to_equivalent_lwe_dimension(polynomial_size)
+            .to_lwe_size(),
+        encoding.ciphertext_modulus,
+    );
+
+    extract_lwe_sample_from_glwe_ciphertext(&spec_lut, &mut lwe_pbs_spec, MonomialDegree(0));
+
+    let decrypted_pbs_spec = decrypt_lwe_ciphertext(&large_lwe_secret_key, &lwe_pbs_spec);
+    let res = encoding.decode(decrypted_pbs_spec);
+
+    assert_eq!(res, SPEC_LUT(MSG_A));
+    store_data(path, &lwe_pbs_spec, "lwe_after_spec_pbs");
+}
+
+fn main() {
+    // backup the README.md file
+    let mut readme_path = PathBuf::new();
+    readme_path.push(DATA_DIR);
+    readme_path.push("README.md");
+    let readme = std::fs::read_to_string(&readme_path).unwrap();
+
+    remove_dir_all(DATA_DIR).unwrap();
+
+    // generate the vectors
+    let res = panic::catch_unwind(|| {
+        generate_test_vectors(
+            PathBuf::from(DATA_DIR).join("valid_params_128"),
+            VALID_LWE_DIMENSION,
+            VALID_GLWE_DIMENSION,
+            VALID_POLYNOMIAL_SIZE,
+            VALID_GAUSSIAN_LWE_NOISE_STDDEV,
+            VALID_GAUSSIAN_GLWE_NOISE_STDDEV,
+            VALID_PBS_DECOMPOSITION_BASE_LOG,
+            VALID_PBS_DECOMPOSITION_LEVEL_COUNT,
+            VALID_KS_DECOMPOSITION_BASE_LOG,
+            VALID_KS_DECOMPOSITION_LEVEL_COUNT,
+            ENCODING,
+        );
+
+        generate_test_vectors(
+            PathBuf::from(DATA_DIR).join("toy_params"),
+            TOY_LWE_DIMENSION,
+            TOY_GLWE_DIMENSION,
+            TOY_POLYNOMIAL_SIZE,
+            TOY_GAUSSIAN_LWE_NOISE_STDDEV,
+            TOY_GAUSSIAN_GLWE_NOISE_STDDEV,
+            TOY_PBS_DECOMPOSITION_BASE_LOG,
+            TOY_PBS_DECOMPOSITION_LEVEL_COUNT,
+            TOY_KS_DECOMPOSITION_BASE_LOG,
+            TOY_KS_DECOMPOSITION_LEVEL_COUNT,
+            ENCODING,
+        );
+    });
+
+    std::fs::write(&readme_path, readme).unwrap();
+
+    if let Err(err) = res {
+        panic::resume_unwind(err);
+    }
+}
