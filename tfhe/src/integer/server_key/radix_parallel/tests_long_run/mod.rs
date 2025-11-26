ee80210ad00635a6d9f6348297b706ed7e07496c
Agnes Leroy <agnes.leroy@zama.ai>
2025-11-26 11:05:37 +0100

    chore(gpu): reduce long run tests number of inputs to avoid timeout on 4090

--- tfhe/src/integer/server_key/radix_parallel/tests_long_run/mod.rs
+++ tfhe/src/integer/server_key/radix_parallel/tests_long_run/mod.rs
@@ -16,7 +16,7 @@ pub(crate) mod test_signed_erc20;
 pub(crate) mod test_signed_random_op_sequence;
 
 pub(crate) const NB_CTXT_LONG_RUN: usize = 32;
-pub(crate) const NB_TESTS_LONG_RUN: usize = 20000;
+pub(crate) const NB_TESTS_LONG_RUN: usize = 8000;
 pub(crate) const NB_TESTS_LONG_RUN_MINIMAL: usize = 200;
 
 pub(crate) fn get_user_defined_seed() -> Option<Seed> {
