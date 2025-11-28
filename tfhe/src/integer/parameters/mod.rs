e8531b74f98ee40e78371d5c8b395d3b71469d62
Nicolas Sarlin <nicolas.sarlin@zama.ai>
2025-11-28 17:34:32 +0100

    chore: detect degree in compact list conformance

--- tfhe/src/integer/parameters/mod.rs
+++ tfhe/src/integer/parameters/mod.rs
@@ -3,7 +3,8 @@ use crate::conformance::ListSizeConstraint;
 use crate::integer::key_switching_key::KeySwitchingKeyView;
 use crate::integer::server_key::ServerKey;
 use crate::shortint::parameters::{
-    CarryModulus, CiphertextConformanceParams, EncryptionKeyChoice, MessageModulus,
+    CarryModulus, CiphertextConformanceParams, CompactCiphertextListExpansionKind,
+    CompactPublicKeyEncryptionParameters, EncryptionKeyChoice, MessageModulus,
 };
 pub use crate::shortint::parameters::{
     DecompositionBaseLog, DecompositionLevelCount, DynamicDistribution, GlweDimension,
@@ -171,16 +172,6 @@ pub struct RadixCiphertextConformanceParams {
 /// Can be used on a server to check if client inputs are well formed
 /// before running a computation on them
 impl RadixCiphertextConformanceParams {
-    pub fn to_ct_list_conformance_parameters(
-        &self,
-        list_constraint: ListSizeConstraint,
-    ) -> CompactCiphertextListConformanceParams {
-        CompactCiphertextListConformanceParams {
-            shortint_params: self.shortint_params,
-            num_elements_constraint: list_constraint,
-        }
-    }
-
     pub fn from_pbs_parameters<P: Into<PBSParameters>>(
         params: P,
         num_blocks_per_integer: usize,
@@ -198,6 +189,38 @@ impl RadixCiphertextConformanceParams {
 /// before running a computation on them
 #[derive(Copy, Clone)]
 pub struct CompactCiphertextListConformanceParams {
-    pub shortint_params: CiphertextConformanceParams,
+    pub encryption_lwe_dimension: LweDimension,
+    pub message_modulus: MessageModulus,
+    pub carry_modulus: CarryModulus,
+    pub ciphertext_modulus: CiphertextModulus,
+    pub expansion_kind: CompactCiphertextListExpansionKind,
     pub num_elements_constraint: ListSizeConstraint,
+    pub allow_unpacked: bool,
+}
+
+impl CompactCiphertextListConformanceParams {
+    pub fn from_parameters_and_size_constraint(
+        value: CompactPublicKeyEncryptionParameters,
+        num_elements_constraint: ListSizeConstraint,
+    ) -> Self {
+        Self {
+            encryption_lwe_dimension: value.encryption_lwe_dimension,
+            message_modulus: value.message_modulus,
+            carry_modulus: value.carry_modulus,
+            ciphertext_modulus: value.ciphertext_modulus,
+            expansion_kind: value.expansion_kind,
+            num_elements_constraint,
+            allow_unpacked: false,
+        }
+    }
+
+    /// Allow the list to be composed of unpacked ciphertexts.
+    ///
+    /// Note that this means that the ciphertexts won't be sanitized.
+    pub fn allow_unpacked(self) -> Self {
+        Self {
+            allow_unpacked: true,
+            ..self
+        }
+    }
 }
