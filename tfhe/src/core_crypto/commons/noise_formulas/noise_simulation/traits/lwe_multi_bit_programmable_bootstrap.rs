3623d62ad0d78aebab67ed853a60a458d3253420
Arthur Meyre <arthur.meyre@zama.ai>
2025-12-03 17:24:39 +0100

    chore: add multi bit noise primitives in core
    - add a fully fledged MultiBit PBS trait required for BR -> ... APs

--- tfhe/src/core_crypto/commons/noise_formulas/noise_simulation/traits/lwe_multi_bit_programmable_bootstrap.rs
+++ tfhe/src/core_crypto/commons/noise_formulas/noise_simulation/traits/lwe_multi_bit_programmable_bootstrap.rs
@@ -31,3 +31,37 @@ pub trait LweMultiBitFft128BlindRotate<Input, Output, Accumulator> {
         side_resources: &mut Self::SideResources,
     );
 }
+
+pub trait AllocateLweMultiBitBootstrapResult {
+    type Output;
+    type SideResources;
+
+    fn allocate_lwe_multi_bit_bootstrap_result(
+        &self,
+        side_resources: &mut Self::SideResources,
+    ) -> Self::Output;
+}
+
+impl<T: AllocateLweMultiBitBlindRotateResult> AllocateLweMultiBitBootstrapResult for T {
+    type Output = <T as AllocateLweMultiBitBlindRotateResult>::Output;
+    type SideResources = <T as AllocateLweMultiBitBlindRotateResult>::SideResources;
+
+    fn allocate_lwe_multi_bit_bootstrap_result(
+        &self,
+        side_resources: &mut Self::SideResources,
+    ) -> Self::Output {
+        self.allocate_lwe_multi_bit_blind_rotate_result(side_resources)
+    }
+}
+
+pub trait LweMultiBitFftBootstrap<Input, Output, Accumulator> {
+    type SideResources;
+
+    fn lwe_multi_bit_fft_bootstrap(
+        &self,
+        input: &Input,
+        output: &mut Output,
+        accumulator: &Accumulator,
+        side_resources: &mut Self::SideResources,
+    );
+}
