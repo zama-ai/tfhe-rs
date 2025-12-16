54e0c1788a3ed719ae1b42d8c4bf4a45218f81a2
Enzo Di Maria <enzo.dimaria@zama.ai>
2025-12-16 16:52:32 +0100

    chore(gpu): some tests on erc20

--- backends/tfhe-cuda-backend/cuda/src/utils/helper_multi_gpu.cu
+++ backends/tfhe-cuda-backend/cuda/src/utils/helper_multi_gpu.cu
@@ -5,7 +5,7 @@
 
 std::mutex m;
 bool p2p_enabled = false;
-const int THRESHOLD_MULTI_GPU = 12;
+const int THRESHOLD_MULTI_GPU = 68;
 
 // Enable bidirectional p2p access between all available GPUs and device_0_id
 int32_t cuda_setup_multi_gpu(int device_0_id) {
