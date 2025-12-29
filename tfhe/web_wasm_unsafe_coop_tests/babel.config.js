d3abc783b311713db2e71f02e7701db74702e9c8
Nicolas Sarlin <nicolas.sarlin@zama.ai>
2025-12-29 15:00:24 +0100

    chore(js): add benchmark for unsafe coop context

---
+++ tfhe/web_wasm_unsafe_coop_tests/babel.config.js
@@ -0,0 +1,3 @@
+const presets = [["@babel/preset-env"]];
+
+module.exports = { presets };
---
+++ tfhe/web_wasm_unsafe_coop_tests/index.js
@@ -0,0 +1,65 @@
+import * as Comlink from "comlink";
+
+function setButtonsDisabledState(buttonIds, state) {
+  for (let id of buttonIds) {
+    let btn = document.getElementById(id);
+    if (btn) {
+      btn.disabled = state;
+    }
+  }
+}
+
+async function setup() {
+  const worker = new Worker(new URL("worker.js", import.meta.url), {
+    type: "module",
+  });
+  const demos = await Comlink.wrap(worker).demos;
+
+  const demoNames = [
+    "compactPublicKeyZeroKnowledgeBench",
+  ];
+
+  function setupBtn(id) {
+    // Handlers are named in the same way as buttons.
+    let fn = demos[id];
+
+    let button = document.getElementById(id);
+    if (button === null) {
+      console.error(`button with id: ${id} not found`);
+      return null;
+    }
+
+    // Assign onclick handler + enable the button.
+    Object.assign(button, {
+      onclick: async () => {
+        document.getElementById("loader").hidden = false;
+        document.getElementById("testSuccess").checked = false;
+        setButtonsDisabledState(demoNames, true);
+
+        console.log(`Running: ${id}`);
+        try {
+          let results = await fn();
+          document.getElementById("testSuccess").checked = true;
+          if (results !== undefined) {
+            document.getElementById("benchmarkResults").value =
+              JSON.stringify(results);
+          }
+        } catch (error) {
+          console.error(`Test Failed: ${error}`);
+          document.getElementById("testSuccess").checked = false;
+        }
+        document.getElementById("loader").hidden = true;
+        setButtonsDisabledState(demoNames, false);
+      },
+      disabled: false,
+    });
+
+    return button;
+  }
+
+  for (let demo of demoNames) {
+    setupBtn(demo);
+  }
+}
+
+setup();
