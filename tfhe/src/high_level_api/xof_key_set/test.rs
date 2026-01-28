bace845aec3b6c100818227cfc02882ebdbe318f
Thomas Montaigu <thomas.montaigu@zama.ai>
2026-01-28 11:28:16 +0100

    feat(hlapi): add decompress_to_gpu for CompressedXofKeySet
    Done using the newly added expand + convert pattern

--- tfhe/src/high_level_api/xof_key_set/test.rs
+++ tfhe/src/high_level_api/xof_key_set/test.rs
@@ -1,132 +1,124 @@
 use super::*;
 use crate::core_crypto::prelude::new_seeder;
 use crate::prelude::*;
+use crate::shortint::parameters::test_params::*;
 use crate::xof_key_set::{CompressedXofKeySet, XofKeySet};
 use crate::*;
 
-#[test]
-fn test_xof_key_set_classic_params() {
-    let params =
-        shortint::parameters::test_params::TEST_PARAM_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M128;
-    let cpk_params =
-        shortint::parameters::test_params::TEST_PARAM_PKE_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M128;
-    let casting_params = shortint::parameters::test_params::TEST_PARAM_KEYSWITCH_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M128;
-    let noise_squashing_params = shortint::parameters::test_params::TEST_PARAM_NOISE_SQUASHING_PARAM_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M128;
-    let compression_params =
-        shortint::parameters::test_params::TEST_COMP_PARAM_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M128;
-    let re_rand_ksk_params = shortint::parameters::test_params::TEST_PARAM_KEYSWITCH_PKE_TO_BIG_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M128;
-    let noise_squashing_compression_params = shortint::parameters::test_params::TEST_PARAM_NOISE_SQUASHING_COMP_PARAM_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M128;
+mod cpu {
+    use super::*;
+    #[test]
+    fn test_xof_key_set_classic_params() {
+        let config = Config::from(TEST_META_PARAM_CPU_2_2_KS_PBS_PKE_TO_SMALL_ZKV2_TUNIFORM_2M128);
 
-    let config = ConfigBuilder::with_custom_parameters(params)
-        .use_dedicated_compact_public_key_parameters((cpk_params, casting_params))
-        .enable_noise_squashing(noise_squashing_params)
-        .enable_noise_squashing_compression(noise_squashing_compression_params)
-        .enable_compression(compression_params)
-        .enable_ciphertext_re_randomization(re_rand_ksk_params)
-        .build();
+        let mut seeder = new_seeder();
+        let private_seed_bytes = seeder.seed().0.to_le_bytes().to_vec();
+        let security_bits = 128;
+        let max_norm_hwt = NormalizedHammingWeightBound::new(0.8).unwrap();
+        let tag = Tag::from("classic_2_2");
 
-    let mut seeder = new_seeder();
-    let private_seed_bytes = seeder.seed().0.to_le_bytes().to_vec();
-    let security_bits = 128;
-    let max_norm_hwt = NormalizedHammingWeightBound::new(0.8).unwrap();
-    let tag = Tag::from("classic_2_2");
+        let (cks, compressed_key_set) = CompressedXofKeySet::generate(
+            config,
+            private_seed_bytes,
+            security_bits,
+            max_norm_hwt,
+            tag.clone(),
+        )
+        .unwrap();
 
-    let (cks, compressed_key_set) = CompressedXofKeySet::generate(
-        config,
-        private_seed_bytes,
-        security_bits,
-        max_norm_hwt,
-        tag.clone(),
-    )
-    .unwrap();
+        assert_eq!(cks.tag(), compressed_key_set.compressed_public_key.tag());
+        assert_eq!(cks.tag(), &tag);
+        test_xof_key_set(&compressed_key_set, config, Device::Cpu, &cks);
+    }
 
-    assert_eq!(cks.tag(), compressed_key_set.compressed_public_key.tag());
-    assert_eq!(cks.tag(), &tag);
-    test_xof_key_set(&compressed_key_set, config, &cks);
+    #[test]
+    fn test_xof_key_set_ks32_params_big_pke() {
+        let config = Config::from(TEST_META_PARAM_CPU_2_2_KS32_PBS_PKE_TO_BIG_ZKV2_TUNIFORM_2M128);
+
+        let mut seeder = new_seeder();
+        let private_seed_bytes = seeder.seed().0.to_le_bytes().to_vec();
+        let security_bits = 128;
+        let max_norm_hwt = NormalizedHammingWeightBound::new(0.8).unwrap();
+        let tag = Tag::from("ks32 big pke");
+
+        let (cks, compressed_key_set) = CompressedXofKeySet::generate(
+            config,
+            private_seed_bytes,
+            security_bits,
+            max_norm_hwt,
+            tag.clone(),
+        )
+        .unwrap();
+
+        assert_eq!(cks.tag(), compressed_key_set.compressed_public_key.tag());
+        assert_eq!(cks.tag(), &tag);
+        test_xof_key_set(&compressed_key_set, config, Device::Cpu, &cks);
+    }
+
+    #[test]
+    fn test_xof_key_set_ks32_params_small_pke() {
+        let config =
+            Config::from(TEST_META_PARAM_CPU_2_2_KS32_PBS_PKE_TO_SMALL_ZKV2_TUNIFORM_2M128);
+
+        let mut seeder = new_seeder();
+        let private_seed_bytes = seeder.seed().0.to_le_bytes().to_vec();
+        let security_bits = 128;
+        let max_norm_hwt = NormalizedHammingWeightBound::new(0.8).unwrap();
+        let tag = Tag::from("ks32 small pke");
+
+        let (cks, compressed_key_set) = CompressedXofKeySet::generate(
+            config,
+            private_seed_bytes,
+            security_bits,
+            max_norm_hwt,
+            tag.clone(),
+        )
+        .unwrap();
+
+        assert_eq!(cks.tag(), compressed_key_set.compressed_public_key.tag());
+        assert_eq!(cks.tag(), &tag);
+        test_xof_key_set(&compressed_key_set, config, Device::Cpu, &cks);
+    }
 }
 
-#[test]
-fn test_xof_key_set_ks32_params_big_pke() {
-    let params =
-        shortint::parameters::test_params::TEST_PARAM_MESSAGE_2_CARRY_2_KS32_PBS_TUNIFORM_2M128;
-    let cpk_params = shortint::parameters::test_params::TEST_PARAM_PKE_TO_BIG_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M128_ZKV2;
-    let casting_params = shortint::parameters::test_params::TEST_PARAM_KEYSWITCH_PKE_TO_BIG_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M128;
-    let noise_squashing_params = shortint::parameters::test_params::TEST_PARAM_NOISE_SQUASHING_PARAM_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M128;
-    let compression_params =
-        shortint::parameters::test_params::TEST_COMP_PARAM_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M128;
-    let re_rand_ksk_params = shortint::parameters::test_params::TEST_PARAM_KEYSWITCH_PKE_TO_BIG_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M128;
-    let noise_squashing_compression_params = shortint::parameters::test_params::TEST_PARAM_NOISE_SQUASHING_COMP_PARAM_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M128;
+#[cfg(feature = "gpu")]
+mod gpu {
+    use super::*;
 
-    let config = ConfigBuilder::with_custom_parameters(params)
-        .use_dedicated_compact_public_key_parameters((cpk_params, casting_params))
-        .enable_noise_squashing(noise_squashing_params)
-        .enable_noise_squashing_compression(noise_squashing_compression_params)
-        .enable_compression(compression_params)
-        .enable_ciphertext_re_randomization(re_rand_ksk_params)
-        .build();
+    #[test]
+    fn test_xof_key_set_multibit_group_4() {
+        let config = Config::from(
+            TEST_META_PARAM_GPU_2_2_MULTI_BIT_GROUP_4_KS_PBS_PKE_TO_SMALL_ZKV2_TUNIFORM_2M128,
+        );
 
-    let mut seeder = new_seeder();
-    let private_seed_bytes = seeder.seed().0.to_le_bytes().to_vec();
-    let security_bits = 128;
-    let max_norm_hwt = NormalizedHammingWeightBound::new(0.8).unwrap();
-    let tag = Tag::from("ks32 big pke");
+        let mut seeder = new_seeder();
+        let private_seed_bytes = seeder.seed().0.to_le_bytes().to_vec();
+        let security_bits = 128;
+        let max_norm_hwt = NormalizedHammingWeightBound::new(0.8).unwrap();
+        let tag = Tag::from("gpu multibit group 4");
 
-    let (cks, compressed_key_set) = CompressedXofKeySet::generate(
-        config,
-        private_seed_bytes,
-        security_bits,
-        max_norm_hwt,
-        tag.clone(),
-    )
-    .unwrap();
+        let (cks, compressed_key_set) = CompressedXofKeySet::generate(
+            config,
+            private_seed_bytes,
+            security_bits,
+            max_norm_hwt,
+            tag.clone(),
+        )
+        .unwrap();
 
-    assert_eq!(cks.tag(), compressed_key_set.compressed_public_key.tag());
-    assert_eq!(cks.tag(), &tag);
-    test_xof_key_set(&compressed_key_set, config, &cks);
+        assert_eq!(cks.tag(), compressed_key_set.compressed_public_key.tag());
+        assert_eq!(cks.tag(), &tag);
+        test_xof_key_set(&compressed_key_set, config, Device::CudaGpu, &cks);
+    }
 }
 
-#[test]
-fn test_xof_key_set_ks32_params_small_pke() {
-    let params =
-        shortint::parameters::test_params::TEST_PARAM_MESSAGE_2_CARRY_2_KS32_PBS_TUNIFORM_2M128;
-    let cpk_params = shortint::parameters::test_params::TEST_PARAM_PKE_TO_SMALL_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M128_ZKV2;
-    let casting_params = shortint::parameters::test_params::TEST_PARAM_KEYSWITCH_PKE_TO_SMALL_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M128;
-    let noise_squashing_params = shortint::parameters::test_params::TEST_PARAM_NOISE_SQUASHING_PARAM_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M128;
-    let compression_params =
-        shortint::parameters::test_params::TEST_COMP_PARAM_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M128;
-    let re_rand_ksk_params = shortint::parameters::test_params::TEST_PARAM_KEYSWITCH_PKE_TO_BIG_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M128;
-    let noise_squashing_compression_params = shortint::parameters::test_params::TEST_PARAM_NOISE_SQUASHING_COMP_PARAM_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M128;
-
-    let config = ConfigBuilder::with_custom_parameters(params)
-        .use_dedicated_compact_public_key_parameters((cpk_params, casting_params))
-        .enable_noise_squashing(noise_squashing_params)
-        .enable_noise_squashing_compression(noise_squashing_compression_params)
-        .enable_compression(compression_params)
-        .enable_ciphertext_re_randomization(re_rand_ksk_params)
-        .build();
-
-    let mut seeder = new_seeder();
-    let private_seed_bytes = seeder.seed().0.to_le_bytes().to_vec();
-    let security_bits = 128;
-    let max_norm_hwt = NormalizedHammingWeightBound::new(0.8).unwrap();
-    let tag = Tag::from("ks32 small pke");
-
-    let (cks, compressed_key_set) = CompressedXofKeySet::generate(
-        config,
-        private_seed_bytes,
-        security_bits,
-        max_norm_hwt,
-        tag.clone(),
-    )
-    .unwrap();
-
-    assert_eq!(cks.tag(), compressed_key_set.compressed_public_key.tag());
-    assert_eq!(cks.tag(), &tag);
-    test_xof_key_set(&compressed_key_set, config, &cks);
-}
-
-fn test_xof_key_set(compressed_key_set: &CompressedXofKeySet, config: Config, cks: &ClientKey) {
-    let compressed_size_limit = 1 << 30;
+fn test_xof_key_set(
+    compressed_key_set: &CompressedXofKeySet,
+    config: Config,
+    device: Device,
+    cks: &ClientKey,
+) {
+    let compressed_size_limit = 1 << 32; // 4GB
     let mut data = vec![];
     crate::safe_serialization::safe_serialize(compressed_key_set, &mut data, compressed_size_limit)
         .unwrap();
@@ -136,21 +128,36 @@ fn test_xof_key_set(compressed_key_set: &CompressedXofKeySet, config: Config, ck
 
     let expected_pk_tag = compressed_key_set.compressed_public_key.tag().clone();
     let expected_sk_tag = compressed_key_set.compressed_server_key.tag().clone();
-    let key_set = compressed_key_set.decompress().unwrap();
 
-    let size_limit = 1 << 32;
-    let mut data = vec![];
-    crate::safe_serialization::safe_serialize(&key_set, &mut data, size_limit).unwrap();
-    let key_set: XofKeySet =
-        crate::safe_serialization::safe_deserialize(data.as_slice(), size_limit).unwrap();
+    let pk = match device {
+        Device::Cpu => {
+            let key_set = compressed_key_set.decompress().unwrap();
+            let size_limit = 1 << 32; // 4GB
+            let mut data = vec![];
+            crate::safe_serialization::safe_serialize(&key_set, &mut data, size_limit).unwrap();
+            let key_set: XofKeySet =
+                crate::safe_serialization::safe_deserialize(data.as_slice(), size_limit).unwrap();
 
-    let (pk, sk) = key_set.into_raw_parts();
+            let (pk, sk) = key_set.into_raw_parts();
+            assert_eq!(sk.tag(), &expected_sk_tag);
+            assert!(sk.is_conformant(&config.into()));
+            set_server_key(sk);
+            pk
+        }
+        #[cfg(feature = "gpu")]
+        Device::CudaGpu => {
+            let key_set = compressed_key_set.decompress_to_gpu().unwrap();
+            let (pk, sk) = key_set.into_raw_parts();
+            assert_eq!(sk.tag(), &expected_sk_tag);
+            set_server_key(sk);
+            pk
+        }
+        #[cfg(feature = "hpu")]
+        Device::Hpu => {
+            panic!("HPU not supported in this test")
+        }
+    };
     assert_eq!(pk.tag(), &expected_pk_tag);
-    assert_eq!(sk.tag(), &expected_sk_tag);
-
-    assert!(sk.is_conformant(&config.into()));
-
-    set_server_key(sk);
 
     let clear_a = rand::random::<u32>();
     let clear_b = rand::random::<u32>();
@@ -172,13 +179,13 @@ fn test_xof_key_set(compressed_key_set: &CompressedXofKeySet, config: Config, ck
     let list = CompactCiphertextList::builder(&pk)
         .push(clear_a)
         .push(clear_b)
-        .build();
+        .build_packed();
     let expander = list.expand().unwrap();
     let mut a = expander.get::<FheUint32>(0).unwrap().unwrap();
     let mut b = expander.get::<FheUint32>(1).unwrap().unwrap();
 
     // Test re-randomization
-    {
+    if config.inner.cpk_re_randomization_ksk_params.is_some() {
         // Simulate a 256 bits nonce
         let nonce: [u8; 256 / 8] = core::array::from_fn(|_| rand::random());
         let compact_public_encryption_domain_separator = *b"TFHE_Enc";
