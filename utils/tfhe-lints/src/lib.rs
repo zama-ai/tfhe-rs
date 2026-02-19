3814368a169a26570ef5c6f8b34c68133e2482de
Theo Souchon <theo.souchon@zama.ai>
2026-02-19 18:08:26 +0100

    chore(lint): add new rules around versionize_dispatch_enum

--- utils/tfhe-lints/src/lib.rs
+++ utils/tfhe-lints/src/lib.rs
@@ -2,6 +2,7 @@
 #![warn(unused_extern_crates)]
 
 extern crate rustc_ast;
+extern crate rustc_errors;
 extern crate rustc_hir;
 extern crate rustc_lint;
 extern crate rustc_middle;
@@ -9,7 +10,7 @@ extern crate rustc_session;
 extern crate rustc_span;
 
 mod serialize_without_versionize;
-mod versions_variants_order;
+mod invalid_versionize_dispatch;
 
 mod utils;
 
@@ -20,12 +21,12 @@ dylint_linting::dylint_library!();
 pub fn register_lints(_sess: &rustc_session::Session, lint_store: &mut rustc_lint::LintStore) {
     lint_store.register_lints(&[
         serialize_without_versionize::SERIALIZE_WITHOUT_VERSIONIZE,
-        versions_variants_order::VERSIONS_VARIANTS_ORDER,
+        invalid_versionize_dispatch::INVALID_VERSIONIZE_DISPATCH,
     ]);
     lint_store.register_late_pass(|_| {
         Box::new(serialize_without_versionize::SerializeWithoutVersionize::default())
     });
     lint_store.register_late_pass(|_| {
-        Box::new(versions_variants_order::VersionsVariantsOrder::default())
+        Box::new(invalid_versionize_dispatch::InvalidVersionizeDispatch::default())
     });
 }
