25c9f46e2c8e7d9ce15f04750ffd6fe119c834e9
Thomas Montaigu <thomas.montaigu@zama.ai>
2026-03-17 09:33:44 +0100

    chore(xof): add backward_compatibility layer for csprng byte start
    Add an enum that allows to represent where the CSPRNG for the
    CompressedXofKeySet must start. This is to accommodate for the csprng bug
    that was fixed in tfhe-csprng 0.9 to still be able to load 1.5.4 (csprng
    0.8.1) data correctly
    
    Defaults to first byte, backward_compatibility handles old data that was
    versioned with it.

--- tfhe/src/high_level_api/backward_compatibility/xof_key_set.rs
+++ tfhe/src/high_level_api/backward_compatibility/xof_key_set.rs
@@ -1,13 +1,50 @@
-use tfhe_versionable::VersionsDispatch;
+use std::convert::Infallible;
+
+use tfhe_csprng::seeders::XofSeed;
+use tfhe_versionable::{Upgrade, Version, VersionsDispatch};
 
 use crate::high_level_api::xof_key_set::{CompressedXofKeySet, XofKeySet};
+use crate::xof_key_set::XofSeedStart;
+use crate::{CompressedCompactPublicKey, CompressedServerKey};
+
+#[derive(Version)]
+pub struct CompressedXofKeySetV0 {
+    seed: XofSeed,
+    compressed_public_key: CompressedCompactPublicKey,
+    compressed_server_key: CompressedServerKey,
+}
+
+impl Upgrade<CompressedXofKeySet> for CompressedXofKeySetV0 {
+    type Error = Infallible;
+
+    fn upgrade(self) -> Result<CompressedXofKeySet, Self::Error> {
+        let Self {
+            seed,
+            compressed_public_key,
+            compressed_server_key,
+        } = self;
+
+        Ok(CompressedXofKeySet::from_raw_parts(
+            // Start on second byte to keep backward compatibility with csprng bug
+            XofSeedStart::SecondByte(seed),
+            compressed_public_key,
+            compressed_server_key,
+        ))
+    }
+}
 
 #[derive(VersionsDispatch)]
 pub enum CompressedXofKeySetVersions {
-    V0(CompressedXofKeySet),
+    V0(CompressedXofKeySetV0),
+    V1(CompressedXofKeySet),
 }
 
 #[derive(VersionsDispatch)]
 pub enum XofKeySetVersions {
     V0(XofKeySet),
+}
+
+#[derive(VersionsDispatch)]
+pub enum XofSeedStartVersions {
+    V0(XofSeedStart),
 }
