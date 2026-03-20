9f164b937fbf8c173488b27327ce55fe03eb86f1
David Palm <dvdplm@gmail.com>
2026-03-20 10:16:47 +0100

    Move safe-serialization to own crate and wire it up with the workspace.

--- utils/tfhe-safe-serialize/src/traits.rs
+++ utils/tfhe-safe-serialize/src/traits.rs
@@ -1,7 +1,15 @@
+
 pub trait Named {
     /// Default name for the type
     const NAME: &'static str;
     /// Aliases that should also be accepted for backward compatibility when checking the name of
     /// values of this type
     const BACKWARD_COMPATIBILITY_ALIASES: &'static [&'static str] = &[];
+}
+
+/// A trait for objects which can be checked to be conformant with a parameter set
+pub trait ParameterSetConformant {
+    type ParameterSet;
+
+    fn is_conformant(&self, parameter_set: &Self::ParameterSet) -> bool;
 }
