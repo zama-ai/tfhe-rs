27cc58e960a40e9748bcc8cf153146779e3977f5
Theo Souchon <theo.souchon@zama.ai>
2026-04-28 16:43:29 +0200

    chore(wasm): create the compact package generation

---
+++ tfhe/src/js_on_wasm_api/js_high_level_api/client/mod.rs
@@ -0,0 +1,12 @@
+mod config;
+mod integers;
+mod keys;
+#[cfg(feature = "zk-pok")]
+mod zk;
+
+pub(crate) use config::*;
+#[cfg(feature = "integer-js-wasm-api")]
+pub(crate) use integers::*;
+pub(crate) use keys::*;
+#[cfg(feature = "zk-pok")]
+pub(crate) use zk::*;
