92eaa2122de8555471d4e1e55f7c6e69a76ba981
Theo Souchon <theo.souchon@zama.ai>
2026-04-28 16:43:29 +0200

    chore(wasm): create the compact package generation

--- tfhe/src/js_on_wasm_api/mod.rs
+++ tfhe/src/js_on_wasm_api/mod.rs
@@ -1,7 +1,7 @@
-#[cfg(feature = "shortint-client-js-wasm-api")]
+#[cfg(feature = "shortint-js-wasm-api")]
 mod shortint;
 
-#[cfg(feature = "boolean-client-js-wasm-api")]
+#[cfg(feature = "boolean-js-wasm-api")]
 mod boolean;
 
 // We need to use the init_thread_pool for it to be publicly visible but it appears unused when
@@ -15,4 +15,22 @@ mod js_high_level_api;
 
 pub(crate) fn into_js_error<E: std::fmt::Debug>(e: E) -> wasm_bindgen::JsError {
     wasm_bindgen::JsError::new(format!("{e:?}").as_str())
+}
+
+pub(crate) fn catch_panic_result<F, R>(closure: F) -> Result<R, wasm_bindgen::JsError>
+where
+    F: FnOnce() -> Result<R, wasm_bindgen::JsError>,
+{
+    std::panic::catch_unwind(std::panic::AssertUnwindSafe(closure))
+        .unwrap_or_else(|_| Err(wasm_bindgen::JsError::new("Operation Failed")))
+}
+
+pub(crate) fn catch_panic<F, R>(closure: F) -> Result<R, wasm_bindgen::JsError>
+where
+    F: FnOnce() -> R,
+{
+    std::panic::catch_unwind(std::panic::AssertUnwindSafe(closure)).map_or_else(
+        |_| Err(wasm_bindgen::JsError::new("Operation Failed")),
+        |ret| Ok(ret),
+    )
 }
