88ae4928c9b5f4ff75ae4453f4835fea78280784
Pedro Alves <pedro.alves@zama.org>
2026-04-29 09:29:52 -0300

    chore(gpu): auto-generate cuda_bind.rs via bindgen instead of hand-writing it

---
+++ backends/tfhe-cuda-common/src/ffi.rs
@@ -0,0 +1,11 @@
+#![allow(warnings)]
+pub type c_void = std::ffi::c_void;
+pub type c_uint = std::ffi::c_uint;
+pub type c_uchar = std::ffi::c_uchar;
+pub type c_ushort = std::ffi::c_ushort;
+pub type c_ulong = std::ffi::c_ulong;
+pub type c_schar = std::ffi::c_schar;
+pub type c_int = std::ffi::c_int;
+pub type c_short = std::ffi::c_short;
+pub type c_long = std::ffi::c_long;
+pub type c_char = std::ffi::c_char;
