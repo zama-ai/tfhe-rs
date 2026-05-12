2bf7ba9d95e591dd446ca54cc5bb7adf2515e962
Thomas Montaigu <thomas.montaigu@zama.ai>
2026-05-12 11:10:02 +0200

    chore: add nist_submission module

--- tfhe/src/high_level_api/mod.rs
+++ tfhe/src/high_level_api/mod.rs
@@ -158,6 +158,7 @@ mod errors;
 mod global_state;
 mod integers;
 pub(crate) mod keys;
+pub mod nist_submission;
 mod re_randomization;
 #[cfg(feature = "strings")]
 mod strings;
@@ -169,7 +170,7 @@ mod gpu_utils;
 
 pub mod array;
 pub mod backward_compatibility;
-mod compact_list;
+pub(crate) mod compact_list;
 mod kv_store;
 mod tag;
 
