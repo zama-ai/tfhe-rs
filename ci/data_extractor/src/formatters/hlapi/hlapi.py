b3945eb4462d13fb8c040b43a7a190fd5926eb55
David Testé <david.teste@zama.ai>
2026-05-18 12:22:25 +0200

    chore(ci): add formatter for kvstore benchmarks
    Also add automatic benchmlark run and SVG generation in documentation
    benchmarks workflow.

--- ci/data_extractor/src/formatters/hlapi/hlapi.py
+++ ci/data_extractor/src/formatters/hlapi/hlapi.py
@@ -1,6 +1,7 @@
 import collections
+from collections.abc import Callable
 
-from benchmark_specs import Backend, BenchDetails, BenchType
+from benchmark_specs import Backend, BenchDetails, BenchType, RustType
 from formatters.common import BenchArray, GenericFormatter
 
 import utils
@@ -87,3 +88,85 @@ class Erc7984Formatter(HlApiFormatter):
         return [
             BenchArray(result_lines, self.layer),
         ]
+
+
+OPERATIONS_DISPLAYS = [
+    "get",
+    "update",
+    "map",
+]
+
+DEFAULT_STORE_SIZE = (
+    2,
+    4,
+    8,
+    16,
+    32,
+    64,
+    128,
+    256,
+    512,
+    1024,
+)
+
+DEFAULT_KVSTORE_VALUE_SIZE = lambda: {
+    8: "N/A",
+    16: "N/A",
+    32: "N/A",
+    64: "N/A",
+    128: "N/A",
+}
+
+
+STORE_SIZE_COLUMN_HEADER = "Store size \\ Value"
+
+
+class KVStoreFormatter(HlApiFormatter):
+    """
+    Formatter for Key-Value store benchmarks.
+    """
+
+    @staticmethod
+    def _format_data(data: dict[BenchDetails : list[int]], conversion_func):
+        results_dict = {}
+
+        for op in OPERATIONS_DISPLAYS:
+            results_dict[op] = {
+                size: DEFAULT_KVSTORE_VALUE_SIZE() for size in DEFAULT_STORE_SIZE
+            }
+
+        for details, timings in data.items():
+            test_name = details.operation_name.lstrip("kv_store::")
+            # This is a hack to get the array size and the precision of the value.
+            # It would be fixed once the data extractor would be rewritten in Rust
+            # thanks to the benchmark specs.
+            case_variation = details.rust_type.split("_")
+            array_size = int(case_variation[-1])
+            value_size = int(case_variation[-3].lstrip("FheUint"))
+            value = conversion_func(timings[-1])
+            results_dict[test_name][array_size][value_size] = value
+
+        return results_dict
+
+    def _generate_arrays(self, data, *args, **kwargs):
+        first_column_header = STORE_SIZE_COLUMN_HEADER
+
+        arrays = []
+
+        for op_name in data.keys():
+            result_lines = []
+            for array_size, values in data[op_name].items():
+                line = {first_column_header: str(array_size)}
+                line.update(
+                    {
+                        RustType.from_int(bench_type).name: v
+                        for bench_type, v in values.items()
+                    }
+                )
+                result_lines.append(line)
+
+            bench_array = BenchArray(result_lines, self.layer)
+            bench_array.metadata = {"operation": op_name}
+            arrays.append(bench_array)
+
+        return arrays
