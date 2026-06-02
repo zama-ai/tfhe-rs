1df46b43724f6f03638378ae1f8eaaec5d0b547c
David Testé <david.teste@zama.ai>
2026-06-02 15:55:26 +0200

    chore(bench): add hlapi oprf benchmarks to the spec

--- tfhe-benchmark/benches/high_level_api/oprf.rs
+++ tfhe-benchmark/benches/high_level_api/oprf.rs
@@ -2,24 +2,41 @@ use benchmark::params_aliases::BENCH_PARAM_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M1
 #[cfg(feature = "gpu")]
 use benchmark::params_aliases::BENCH_PARAM_MULTI_BIT_GROUP_4_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M128;
 
+use benchmark::utilities::{write_to_json, OperatorType};
+use benchmark_spec::tfhe::hlapi::oprf::OprfKind;
+use benchmark_spec::{BenchmarkSpec, BenchmarkType, HlapiBench, OperandType};
 use criterion::{black_box, criterion_group, Criterion};
 use std::num::NonZeroU64;
+use tfhe::keycache::NamedParam;
 use tfhe::{set_server_key, ClientKey, ConfigBuilder, FheUint64, RangeForRandom, Seed, ServerKey};
 
-fn oprf_any_range_bench(c: &mut Criterion, bench_name: &str) {
-    let mut bench_group = c.benchmark_group(bench_name);
+fn oprf_any_range_bench(c: &mut Criterion, cks: &ClientKey) {
+    let mut bench_group = c.benchmark_group("oprf");
     bench_group
         .sample_size(15)
         .measurement_time(std::time::Duration::from_secs(30));
 
+    let hlapi_op = HlapiBench::Oprf(OprfKind::AnyRange);
+    let param = cks.computation_parameters();
+    let param_name = param.name();
+
     for excluded_upper_bound in [3, 52] {
+        let bound_type = format!("bound_{excluded_upper_bound}",);
+        let benchmark_spec = BenchmarkSpec::new_hlapi(
+            hlapi_op,
+            &param_name,
+            OperandType::CipherText,
+            Some(bound_type.as_str()),
+            BenchmarkType::Latency,
+            None,
+        );
+        let bench_id = benchmark_spec.to_string();
+
         let range = RangeForRandom::new_from_excluded_upper_bound(
             NonZeroU64::new(excluded_upper_bound).unwrap(),
         );
 
-        let bench_id_oprf = format!("{bench_name}::bound_{excluded_upper_bound}");
-
-        bench_group.bench_function(&bench_id_oprf, |b| {
+        bench_group.bench_function(&bench_id, |b| {
             b.iter(|| {
                 _ = black_box(FheUint64::generate_oblivious_pseudo_random_custom_range(
                     Seed(0),
@@ -28,6 +45,15 @@ fn oprf_any_range_bench(c: &mut Criterion, bench_name: &str) {
                 ));
             })
         });
+
+        write_to_json::<u64, _, _>(
+            &benchmark_spec,
+            param,
+            hlapi_op.to_string(),
+            &OperatorType::Atomic,
+            64,
+            vec![],
+        );
     }
 
     bench_group.finish()
@@ -43,7 +69,7 @@ pub fn oprf_any_range_cpu(c: &mut Criterion) {
     rayon::broadcast(|_| set_server_key(sks.clone()));
     set_server_key(sks);
 
-    oprf_any_range_bench(c, "hlapi::oprf_any_range_cpu");
+    oprf_any_range_bench(c, &cks);
 }
 
 #[cfg(feature = "gpu")]
@@ -56,7 +82,7 @@ pub fn oprf_any_range_gpu(c: &mut Criterion) {
 
     set_server_key(sks);
 
-    oprf_any_range_bench(c, "hlapi::oprf_any_range_gpu");
+    oprf_any_range_bench(c, &cks);
 }
 
 #[cfg(not(feature = "gpu"))]
