8c9cc9e3b756e8df468c204ec5044148305d74d9
Arthur Meyre <arthur.meyre@zama.ai>
2026-06-05 18:19:24 +0200

    feat(hl): plug shuffle with re-randomized keys in the HL API
    - updated FheUint and FheInt test to verify equivalency between both
    implems and for the various hash algos for rerand version

--- tfhe/src/high_level_api/integers/shuffle.rs
+++ tfhe/src/high_level_api/integers/shuffle.rs
@@ -1,7 +1,9 @@
 use crate::high_level_api::global_state;
 use crate::high_level_api::integers::FheIntegerType;
 use crate::high_level_api::keys::InternalServerKey;
-use crate::high_level_api::re_randomization::ReRandomizationMetadata;
+use crate::high_level_api::re_randomization::{
+    ReRandomizationHashAlgo, ReRandomizationMetadata, ReRandomizationMode,
+};
 use crate::integer::server_key::radix_parallel::bitonic_shuffle::BitonicShuffleKeySize;
 use crate::OprfSeed;
 
@@ -51,18 +53,74 @@ where
     })
 }
 
+pub fn re_randomized_keys_bitonic_shuffle<T, S>(
+    data: Vec<T>,
+    key_size: BitonicShuffleKeySize,
+    seed: S,
+    re_randomization_mode: ReRandomizationMode,
+    re_randomization_hash_algo: ReRandomizationHashAlgo,
+) -> Result<Vec<T>, crate::Error>
+where
+    T: FheIntegerType,
+    S: OprfSeed,
+{
+    global_state::with_internal_keys(|key| match key {
+        InternalServerKey::Cpu(cpu_key) => {
+            let re_randomization_key =
+                cpu_key.integer_re_randomization_key_from_mode(re_randomization_mode)?;
+            let inner = data.into_iter().map(|v| v.into_cpu()).collect();
+            let result = cpu_key.pbs_key().re_randomized_keys_bitonic_shuffle(
+                &cpu_key.oprf_key(),
+                inner,
+                key_size,
+                seed,
+                &re_randomization_key,
+                re_randomization_hash_algo,
+            )?;
+            Ok(result
+                .into_iter()
+                .map(|ct| T::from_cpu(ct, cpu_key.tag.clone(), ReRandomizationMetadata::default()))
+                .collect())
+        }
+        #[cfg(feature = "gpu")]
+        InternalServerKey::Cuda(_) => Err(crate::Error::new(
+            "bitonic_shuffle is not supported on Cuda".to_string(),
+        )),
+        #[cfg(feature = "hpu")]
+        InternalServerKey::Hpu(_) => Err(crate::Error::new(
+            "bitonic_shuffle is not supported on Hpu".to_string(),
+        )),
+    })
+}
+
 #[cfg(test)]
 mod test {
-    use super::{bitonic_shuffle, BitonicShuffleKeySize};
+    use super::*;
     use crate::core_crypto::prelude::new_seeder;
     use crate::high_level_api::prelude::*;
     use crate::high_level_api::tests::setup_default_cpu;
+    use crate::high_level_api::{set_server_key, ClientKey, ConfigBuilder, ServerKey};
+    use crate::shortint::parameters::ReRandomizationParameters;
     use crate::{FheInt8, FheUint8};
     use rand::Rng;
 
     #[test]
     fn test_bitonic_shuffle_fheuint() {
-        let cks = setup_default_cpu();
+        let cks = {
+            let config = ConfigBuilder::default()
+                .use_dedicated_oprf_key(true)
+                .enable_ciphertext_re_randomization(
+                    ReRandomizationParameters::DerivedCPKWithoutKeySwitch,
+                )
+                .build();
+
+            let cks = ClientKey::generate(config);
+            let sks = ServerKey::new(&cks);
+
+            set_server_key(sks);
+
+            cks
+        };
         let mut rng = rand::thread_rng();
         let mut clear_values: Vec<u8> = (0..15).map(|_| rng.gen()).collect();
 
@@ -73,19 +131,70 @@ mod test {
 
         let seed = new_seeder().seed();
         println!("seed: {seed:?}");
-        let shuffled =
-            bitonic_shuffle(encrypted, BitonicShuffleKeySize::num_bits(32), seed).unwrap();
+        let key_size = BitonicShuffleKeySize::num_bits(32);
+        let shuffled = bitonic_shuffle(encrypted.clone(), key_size, seed).unwrap();
+        let shuffled_rerand = {
+            let mut shuffled_rerand = vec![];
 
-        let mut decrypted: Vec<u8> = shuffled.iter().map(|ct| ct.decrypt(&cks)).collect();
+            for algo in [
+                ReRandomizationHashAlgo::Blake3,
+                ReRandomizationHashAlgo::Shake256,
+            ] {
+                shuffled_rerand.push(
+                    re_randomized_keys_bitonic_shuffle(
+                        encrypted.clone(),
+                        key_size,
+                        seed,
+                        ReRandomizationMode::UseAvailableMode,
+                        algo,
+                    )
+                    .unwrap(),
+                );
+            }
+
+            shuffled_rerand
+        };
+
+        let decrypted_ref: Vec<u8> = shuffled.iter().map(|ct| ct.decrypt(&cks)).collect();
 
         clear_values.sort_unstable();
-        decrypted.sort_unstable();
-        assert_eq!(decrypted, clear_values);
+        let decrypted_sorted = {
+            let mut tmp = decrypted_ref.clone();
+            tmp.sort_unstable();
+            tmp
+        };
+
+        assert_eq!(decrypted_sorted, clear_values);
+
+        for (idx, rerand_result) in shuffled_rerand.into_iter().enumerate() {
+            let decrypted_rerand: Vec<u8> =
+                rerand_result.iter().map(|ct| ct.decrypt(&cks)).collect();
+
+            assert_eq!(
+                decrypted_ref, decrypted_rerand,
+                "failed at index {idx} of decrypted_rerand"
+            );
+        }
     }
 
     #[test]
     fn test_bitonic_shuffle_fheint() {
-        let cks = setup_default_cpu();
+        let cks = {
+            let config = ConfigBuilder::default()
+                .use_dedicated_oprf_key(true)
+                .enable_ciphertext_re_randomization(
+                    ReRandomizationParameters::DerivedCPKWithoutKeySwitch,
+                )
+                .build();
+
+            let cks = ClientKey::generate(config);
+            let sks = ServerKey::new(&cks);
+
+            set_server_key(sks);
+
+            cks
+        };
+
         let mut rng = rand::thread_rng();
         let mut clear_values: Vec<i8> = (0..15).map(|_| rng.gen()).collect();
 
@@ -96,14 +205,50 @@ mod test {
 
         let seed = new_seeder().seed();
         println!("seed: {seed:?}");
-        let shuffled =
-            bitonic_shuffle(encrypted, BitonicShuffleKeySize::num_bits(32), seed).unwrap();
+        let key_size = BitonicShuffleKeySize::num_bits(32);
+        let shuffled = bitonic_shuffle(encrypted.clone(), key_size, seed).unwrap();
+        let shuffled_rerand = {
+            let mut shuffled_rerand = vec![];
 
-        let mut decrypted: Vec<i8> = shuffled.iter().map(|ct| ct.decrypt(&cks)).collect();
+            for algo in [
+                ReRandomizationHashAlgo::Blake3,
+                ReRandomizationHashAlgo::Shake256,
+            ] {
+                shuffled_rerand.push(
+                    re_randomized_keys_bitonic_shuffle(
+                        encrypted.clone(),
+                        key_size,
+                        seed,
+                        ReRandomizationMode::UseAvailableMode,
+                        algo,
+                    )
+                    .unwrap(),
+                );
+            }
+
+            shuffled_rerand
+        };
+
+        let decrypted_ref: Vec<i8> = shuffled.iter().map(|ct| ct.decrypt(&cks)).collect();
 
         clear_values.sort_unstable();
-        decrypted.sort_unstable();
-        assert_eq!(decrypted, clear_values);
+        let decrypted_sorted = {
+            let mut tmp = decrypted_ref.clone();
+            tmp.sort_unstable();
+            tmp
+        };
+
+        assert_eq!(decrypted_sorted, clear_values);
+
+        for (idx, rerand_result) in shuffled_rerand.into_iter().enumerate() {
+            let decrypted_rerand: Vec<i8> =
+                rerand_result.iter().map(|ct| ct.decrypt(&cks)).collect();
+
+            assert_eq!(
+                decrypted_ref, decrypted_rerand,
+                "failed at index {idx} of decrypted_rerand"
+            );
+        }
     }
 
     #[test]
