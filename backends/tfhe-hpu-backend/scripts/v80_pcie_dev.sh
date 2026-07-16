f994d78ef383755da0ab576042a700f9455b058e
pgardratzama <pierre.gardrat@zama.ai>
2026-07-16 14:22:40 +0200

    chore(hpu): update profile.d script linked to HPU cluster description to include MAC address, add sudo rules for drivers & access to PCI devices

--- backends/tfhe-hpu-backend/scripts/v80_pcie_dev.sh
+++ backends/tfhe-hpu-backend/scripts/v80_pcie_dev.sh
@@ -14,27 +14,35 @@
 declare -gA V80_BOARDS_MAP=(
 ["0,pcie_id"]="PcieIdOfBoard0"
 ["0,serial_number"]="SerialNumberOfBoard0"
+["0,mac_address"]="0x123400"
 
 ["1,pcie_id"]="PcieIdOfBoard1"
 ["1,serial_number"]="SerialNumberOfBoard1"
+["1,mac_address"]="0x123401"
 
 ["2,pcie_id"]="PcieIdOfBoard2"
 ["2,serial_number"]="SerialNumberOfBoard2"
+["2,mac_address"]="0x123402"
 
 ["3,pcie_id"]="PcieIdOfBoard3"
 ["3,serial_number"]="SerialNumberOfBoard3"
+["3,mac_address"]="0x123403"
 
 ["4,pcie_id"]="PcieIdOfBoard4"
 ["4,serial_number"]="SerialNumberOfBoard4"
+["4,mac_address"]="0x123404"
 
 ["5,pcie_id"]="PcieIdOfBoard5"
 ["5,serial_number"]="SerialNumberOfBoard5"
+["5,mac_address"]="0x123405"
 
 ["6,pcie_id"]="PcieIdOfBoard6"
 ["6,serial_number"]="SerialNumberOfBoard6"
+["6,mac_address"]="0x123406"
 
 ["7,pcie_id"]="PcieIdOfBoard7"
 ["7,serial_number"]="SerialNumberOfBoard7"
+["7,mac_address"]="0x123407"
 )
 export V80_BOARDS_NB=8
 ###############################################################################
