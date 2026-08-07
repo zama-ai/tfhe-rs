f977daae5975b6e5e6184ac7d8f9fb2ef60598dd
Nicolas Sarlin <nicolas.sarlin@zama.ai>
2026-08-07 15:19:41 +0000

    feat(transciphering): add OPRF generation
    Co-authored-by: Thomas Montaigu <thomas.montaigu@zama.ai>
    Co-authored-by: Arthur Meyre <arthur.meyre@zama.ai>

--- tfhe/src/transciphering/backward_compatibility/mod.rs
+++ tfhe/src/transciphering/backward_compatibility/mod.rs
@@ -1,6 +1,10 @@
 use tfhe_versionable::VersionsDispatch;
 
-use crate::transciphering::{StreamCipherKind, StreamCiphertext};
+use crate::transciphering::{
+    AesIv, CompressedTranscipheringServerKey, KreyviumIV, OneTimePadFheSecretMask,
+    OneTimePadPlainSecretMask, SerializableAesFheKey, SerializableKreyviumFheKey, StreamCipherKind,
+    StreamCiphertext, TranscipheringPrivateKey, TranscipheringServerKey,
+};
 
 #[derive(VersionsDispatch)]
 pub enum StreamCipherKindVersions {
@@ -10,4 +14,49 @@ pub enum StreamCipherKindVersions {
 #[derive(VersionsDispatch)]
 pub enum StreamCiphertextVersions {
     V0(StreamCiphertext),
+}
+
+#[derive(VersionsDispatch)]
+pub enum SerializableKreyviumFheKeyVersions {
+    V0(SerializableKreyviumFheKey),
+}
+
+#[derive(VersionsDispatch)]
+pub enum KreyviumIVVersions {
+    V0(KreyviumIV),
+}
+
+#[derive(VersionsDispatch)]
+pub enum SerializableAesFheKeyVersions {
+    V0(SerializableAesFheKey),
+}
+
+#[derive(VersionsDispatch)]
+pub enum AesIvVersions {
+    V0(AesIv),
+}
+
+#[derive(VersionsDispatch)]
+pub enum OneTimePadFheSecretMaskVersions {
+    V0(OneTimePadFheSecretMask),
+}
+
+#[derive(VersionsDispatch)]
+pub enum OneTimePadPlainSecretMaskVersions {
+    V0(OneTimePadPlainSecretMask),
+}
+
+#[derive(VersionsDispatch)]
+pub enum TranscipheringPrivateKeyVersions {
+    V0(TranscipheringPrivateKey),
+}
+
+#[derive(VersionsDispatch)]
+pub enum TranscipheringServerKeyVersions {
+    V0(TranscipheringServerKey),
+}
+
+#[derive(VersionsDispatch)]
+pub enum CompressedTranscipheringServerKeyVersions {
+    V0(CompressedTranscipheringServerKey),
 }
