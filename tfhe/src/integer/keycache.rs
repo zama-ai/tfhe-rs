7c0228b2568188e12cf96e6a30a6b561086eb948
Baptiste Roux <baptiste.roux@zama.ai>
2026-09-16 21:34:01 +0200

    fix(hpu): Update examples and test with new signature interfaces
    Also fix gen_lut issue in integer/hl_api

--- tfhe/src/integer/keycache.rs
+++ tfhe/src/integer/keycache.rs
@@ -65,8 +65,12 @@ impl IntegerKeyCache {
                 let config_file = ShellString::new(
                     "${HPU_BACKEND_DIR}/config_store/${HPU_CONFIG}/hpu_config.toml".to_string(),
                 );
-                HpuDevice::from_config(&config_file.expand(), false)
-                    .expect("HpuDevice failed to open with required configuration")
+                HpuDevice::from_config(
+                    &config_file.expand(),
+                    false,
+                    &crate::core_crypto::hpu::create_hpu_lookuptable,
+                )
+                .expect("HpuDevice failed to open with required configuration")
             };
             // Check compatibility with key
             let hpu_pbs_params =
