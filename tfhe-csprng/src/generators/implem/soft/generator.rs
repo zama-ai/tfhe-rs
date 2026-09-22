5b5eb86f8e48b431a334ac3667951e86a5bab504
tmontaigu <thomas.montaigu@zama.ai>
2026-09-22 12:24:48 +0200

    refactor(hlapi): Add AES-256 for XofSeed
    * Break some APIs and remove some From convertions
      to force explicit choice of random generator initialized by the Xof Seed
    * Change XofSeetStart to XofGenerationParams as the mean to give the
      seed andderivation mode of the random generator
    * CompressedXofKeySet::generate still receives the raw xof seed bytes
      and will init a Aes256
    
    BREAKING_CHANGES: some From convertions related to XofSeed are removed,
    XofSeedStart deleted in favor of a different struct XofGenerationParms

--- tfhe-csprng/src/generators/implem/soft/generator.rs
+++ tfhe-csprng/src/generators/implem/soft/generator.rs
@@ -160,4 +160,9 @@ mod test {
     fn test_vector_xof_seed_bytes() {
         generator_generic_test::test_vectors_xof_seed_bytes::<SoftwareRandomGenerator>();
     }
+
+    #[test]
+    fn test_vector_xof_seed_aes256() {
+        generator_generic_test::test_vectors_xof_seed_aes256::<SoftwareRandomGenerator>();
+    }
 }
