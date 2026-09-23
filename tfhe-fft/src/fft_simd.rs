90369a578405f581ff445bd0df3d84695d886a14
Nicolas Sarlin <nicolas.sarlin@zama.ai>
2026-09-23 16:51:33 +0200

    chore(lints): update dylint to 6.1.0

--- tfhe-fft/src/fft_simd.rs
+++ tfhe-fft/src/fft_simd.rs
@@ -1,5 +1,5 @@
 use crate::c64;
-use core::{f64, fmt::Debug, marker::PhantomData};
+use core::{fmt::Debug, marker::PhantomData};
 
 #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 #[derive(Copy, Clone, Debug)]
@@ -276,7 +276,7 @@ pub fn sincospi64(mut a: f64) -> (f64, f64) {
     let s = s * t;
     r *= s;
 
-    let mut s = fma(t, f64::consts::PI, r);
+    let mut s = fma(t, core::f64::consts::PI, r);
     // map results according to quadrant
 
     if (i & 2) != 0 {
